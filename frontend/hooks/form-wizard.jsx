5a6f5460dd61e073e6557afe3056ac2636122ab3
Guy Romelle Magayano <guy@epicdesignlabs.com>
2020-05-26 00:30:29 +0800

    [WIP] Add form wizard hook

---
+++ frontend/hooks/form-wizard.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const FormWizardDiv = styled.div``
+
+const FormWizard = () => {
+    
+}
+
+export default FormWizard
