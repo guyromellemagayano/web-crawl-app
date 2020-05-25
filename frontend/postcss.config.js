6ca9a01fcb7d867037218da5efe391bd1ea53fee
Guy Romelle Magayano <guy@epicdesignlabs.com>
2020-05-26 00:34:13 +0800

    Update postcss and tailwind JS config files

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,8 +1,25 @@
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project
+  content: [
+    './components/**/*.jsx',
+    './hooks/*.jsx',
+    './pages/*.jsx',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
+})
+
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'tailwindcss': {},
-    'autoprefixer': {},
+    tailwindcss: {},
+    autoprefixer: {},
     'postcss-preset-env': { stage: 2 },
-  },
-};
+    ...process.env.NODE_ENV == 'production'
+      ? [purgecss]
+      : []
+  }
+}
