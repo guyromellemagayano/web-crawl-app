7f7eaf30e441f8095390c565be32856d3c814fb1
Guy Romelle Magayano <guy@epicdesignlabs.com>
2020-05-30 01:16:08 +0800

    Add new frontend hooks
    - Add fetch-json.jsx
    - Add use-user.jsx
    - Remove form-wizard.jsx

---
+++ frontend/hooks/fetch-json.jsx
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch'
+
+const fetcher = async(...args) => {
+  try {
+    const response = await fetch(...args)
+
+    const data = await response.json()
+
+    if (response.ok) {
+      return data
+    } else {
+      const error = new Error(response.statusText())
+
+      error.response = response
+      error.data = data
+
+      throw error
+    }
+  } catch (error) {
+    if (!error.data) {
+      error.data = { message: error.message }
+    }
+
+    throw error
+  }
+}
+
+export default fetcher
