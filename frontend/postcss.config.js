2c2d0415cda3d2dda3c1906f3781b625ddfff7e3
Guy Romelle Magayano <guy@epicdesignlabs.com>
2020-05-31 05:57:57 +0800

    Fix production issues
    - Update tailwind init in app.css
    - Add axios npm package
    - Add react-html-parser npm package
    - Fix postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,25 +1,9 @@
 // postcss.config.js
-const purgecssconfig = {
-
-  // Specify the paths to all of the template files in your project
-  content: [
-    './components/**/*.jsx',
-    './hooks/*.jsx',
-    './pages/*.jsx',
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
-};
-
 module.exports = {
   plugins: [
     'postcss-import',
     'tailwindcss',
     'autoprefixer',
     ['postcss-preset-env', { stage: 2 }],
-    ...process.env.NODE_ENV == 'production'
-      ? [['@fullhuman/postcss-purgecss', purgecssconfig]]
-      : []
   ]
 }
