b341134e26865d0451903fa75634a9c054d4f5c0
matematik7 <domen.ipavec@gmail.com>
2020-06-01 12:22:00 +0200

    Better build for prod images

--- deploy/staging.py
+++ deploy/staging.py
@@ -3,19 +3,6 @@
 import common
 
 
-def build_image(c, name):
-    image_name = f"400936075989.dkr.ecr.us-east-1.amazonaws.com/crawl-app-{name}"
-    c.local(f"docker build -t {image_name} {name}/")
-    c.local(f"docker push {image_name}")
-
-
-def build(c):
-    c.local("source environment && $(aws ecr get-login --no-include-email)")
-    build_image(c, "backend")
-    build_image(c, "frontend")
-    build_image(c, "crawler")
-
-
 def docker_compose(c, name):
     c.run("$(aws ecr get-login --no-include-email --region us-east-1)")
 
@@ -28,7 +15,6 @@ def docker_compose(c, name):
 
 
 def deploy(c):
-    build(c)
     docker_compose(c, "staging")
 
 
