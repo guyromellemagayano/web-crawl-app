beb1420f61dfe9e3a0814ac41e7d4a979154b2d1
Guy Romelle Magayano <guy@epicdesignlabs.com>
2020-06-08 07:15:05 +0800

    Fix primary menu when sites wizard is used

--- frontend/components/sidebar/primary-menu.jsx
+++ frontend/components/sidebar/primary-menu.jsx
@@ -15,7 +15,7 @@ const PrimaryMenu = () => {
             <Fragment key={key}>
               <Link href={val.url}>
                 <a
-                  className={`${useRouter().pathname === val.url ? "group mt-1 flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-900 rounded-md bg-gray-100 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150" : "mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition ease-in-out duration-150"}`}
+                  className={`${useRouter().pathname.indexOf(val.url) == 0 ? "group mt-1 flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-900 rounded-md bg-gray-100 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150" : "mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition ease-in-out duration-150"}`}
                 >
                   <svg
                     className={`mr-3 h-6 w-5 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150`}
