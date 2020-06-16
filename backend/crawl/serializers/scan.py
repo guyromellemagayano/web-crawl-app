28c5f0a10aa37853d0ace3e9a0a29d4f5cc900b6
matematik7 <domen.ipavec@gmail.com>
2020-06-16 20:00:47 +0200

    Add summary fields to scan

--- backend/crawl/serializers/scan.py
+++ backend/crawl/serializers/scan.py
@@ -11,9 +11,23 @@ class ScanSerializer(serializers.ModelSerializer):
 
 
 class ScanDetailSerializer(serializers.ModelSerializer):
+    num_pages = serializers.IntegerField(read_only=True)
     num_links = serializers.IntegerField(read_only=True)
+    num_ok_links = serializers.IntegerField(read_only=True)
+    num_non_ok_links = serializers.IntegerField(read_only=True)
+    num_external_links = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Scan
-        fields = ["id", "started_at", "finished_at", "site_id", "num_links"]
-        read_only_fields = ["id", "started_at", "finished_at", "site_id", "num_links"]
+        fields = [
+            "id",
+            "started_at",
+            "finished_at",
+            "site_id",
+            "num_pages",
+            "num_links",
+            "num_ok_links",
+            "num_non_ok_links",
+            "num_external_links",
+        ]
+        read_only_fields = ["id", "started_at", "finished_at", "site_id"]
--- backend/crawl/views/scan.py
+++ backend/crawl/views/scan.py
@@ -1,8 +1,8 @@
-from django.db.models import Count
+from django.db.models import Count, F, Q
 from rest_framework import viewsets, mixins
 from rest_framework_extensions.mixins import DetailSerializerMixin, NestedViewSetMixin
 
-from crawl.models import Scan
+from crawl.models import Link, Scan
 from crawl.serializers import ScanDetailSerializer, ScanSerializer
 
 
@@ -21,5 +21,11 @@ class ScanViewSet(
     def get_queryset(self):
         queryset = super().get_queryset().filter(site__user=self.request.user)
         if self._is_request_to_detail_endpoint():
-            queryset = queryset.annotate(num_links=Count("link"))
+            queryset = (
+                queryset.annotate(num_pages=Count("link", filter=Q(link__type=Link.TYPE_PAGE)))
+                .annotate(num_links=Count("link"))
+                .annotate(num_ok_links=Count("link", filter=Q(link__status=Link.STATUS_OK)))
+                .annotate(num_non_ok_links=F("num_links") - F("num_ok_links"))
+                .annotate(num_external_links=Count("link", filter=Q(link__type=Link.TYPE_EXTERNAL)))
+            )
         return queryset
