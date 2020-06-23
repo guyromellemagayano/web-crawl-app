d8f86fcba09049c3b39d8241f9ef0abbe62228d0
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-06-23 19:22:03 +0800

    Fix menu items in profile

--- frontend/components/sidebar/ProfileMenu.jsx
+++ frontend/components/sidebar/ProfileMenu.jsx
@@ -1,10 +1,13 @@
-import { Fragment } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import styled from 'styled-components'
 import ProfilePages from '../../config/profile-pages.json'
 
-const PrimaryMenuDiv = styled.nav``
+const PrimaryMenuDiv = styled.nav`
+  a:first-child {
+    margin-bottom: 1rem;
+  }
+`
 
 const PrimaryMenu = () => {
   return (
@@ -12,28 +15,34 @@ const PrimaryMenu = () => {
       {
         ProfilePages.map((val, key) => {
           return (
-            <Fragment key={key}>
-              <Link href={val.url}>
-                <a
-                  className={`${useRouter().pathname === val.url ? "group mt-1 flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-900 rounded-md bg-gray-100 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150" : "mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition ease-in-out duration-150"}`}
+            <Link key={key} href={val.url}>
+              <a
+                className={`${'/profile/' + val.url === useRouter().asPath ? "group mt-1 flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-900 rounded-md bg-gray-100 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150" : "mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition ease-in-out duration-150"}`}
+              >
+                <svg
+                  className={`mr-3 h-6 w-5 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150`}
+                  stroke={`currentColor`}
+                  fill={`none`}
+                  viewBox={`0 0 24 24`}
                 >
-                  <svg
-                    className={`mr-3 h-6 w-5 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150`}
-                    stroke={`currentColor`}
-                    fill={`none`}
-                    viewBox={`0 0 24 24`}
-                  >
+                  <path
+                    strokeLinecap={`round`}
+                    strokeLinejoin={`round`}
+                    strokeWidth={`2`}
+                    d={val.icon}
+                  />
+                  {val.icon2 ? (
                     <path
                       strokeLinecap={`round`}
                       strokeLinejoin={`round`}
                       strokeWidth={`2`}
-                      d={val.icon}
+                      d={val.icon2}
                     />
-                  </svg>
-                  {val.title}
-                </a>
-              </Link>
-            </Fragment>
+                  ) : null}
+                </svg>
+                {val.title}
+              </a>
+            </Link>
           )
         })
       }
