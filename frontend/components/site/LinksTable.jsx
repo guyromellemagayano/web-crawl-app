5f527735a5d44d2ffe27b9ab17f83145e29a0d49
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-06-25 19:05:53 +0800

    Remove Status in LInksTable component

--- frontend/components/site/LinksTable.jsx
+++ frontend/components/site/LinksTable.jsx
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 
 const LinksTableDiv = styled.tbody``
 
-const LinksTable = props => {    
+const LinksTable = props => {
+  console.log(props.val)
   return (
     <LinksTableDiv className={`bg-white`}>
       <tr>
@@ -38,21 +39,6 @@ const LinksTable = props => {
         <td
           className={`px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500`}
         >
-          <svg
-            fill={`currentColor`}
-            viewBox={`0 0 24 24`}
-            className={`inline-block ml-2 h-5 w-5 text-green-400`}
-          >
-            <path
-              d={`M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z`}
-              clipRule={`evenodd`}
-              fillRule={`evenodd`}
-            ></path>
-          </svg>
-        </td>
-        <td
-          className={`px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500`}
-        >
           {props.val.num_links}
         </td>
         <td
@@ -68,17 +54,9 @@ const LinksTable = props => {
         <td
           className={`flex-grow px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium`}
         >
-          {/* <Link href="/site/[id]" as={`/site/${props.site.id}`}>
+          <Link href="/site/[id]" as={`/site/${props.val.id}`}>
             <a
               className={`text-sm leading-6 font-semibold text-indigo-600 hover:text-indigo-500 transition ease-in-out duration-150`}
-            >
-              View Stats
-            </a>
-          </Link> */}
-          <Link href="#">
-            <a
-              disabled={`disabled`}
-              className={`text-sm leading-6 font-semibold text-indigo-600 hover:text-indigo-500 transition ease-in-out duration-150 color-gray-300 opacity-50 cursor-not-allowed`}
             >
               View Detail
             </a>
