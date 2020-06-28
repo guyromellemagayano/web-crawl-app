5994d8eb6d1830c03bfdf14a7e6332c4b1fad3d2
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-06-28 18:28:10 +0800

    Update Overview sites component

--- frontend/components/sites/Overview.jsx
+++ frontend/components/sites/Overview.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
 import styled from 'styled-components'
+import Moment from 'react-moment'
 
 const SitesOverviewDiv = styled.div``
 
 const SitesOverview = props => {
+  const calendarStrings = {
+    lastDay : '[Yesterday], dddd [at]',
+    sameDay : '[Today], dddd [at]',
+    lastWeek : 'MMMM DD, YYYY',
+    sameElse : 'MMMM DD, YYYY'
+  }
+
   return (
     <SitesOverviewDiv className={`bg-white shadow sm:rounded-lg`}>
       <div className={`px-4 py-5 sm:p-6`}>
@@ -12,7 +21,7 @@ const SitesOverview = props => {
             {props.url}
           </a>
         </h2>
-        <div className={`mt-5 mb-3 max-w-xl text-sm leading-5 text-gray-500`}>
+        <div className={`mt-5 mb-3 max-w-lg text-sm leading-5 text-gray-500`}>
           <div className={`mb-2 flex`}>
             <strong>Site Status:</strong>
             {props.verified ? (
@@ -25,6 +34,21 @@ const SitesOverview = props => {
               </div>
             )}
           </div>
+        </div>
+        <div className={`my-2 max-w-xl text-sm leading-5 text-gray-500`}>
+          <p>
+            <strong>Updated last:</strong> <Moment local calendar={calendarStrings} date={props.updatedAt} />&nbsp;
+            <Moment local date={props.createdAt} format="hh:mm:ss A" />
+          </p>
+        </div>
+        <div className={`mt-4`}>
+          <button
+            disabled={`disabled`}
+            type={`button`}
+            className={`mt-3 mr-3 rounded-md shadow sm:mt-0 relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-600 opacity-50 cursor-not-allowed`}
+          >
+            Run Crawler
+          </button>
         </div>
       </div>
     </SitesOverviewDiv>
