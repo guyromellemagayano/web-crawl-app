97b5e5c6476ef95146c998e59d6075102c47eb06
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-06-30 00:33:27 +0800

    Fix class names issue

--- frontend/components/site/LinkFilter.jsx
+++ frontend/components/site/LinkFilter.jsx
@@ -15,33 +15,33 @@ const LinkFilter = () => {
 					<h4 className={`ml-3 mt-1 mr-1 text-md leading-4 font-semibold text-gray-600`}>Filter</h4>
           <div className={`ml-4 mt-2 mr-2`}>
             <div>
-							<label class="flex items-center">
-								<input type="checkbox" class="form-checkbox" />
-								<span class="ml-2 text-left text-xs leading-4 font-normal text-gray-500">All Links</span>
+							<label className={`flex items-center`}>
+								<input type="checkbox" className={`form-checkbox`} />
+								<span className={`ml-2 text-left text-xs leading-4 font-normal text-gray-500`}>All Links</span>
 							</label>
             </div>
           </div>
 					<div className={`ml-4 mt-2 mr-2`}>
             <div>
-							<label class="flex items-center">
-								<input type="checkbox" class="form-checkbox" />
-								<span class="ml-2 text-left text-xs leading-4 font-normal text-gray-500">Links with Issues</span>
+							<label className={`flex items-center`}>
+								<input type="checkbox" className={`form-checkbox`} />
+								<span className={`ml-2 text-left text-xs leading-4 font-normal text-gray-500`}>Links with Issues</span>
 							</label>
             </div>
           </div>
 					<div className={`ml-4 mt-2 mr-2`}>
             <div>
-							<label class="flex items-center">
-								<input type="checkbox" class="form-checkbox" />
-								<span class="ml-2 text-left text-xs leading-4 font-normal text-gray-500">Internal Links</span>
+							<label className={`flex items-center`}>
+								<input type="checkbox" className={`form-checkbox`} />
+								<span className={`ml-2 text-left text-xs leading-4 font-normal text-gray-500`}>Internal Links</span>
 							</label>
             </div>
           </div>
 					<div className={`ml-4 mt-2 mr-2`}>
             <div>
-							<label class="flex items-center">
-								<input type="checkbox" class="form-checkbox" />
-								<span class="ml-2 text-left text-xs leading-4 font-normal text-gray-500">External Links</span>
+							<label className={`flex items-center`}>
+								<input type="checkbox" className={`form-checkbox`} />
+								<span className={`ml-2 text-left text-xs leading-4 font-normal text-gray-500`}>External Links</span>
 							</label>
             </div>
           </div>
