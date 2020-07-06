08c7e7d2e1626b03aba9e06a7c0434eecd87a8d5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-07-06 19:03:15 +0800

    Add redux folders

---
+++ frontend/redux/reducers/timestampReducer.js
@@ -0,0 +1,11 @@
+// const counterReducer = (state = {value: 0}, action) => {
+// 	return {...state};
+// };
+
+// export default counterReducer;
+
+const timestampReducer = (state = {value: 0}, action) => {
+	return {...state};
+}
+
+export default timestampReducer;
