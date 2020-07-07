6d4422c1726443dace49b1e8d102665e7db5dbdc
matematik7 <domen.ipavec@gmail.com>
2020-07-07 12:22:04 +0200

    Add settings to user profile

--- backend/crawl/serializers/__init__.py
+++ backend/crawl/serializers/__init__.py
@@ -2,3 +2,4 @@ from .link import LinkDetailSerializer, LinkSerializer
 from .page import PageSerializer
 from .site import SiteSerializer
 from .scan import ScanDetailSerializer, ScanSerializer
+from .user import UserSerializer
