284282f0783ec6c94d41a40b64deec135a25c067
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-07-10 00:36:09 +0800

    Remove Link Location in links table

--- frontend/components/site/LinkUrlTable.jsx
+++ frontend/components/site/LinkUrlTable.jsx
@@ -134,11 +134,6 @@ const LinkUrlTable = (props) => {
           <td
             className={`px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500`}
           >
-            /fat-cat/
-          </td>
-          <td
-            className={`px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500`}
-          >
             {props.val.occurences}
           </td>
         </tr>
