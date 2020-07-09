9ea49bc766ef8b377d9d183d2b57e5d77f07e8f2
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-07-10 04:37:57 +0800

    Remove log out button in SettingsMenu component, update MobileSettingsMenu component

--- frontend/components/sidebar/MobileSettingsMenu.jsx
+++ frontend/components/sidebar/MobileSettingsMenu.jsx
@@ -41,28 +41,6 @@ const PrimaryMenu = () => {
           )
         })
       }
-      {
-        <Link href="/logout">
-          <a
-            className={`mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition ease-in-out duration-150`}
-          >
-            <svg
-              className={`mr-3 h-6 w-5 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150`}
-              stroke={`currentColor`}
-              fill={`none`}
-              viewBox={`0 0 24 24`}
-            >
-              <path
-                strokeLinecap={`round`}
-                strokeLinejoin={`round`}
-                strokeWidth={`2`}
-                d={`M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1`}
-              />
-            </svg>
-            Sign Out
-          </a>
-        </Link>
-      }
     </PrimaryMenuDiv>
   );
 }
