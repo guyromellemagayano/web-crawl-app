0e1818d8150b15870d0baafccebdf73f9dac33ad
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-07-17 19:10:40 +0800

    Setup LogRocket in app

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { SWRConfig } from 'swr'
+import LogRocket from 'logrocket'
+import setupLogRocketReact from 'logrocket-react'
 import fetchJson from '../hooks/fetchJson'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -7,6 +9,11 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import '../css/styles.css'
 
 library.add(fab)
+
+if (typeof window !== 'undefined') {
+  LogRocket.init('app/id');
+  setupLogRocketReact(LogRocket);
+}
 
 const App = ({ Component, pageProps }) => {
   return (
