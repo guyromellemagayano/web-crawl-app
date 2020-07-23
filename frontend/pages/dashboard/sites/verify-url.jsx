a239280316ec8d930fc71cd110ee137532d4b8cc
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-07-23 22:39:20 +0800

    Implement notification UI for verify url page

--- frontend/pages/dashboard/sites/verify-url.jsx
+++ frontend/pages/dashboard/sites/verify-url.jsx
@@ -1,4 +1,4 @@
-import { useState, Fragment } from 'react'
+import { useState, useEffect, Fragment } from 'react'
 import fetch from 'node-fetch'
 import Cookies from 'js-cookie'
 import Head from 'next/head'
@@ -6,6 +6,7 @@ import styled from 'styled-components'
 import Link from 'next/link'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import PropTypes from 'prop-types'
+import Transition from '../../../hooks/Transition'
 import Layout from '../../../components/Layout'
 import MobileSidebar from '../../../components/sidebar/MobileSidebar'
 import MainSidebar from '../../../components/sidebar/MainSidebar'
@@ -31,6 +32,7 @@ const SitesVerifyUrl = props => {
   const [disableSiteVerify, setDisableSiteVerify] = useState(false)
   const [enableNextStep, setEnableNextStep] = useState(false)
   const [openMobileSidebar, setOpenMobileSidebar] = useState(false)
+  const [showNotificationStatus, setShowNotificationStatus] = useState(false)
   const pageTitle = 'Verify Site URL'
 
   const handleInputChange = ({ copyValue }) => {
@@ -68,11 +70,13 @@ const SitesVerifyUrl = props => {
     const data = await response.json()
     
     if (response.ok && data.verified) {
-      setSuccessMsg('Site verification success. Proceed to the next step.')
+      setSuccessMsg('You can now proceed to the site overview.')
+      setTimeout(() => setShowNotificationStatus(true), 1500)
       setDisableSiteVerify(!disableSiteVerify)
-      setEnableNextStep(!enableNextStep)
+      setTimeout(() => setEnableNextStep(!enableNextStep), 1500)
     } else if (response.ok && !data.verified) {
-      setErrorMsg('Site verification failed. You have not verify the site yet.')
+      setErrorMsg('You have not verify the site yet.')
+      setTimeout(() => setShowNotificationStatus(true), 1500)
     } else {
       const error = new Error(response.statusText)
 
@@ -82,6 +86,11 @@ const SitesVerifyUrl = props => {
       throw error
     }
   }
+
+  useEffect(() => {
+    if (showNotificationStatus === true)
+    setTimeout(() => setShowNotificationStatus(false), 7500)
+  }, [showNotificationStatus])
 
   return (
     <Layout>
@@ -95,12 +104,132 @@ const SitesVerifyUrl = props => {
         <MobileSidebar show={openMobileSidebar} />
         <MainSidebar />
 
+        <Transition show={showNotificationStatus}>
+          <div
+            className={`fixed z-50 inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end`}
+          >
+            <Transition
+              enter="transform ease-out duration-300 transition"
+              enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+              enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div
+                className={`max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto`}
+              >
+                <div className={`rounded-lg shadow-xs overflow-hidden`}>
+                  <div className={`p-4`}>
+                    <div className={`flex items-start`}>
+                      <div className={`flex-shrink-0`}>
+                        {errorMsg ? (
+                          <svg
+                            className={`h-8 w-8 text-red-400`}
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
+                        ) : successMsg ? (
+                          <svg
+                            className={`h-8 w-8 text-green-400`}
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
+                        ) : (
+                          <svg
+                            className={`h-8 w-8 text-gray-400`}
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
+                        )}
+                      </div>
+                      <div className={`ml-3 w-0 flex-1 pt-0.5`}>
+                        <p
+                          className={`text-sm leading-5 font-medium ${
+                            errorMsg !== undefined && errorMsg !== ""
+                              ? "text-red-500"
+                              : "text-gray-900"
+                          } ${
+                            successMsg !== undefined && successMsg !== ""
+                              ? "text-green-500"
+                              : "text-gray-900"
+                          }`}
+                        >
+                          {errorMsg !== undefined && errorMsg !== ""
+                            ? "Site Verification Failed!"
+                            : successMsg !== undefined && successMsg !== ""
+                            ? "Site Verification Success!"
+                            : "Verifying..."}
+                        </p>
+                        <p className={`mt-1 text-sm leading-5 text-gray-500`}>
+                          {errorMsg !== undefined && errorMsg !== ""
+                            ? errorMsg
+                            : successMsg}
+                        </p>
+                      </div>
+                      <div className={`ml-4 flex-shrink-0 flex`}>
+                        <button
+                          className={`inline-flex text-gray-400 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150`}
+                          onClick={() =>
+                            setTimeout(
+                              () =>
+                                setShowNotificationStatus(
+                                  !showNotificationStatus
+                                ),
+                              150
+                            )
+                          }
+                        >
+                          <svg
+                            className={`h-5 w-5`}
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition>
+          </div>
+        </Transition>
+
         <div className={`flex flex-col w-0 flex-1 overflow-hidden`}>
           <div className={`md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3`}>
             <button
               className={`-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150`}
               aria-label={`Open sidebar`}
-              onClick={() => setTimeout(() => setOpenMobileSidebar(!openMobileSidebar), 150)}
+              onClick={() =>
+                setTimeout(() => setOpenMobileSidebar(!openMobileSidebar), 150)
+              }
             >
               <svg
                 className={`h-6 w-5`}
@@ -121,8 +250,12 @@ const SitesVerifyUrl = props => {
             className={`flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6`}
             tabIndex={`0`}
           >
-            <div className={`max-w-full mx-auto px-4 md:py-4 sm:px-6 md:px-8 grid gap-16 lg:grid-cols-3 lg:col-gap-5 lg:row-gap-12`}>
-              <div className={`lg:col-span-2 bg-white overflow-hidden shadow-xs rounded-lg`}>
+            <div
+              className={`max-w-full mx-auto px-4 md:py-4 sm:px-6 md:px-8 grid gap-16 lg:grid-cols-3 lg:col-gap-5 lg:row-gap-12`}
+            >
+              <div
+                className={`lg:col-span-2 bg-white overflow-hidden shadow-xs rounded-lg`}
+              >
                 <div className={`px-8 pt-4 sm:px-8 sm:pt-8`}>
                   <div className={`max-w-full pt-4 m-auto`}>
                     <h4
@@ -145,25 +278,39 @@ const SitesVerifyUrl = props => {
                     <p
                       className={`max-w-2xl mt-4 text-sm leading-2 font-medium text-green-500`}
                     >
-                      <span className={`max-w-xs inline-flex items-center justify-between`}>
-                        <svg className={`w-5`} fill="currentColor" viewBox="0 0 20 20">
+                      <span
+                        className={`max-w-xs inline-flex items-center justify-between`}
+                      >
+                        <svg
+                          className={`w-5`}
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                        >
                           <path
                             fillRule="evenodd"
                             d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
                             clipRule="evenodd"
                           ></path>
-                        </svg>&nbsp;
-                        Fill in site information
+                        </svg>
+                        &nbsp; Fill in site information
                       </span>
                     </p>
                   </div>
                   <div className={`wizard-indicator bg-green-500`}>
                     <p
-                      className={`${enableNextStep ? "text-green-500" : "text-black-400"} font-medium  max-w-2xl mt-4 text-sm leading-2`}
+                      className={`${
+                        enableNextStep ? "text-green-500" : "text-black-400"
+                      } font-medium  max-w-2xl mt-4 text-sm leading-2`}
                     >
-                      <span className={`max-w-xs inline-flex items-center justify-between`}>
+                      <span
+                        className={`max-w-xs inline-flex items-center justify-between`}
+                      >
                         {enableNextStep ? (
-                          <svg className={`w-5`} fill="currentColor" viewBox="0 0 20 20">
+                          <svg
+                            className={`w-5`}
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                          >
                             <path
                               fillRule="evenodd"
                               d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
@@ -179,7 +326,7 @@ const SitesVerifyUrl = props => {
                   </div>
                 </div>
 
-                <div className={`inline-block px-8 pt-8 pb-12 sm:px-8`}>
+                <div className={`max-w-full block px-8 pt-8 pb-12 sm:px-8`}>
                   <div className={`max-w-full py-4 m-auto`}>
                     <div>
                       <h4
@@ -282,67 +429,39 @@ const SitesVerifyUrl = props => {
                         </span>
 
                         <span className={`inline-flex rounded-md shadow-xs-sm`}>
-                        {disableSiteVerify ? (
-                          <Link
-                            href={{
-                              pathname: "/dashboard/sites/information",
-                              query: {
-                                sid: props.sid,
-                              },
-                            }}
-                          >
-                            <a
-                              disabled={`disabled`}
-                              className={`inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 shadow-xs-sm sm:text-sm sm:leading-5 opacity-50 cursor-not-allowed`}
+                          {disableSiteVerify ? (
+                            <Link
+                              href={{
+                                pathname: "/dashboard/sites/information",
+                                query: {
+                                  sid: props.sid,
+                                },
+                              }}
                             >
-                              Update Site Information
-                            </a>
-                          </Link>
-                        ) : (
-                          <Link
-                            href={{
-                              pathname: "/dashboard/sites/information",
-                              query: {
-                                sid: props.sid,
-                              },
-                            }}
-                          >
-                            <a
-                              className={`inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 shadow-xs-sm transition ease-in-out duration-150 sm:text-sm sm:leading-5 hover:text-gray-500 focus:outline-none`}
+                              <a
+                                disabled={`disabled`}
+                                className={`inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 shadow-xs-sm sm:text-sm sm:leading-5 opacity-50 cursor-not-allowed`}
+                              >
+                                Update Site Information
+                              </a>
+                            </Link>
+                          ) : (
+                            <Link
+                              href={{
+                                pathname: "/dashboard/sites/information",
+                                query: {
+                                  sid: props.sid,
+                                },
+                              }}
                             >
-                              Update Site Information
-                            </a>
-                          </Link>
-                        )}
+                              <a
+                                className={`inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 shadow-xs-sm transition ease-in-out duration-150 sm:text-sm sm:leading-5 hover:text-gray-500 focus:outline-none`}
+                              >
+                                Update Site Information
+                              </a>
+                            </Link>
+                          )}
                         </span>
-
-                        {errorMsg && (
-                          <div className={`inline-block mt-3 py-2 lg:m-0 lg:ml-2`}>
-                            <div className={`flex`}>
-                              <div>
-                                <h3
-                                  className={`text-sm leading-5 font-medium text-red-800 break-words`}
-                                >
-                                  {errorMsg}
-                                </h3>
-                              </div>
-                            </div>
-                          </div>
-                        )}
-
-                        {successMsg && (
-                          <div className={`inline-block mt-3 py-2 lg:m-0 lg:ml-2`}>
-                            <div className={`flex`}>
-                              <div>
-                                <h3
-                                  className={`text-sm leading-5 font-medium text-green-800 break-words`}
-                                >
-                                  {successMsg}
-                                </h3>
-                              </div>
-                            </div>
-                          </div>
-                        )}
                       </form>
                     </div>
 
@@ -365,8 +484,10 @@ const SitesVerifyUrl = props => {
                   </div>
                 </div>
               </div>
-            
-              <div className={`lg:col-span-1 bg-white overflow-hidden shadow-xs rounded-lg`}>
+
+              <div
+                className={`lg:col-span-1 bg-white overflow-hidden shadow-xs rounded-lg`}
+              >
                 <HowToSetup />
               </div>
             </div>
