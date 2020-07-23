4011b9da773cabbb200e5f9f191da6e8dc6dd1be
Elias Diek <elias@holamicasa.com>
2020-07-24 01:26:27 +0800

    apply secure router in sites page

--- frontend/hooks/useUser.js
+++ frontend/hooks/useUser.js
@@ -1,21 +1,44 @@
 import { useEffect } from "react";
 import Router from "next/router";
 import useSWR from "swr";
+import Cookies from 'js-cookie'
+
+const fetcher = async (url) => {
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "X-CSRFToken": Cookies.get("csrftoken"),
+    },
+  })
+
+  const data = await res.json()
+
+  if (res.status !== 200) {
+    throw new Error(res.status)
+  }
+
+  return data
+}
 
 const useUser = ({ redirectTo = false, redirectIfFound = false} = {}) => {
-  const { data: user, mutate: mutateUser } = useSWR("/api/auth/user/");
+  const { data: user, mutate: mutateUser, error: userError } = useSWR("/api/auth/user/", fetcher);
 
-  useEffect(() => {    
+  useEffect(() => {
+    if (userError == 'Error: 403' && !redirectIfFound) Router.push(redirectTo)
+
     if (user && redirectIfFound) {
       if ("key" in user) {
+        console.log('[user]', user)
         Router.push(redirectTo);
       }
     } else {
       return;
     }
-  }, [user, redirectIfFound, redirectTo]);
+  }, [user, redirectIfFound, redirectTo, userError]);
 
-  return { user, mutateUser };
+  return { user, mutateUser, userError };
 };
 
 export default useUser;
