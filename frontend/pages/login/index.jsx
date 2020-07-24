790657643464096fd346253ed3bf5ad6a2737cce
Elias Diek <elias@holamicasa.com>
2020-07-24 09:38:31 +0800

    redirect users to previous page upon login success

--- frontend/pages/login/index.jsx
+++ frontend/pages/login/index.jsx
@@ -34,7 +34,7 @@ const Login = () => {
   useEffect(() => {
     if(Cookies.get("errLogin")) {
       setErrorMsg(Cookies.get("errLogin"))
-      // Cookies.remove("errLogin")
+      Cookies.remove("errLogin")
     }
     
     if(query.redirect !== undefined)
