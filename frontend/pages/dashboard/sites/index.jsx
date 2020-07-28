b2c2c0e77d58794c228e9d1a39ee4d56b8c9da33
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-07-28 18:19:08 +0800

    Add Skeleton to sites page

--- frontend/pages/dashboard/sites/index.jsx
+++ frontend/pages/dashboard/sites/index.jsx
@@ -1,4 +1,4 @@
-import { Fragment, Suspense, useState } from 'react'
+import { Fragment, useState } from 'react'
 import { useRouter } from 'next/router'
 import LogRocket from 'logrocket'
 import setupLogRocketReact from 'logrocket-react'
@@ -6,6 +6,7 @@ import Cookies from 'js-cookie'
 import Head from 'next/head'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import Skeleton from 'react-loading-skeleton'
 import useSWR from 'swr'
 import DataTableHeadsContent from 'public/data/data-table-heads.json'
 import Layout from 'components/Layout'
@@ -59,7 +60,75 @@ const Sites = props => {
 
   if (siteError) return <div>{siteError.message}</div>
   if (userError) return <div>{userError.message}</div>
-  if (!site || !user) return <div>Loading...</div>
+  if (!site || !user) {
+    return (
+      <Layout>
+        <SitesDiv className={`h-screen flex overflow-hidden bg-gray-100`}>
+          <MainSidebar />
+        
+          <div className={`flex flex-col w-0 flex-1 overflow-hidden`}>
+            <div className={`md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3`}>
+              <span
+                className={`-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center`}
+              >
+                <Skeleton duration={2} width={30} height={30} />
+              </span>
+            </div>
+            <main
+              className={`flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6`}
+              tabIndex={`0`}
+            >
+              <div className={`max-w-full mx-auto px-4 md:py-4 sm:px-6 md:px-8`}>
+                <div>
+                  <Skeleton duration={2} width={120} />
+                </div>
+                <div className={`mt-2 md:flex md:items-center md:justify-between`}>
+                  <div className={`flex-1 min-w-0`}>
+                    <Skeleton duration={2} width={280} />
+                  </div>
+                </div>
+              </div>
+              <div className={`max-w-full mx-auto px-4 sm:px-6 md:px-8`}>
+                <AddSite />
+                <div className={`pb-4`}>
+                  <div className={`flex flex-col`}>
+                    <div
+                      className={`-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8`}
+                    >
+                      <div
+                        className={`align-middle inline-block min-w-full shadow-xs overflow-hidden rounded-lg border-gray-200`}
+                      >
+                        <table className={`min-w-full`}>
+                          <thead>
+                            <tr>
+                              {DataTableHeadsContent.map((site, key) => {
+                                return (
+                                  <Fragment key={key}>
+                                    <th
+                                      className={`py-3 bg-white`}
+                                    >
+                                      <span className={`flex-none px-6 py-4 whitespace-no-wrap border-b border-gray-200`}><Skeleton duration={2} width={200} /></span>
+                                    </th>
+                                  </Fragment>
+                                );
+                              })}
+                            </tr>
+                          </thead>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <Pagination />
+                
+              </div>
+            </main>
+          </div>
+        </SitesDiv>
+      </Layout>
+    )
+  }
 
   LogRocket.identify('epic-design-labs/link-app', {
     name: user.first_name + ' ' + user.last_name,
@@ -72,97 +141,95 @@ const Sites = props => {
         <title>{pageTitle}</title>
       </Head>
 
-      <Suspense fallback={<div>loading...</div>}>
-        <SitesDiv className={`h-screen flex overflow-hidden bg-gray-100`}>
-          <MobileSidebar show={openMobileSidebar} />
-          <MainSidebar />
+      <SitesDiv className={`h-screen flex overflow-hidden bg-gray-100`}>
+        <MobileSidebar show={openMobileSidebar} />
+        <MainSidebar />
 
-          <div className={`flex flex-col w-0 flex-1 overflow-hidden`}>
-            <div className={`md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3`}>
-              <button
-                className={`-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150`}
-                aria-label={`Open sidebar`}
-                onClick={() => setTimeout(() => setOpenMobileSidebar(!openMobileSidebar), 150)}
-              >
-                <svg
-                  className={`h-6 w-5`}
-                  stroke={`currentColor`}
-                  fill={`none`}
-                  viewBox={`0 0 24 24`}
-                >
-                  <path
-                    strokeLinecap={`round`}
-                    strokeLinejoin={`round`}
-                    strokeWidth={`2`}
-                    d={`M4 6h16M4 12h16M4 18h16`}
-                  />
-                </svg>
-              </button>
-            </div>
-            <main
-              className={`flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6`}
-              tabIndex={`0`}
+        <div className={`flex flex-col w-0 flex-1 overflow-hidden`}>
+          <div className={`md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3`}>
+            <button
+              className={`-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150`}
+              aria-label={`Open sidebar`}
+              onClick={() => setTimeout(() => setOpenMobileSidebar(!openMobileSidebar), 150)}
             >
-              <div className={`max-w-full mx-auto px-4 md:py-4 sm:px-6 md:px-8`}>
-                <div className={`mt-2 md:flex md:items-center md:justify-between`}>
-                  <div className={`flex-1 min-w-0`}>
-                    <h2 className={`text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate lg:overflow-visible`}>
-                      Sites
-                    </h2>
-                  </div>
+              <svg
+                className={`h-6 w-5`}
+                stroke={`currentColor`}
+                fill={`none`}
+                viewBox={`0 0 24 24`}
+              >
+                <path
+                  strokeLinecap={`round`}
+                  strokeLinejoin={`round`}
+                  strokeWidth={`2`}
+                  d={`M4 6h16M4 12h16M4 18h16`}
+                />
+              </svg>
+            </button>
+          </div>
+          <main
+            className={`flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6`}
+            tabIndex={`0`}
+          >
+            <div className={`max-w-full mx-auto px-4 md:py-4 sm:px-6 md:px-8`}>
+              <div className={`mt-2 md:flex md:items-center md:justify-between`}>
+                <div className={`flex-1 min-w-0`}>
+                  <h2 className={`text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate lg:overflow-visible`}>
+                    Sites
+                  </h2>
                 </div>
               </div>
-              <div className={`max-w-full mx-auto px-4 sm:px-6 md:px-8`}>
-                <AddSite />
-                <div className={`pb-4`}>
-                  <div className={`flex flex-col`}>
+            </div>
+            <div className={`max-w-full mx-auto px-4 sm:px-6 md:px-8`}>
+              <AddSite />
+              <div className={`pb-4`}>
+                <div className={`flex flex-col`}>
+                  <div
+                    className={`-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8`}
+                  >
                     <div
-                      className={`-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8`}
+                      className={`align-middle inline-block min-w-full shadow-xs overflow-hidden sm:rounded-lg border-gray-200`}
                     >
-                      <div
-                        className={`align-middle inline-block min-w-full shadow-xs overflow-hidden sm:rounded-lg border-gray-200`}
-                      >
-                        <table className={`min-w-full`}>
-                          <thead>
-                            <tr>
-                              {DataTableHeadsContent.map((site, key) => {
-                                return (
-                                  <Fragment key={key}>
-                                    <th
-                                      className={`px-6 py-3 border-b border-gray-200 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider`}
-                                    >
-                                      <div className="flex items-center justify-between">
-                                        {site.label}
-                                      </div>
-                                    </th>
-                                  </Fragment>
-                                );
-                              })}
-                            </tr>
-                          </thead>
-                          <tbody className={`bg-white`}>
-                            {site.results ?
-                              site.results.map((val, key) => (
-                                <DataTable key={key} site={val} user={user} />
-                              )) : null}
-                          </tbody>
-                        </table>
-                      </div>
+                      <table className={`min-w-full`}>
+                        <thead>
+                          <tr>
+                            {DataTableHeadsContent.map((site, key) => {
+                              return (
+                                <Fragment key={key}>
+                                  <th
+                                    className={`px-6 py-3 border-b border-gray-200 bg-white text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider`}
+                                  >
+                                    <div className="flex items-center justify-between">
+                                      {site.label}
+                                    </div>
+                                  </th>
+                                </Fragment>
+                              );
+                            })}
+                          </tr>
+                        </thead>
+                        <tbody className={`bg-white`}>
+                          {site.results ?
+                            site.results.map((val, key) => (
+                              <DataTable key={key} site={val} user={user} />
+                            )) : null}
+                        </tbody>
+                      </table>
                     </div>
                   </div>
                 </div>
-
-                <Pagination
-                  pathName={router.pathname}
-                  apiEndpoint={sitesApiEndpoint} 
-                  page={props.page ? props.page : 0}
-                />
-
               </div>
-            </main>
-          </div>
-        </SitesDiv>
-      </Suspense>
+
+              <Pagination
+                pathName={router.pathname}
+                apiEndpoint={sitesApiEndpoint} 
+                page={props.page ? props.page : 0}
+              />
+
+            </div>
+          </main>
+        </div>
+      </SitesDiv>
     </Layout>
   )
 }
