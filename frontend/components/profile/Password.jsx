dba64a52df5cc83281ef7117eda69b3372d5285a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-07-28 23:41:02 +0800

    Update Password, Personal, and Timestamp components

--- frontend/components/profile/Password.jsx
+++ frontend/components/profile/Password.jsx
@@ -1,18 +1,26 @@
-import { useState, useEffect } from 'react'
+import { Fragment, useState, useEffect } from 'react'
 import fetch from 'node-fetch'
 import Cookies from 'js-cookie'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import Skeleton from 'react-loading-skeleton'
+import useUser from 'hooks/useUser'
 
-const ProfileSettingsPersonalDiv = styled.div``
+const ProfileSettingsPasswordDiv = styled.div``
 
-const ProfileSettingsPersonal = () => {
+const ProfileSettingsPassword = () => {
   const [errorMsg, setErrorMsg] = useState('')
   const [successMsg, setSuccessMsg] = useState('')
   const [disablePasswordFields, setDisablePasswordFields] = useState(true)
   const [disableSubmitButton, setDisableSubmitButton] = useState(false)
   const [password1, setPassword1] = useState('')
   const [password2, setPassword2] = useState('')
+
+  useEffect(() => {
+    if (password1 === password2) {
+      setDisableSubmitButton(!disableSubmitButton)
+    }
+  }, [password1, password2])
 
   const handlePasswordSubmission = async (e) => {
     e.preventDefault()
@@ -80,164 +88,173 @@ const ProfileSettingsPersonal = () => {
     setPassword2(e.target.value)
   }
 
-  useEffect(() => {
-    if (password1 === password2) {
-      setDisableSubmitButton(!disableSubmitButton)
-    }
-  }, [password1, password2])
+  const { user: user, userError: userError } = useUser({
+    redirectTo: '/login',
+    redirectIfFound: false
+  })
+
+  if (userError) return <div>{userError.message}</div>
 
   return (
-    <ProfileSettingsPersonalDiv className={`mt-5 max-w-3xl bg-white shadow-xs rounded-lg`}>
-      <div className={`px-4 py-5 sm:p-6`}>
-        <form onSubmit={handlePasswordSubmission}>
-          <div>
-            <div>
+    <Fragment>
+      {!user ? (
+        <ProfileSettingsPasswordDiv className={`mt-5 max-w-full`}>
+          <Skeleton duration={2} width={320} height={586} />
+        </ProfileSettingsPasswordDiv>
+      ) : (
+        <ProfileSettingsPasswordDiv className={`mt-5 max-w-3xl bg-white shadow-xs rounded-lg`}>
+          <div className={`px-4 py-5 sm:p-6`}>
+            <form onSubmit={handlePasswordSubmission}>
               <div>
-                <h3 className={`text-lg leading-6 font-medium text-gray-900`}>
-                  Password Change
-                </h3>
-                <p className={`mt-1 text-sm leading-5 text-gray-500`}>
-                  User generated content in real-time will have multiple touchpoints for offshoring.
-                </p>
-              </div>
-              <div
-                className={`mt-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6`}
-              >
-                <div className={`sm:col-span-6`}>
-                  <label
-                    htmlFor={`password`}
-                    className={`block text-sm font-medium leading-5 text-gray-700`}
-                  >
-                    New Password
-                  </label>
-                  <div className={`mt-1 flex rounded-md shadow-xs-sm`}>
-                    <input
-                      type={`password`}
-                      id={`password1`}
-                      value={password1}
-                      name={`password1`}
-                      disabled={disablePasswordFields ? true : false}
-                      className={`form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5 ${
-                        disablePasswordFields &&
-                        "opacity-50 bg-gray-300 cursor-not-allowed"
-                        }`}
-                      onChange={handlePasswordOneInputChange}
-                      onBlur={handlePasswordOneInputChange}
-                    />
+                <div>
+                  <div>
+                    <h3 className={`text-lg leading-6 font-medium text-gray-900`}>
+                      Password Change
+                    </h3>
+                    <p className={`mt-1 text-sm leading-5 text-gray-500`}>
+                      User generated content in real-time will have multiple touchpoints for offshoring.
+                    </p>
                   </div>
-                </div>
-              </div>
-              <div
-                className={`mt-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6`}
-              >
-                <div className={`sm:col-span-6`}>
-                  <label
-                    htmlFor={`password1`}
-                    className={`block text-sm font-medium leading-5 text-gray-700`}
+                  <div
+                    className={`mt-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6`}
                   >
-                    Confirm Password
-                  </label>
-                  <div className={`mt-1 flex rounded-md shadow-xs-sm`}>
-                    <input
-                      type={`password`}
-                      id={`password2`}
-                      value={password2}
-                      name={`password2`}
-                      disabled={disablePasswordFields ? true : false}
-                      className={`form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5 ${
-                        disablePasswordFields &&
-                        "opacity-50 bg-gray-300 cursor-not-allowed"
-                        }`}
-                      onChange={handlePasswordTwoInputChange}
-                      onBlur={handlePasswordTwoInputChange}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={`mt-8 border-t border-gray-200 pt-5`}>
-            <div className={`flex justify-between`}>
-              <div className={`flex justify-start`}>
-                <span className={`inline-flex rounded-md shadow-xs-sm`}>
-                  <button
-                    type={`submit`}
-                    disabled={!disablePasswordFields ? true : false}
-                    className={`inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 transition duration-150 ease-in-out ${
-                      !disablePasswordFields ?
-                        "opacity-50 bg-indigo-300 cursor-not-allowed" : "hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-xs-outline-indigo active:bg-indigo-700"
-                      }`}
-                    onClick={handleEditPasswordProfile}
-                  >
-                    Update Password
-                  </button>
-                </span>
-
-                <span className={`inline-flex rounded-md shadow-xs-sm`}>
-                  <button
-                    disabled={!disablePasswordFields ? false : true}
-                    className={`inline-flex justify-center w-full rounded-md border border-gray-300 sm:ml-3 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 shadow-xs-sm transition ease-in-out duration-150 sm:text-sm sm:leading-5 ${
-                      !disablePasswordFields ?
-                        "hover:text-gray-500 focus:outline-none" : "opacity-50 cursor-not-allowed"
-                      }`}
-                    onClick={handleEditPasswordProfile}
-                  >
-                    Cancel Edit
-                  </button>
-                </span>
-
-                {errorMsg && (
-                  <div className={`inline-block ml-2 p-2`}>
-                    <div className={`flex`}>
-                      <div>
-                        <h3
-                          className={`text-sm leading-5 font-medium text-red-800 break-words`}
-                        >
-                          {errorMsg}
-                        </h3>
+                    <div className={`sm:col-span-6`}>
+                      <label
+                        htmlFor={`password`}
+                        className={`block text-sm font-medium leading-5 text-gray-700`}
+                      >
+                        New Password
+                      </label>
+                      <div className={`mt-1 flex rounded-md shadow-xs-sm`}>
+                        <input
+                          type={`password`}
+                          id={`password1`}
+                          value={password1}
+                          name={`password1`}
+                          disabled={disablePasswordFields ? true : false}
+                          className={`form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5 ${
+                            disablePasswordFields &&
+                            "opacity-50 bg-gray-300 cursor-not-allowed"
+                            }`}
+                          onChange={handlePasswordOneInputChange}
+                          onBlur={handlePasswordOneInputChange}
+                        />
                       </div>
                     </div>
                   </div>
-                )}
-
-                {successMsg && (
-                  <div className={`inline-block ml-2 p-2`}>
-                    <div className={`flex`}>
-                      <div>
-                        <h3
-                          className={`text-sm leading-5 font-medium text-green-800 break-words`}
-                        >
-                          {successMsg}
-                        </h3>
+                  <div
+                    className={`mt-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6`}
+                  >
+                    <div className={`sm:col-span-6`}>
+                      <label
+                        htmlFor={`password1`}
+                        className={`block text-sm font-medium leading-5 text-gray-700`}
+                      >
+                        Confirm Password
+                      </label>
+                      <div className={`mt-1 flex rounded-md shadow-xs-sm`}>
+                        <input
+                          type={`password`}
+                          id={`password2`}
+                          value={password2}
+                          name={`password2`}
+                          disabled={disablePasswordFields ? true : false}
+                          className={`form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5 ${
+                            disablePasswordFields &&
+                            "opacity-50 bg-gray-300 cursor-not-allowed"
+                            }`}
+                          onChange={handlePasswordTwoInputChange}
+                          onBlur={handlePasswordTwoInputChange}
+                        />
                       </div>
                     </div>
                   </div>
-                )}
+                </div>
               </div>
-              <div className={`flex justify-end`}>
-                <span className={`ml-3 inline-flex rounded-md shadow-xs-sm`}>
-                  <button
-                    type={`submit`}
-                    className={`inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-600 transition duration-150 ease-in-out ${
-                      disableSubmitButton ?
-                        "opacity-50 bg-green-300 cursor-not-allowed" : "hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-xs-outline-green active:bg-green-700"
-                      }`}
-                  >
-                    Save Password
-                  </button>
-                </span>
+              <div className={`mt-8 border-t border-gray-200 pt-5`}>
+                <div className={`flex justify-between`}>
+                  <div className={`flex justify-start`}>
+                    <span className={`inline-flex rounded-md shadow-xs-sm`}>
+                      <button
+                        type={`submit`}
+                        disabled={!disablePasswordFields ? true : false}
+                        className={`inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 transition duration-150 ease-in-out ${
+                          !disablePasswordFields ?
+                            "opacity-50 bg-indigo-300 cursor-not-allowed" : "hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-xs-outline-indigo active:bg-indigo-700"
+                          }`}
+                        onClick={handleEditPasswordProfile}
+                      >
+                        Update Password
+                      </button>
+                    </span>
+
+                    <span className={`inline-flex rounded-md shadow-xs-sm`}>
+                      <button
+                        disabled={!disablePasswordFields ? false : true}
+                        className={`inline-flex justify-center w-full rounded-md border border-gray-300 sm:ml-3 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 shadow-xs-sm transition ease-in-out duration-150 sm:text-sm sm:leading-5 ${
+                          !disablePasswordFields ?
+                            "hover:text-gray-500 focus:outline-none" : "opacity-50 cursor-not-allowed"
+                          }`}
+                        onClick={handleEditPasswordProfile}
+                      >
+                        Cancel Edit
+                      </button>
+                    </span>
+
+                    {errorMsg && (
+                      <div className={`inline-block ml-2 p-2`}>
+                        <div className={`flex`}>
+                          <div>
+                            <h3
+                              className={`text-sm leading-5 font-medium text-red-800 break-words`}
+                            >
+                              {errorMsg}
+                            </h3>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+
+                    {successMsg && (
+                      <div className={`inline-block ml-2 p-2`}>
+                        <div className={`flex`}>
+                          <div>
+                            <h3
+                              className={`text-sm leading-5 font-medium text-green-800 break-words`}
+                            >
+                              {successMsg}
+                            </h3>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                  <div className={`flex justify-end`}>
+                    <span className={`ml-3 inline-flex rounded-md shadow-xs-sm`}>
+                      <button
+                        type={`submit`}
+                        className={`inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-600 transition duration-150 ease-in-out ${
+                          disableSubmitButton ?
+                            "opacity-50 bg-green-300 cursor-not-allowed" : "hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-xs-outline-green active:bg-green-700"
+                          }`}
+                      >
+                        Save Password
+                      </button>
+                    </span>
+                  </div>
+                </div>
               </div>
-            </div>
+            </form>
           </div>
-        </form>
-      </div>
-    </ProfileSettingsPersonalDiv>
+        </ProfileSettingsPasswordDiv>
+      )}
+    </Fragment>
   )
 }
 
-export default ProfileSettingsPersonal
+export default ProfileSettingsPassword
 
-ProfileSettingsPersonal.propTypes = {
+ProfileSettingsPassword.propTypes = {
   errorMsg: PropTypes.string,
   successMsg: PropTypes.string,
   disablePasswordFields: PropTypes.bool,
--- frontend/components/settings/Timestamp.jsx
+++ frontend/components/settings/Timestamp.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
+import { Fragment, useState, useEffect } from 'react'
 import fetch from 'node-fetch'
 import Cookies from 'js-cookie'
 import useSWR from 'swr'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import Skeleton from 'react-loading-skeleton'
 import fetchJson from 'hooks/fetchJson'
 
 const TimestampSettingsDiv = styled.div``
@@ -67,119 +68,123 @@ const TimestampSettings = () => {
 		await updateTimestampSettings(`/api/auth/user/`, !user.settings.disableLocalTime)
 	}
 
-	if (!user) {
-		return (<div>Loading...</div>)
-	}
-
 	return (
-		<TimestampSettingsDiv
-			className={`mt-5 max-w-full bg-white shadow-xs rounded-lg`}
-		>
-			<div className={`px-4 py-5 sm:p-6`}>
-				<div>
-					<div>
+		<Fragment>
+      {!user ? (
+        <TimestampSettingsDiv className={`mt-5 max-w-full`}>
+          <Skeleton duration={2} width={320} height={213} />
+        </TimestampSettingsDiv>
+      ) : (
+				<TimestampSettingsDiv
+					className={`mt-5 max-w-full bg-white shadow-xs rounded-lg`}
+				>
+					<div className={`px-4 py-5 sm:p-6`}>
 						<div>
-							<h3 className={`text-lg leading-6 font-medium text-gray-900`}>
-								Site Settings
-							</h3>
-							<p className={`mt-1 text-sm leading-5 text-gray-500`}>
-								User generated content in real-time will have multiple
-								touchpoints for offshoring.
-							</p>
-						</div>
-						<div
-							className={`mt-6 grid sm:grid sm:grid-cols-3 sm:gap-4 sm:items-baseline`}
-						>
-							<div className={`sm:col-span-6`}>
+							<div>
+								<div>
+									<h3 className={`text-lg leading-6 font-medium text-gray-900`}>
+										Site Settings
+									</h3>
+									<p className={`mt-1 text-sm leading-5 text-gray-500`}>
+										User generated content in real-time will have multiple
+										touchpoints for offshoring.
+									</p>
+								</div>
+								<div
+									className={`mt-6 grid sm:grid sm:grid-cols-3 sm:gap-4 sm:items-baseline`}
+								>
+									<div className={`sm:col-span-6`}>
 
-								<div className={`relative flex items-center`}>
-									<div className={`absolute flex items-center h-5`}>
-										<span
-											role="checkbox"
-											tabIndex="0"
-											aria-checked={!user.settings.disableLocalTime}
-											className={`${!user.settings.disableLocalTime ? "bg-indigo-600" : "bg-gray-200"} relative inline-flex flex-shrink-0 h-6 w-10 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-xs-outline`}
-											onClick={handleToggleTimestamp}
-										>
-											<span
-												aria-hidden="true"
-												className={`${!user.settings.disableLocalTime ? "translate-x-4" : "translate-x-0"} relative inline-block h-5 w-5 rounded-full bg-white shadow-xs transform transition ease-in-out duration-200`}
-											>
+										<div className={`relative flex items-center`}>
+											<div className={`absolute flex items-center h-5`}>
 												<span
-													className={`${!user.settings.disableLocalTime ? "opacity-0 ease-out duration-100" : "opacity-100 ease-in duration-200"} absolute inset-0 h-full w-full flex items-center justify-center transition-opacity`}
+													role="checkbox"
+													tabIndex="0"
+													aria-checked={!user.settings.disableLocalTime}
+													className={`${!user.settings.disableLocalTime ? "bg-indigo-600" : "bg-gray-200"} relative inline-flex flex-shrink-0 h-6 w-10 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-xs-outline`}
+													onClick={handleToggleTimestamp}
 												>
-													<svg
-														className={`h-3 w-3 text-gray-400`}
-														fill="none"
-														viewBox="0 0 12 12"
+													<span
+														aria-hidden="true"
+														className={`${!user.settings.disableLocalTime ? "translate-x-4" : "translate-x-0"} relative inline-block h-5 w-5 rounded-full bg-white shadow-xs transform transition ease-in-out duration-200`}
 													>
-														<path
-															d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
-															stroke="currentColor"
-															strokeWidth="2"
-															strokeLinecap="round"
-															strokeLinejoin="round"
-														/>
-													</svg>
+														<span
+															className={`${!user.settings.disableLocalTime ? "opacity-0 ease-out duration-100" : "opacity-100 ease-in duration-200"} absolute inset-0 h-full w-full flex items-center justify-center transition-opacity`}
+														>
+															<svg
+																className={`h-3 w-3 text-gray-400`}
+																fill="none"
+																viewBox="0 0 12 12"
+															>
+																<path
+																	d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
+																	stroke="currentColor"
+																	strokeWidth="2"
+																	strokeLinecap="round"
+																	strokeLinejoin="round"
+																/>
+															</svg>
+														</span>
+														<span
+															className={`${!user.settings.disableLocalTime ? "opacity-100 ease-in duration-200" : "opacity-0 ease-out duration-100"} absolute inset-0 h-full w-full flex items-center justify-center transition-opacity`}
+														>
+															<svg
+																className={`h-3 w-3 text-indigo-600`}
+																fill="currentColor"
+																viewBox="0 0 12 12"
+															>
+																<path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
+															</svg>
+														</span>
+													</span>
 												</span>
-												<span
-													className={`${!user.settings.disableLocalTime ? "opacity-100 ease-in duration-200" : "opacity-0 ease-out duration-100"} absolute inset-0 h-full w-full flex items-center justify-center transition-opacity`}
-												>
-													<svg
-														className={`h-3 w-3 text-indigo-600`}
-														fill="currentColor"
-														viewBox="0 0 12 12"
-													>
-														<path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
-													</svg>
-												</span>
-											</span>
-										</span>
-									</div>
-									<div className={`ml-2 pl-12 text-sm leading-5`}>
-										<label htmlFor="candidates" className={`font-medium text-gray-700`}>Enable Local Time</label>
-										<p className={`text-gray-500`}>Set all sites to use local times instead of UTC</p>
+											</div>
+											<div className={`ml-2 pl-12 text-sm leading-5`}>
+												<label htmlFor="candidates" className={`font-medium text-gray-700`}>Enable Local Time</label>
+												<p className={`text-gray-500`}>Set all sites to use local times instead of UTC</p>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>
-				<div className={`mt-8 border-t border-gray-200 pt-5`}>
-					<div className={`flex justify-between`}>
-						<div className={`flex justify-start`}>
-							{errorMsg && (
-								<div className={`inline-block p-2`}>
-									<div className={`flex`}>
-										<div>
-											<h3
-												className={`text-sm leading-5 font-medium text-red-800 break-words`}
-											>
-												{errorMsg}
-											</h3>
+						<div className={`mt-8 border-t border-gray-200 pt-5`}>
+							<div className={`flex justify-between`}>
+								<div className={`flex justify-start`}>
+									{errorMsg && (
+										<div className={`inline-block p-2`}>
+											<div className={`flex`}>
+												<div>
+													<h3
+														className={`text-sm leading-5 font-medium text-red-800 break-words`}
+													>
+														{errorMsg}
+													</h3>
+												</div>
+											</div>
 										</div>
-									</div>
-								</div>
-							)}
+									)}
 
-							{successMsg && (
-								<div className={`inline-block p-2`}>
-									<div className={`flex`}>
-										<div>
-											<h3
-												className={`text-sm leading-5 font-medium text-green-800 break-words`}
-											>
-												{successMsg}
-											</h3>
+									{successMsg && (
+										<div className={`inline-block p-2`}>
+											<div className={`flex`}>
+												<div>
+													<h3
+														className={`text-sm leading-5 font-medium text-green-800 break-words`}
+													>
+														{successMsg}
+													</h3>
+												</div>
+											</div>
 										</div>
-									</div>
+									)}
 								</div>
-							)}
+							</div>
 						</div>
 					</div>
-				</div>
-			</div>
-		</TimestampSettingsDiv>
+				</TimestampSettingsDiv>
+			)}
+		</Fragment>
 	)
 }
 
