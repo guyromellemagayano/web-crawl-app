7afd8eaf55b976e3d02f68bb60735c3517b10f5d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-08-03 22:52:27 +0800

    Initialize images, scripts, and stylesheets pages

---
+++ frontend/pages/dashboard/site/[siteId]/images.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+const ImagesDiv = styled.section``
+
+const Images = () => {
+	return (
+		<ImagesDiv></ImagesDiv>
+	)
+}
+
+export default Images
---
+++ frontend/pages/dashboard/site/[siteId]/stylesheets.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+const StylesheetsDiv = styled.section``
+
+const Stylesheets = () => {
+	return (
+		<StylesheetsDiv></StylesheetsDiv>
+	)
+}
+
+export default Stylesheets
