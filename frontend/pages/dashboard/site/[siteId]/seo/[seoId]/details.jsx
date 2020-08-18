59b1f64595d24de0ac2aa7005f8e77423f88f739
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-08-18 19:55:12 +0800

    Remove extra code in some detail pages

--- frontend/pages/dashboard/site/[siteId]/seo/[seoId]/details.jsx
+++ frontend/pages/dashboard/site/[siteId]/seo/[seoId]/details.jsx
@@ -10,9 +10,6 @@ import Moment from 'react-moment'
 import Layout from 'components/Layout'
 import MobileSidebar from 'components/sidebar/MobileSidebar'
 import MainSidebar from 'components/sidebar/MainSidebar'
-import SiteDangerBadge from 'components/badges/SiteDangerBadge'
-import SiteSuccessBadge from 'components/badges/SiteSuccessBadge'
-import SiteWarningBadge from 'components/badges/SiteWarningBadge'
 
 const fetcher = async (url) => {
   const res = await fetch(url, {
@@ -156,7 +153,7 @@ const SeoDetail = () => {
                         <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"/>
                       </svg>
                       <Link href='/dashboard/site/[siteId]/seo' as={'/dashboard/site/' + query.siteId + '/seo'}>
-                        <a className={`whitespace-no-wrap font-medium text-gray-500 hover:text-gray-700 transition duration-150 ease-in-out`}>All SEO pages</a>
+                        <a className={`whitespace-no-wrap font-medium text-gray-500 hover:text-gray-700 transition duration-150 ease-in-out`}>All SEO</a>
                       </Link>
                       <svg className={`flex-shrink-0 mx-2 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor`}>
                         <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"/>
