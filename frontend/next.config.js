839a64a011bb063feffbc6cf94d54fa682fcb397
Guy Romelle Magayano <mguyromelle@mguyromelle-pop-os.local>
2020-08-25 01:17:27 +0800

    Add new redirect feature from nextjs v9.5.2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*'
+      },
+      {
+        source: '/api/:path*',
+        destination: `/`
+      }
+    ]
+  },
   devIndicators: {
     autoPrerender: false,
   },
