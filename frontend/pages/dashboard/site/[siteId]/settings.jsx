0a09fc47e6851ae0481eaa696b54c8c0ea60ccc5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-08-27 21:33:20 +0800

    Update site settings dialogs to notifications

--- frontend/pages/dashboard/site/[siteId]/settings.jsx
+++ frontend/pages/dashboard/site/[siteId]/settings.jsx
@@ -26,6 +26,7 @@ const SiteSettings = () => {
   const [siteName, setSiteName] = useState('')
   const [siteUrl, setSiteUrl] = useState('')
   const [showModal, setShowModal] = useState(false)
+  const [showNotificationStatus, setShowNotificationStatus] = useState(false)
 
   const { user: user, userError: userError } = useUser({
     redirectTo: '/login',
@@ -64,8 +65,12 @@ const SiteSettings = () => {
   
     if (response.ok && response.status === 200) {
       if (data) {
-        setSuccessMsg('Site information update success.')
+        setSuccessMsg('Site information update successfully.')
+        setTimeout(() => setShowNotificationStatus(true), 1500)
         setDisableInputFields(!disableInputFields)
+      } else {
+        setErrorMsg('Site information update failed. Please try again.')
+        setTimeout(() => setShowNotificationStatus(true), 1500)
       }
     } else {
       const error = new Error(response.statusText)
@@ -74,6 +79,7 @@ const SiteSettings = () => {
       error.data = data
   
       setErrorMsg('An unexpected error occurred. Please try again.')
+      setTimeout(() => setShowNotificationStatus(true), 1500)
   
       throw error
     }
@@ -131,6 +137,11 @@ const SiteSettings = () => {
     }
   }, [site])
 
+  useEffect(() => {
+    if (showNotificationStatus === true)
+    setTimeout(() => setShowNotificationStatus(false), 7500)
+  }, [showNotificationStatus])
+
   {userError || siteError && <Layout>{userError.message || siteError.message}</Layout>}
 
   return (
@@ -144,6 +155,124 @@ const SiteSettings = () => {
           <SiteSettingsDiv className={`h-screen flex overflow-hidden bg-gray-100`}>
             <MobileSidebar show={openMobileSidebar} />
             <MainSidebar />
+
+            <Transition show={showNotificationStatus}>
+              <div
+                className={`fixed z-50 inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end`}
+              >
+                <Transition
+                  enter="transform ease-out duration-300 transition"
+                  enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+                  enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div
+                    className={`max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto`}
+                  >
+                    <div className={`rounded-lg shadow-xs overflow-hidden`}>
+                      <div className={`p-4`}>
+                        <div className={`flex items-start`}>
+                          <div className={`flex-shrink-0`}>
+                            {errorMsg ? (
+                              <svg
+                                className={`h-8 w-8 text-red-400`}
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                                  clipRule="evenodd"
+                                ></path>
+                              </svg>
+                            ) : successMsg ? (
+                              <svg
+                                className={`h-8 w-8 text-green-400`}
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                  clipRule="evenodd"
+                                ></path>
+                              </svg>
+                            ) : (
+                              <svg
+                                className={`h-8 w-8 text-gray-400`}
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+                                  clipRule="evenodd"
+                                ></path>
+                              </svg>
+                            )}
+                          </div>
+                          <div className={`ml-3 w-0 flex-1 pt-0.5`}>
+                            <p
+                              className={`text-sm leading-5 font-medium ${
+                                errorMsg !== undefined && errorMsg !== ""
+                                  ? "text-red-500"
+                                  : "text-gray-900"
+                              } ${
+                                successMsg !== undefined && successMsg !== ""
+                                  ? "text-green-500"
+                                  : "text-gray-900"
+                              }`}
+                            >
+                              {errorMsg !== undefined && errorMsg !== ""
+                                ? "Update Failed!"
+                                : successMsg !== undefined && successMsg !== ""
+                                ? "Update Success!"
+                                : "Verifying..."}
+                            </p>
+                            <p className={`mt-1 text-sm leading-5 text-gray-500`}>
+                              {errorMsg !== undefined && errorMsg !== ""
+                                ? errorMsg
+                                : successMsg}
+                            </p>
+                          </div>
+                          <div className={`ml-4 flex-shrink-0 flex`}>
+                            <button
+                              className={`inline-flex text-gray-400 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150`}
+                              onClick={() =>
+                                setTimeout(
+                                  () =>
+                                    setShowNotificationStatus(
+                                      !showNotificationStatus
+                                    ),
+                                  150
+                                )
+                              }
+                            >
+                              <svg
+                                className={`h-5 w-5`}
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                  clipRule="evenodd"
+                                />
+                              </svg>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Transition>
+              </div>
+            </Transition>
 
             <div className={`flex flex-col w-0 flex-1 overflow-hidden`}>
               <div className={`md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3`}>
@@ -213,7 +342,7 @@ const SiteSettings = () => {
                               <h3
                                 className={`text-lg leading-6 font-medium text-gray-900`}
                               >
-                                Update Site
+                                Site Information
                               </h3>
                               <p className={`mt-1 text-sm leading-5 text-gray-500`}>
                                 User generated content in real-time will have
@@ -302,34 +431,6 @@ const SiteSettings = () => {
                                   Cancel Edit
                                 </button>
                               </span>
-
-                              {errorMsg && (
-                                <div className={`inline-block ml-2 p-2`}>
-                                  <div className={`flex`}>
-                                    <div>
-                                      <h3
-                                        className={`text-sm leading-5 font-medium text-red-800 break-words`}
-                                      >
-                                        {errorMsg}
-                                      </h3>
-                                    </div>
-                                  </div>
-                                </div>
-                              )}
-
-                              {successMsg && (
-                                <div className={`inline-block ml-2 p-2`}>
-                                  <div className={`flex`}>
-                                    <div>
-                                      <h3
-                                        className={`text-sm leading-5 font-medium text-green-800 break-words`}
-                                      >
-                                        {successMsg}
-                                      </h3>
-                                    </div>
-                                  </div>
-                                </div>
-                              )}
                             </div>
                             <div className={`flex justify-end`}>
                               <span
