ceee42f01c1d3a1eb1f0314afd821d00cc86dd00
matematik7 <domen.ipavec@gmail.com>
2020-09-07 18:50:33 +0200

    Limit url length in db

--- go/crawler/service_scan.go
+++ go/crawler/service_scan.go
@@ -286,7 +286,7 @@ func (s *scanner) loadURL(log *zap.SugaredLogger, url *url.URL) (*common.CrawlLi
 	crawlLink := &common.CrawlLink{
 		ScanID:    s.Scan.ID,
 		CreatedAt: time.Now(),
-		Url:       url.String(),
+		Url:       lenLimit(url.String(), 2047),
 		Type:      TYPE_OTHER,
 		Status:    STATUS_OK,
 		TlsStatus: TLS_OK,
@@ -308,10 +308,7 @@ func (s *scanner) loadURL(log *zap.SugaredLogger, url *url.URL) (*common.CrawlLi
 		} else {
 			log.Errorf("Other error for link %v: %v", url, err)
 			crawlLink.Status = STATUS_OTHER_ERROR
-			errStr := err.Error()
-			if len(errStr) > 255 {
-				errStr = errStr[:255]
-			}
+			errStr := lenLimit(err.Error(), 255)
 			crawlLink.Error = &errStr
 		}
 	}
@@ -499,4 +496,11 @@ func (r *SizeReader) Read(p []byte) (int, error) {
 	n, err := r.Child.Read(p)
 	r.Size += n
 	return n, err
+}
+
+func lenLimit(s string, limit int) string {
+	if len(s) > limit {
+		return s[:limit]
+	}
+	return s
 }
