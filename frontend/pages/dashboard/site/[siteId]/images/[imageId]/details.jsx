f140b90df26bdf57cf266346a563b642796e4ac1
Guy Romelle Magayano <guy@epicdesignlabs.com>
2020-09-12 02:14:00 +0800

    Remove response time in image detail page

--- frontend/pages/dashboard/site/[siteId]/images/[imageId]/details.jsx
+++ frontend/pages/dashboard/site/[siteId]/images/[imageId]/details.jsx
@@ -372,20 +372,6 @@ const ImagesDetail = () => {
                             )}
                           </dd>
                         </div>
-                        <div
-                          className={`mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5`}
-                        >
-                          <dt
-                            className={`text-sm leading-5 font-medium text-gray-500`}
-                          >
-                            Response Time
-                          </dt>
-                          <dd
-                            className={`mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2`}
-                          >
-                            {imageLocation.response_time} ms
-                          </dd>
-                        </div>
                         {imageLocation.error !== null &&
                         imageLocation.error !== undefined ? (
                           <div
