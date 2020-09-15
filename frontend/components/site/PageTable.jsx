38b505b1566fd89899a383ec3060c522265a9a44
Guy Romelle Magayano <guy@epicdesignlabs.com>
2020-09-15 20:02:09 +0800

    Adjust padding of SSL data in page table

--- frontend/components/site/PageTable.jsx
+++ frontend/components/site/PageTable.jsx
@@ -118,7 +118,7 @@ const PagesTable = props => {
           <SiteSuccessIcon />
         </td> */}
         <td
-          className={`px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500`}
+          className={`px-16 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500`}
         >
           {props.val.tls_total == true ? <SiteSuccessIcon /> : <SiteDangerIcon />}
         </td>
