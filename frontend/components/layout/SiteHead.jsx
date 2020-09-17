26ab15bd22e42176059e07189a5e8ab3dd629b16
Guy Romelle Magayano <guy@epicdesignlabs.com>
2020-09-17 23:26:41 +0800

    Update site head component

--- frontend/components/layout/SiteHead.jsx
+++ frontend/components/layout/SiteHead.jsx
@@ -10,8 +10,8 @@ const Head = () => {
 			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
 			<meta name="apple-mobile-web-app-title" content="Site Crawler" />
 			<meta name="application-name" content="Site Crawler" />
-			<meta name="msapplication-TileColor" content="#ffffff" />
-			<meta name="theme-color" content="#ffffff"></meta>
+			<meta name="msapplication-TileColor" content="#b91d47" />
+			<meta name="theme-color" content="#ffffff" />
 		</Fragment>
 	)
 }
