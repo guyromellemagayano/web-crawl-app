384dc34e7cdffbd20409402bf2668fc68d027663
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-10-06 01:27:57 +0800

    Update global settings page with label configs

--- frontend/components/settings/Timestamp.jsx
+++ frontend/components/settings/Timestamp.jsx
@@ -1,19 +1,23 @@
-import { Fragment, useState, useEffect } from 'react'
-import fetch from 'node-fetch'
-import Cookies from 'js-cookie'
-import useSWR from 'swr'
-import styled from 'styled-components'
-import PropTypes from 'prop-types'
-import Skeleton from 'react-loading-skeleton'
-import fetchJson from 'hooks/fetchJson'
+import { Fragment, useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
+import fetch from 'node-fetch';
+import fetchJson from 'hooks/fetchJson';
+import GlobalLabels from 'public/label/pages/global.json';
+import PropTypes from 'prop-types';
+import Skeleton from 'react-loading-skeleton';
+import styled from 'styled-components';
+import useSWR from 'swr';
 
-const TimestampSettingsDiv = styled.div``
+const TimestampSettingsDiv = styled.div``;
 
 const TimestampSettings = () => {
-	const [errorMsg, setErrorMsg] = useState('')
-	const [successMsg, setSuccessMsg] = useState('')
+	const [errorMsg, setErrorMsg] = useState('');
+	const [successMsg, setSuccessMsg] = useState('');
 
-	const { data: user, mutate: updateUser } = useSWR(() => ('/api/auth/user/'), () => fetchUserSettings(`/api/auth/user/`))
+	const { data: user, mutate: updateUser } = useSWR(
+		() => '/api/auth/user/',
+		() => fetchUserSettings(`/api/auth/user/`)
+	);
 
 	const fetchUserSettings = async (endpoint) => {
 		const userSettingsData = await fetchJson(endpoint, {
@@ -21,60 +25,63 @@ const TimestampSettings = () => {
 			headers: {
 				'Accept': 'application/json',
 				'Content-Type': 'application/json',
-				'X-CSRFToken': Cookies.get('csrftoken'),
+				'X-CSRFToken': Cookies.get('csrftoken')
 			}
-		})
+		});
 
-		return userSettingsData
-	}
+		return userSettingsData;
+	};
 
 	const updateTimestampSettings = async (endpoint, state) => {
-		user.settings.disableLocalTime = state
+		user.settings.disableLocalTime = state;
 
 		const response = await fetch(endpoint, {
 			method: 'PATCH',
 			headers: {
 				'Accept': 'application/json',
 				'Content-Type': 'application/json',
-				'X-CSRFToken': Cookies.get('csrftoken'),
+				'X-CSRFToken': Cookies.get('csrftoken')
 			},
-			body: JSON.stringify(user),
-		})
+			body: JSON.stringify(user)
+		});
 
-		const data = await response.json()
+		const data = await response.json();
 
 		if (response.ok && response.status === 200) {
 			if (data.settings.disableLocalTime) {
-				setSuccessMsg('Local time disabled globally.')
+				setSuccessMsg('Local time disabled globally.');
 			} else {
-				setSuccessMsg('Local time enabled globally.')
+				setSuccessMsg('Local time enabled globally.');
 			}
-			updateUser(data)
+			updateUser(data);
 		} else {
-			const error = new Error(response.statusText)
+			const error = new Error(response.statusText);
 
-			error.response = response
-			error.data = data
+			error.response = response;
+			error.data = data;
 
-			setErrorMsg('An unexpected error occurred. Please try again.')
+			setErrorMsg('An unexpected error occurred. Please try again.');
 
-			throw error
+			throw error;
 		}
-	}
+	};
 
 	const handleToggleTimestamp = async (e) => {
-		e.preventDefault()
+		e.preventDefault();
 
-		await updateTimestampSettings(`/api/auth/user/`, !user.settings.disableLocalTime)
-	}
+		await updateTimestampSettings(
+			`/api/auth/user/`,
+			!user.settings.disableLocalTime
+		);
+	};
 
 	return (
 		<Fragment>
-      {!user ? (
-        <TimestampSettingsDiv className={`max-w-full`}>
-          <Skeleton duration={2} width={320} height={213} />
-        </TimestampSettingsDiv>
-      ) : (
+			{!user ? (
+				<TimestampSettingsDiv className={`max-w-full`}>
+					<Skeleton duration={2} width={320} height={213} />
+				</TimestampSettingsDiv>
+			) : (
 				<TimestampSettingsDiv
 					className={`max-w-full bg-white shadow-xs rounded-lg`}
 				>
@@ -83,64 +90,86 @@ const TimestampSettings = () => {
 							<div>
 								<div>
 									<h3 className={`text-lg leading-6 font-medium text-gray-900`}>
-										Site Settings
+										{GlobalLabels[0].label}
 									</h3>
 									<p className={`mt-1 text-sm leading-5 text-gray-500`}>
-									These settings apply globally. For site-specific settings, click into a site and then click into "Site Settings" in the menu bar.
+										{GlobalLabels[0].description}
 									</p>
 								</div>
 								<div
 									className={`mt-6 grid sm:grid sm:grid-cols-3 sm:gap-4 sm:items-baseline`}
 								>
 									<div className={`sm:col-span-6`}>
-
 										<div className={`relative flex items-center`}>
 											<div className={`absolute flex items-center h-5`}>
 												<span
-													role="checkbox"
-													tabIndex="0"
+													role='checkbox'
+													tabIndex='0'
 													aria-checked={!user.settings.disableLocalTime}
-													className={`${!user.settings.disableLocalTime ? "bg-indigo-600" : "bg-gray-200"} relative inline-flex flex-shrink-0 h-6 w-10 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-xs-outline`}
+													className={`${
+														!user.settings.disableLocalTime
+															? 'bg-indigo-600'
+															: 'bg-gray-200'
+													} relative inline-flex flex-shrink-0 h-6 w-10 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-xs-outline`}
 													onClick={handleToggleTimestamp}
 												>
 													<span
-														aria-hidden="true"
-														className={`${!user.settings.disableLocalTime ? "translate-x-4" : "translate-x-0"} relative inline-block h-5 w-5 rounded-full bg-white shadow-xs transform transition ease-in-out duration-200`}
+														aria-hidden='true'
+														className={`${
+															!user.settings.disableLocalTime
+																? 'translate-x-4'
+																: 'translate-x-0'
+														} relative inline-block h-5 w-5 rounded-full bg-white shadow-xs transform transition ease-in-out duration-200`}
 													>
 														<span
-															className={`${!user.settings.disableLocalTime ? "opacity-0 ease-out duration-100" : "opacity-100 ease-in duration-200"} absolute inset-0 h-full w-full flex items-center justify-center transition-opacity`}
+															className={`${
+																!user.settings.disableLocalTime
+																	? 'opacity-0 ease-out duration-100'
+																	: 'opacity-100 ease-in duration-200'
+															} absolute inset-0 h-full w-full flex items-center justify-center transition-opacity`}
 														>
 															<svg
 																className={`h-3 w-3 text-gray-400`}
-																fill="none"
-																viewBox="0 0 12 12"
+																fill='none'
+																viewBox='0 0 12 12'
 															>
 																<path
-																	d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
-																	stroke="currentColor"
-																	strokeWidth="2"
-																	strokeLinecap="round"
-																	strokeLinejoin="round"
+																	d='M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2'
+																	stroke='currentColor'
+																	strokeWidth='2'
+																	strokeLinecap='round'
+																	strokeLinejoin='round'
 																/>
 															</svg>
 														</span>
 														<span
-															className={`${!user.settings.disableLocalTime ? "opacity-100 ease-in duration-200" : "opacity-0 ease-out duration-100"} absolute inset-0 h-full w-full flex items-center justify-center transition-opacity`}
+															className={`${
+																!user.settings.disableLocalTime
+																	? 'opacity-100 ease-in duration-200'
+																	: 'opacity-0 ease-out duration-100'
+															} absolute inset-0 h-full w-full flex items-center justify-center transition-opacity`}
 														>
 															<svg
 																className={`h-3 w-3 text-indigo-600`}
-																fill="currentColor"
-																viewBox="0 0 12 12"
+																fill='currentColor'
+																viewBox='0 0 12 12'
 															>
-																<path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
+																<path d='M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z' />
 															</svg>
 														</span>
 													</span>
 												</span>
 											</div>
 											<div className={`ml-2 pl-12 text-sm leading-5`}>
-												<label htmlFor="candidates" className={`font-medium text-gray-700`}>Enable Local Time</label>
-												<p className={`text-gray-500`}>Set all sites to use local times instead of UTC</p>
+												<label
+													htmlFor='candidates'
+													className={`font-medium text-gray-700`}
+												>
+													{GlobalLabels[1].label}
+												</label>
+												<p className={`text-gray-500`}>
+													{GlobalLabels[1].description}
+												</p>
 											</div>
 										</div>
 									</div>
@@ -184,9 +213,9 @@ const TimestampSettings = () => {
 				</TimestampSettingsDiv>
 			)}
 		</Fragment>
-	)
-}
+	);
+};
 
-export default TimestampSettings
+export default TimestampSettings;
 
-TimestampSettings.propTypes = {}
+TimestampSettings.propTypes = {};
--- frontend/pages/dashboard/settings/global/index.jsx
+++ frontend/pages/dashboard/settings/global/index.jsx
@@ -1,103 +1,103 @@
-import { useState, Fragment } from "react";
-import Head from "next/head";
-import styled from "styled-components";
-import PropTypes from "prop-types";
-import Skeleton from "react-loading-skeleton";
-import useUser from "hooks/useUser";
-import Layout from "components/Layout";
-import MobileSidebar from "components/sidebar/MobileSidebar";
-import MainSidebar from "components/sidebar/MainSidebar";
-import TimestampSettings from "components/settings/Timestamp";
-import SiteFooter from "components/footer/SiteFooter";
+import { useState, Fragment } from 'react';
+import Head from 'next/head';
+import Layout from 'components/Layout';
+import MainSidebar from 'components/sidebar/MainSidebar';
+import MobileSidebar from 'components/sidebar/MobileSidebar';
+import PropTypes from 'prop-types';
+import SiteFooter from 'components/footer/SiteFooter';
+import Skeleton from 'react-loading-skeleton';
+import styled from 'styled-components';
+import TimestampSettings from 'components/settings/Timestamp';
+import useUser from 'hooks/useUser';
 
 const GlobalSettingsDiv = styled.section``;
 
 const GlobalSettings = () => {
-  const [openMobileSidebar, setOpenMobileSidebar] = useState(false);
-  const pageTitle = "Global Settings";
+	const [openMobileSidebar, setOpenMobileSidebar] = useState(false);
+	const pageTitle = 'Global Settings';
 
-  const { user: user, userError: userError } = useUser({
-    redirectTo: "/",
-    redirectIfFound: false,
-  });
+	const { user: user, userError: userError } = useUser({
+		redirectTo: '/',
+		redirectIfFound: false
+	});
 
-  {
-    userError && <Layout>{userError.message}</Layout>;
-  }
+	{
+		userError && <Layout>{userError.message}</Layout>;
+	}
 
-  return (
-    <Layout>
-      {user ? (
-        <Fragment>
-          <Head>
-            <title>{pageTitle}</title>
-          </Head>
+	return (
+		<Layout>
+			{user ? (
+				<Fragment>
+					<Head>
+						<title>{pageTitle}</title>
+					</Head>
 
-          <GlobalSettingsDiv
-            className={`h-screen flex overflow-hidden bg-gray-1200`}
-          >
-            <MobileSidebar show={openMobileSidebar} />
-            <MainSidebar />
+					<GlobalSettingsDiv
+						className={`h-screen flex overflow-hidden bg-gray-1200`}
+					>
+						<MobileSidebar show={openMobileSidebar} />
+						<MainSidebar />
 
-            <div className={`flex flex-col w-0 flex-1 overflow-hidden`}>
-              <div className={`md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3`}>
-                <button
-                  className={`-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150`}
-                  aria-label={`Open sidebar`}
-                  onClick={() =>
-                    setTimeout(
-                      () => setOpenMobileSidebar(!openMobileSidebar),
-                      150
-                    )
-                  }
-                >
-                  <svg
-                    className={`h-6 w-5`}
-                    stroke={`currentColor`}
-                    fill={`none`}
-                    viewBox={`0 0 24 24`}
-                  >
-                    <path
-                      strokeLinecap={`round`}
-                      strokeLinejoin={`round`}
-                      strokeWidth={`2`}
-                      d={`M4 6h16M4 12h16M4 18h16`}
-                    />
-                  </svg>
-                </button>
-              </div>
-              <main
-                className={`flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6`}
-                tabIndex={`0`}
-              >
-                <div
-                  className={`max-w-full mx-auto px-4 md:py-4 sm:px-6 md:px-8`}
-                >
-                  <h1 className={`text-2xl font-semibold text-gray-900`}>
-                    {pageTitle}
-                  </h1>
-                </div>
-                <div className={`max-w-2xl px-4 py-4 sm:px-6 md:px-8`}>
-                  <TimestampSettings />
-                </div>
+						<div className={`flex flex-col w-0 flex-1 overflow-hidden`}>
+							<div className={`md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3`}>
+								<button
+									className={`-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150`}
+									aria-label={`Open sidebar`}
+									onClick={() =>
+										setTimeout(
+											() => setOpenMobileSidebar(!openMobileSidebar),
+											150
+										)
+									}
+								>
+									<svg
+										className={`h-6 w-5`}
+										stroke={`currentColor`}
+										fill={`none`}
+										viewBox={`0 0 24 24`}
+									>
+										<path
+											strokeLinecap={`round`}
+											strokeLinejoin={`round`}
+											strokeWidth={`2`}
+											d={`M4 6h16M4 12h16M4 18h16`}
+										/>
+									</svg>
+								</button>
+							</div>
+							<main
+								className={`flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6`}
+								tabIndex={`0`}
+							>
+								<div
+									className={`max-w-full mx-auto px-4 md:py-4 sm:px-6 md:px-8`}
+								>
+									<h1 className={`text-2xl font-semibold text-gray-900`}>
+										{pageTitle}
+									</h1>
+								</div>
+								<div className={`max-w-2xl px-4 py-4 sm:px-6 md:px-8`}>
+									<TimestampSettings />
+								</div>
 
-                <div
-                  className={`static bottom-0 w-full mx-auto px-4 sm:px-6 py-4`}
-                >
-                  <SiteFooter />
-                </div>
-              </main>
-            </div>
-          </GlobalSettingsDiv>
-        </Fragment>
-      ) : null}
-    </Layout>
-  );
+								<div
+									className={`static bottom-0 w-full mx-auto px-4 sm:px-6 py-4`}
+								>
+									<SiteFooter />
+								</div>
+							</main>
+						</div>
+					</GlobalSettingsDiv>
+				</Fragment>
+			) : null}
+		</Layout>
+	);
 };
 
 export default GlobalSettings;
 
 GlobalSettings.propTypes = {
-  openMobileSidebar: PropTypes.string,
-  pageTitle: PropTypes.string,
+	openMobileSidebar: PropTypes.string,
+	pageTitle: PropTypes.string
 };
