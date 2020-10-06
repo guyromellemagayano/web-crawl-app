b50df13b7474804d15fef1ee909eaa870fe55aaf
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2020-10-06 23:44:45 +0800

    Update site addition components and pages with label configs

--- frontend/components/sites/HowToSetup.jsx
+++ frontend/components/sites/HowToSetup.jsx
@@ -1,77 +1,77 @@
-import { useState } from 'react'
-import styled from 'styled-components'
-import ReactPlayer from 'react-player/lazy'
+import { useState } from 'react';
+import HowToSetupLabel from 'public/label/components/sites/HowToSetup.json';
+import ReactPlayer from 'react-player/lazy';
+import styled from 'styled-components';
 
 const tabItems = [
 	{
-		"id": 1,
-		"title": "WordPress",
-		"video": {
-			"src": "https://youtu.be/FKTOwvjJmlY"
+		id: 1,
+		title: 'WordPress',
+		video: {
+			src: 'https://youtu.be/FKTOwvjJmlY'
 		}
 	},
 	{
-		"id": 2,
-		"title": "BigCommerce",
-		"video": {
-			"src": "https://youtu.be/Pgoj-AwHOiM"
+		id: 2,
+		title: 'BigCommerce',
+		video: {
+			src: 'https://youtu.be/Pgoj-AwHOiM'
 		}
 	}
-]
+];
 
-const HowToSetupDiv = styled.div``
+const HowToSetupDiv = styled.div``;
 
 const HowToSetup = () => {
-	const [tabActive, setTabActive] = useState(1)
+	const [tabActive, setTabActive] = useState(1);
 
 	return (
-    <HowToSetupDiv
-      className={`relative bg-gray-50 pt-12 pb-16 px-4 sm:px-6 lg:pt-16 lg:pb-20 lg:px-8`}
-    >
-      <div className={`absolute inset-0`}>
-        <div className={`bg-white h-1/3 sm:h-2/3`}></div>
-      </div>
-      <div className={`relative max-w-7xl mx-auto`}>
-        <div className={`text-center mb-10`}>
-          <h3
-            className={`text-2xl leading-9 tracking-tight font-bold text-gray-900 sm:text-3xl sm:leading-10`}
-          >
-            How to add site
-          </h3>
-          <p
-            className={`mt-3 max-w-2xl mx-auto text-md leading-6 text-gray-500 sm:mt-4`}
-          >
-            Watch this video to see how to install the meta tag onto your site.
-          </p>
-        </div>
-        <div
-          className={`relative mx-auto w-full rounded-lg shadow-xs-lg lg:max-w-md mb-8`}
-        >
+		<HowToSetupDiv
+			className={`relative bg-gray-50 pt-12 pb-16 px-4 sm:px-6 lg:pt-16 lg:pb-20 lg:px-8`}
+		>
+			<div className={`absolute inset-0`}>
+				<div className={`bg-white h-1/3 sm:h-2/3`}></div>
+			</div>
+			<div className={`relative max-w-7xl mx-auto`}>
+				<div className={`text-center mb-10`}>
+					<h3
+						className={`text-2xl leading-9 tracking-tight font-bold text-gray-900 sm:text-3xl sm:leading-10`}
+					>
+						{HowToSetupLabel[0].label}
+					</h3>
+					<p
+						className={`mt-3 max-w-2xl mx-auto text-md leading-6 text-gray-500 sm:mt-4`}
+					>
+						{HowToSetupLabel[1].label}
+					</p>
+				</div>
+				<div
+					className={`relative mx-auto w-full rounded-lg shadow-xs-lg lg:max-w-md mb-8`}
+				>
 					{tabItems.map(({ id, video }) => {
 						return tabActive === id ? (
-							<ReactPlayer 
+							<ReactPlayer
 								key={id}
 								url={video.src}
 								width={'auto'}
 								height={240}
 								controls={true}
 							/>
-						) : null
+						) : null;
 					})}
-        </div>
-        <div>
-          <div className={`sm:hidden`}>
-            <select
-              aria-label="Selected tab"
-              className={`form-select block w-full`}
-            >
-              <option defaultValue>WordPress</option>
-              <option>Shopify</option>
-              <option>BigCommerce</option>
-            </select>
-          </div>
-          <div className={`hidden sm:block`}>
-            <nav className={`flex justify-center`}>
+				</div>
+				<div>
+					<div className={`sm:hidden`}>
+						<select
+							aria-label='Selected tab'
+							className={`form-select block w-full`}
+						>
+							<option defaultValue>WordPress</option>
+							<option>BigCommerce</option>
+						</select>
+					</div>
+					<div className={`hidden sm:block`}>
+						<nav className={`flex justify-center`}>
 							{tabItems.map(({ id, title }) => (
 								<TabItem
 									key={id}
@@ -81,13 +81,13 @@ const HowToSetup = () => {
 									isTabActive={tabActive === id}
 								/>
 							))}
-            </nav>
-          </div>
-        </div>
-      </div>
-    </HowToSetupDiv>
-  );
-}
+						</nav>
+					</div>
+				</div>
+			</div>
+		</HowToSetupDiv>
+	);
+};
 
 const TabItem = ({
 	id = '',
@@ -97,14 +97,20 @@ const TabItem = ({
 }) => {
 	return (
 		<a
-			href="#"
-			className={`${isTabActive ? "text-white bg-indigo-600 focus:outline-none focus:bg-indigo-600" : "text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:bg-gray-100"} ${id !== 1 ? "ml-2 px-3 py-2" : "px-3 py-2"} font-medium text-sm leading-5 rounded-md `}
-			aria-current="page"
+			href='#'
+			className={`${
+				isTabActive
+					? 'text-white bg-indigo-600 focus:outline-none focus:bg-indigo-600'
+					: 'text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:bg-gray-100'
+			} ${
+				id !== 1 ? 'ml-2 px-3 py-2' : 'px-3 py-2'
+			} font-medium text-sm leading-5 rounded-md `}
+			aria-current='page'
 			onClick={onItemClicked}
 		>
 			{title}
 		</a>
-	)
-}
+	);
+};
 
-export default HowToSetup
+export default HowToSetup;
