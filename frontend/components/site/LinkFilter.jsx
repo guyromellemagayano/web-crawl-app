6765bbf1e11a95a8b93e676dc37338a29784b217
Guy Romelle Magayano <guy@epicdesignlabs.com>
2020-12-07 15:22:52 +0800

    Update styling of images, links, pages, and seo filters

--- frontend/components/site/LinkFilter.jsx
+++ frontend/components/site/LinkFilter.jsx
@@ -18,13 +18,13 @@ const LinkFilter = ({
 	return (
 		<LinkFilterDiv className={`pb-4`}>
 			<div
-				className={`bg-white px-4 py-5 border-b border-gray-300 sm:px-6 bg-white rounded-lg sm:shadow-xs`}
+				className={`px-4 py-5 border-b border-gray-300 sm:px-6 bg-white rounded-lg sm:shadow-xs lg:flex lg:justify-between`}
 			>
 				<div
 					className={`-ml-4 lg:-mt-2 lg:flex items-center flex-start flex-wrap sm:flex-no-wrap`}
 				>
 					<h4
-						className={`ml-3 mb-4 lg:mb-0 mt-1 mr-1 text-md leading-4 font-semibold text-gray-600`}
+						className={`ml-4 mb-4 lg:mb-0 mt-2 mr-1 text-md leading-4 font-semibold text-gray-600`}
 					>
 						Filter
 					</h4>
@@ -42,24 +42,6 @@ const LinkFilter = ({
 									className={`ml-2 text-left text-xs leading-4 font-normal text-gray-500`}
 								>
 									All Links
-								</span>
-							</label>
-						</div>
-					</div>
-					<div className={`ml-4 mt-2 mr-2`}>
-						<div>
-							<label className={`flex items-center`}>
-								<input
-									type='checkbox'
-									className={`form-checkbox`}
-									onChange={filterHandler}
-									checked={noIssueFilter}
-									value='no-issues'
-								/>
-								<span
-									className={`ml-2 text-left text-xs leading-4 font-normal text-gray-500`}
-								>
-									Links with No Issues
 								</span>
 							</label>
 						</div>
@@ -114,6 +96,28 @@ const LinkFilter = ({
 									className={`ml-2 text-left text-xs leading-4 font-normal text-gray-500`}
 								>
 									External Links
+								</span>
+							</label>
+						</div>
+					</div>
+				</div>
+				<div
+					className={`lg:-mt-2 lg:flex items-center align-end justify-end flex-end flex-wrap sm:flex-no-wrap`}
+				>
+					<div className={`mt-2`}>
+						<div>
+							<label className={`flex items-center`}>
+								<input
+									type='checkbox'
+									className={`form-checkbox`}
+									onChange={filterHandler}
+									checked={noIssueFilter}
+									value='no-issues'
+								/>
+								<span
+									className={`ml-2 text-left text-xs leading-4 font-normal text-gray-500`}
+								>
+									No Issues
 								</span>
 							</label>
 						</div>
--- frontend/components/site/PageFilter.jsx
+++ frontend/components/site/PageFilter.jsx
@@ -17,13 +17,13 @@ const PageFilter = ({
 	return (
 		<PageFilterDiv className={`pb-4`}>
 			<div
-				className={`bg-white px-4 py-5 border-b border-gray-300 sm:px-6 rounded-lg sm:shadow-xs`}
+				className={`px-4 py-5 border-b border-gray-300 sm:px-6 bg-white rounded-lg sm:shadow-xs lg:flex lg:justify-between`}
 			>
 				<div
 					className={`-ml-4 lg:-mt-2 lg:flex items-center flex-start flex-wrap sm:flex-no-wrap`}
 				>
 					<h4
-						className={`ml-3 mb-4 lg:mb-0 mt-1 mr-1 text-md leading-4 font-semibold text-gray-600`}
+						className={`ml-4 mb-4 lg:mb-0 mt-2 mr-1 text-md leading-4 font-semibold text-gray-600`}
 					>
 						Filter
 					</h4>
@@ -41,24 +41,6 @@ const PageFilter = ({
 									className={`ml-2 text-left text-xs leading-4 font-normal text-gray-500`}
 								>
 									All Pages
-								</span>
-							</label>
-						</div>
-					</div>
-					<div className={`ml-4 mt-2 mr-2`}>
-						<div>
-							<label className={`flex items-center`}>
-								<input
-									type='checkbox'
-									className={`form-checkbox`}
-									onChange={filterHandler}
-									checked={noIssueFilter}
-									value='no-issues'
-								/>
-								<span
-									className={`ml-2 text-left text-xs leading-4 font-normal text-gray-500`}
-								>
-									Pages with No Issues
 								</span>
 							</label>
 						</div>
@@ -95,6 +77,28 @@ const PageFilter = ({
 									className={`ml-2 text-left text-xs leading-4 font-normal text-gray-500`}
 								>
 									Broken Security
+								</span>
+							</label>
+						</div>
+					</div>
+				</div>
+				<div
+					className={`lg:-mt-2 lg:flex items-center align-end justify-end flex-end flex-wrap sm:flex-no-wrap`}
+				>
+					<div className={`mt-2`}>
+						<div>
+							<label className={`flex items-center`}>
+								<input
+									type='checkbox'
+									className={`form-checkbox`}
+									onChange={filterHandler}
+									checked={noIssueFilter}
+									value='no-issues'
+								/>
+								<span
+									className={`ml-2 text-left text-xs leading-4 font-normal text-gray-500`}
+								>
+									No Issues
 								</span>
 							</label>
 						</div>
