f8c5c3dada5ead7886a86a73e2e90ab962d83459
Guy Romelle Magayano <guy@epicdesignlabs.com>
2020-12-16 00:07:00 +0800

    Update logic of add site component

--- frontend/components/sites/AddSite.jsx
+++ frontend/components/sites/AddSite.jsx
@@ -194,7 +194,8 @@ const AddSite = () => {
 						</div>
 						<div className={`ml-4 mt-2 flex-shrink-0`}>
 							<span className={`inline-flex rounded-md shadow-xs-sm`}>
-								{siteLimitCounter === maxSiteLimit ? (
+								{siteLimitCounter === maxSiteLimit ||
+								siteLimitCounter > maxSiteLimit ? (
 									<button
 										type={`button`}
 										className={`relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:shadow-xs-outline-green focus:border-green-700 active:bg-green-700`}
