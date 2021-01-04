fc329777d96100ebd206aaa7ee2956c773005e78
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-01-05 01:40:16 +0800

    Update logo label component

--- frontend/components/form/LogoLabel.jsx
+++ frontend/components/form/LogoLabel.jsx
@@ -1,28 +1,38 @@
-import Link from 'next/link'
-import styled from 'styled-components'
-import AppLogo from 'components/logo/AppLogo'
+import Link from 'next/link';
+import styled from 'styled-components';
+import AppLogo from 'components/logo/AppLogo';
 
-const LogoLabelDiv = styled.div``
+const LogoLabelDiv = styled.div``;
 
-const LogoLabel = ({ isLogin, isSignUp, isResetPassword }) => {
-  return (
-    <LogoLabelDiv className={`sm:mx-auto sm:w-full sm:max-w-md`}>
-      {!isLogin ? (
-        <Link href="/">
-          <a>
-            <AppLogo
-              className={`h-10 w-auto mx-auto`}
-              src={`/img/logos/site-logo-dark.svg`}
-              alt={`app-logo`}
-            />
-          </a>
-        </Link>
-      ) : null}
-      <h2 className={`mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900`}>
-        {isLogin ? 'Log In' : isSignUp ? 'Sign up' : isResetPassword ? 'Reset Password' : 'Reset Password Form'}
-      </h2>
-    </LogoLabelDiv>
-  )
-}
+const LogoLabel = ({ isLogin, isSignUp, isResetPassword, isAddPassword }) => {
+	return (
+		<LogoLabelDiv className={`sm:mx-auto sm:w-full sm:max-w-md`}>
+			{!isLogin ? (
+				<Link href='/'>
+					<a>
+						<AppLogo
+							className={`h-10 w-auto mx-auto`}
+							src={`/img/logos/site-logo-dark.svg`}
+							alt={`app-logo`}
+						/>
+					</a>
+				</Link>
+			) : null}
+			<h2
+				className={`mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900`}
+			>
+				{isLogin
+					? 'Log In'
+					: isSignUp
+					? 'Sign up'
+					: isResetPassword
+					? 'Reset Password'
+					: isAddPassword
+					? 'Add Password'
+					: 'Reset Password Form'}
+			</h2>
+		</LogoLabelDiv>
+	);
+};
 
-export default LogoLabel
+export default LogoLabel;
