f68da5b33db0cb57f8badb09269818c236ad1e30
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-01-05 03:16:08 +0800

    Add more env variables in next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,14 @@
 require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}` });
 
 module.exports = {
-  trailingSlash: true,
-  devIndicators: {
-    autoPrerender: false,
-  },
-  env: {
-    ENDPOINT: process.env.API_ENDPOINT,
-    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
-  }
-}
+	trailingSlash: true,
+	devIndicators: {
+		autoPrerender: false
+	},
+	env: {
+		ENDPOINT: process.env.API_ENDPOINT,
+		STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
+		BEACON_PROVIDER_KEY: process.env.BEACON_PROVIDER_KEY,
+		USETIFUL_DATASET_TOKEN: process.env.USETIFUL_DATASET_TOKEN
+	}
+};
