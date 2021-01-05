38dc8b40eaedcfcffbcf33cf6da20d3f3ad8dadf
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-01-05 21:47:00 +0800

    Refactor logout page and replace fetch into axios

--- frontend/src/pages/logout/index.jsx
+++ frontend/src/pages/logout/index.jsx
@@ -1,58 +1,75 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import Head from 'next/head'
-import Cookies from 'js-cookie'
-import PropTypes from 'prop-types'
-import Layout from 'components/Layout'
+// React
+import React, { useEffect, useState } from 'react';
+
+// NextJS
+import { useRouter } from 'next/router';
+
+// External
+import 'core-js';
+import { NextSeo } from 'next-seo';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+// Hooks
+import usePostMethod from 'src/hooks/usePostMethod';
+
+// Components
+import Layout from 'src/components/Layout';
+
+const LogoutDiv = styled.div``;
+
+const sleep = async (ms) => await new Promise((r) => setTimeout(r, ms));
 
 const Logout = () => {
-  const router = useRouter()
-  const pageTitle = 'Log Out'
+	const [logoutDetail, setLogoutDetail] = useState(null);
 
-  const handleLogoutUser = async () => {
-    const response = await fetch('/api/auth/logout/', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'X-CSRFToken': Cookies.get('csrftoken'),
-      }
-    })
+	const pageTitle = 'Logout';
+	const logoutApiEndpoint = '/api/auth/logout/';
+	const loginApiEndpoint = '/';
 
-    const data = await response.json()
+	const router = useRouter();
 
-    if (response.ok) {
-      if (data) {      
-        window.setTimeout(() => {
-          router.push('/')
-        }, 1000)
-      }
-    } else {
-      const error = new Error(response.statusText)
+	useEffect(() => {
+		(async () => {
+			await sleep(500);
 
-      error.response = response
-      error.data = data
+			const response = await usePostMethod(logoutApiEndpoint, 'POST');
+			const data = await response.data;
 
-      throw error
-    }
-  }
+			if (response.statusText === 'OK' && response.status === 200) {
+				if (data) {
+					setLogoutDetail(data.detail);
 
-  useEffect(() => {
-    handleLogoutUser()
-  })
+					window.setTimeout(() => {
+						router.push(loginApiEndpoint);
+					}, 1500);
+				}
+			} else {
+				const error = new Error(response.statusText);
 
-  return (
-    <Layout>
-      <Head>
-        <title>{pageTitle}</title>
-      </Head>
-      <p className={`text-2xl font-bold leading-7 text-gray-900 sm:text-1xl sm:leading-9 sm:truncate`}>Logging out...</p>
-    </Layout>
-  )
-}
+				error.response = response;
+				error.data = data;
 
-export default Logout
+				throw error;
+			}
+		})();
+	});
+
+	return (
+		<Layout>
+			<NextSeo title={pageTitle} />
+
+			<LogoutDiv className="bg-white p-3">
+				<p className="text-2xl font-bold leading-7 text-gray-900 sm:text-1xl sm:leading-9 sm:truncate">
+					{logoutDetail}
+				</p>
+			</LogoutDiv>
+		</Layout>
+	);
+};
 
 Logout.propTypes = {
-  handleLogoutUser: PropTypes.func
-}
+	logoutDetail: PropTypes.string
+};
+
+export default Logout;
