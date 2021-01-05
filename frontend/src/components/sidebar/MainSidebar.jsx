a7da939478bdcad239467f72795ad00c4fa248db
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-01-06 05:13:42 +0800

    Refactor main sidebar component

--- frontend/src/components/sidebar/MainSidebar.jsx
+++ frontend/src/components/sidebar/MainSidebar.jsx
@@ -1,78 +1,62 @@
-import { Fragment } from 'react';
-import AppLogo from 'components/logo/AppLogo';
+// React
+import React from 'react';
+
+// NextJS
 import Link from 'next/link';
-import PrimaryMenu from 'components/sidebar/PrimaryMenu';
-import ProfileSidebar from 'components/profile/Sidebar';
+
+// External
 import PropTypes from 'prop-types';
-import SettingsMenu from 'components/sidebar/SettingsMenu';
-import SiteMenu from 'components/sidebar/SiteMenu';
 import styled from 'styled-components';
-import useUser from 'hooks/useUser';
+
+// Hooks
+import useUser from 'src/hooks/useUser';
+
+// Components
+import AppLogo from 'src/components/logo/AppLogo';
+import PrimaryMenu from 'src/components/sidebar/PrimaryMenu';
+import ProfileSidebar from 'src/components/profile/Sidebar';
+import SettingsMenu from 'src/components/sidebar/SettingsMenu';
+import SiteMenu from 'src/components/sidebar/SiteMenu';
 
 const MainSidebarDiv = styled.aside``;
 
 const MainSidebar = (props) => {
+	const siteDashboardLink = '/dashboard/sites';
+
 	const { user: user } = useUser({
 		redirectTo: '/',
 		redirectIfFound: false
 	});
 
-	return (
-		<Fragment>
-			{user ? (
-				<MainSidebarDiv className={`hidden md:flex md:flex-shrink-0`}>
-					<div
-						className={`flex flex-col border-r border-gray-300 bg-gray-1000 w-64`}
-					>
-						<div
-							className={`h-0 flex-1 flex flex-col pt-5 pb-4 overflow-y-auto`}
-						>
-							<div
-								className={`flex items-center flex-shrink-0 flex-column px-5`}
-							>
-								<Link href='/dashboard/sites'>
-									<a className={`block w-full`}>
-										<AppLogo
-											className={`h-8 w-auto`}
-											src={`/img/logos/site-logo-white.svg`}
-											alt={`app-logo`}
-										/>
-									</a>
-								</Link>
-							</div>
-							{window.location.href.indexOf('/site/') > -1 ? (
-								<SiteMenu crawlableHandler={props.crawlableHandler} />
-							) : window.location.href.indexOf('/settings/') > -1 ? (
-								<SettingsMenu />
-							) : (
-								<PrimaryMenu />
-							)}
-						</div>
-						<ProfileSidebar />
+	return user ? (
+		<MainSidebarDiv className="hidden md:flex md:flex-shrink-0">
+			<div className="flex flex-col border-r border-gray-300 bg-gray-1000 w-64">
+				<div className="h-0 flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+					<div className="flex items-center flex-shrink-0 flex-column px-5">
+						<Link href={siteDashboardLink}>
+							<a className="block w-full">
+								<AppLogo
+									className="h-8 w-auto"
+									src="/img/logos/site-logo-white.svg"
+									alt="app-logo"
+								/>
+							</a>
+						</Link>
 					</div>
-				</MainSidebarDiv>
-			) : null}
-		</Fragment>
-	);
+					{window.location.href.indexOf('/site/') > -1 ? (
+						<SiteMenu crawlableHandler={props.crawlableHandler} />
+					) : window.location.href.indexOf('/settings/') > -1 ? (
+						<SettingsMenu />
+					) : (
+						<PrimaryMenu />
+					)}
+				</div>
+				<ProfileSidebar />
+			</div>
+		</MainSidebarDiv>
+	) : null;
 };
+
+MainSidebar.propTypes = {};
 
 export default MainSidebar;
-
-MainSidebar.proptypes = {
-	windowSettingsLocation: PropTypes.bool,
-	windowSiteLocation: PropTypes.bool,
-	user: PropTypes.oneOfType([
-		PropTypes.func,
-		PropTypes.object,
-		PropTypes.array,
-		PropTypes.string,
-		PropTypes.shape({ current: PropTypes.any })
-	]),
-	props: PropTypes.oneOfType([
-		PropTypes.func,
-		PropTypes.object,
-		PropTypes.array,
-		PropTypes.string,
-		PropTypes.shape({ current: PropTypes.any })
-	])
-};
