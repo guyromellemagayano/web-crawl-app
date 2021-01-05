eb72d7305eaa484a9a48747aa3e7842fd35dfe23
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-01-05 13:11:15 +0800

    Add next seo enum JS

---
+++ frontend/src/enum/nextSeo.js
@@ -0,0 +1,19 @@
+export default {
+	title: 'Welcome',
+	titleTemplate: '%s | Site Crawler',
+	description: 'Monitor and Strengthen Your Websites',
+	canonical: 'https://app.sitecrawler.com',
+	openGraph: {
+		locale: 'en_US',
+		site_name: 'Site Crawler',
+		type: 'website',
+		url: 'https://app.sitecrawler.com/',
+		title: 'Site Crawler | Monitor and Strengthen Your Websites',
+		profile: {
+			firstName: 'Kalon',
+			lastName: 'Wiggins',
+			username: 'kal',
+			gender: 'male'
+		}
+	}
+};
