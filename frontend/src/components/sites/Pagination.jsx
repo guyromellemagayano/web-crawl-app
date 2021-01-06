5443fa484cf6a52194b968aec414211d1d827c67
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-01-06 12:05:05 +0800

    Refactor and update pagination component

--- frontend/src/components/sites/Pagination.jsx
+++ frontend/src/components/sites/Pagination.jsx
@@ -1,44 +1,21 @@
+// React
+import React from 'react';
+
+// NextJS
 import Router from 'next/router';
-import Cookies from 'js-cookie';
-import styled from 'styled-components';
-import Skeleton from 'react-loading-skeleton';
-import useSWR from 'swr';
+
+// External
 import Pagination from 'rc-pagination';
-import { removeURLParameter } from 'helpers/functions';
+import PropTypes from 'prop-types';
+import Skeleton from 'react-loading-skeleton';
+import styled from 'styled-components';
+import useSWR from 'swr';
 
-const fetcher = async (url) => {
-	const res = await fetch(url, {
-		method: 'GET',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json',
-			'X-CSRFToken': Cookies.get('csrftoken')
-		}
-	});
+// Hooks
+import useFetcher from 'src/hooks/useFetcher';
 
-	const data = await res.json();
-
-	if (res.status !== 200) {
-		throw new Error(data.message);
-	}
-
-	return data;
-};
-
-const PaginationLocale = {
-	items_per_page: 'Rows per Page',
-	jump_to: 'Goto',
-	jump_to_confirm: 'Goto',
-	page: 'Page',
-
-	// Pagination.jsx
-	prev_page: 'Previous',
-	next_page: 'Next',
-	prev_5: 'Prev 5',
-	next_5: 'Next 5',
-	prev_3: 'Prev 3',
-	next_3: 'Next 3'
-};
+// Helpers
+import { removeURLParameter } from 'src/helpers/functions';
 
 const PaginationDiv = styled.nav`
 .rc-pagination li {
@@ -86,43 +63,46 @@ const PaginationDiv = styled.nav`
 }
 `;
 
-const MyPagination = (props) => {
+const PaginationLocale = {
+	items_per_page: 'Rows per Page',
+	jump_to: 'Goto',
+	jump_to_confirm: 'Goto',
+	page: 'Page',
+
+	// Pagination.jsx
+	prev_page: 'Previous',
+	next_page: 'Next',
+	prev_5: 'Prev 5',
+	next_5: 'Next 5',
+	prev_3: 'Prev 3',
+	next_3: 'Next 3'
+};
+
+const MyPagination = ({
+	href,
+	pathName,
+	apiEndpoint,
+	page,
+	linksPerPage,
+	onItemsPerPageChange
+}) => {
 	const pageNumbers = [];
 	const values = [20, 25, 50, 100];
-	const currentPage = parseInt(props.page) || 1;
+	const currentPage = parseInt(page) || 1;
 	const linkNumbers = [];
-	const offset = (currentPage - 1) * props.linksPerPage;
+	const offset = (currentPage - 1) * linksPerPage;
 
-	const { data: page, error: pageError } = useSWR(props.apiEndpoint, fetcher);
+	const { data: page } = useSWR(apiEndpoint, useFetcher);
 
-	// console.log(props.pathName);
+	// console.log(pathName);
 
 	const handlePageChange = (pageNum) => {
 		// console.log('[pageNum]', pageNum);
-		const newPath = removeURLParameter(props.pathName, 'page');
+		const newPath = removeURLParameter(pathName, 'page');
 		Router.push(`${newPath}page=${pageNum}`);
 	};
 
-	if (pageError) return <div>{pageError.message}</div>;
-	if (!page) {
-		return (
-			<PaginationDiv
-				className={`bg-white px-4 py-4 flex items-start justify-between sm:px-6 align-middle shadow-xs rounded-lg`}
-			>
-				<div className={`w-0 flex-1 flex`}>
-					<Skeleton duration={2} width={120} />
-				</div>
-				<div className={`hidden md:flex`}>
-					<Skeleton duration={2} width={280} />
-				</div>
-				<div className={`w-0 flex-1 flex justify-end`}>
-					<Skeleton duration={2} width={120} />
-				</div>
-			</PaginationDiv>
-		);
-	}
-
-	const totalPages = Math.ceil(page.count / props.linksPerPage);
+	const totalPages = Math.ceil(page.count / linksPerPage);
 
 	for (let i = 1; i <= totalPages; i++) {
 		pageNumbers.push(i);
@@ -134,25 +114,21 @@ const MyPagination = (props) => {
 		linkNumbers.push(i);
 	}
 
-	const paginatedItems = linkNumbers.slice(offset).slice(0, props.linksPerPage);
+	const paginatedItems = linkNumbers.slice(offset).slice(0, linksPerPage);
 
-	return (
-		<PaginationDiv
-			className={`bg-white px-4 mb-4 py-2 lg:flex items-center justify-between sm:px-6 align-middle shadow-xs rounded-lg`}
-		>
-			<div className={`flex items-center mb-8 lg:m-0`}>
-				<div className={`mt-2 lg:my-0`}>
-					<p
-						className={`text-center lg:text-left text-sm leading-5 text-gray-700`}
-					>
+	return page ? (
+		<PaginationDiv className="bg-white px-4 mb-4 py-2 lg:flex items-center justify-between sm:px-6 align-middle shadow-xs rounded-lg">
+			<div className="flex items-center mb-8 lg:m-0">
+				<div className="mt-2 lg:my-0">
+					<p className="text-center lg:text-left text-sm leading-5 text-gray-700">
 						Showing
-						<span className={`px-1 font-medium`}>{paginatedItems[0]}</span>
+						<span className="px-1 font-medium">{paginatedItems[0]}</span>
 						to
-						<span className={`px-1 font-medium`}>
+						<span className="px-1 font-medium">
 							{paginatedItems[paginatedItems.length - 1]}
 						</span>
 						of
-						<span className={`px-1 font-medium`}>{page.count}</span>
+						<span className="px-1 font-medium">{page.count}</span>
 						results
 					</p>
 				</div>
@@ -161,28 +137,26 @@ const MyPagination = (props) => {
 			<Pagination
 				showPrevNextJumpers={true}
 				defaultPageSize={20}
-				pageSize={props.linksPerPage}
+				pageSize={linksPerPage}
 				defaultCurrent={currentPage}
 				current={currentPage}
-				total={totalPages * props.linksPerPage}
-				className={`flex`}
+				total={totalPages * linksPerPage}
+				className="flex"
 				onChange={handlePageChange}
 				locale={PaginationLocale}
-				prevIcon={`Previous`}
-				nextIcon={`Next`}
+				prevIcon="Previous"
+				nextIcon="Next"
 			/>
 
-			<div className={`flex items-center mt-4 lg:m-0`}>
-				<h1
-					className={`-mt-px pr-4 inline-flex items-center text-sm leading-5 font-normal text-gray-500`}
-				>
+			<div className="flex items-center mt-4 lg:m-0">
+				<h1 className="-mt-px pr-4 inline-flex items-center text-sm leading-5 font-normal text-gray-500">
 					Rows per page
 				</h1>
 				<div>
 					<select
-						onChange={props.onItemsPerPageChange}
-						value={props.linksPerPage}
-						className={`form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5`}
+						onChange={onItemsPerPageChange}
+						value={linksPerPage}
+						className="form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
 					>
 						{values.map((val, key) => {
 							return (
@@ -195,7 +169,28 @@ const MyPagination = (props) => {
 				</div>
 			</div>
 		</PaginationDiv>
+	) : (
+		<PaginationDiv className="bg-white px-4 py-4 flex items-start justify-between sm:px-6 align-middle shadow-xs rounded-lg">
+			<div className="w-0 flex-1 flex">
+				<Skeleton duration={2} width={120} />
+			</div>
+			<div className="hidden md:flex">
+				<Skeleton duration={2} width={280} />
+			</div>
+			<div className="w-0 flex-1 flex justify-end">
+				<Skeleton duration={2} width={120} />
+			</div>
+		</PaginationDiv>
 	);
+};
+
+MyPagination.propTypes = {
+	href: PropTypes.string,
+	pathName: PropTypes.string,
+	apiEndpoint: PropTypes.string,
+	page: PropTypes.number,
+	linksPerPage: PropTypes.number,
+	onItemsPerPageChange: PropTypes.func
 };
 
 export default MyPagination;
