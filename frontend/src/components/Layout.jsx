ed38ca0f04ea356f4dbc1f124237ba84d53452b6
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-01-07 00:18:45 +0800

    Update layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,15 +2,12 @@
 import React from 'react';
 
 // External
-
 import PropTypes from 'prop-types';
 
 const Layout = ({ children }) => {
 	return <div id="root">{children}</div>;
 };
 
-Layout.propTypes = {
-	children: PropTypes.array
-};
+Layout.propTypes = {};
 
 export default Layout;
