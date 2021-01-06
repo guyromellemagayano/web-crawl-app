516c84756399969a28ee65d920f72f7a94581ced
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-01-07 00:19:50 +0800

    Update app page

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 
 // External
 import 'core-js';
+import { DefaultSeo } from 'next-seo';
 import { Elements } from '@stripe/react-stripe-js';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { loadStripe } from '@stripe/stripe-js';
-import { DefaultSeo } from 'next-seo';
+import { SWRConfig } from 'swr';
 import PropTypes from 'prop-types';
 
 // Enums
 import SEO from 'src/enum/nextSEO';
+
+// Hooks
+import useFetcher from 'src/hooks/useFetcher';
 
 // Components
 import SiteHead from 'src/components/layout/SiteHead';
@@ -26,17 +30,20 @@ const App = ({ Component, pageProps }) => {
 	const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY);
 
 	return (
-		<Elements stripe={stripePromise}>
-			<DefaultSeo {...SEO} />
-			<SiteHead />
-			<Component {...pageProps} />
-		</Elements>
+		<SWRConfig
+			value={{
+				fetcher: useFetcher
+			}}
+		>
+			<Elements stripe={stripePromise}>
+				<DefaultSeo {...SEO} />
+				<SiteHead />
+				<Component {...pageProps} />
+			</Elements>
+		</SWRConfig>
 	);
 };
 
-App.propTypes = {
-	Component: PropTypes.func,
-	pageProps: PropTypes.object
-};
+App.propTypes = {};
 
 export default App;
