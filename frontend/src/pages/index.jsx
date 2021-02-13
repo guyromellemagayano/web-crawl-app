1e4980789e2d7eead24c15f3854f1c08aaac4814
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-02-13 15:30:09 +0800

    Add loading effect in submit button in login page

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -294,7 +294,9 @@ const Login = () => {
 																				disabled={isSubmitting}
 																				className="w-full flex justify-center mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-xs-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
 																			>
-																				{LoginLabel[6].label}
+																				{isSubmitting
+																					? LoginLabel[14].label
+																					: LoginLabel[6].label}
 																			</button>
 																		</span>
 																	</div>
