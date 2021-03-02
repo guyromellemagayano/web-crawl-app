7f2ab169f4f2152629f95a61b093c0f4c70f4a76
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-03-02 22:14:34 +0800

    Add Tailwind global styles and _document page

---
+++ frontend/src/components/GlobalStyles.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { GlobalStyles } from 'twin.macro';
+
+const GlobalStylesComponent = () => {
+	return <GlobalStyles />;
+};
+
+export default GlobalStylesComponent;
