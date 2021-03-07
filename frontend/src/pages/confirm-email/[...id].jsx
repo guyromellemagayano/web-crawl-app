70b330bd7b5bc58be9972cc543c1e0b719fa490b
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-03-07 22:28:56 +0800

    Update confirm email page

--- frontend/src/pages/confirm-email/[...id].jsx
+++ frontend/src/pages/confirm-email/[...id].jsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 // External
 import { NextSeo } from 'next-seo';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import tw from 'twin.macro';
 
 // JSON
 import ConfirmEmailLabel from 'public/label/pages/confirm-email.json';
@@ -17,11 +17,11 @@ import usePostMethod from 'src/hooks/usePostMethod';
 
 // Components
 import Layout from 'src/components/Layout';
-
-const ConfirmEmailDiv = styled.div``;
+import SiteFooter from 'src/components/footer/SiteFooter';
 
 const ConfirmEmail = () => {
 	const [success, setSuccess] = useState(false);
+	const [failure, setFailure] = useState(false);
 	const [errorMsg, setErrorMsg] = useState('');
 	const [successMsg, setSuccessMsg] = useState('');
 
@@ -35,13 +35,27 @@ const ConfirmEmail = () => {
 	};
 
 	const handleSendPostRequest = async (data) => {
-		const response = await usePostMethod(confirmEmailApiEndpoint, data);
+		try {
+			const response = await usePostMethod(confirmEmailApiEndpoint, data);
 
-		if (Math.floor(response.status / 200) === 1) {
-			setSuccess(!success);
-			setSuccessMsg(ConfirmEmailLabel[0].label);
-		} else {
-			setErrorMsg(ConfirmEmailLabel[1].label);
+			if (Math.floor(response.status / 200) === 1) {
+				setErrorMsg('');
+				setSuccess(!success);
+				setSuccessMsg(ConfirmEmailLabel[0].label);
+			} else {
+				if (response.data) {
+					setSuccessMsg('');
+					setFailure(!failure);
+
+					// FIXME: response.data of handleSendPostRequest function
+					console.log(response.data);
+				} else {
+					setErrorMsg(ConfirmEmailLabel[1].label);
+				}
+			}
+		} catch (error) {
+			// FIXME: add logging solution here
+			return null;
 		}
 	};
 
@@ -63,46 +77,52 @@ const ConfirmEmail = () => {
 		<Layout>
 			<NextSeo title={pageTitle} />
 
-			<ConfirmEmailDiv
-				className={`min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8`}
-			>
-				<div className={`sm:mx-auto sm:w-full sm:max-w-md`}>
-					<div className={`bg-white shadow-xs rounded-lg`}>
-						<div className={`px-4 py-5 sm:p-6`}>
-							<h3 className={`text-lg leading-6 font-medium text-gray-900`}>
-								{ConfirmEmailLabel[2].label} {success ? 'Success' : 'Failed'}
-							</h3>
-							<div className={`mt-2 max-w-xl text-sm leading-5 text-gray-500`}>
-								{successMsg && <p>{successMsg}</p>}
-								{errorMsg && <p>{errorMsg}</p>}
-							</div>
-
-							{successMsg ? (
-								<div className={`mt-5`}>
+			<div tw="bg-gray-50 min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+				<div tw="sm:mx-auto sm:w-full sm:max-w-md">
+					<div tw="bg-white shadow rounded-lg">
+						{success && (
+							<div tw="px-4 py-5 sm:p-6">
+								<h3 tw="text-lg leading-6 font-medium text-gray-900">
+									{ConfirmEmailLabel[2].label} {ConfirmEmailLabel[6].label}
+								</h3>
+								<div tw="mt-2 max-w-xl text-sm leading-5 text-gray-500">
+									<p>{successMsg}</p>
+								</div>
+								<div tw="mt-5">
 									<Link href="/">
 										<a
 											type="button"
-											className={`relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:shadow-xs-outline-green focus:border-green-700 active:bg-green-700`}
+											tw="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
 										>
 											{ConfirmEmailLabel[3].label}
 										</a>
 									</Link>
 								</div>
-							) : (
-								<div className={`mt-5`}>
+							</div>
+						)}
+
+						{failure && (
+							<div tw="px-4 py-5 sm:p-6">
+								<h3 tw="text-lg leading-6 font-medium text-gray-900">
+									{ConfirmEmailLabel[2].label} {ConfirmEmailLabel[7].label}
+								</h3>
+								<div tw="mt-2 max-w-xl text-sm leading-5 text-gray-500">
+									<p>{errorMsg}</p>
+								</div>
+								<div tw="mt-5">
 									<button
 										type="button"
-										className={`relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:shadow-xs-outline-red focus:border-red-700 active:bg-red-700`}
+										tw="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
 										onClick={handlePageRefresh}
 									>
 										{ConfirmEmailLabel[4].label}
 									</button>
 								</div>
-							)}
-						</div>
+							</div>
+						)}
 					</div>
 				</div>
-			</ConfirmEmailDiv>
+			</div>
 		</Layout>
 	);
 };
