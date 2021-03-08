d6b1e0c11d77ed32717848917c8966ab3319b552
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-03-09 01:07:10 +0800

    Add access denied page

---
+++ frontend/src/pages/access-denied/index.jsx
@@ -0,0 +1,21 @@
+// React
+import React from 'react';
+
+// NextJS
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+// External
+import { NextSeo } from 'next-seo';
+import PropTypes from 'prop-types';
+import tw from 'twin.macro';
+
+// JSON
+// Components
+import Layout from 'src/components/Layout';
+
+// Icons
+
+const AccessDenied = () => <Layout></Layout>;
+
+export default AccessDenied;
