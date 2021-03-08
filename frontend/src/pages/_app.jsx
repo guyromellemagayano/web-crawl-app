7cf311c785fd61a55b1b50762e90ab22ac750484
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-03-09 01:06:52 +0800

    Update app page

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,16 +1,14 @@
 // React
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // External
+// import { Elements } from '@stripe/react-stripe-js';
+// import { loadStripe } from '@stripe/stripe-js';
 import 'core-js';
 import 'tailwindcss/tailwind.css';
 import { DefaultSeo } from 'next-seo';
-import { Elements } from '@stripe/react-stripe-js';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { loadStripe } from '@stripe/stripe-js';
-import { SWRConfig } from 'swr';
-import axios from 'axios';
 import PropTypes from 'prop-types';
 
 // Enums
@@ -26,44 +24,14 @@ import SiteHead from 'src/components/layout/SiteHead';
 // Font Awesome
 library.add(fab);
 
-const getStripeKey = async (...args) => {
-	try {
-		const response = await axios.get(...args);
-		const data = await response.data;
-
-		return loadStripe(data.publishable_key);
-	} catch (error) {
-		// Debugging purpose only
-		// console.log('Error', error.config);
-		// console.log('Error', error.response.data);
-		// console.log('Error', error.response.headers);
-		// console.log('Error', error.response.status);
-		// console.log('Error', error.request);
-
-		throw error.message;
-	}
-};
-
-const App = ({ Component, pageProps }) => {
-	const stripeConfigApiEndpoint = '/api/stripe/config/';
-
-	const stripePromise = getStripeKey(stripeConfigApiEndpoint, useFetcher);
-
-	return (
-		<SWRConfig
-			value={{
-				fetcher: useFetcher
-			}}
-		>
-			<Elements stripe={stripePromise}>
-				<DefaultSeo {...SEO} />
-				<SiteHead />
-				<GlobalStyles />
-				<Component {...pageProps} />
-			</Elements>
-		</SWRConfig>
-	);
-};
+const App = ({ Component, pageProps }) => (
+	<>
+		<DefaultSeo {...SEO} />
+		<SiteHead />
+		<GlobalStyles />
+		<Component {...pageProps} />
+	</>
+);
 
 App.propTypes = {};
 
