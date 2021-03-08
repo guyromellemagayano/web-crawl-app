0605fda1865df2c9ec46ff030f3aa2bab06d0637
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-03-08 17:06:50 +0800

    Fix stripe integration

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -10,6 +10,7 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { loadStripe } from '@stripe/stripe-js';
 import { SWRConfig } from 'swr';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 
 // Enums
@@ -25,8 +26,32 @@ import SiteHead from 'src/components/layout/SiteHead';
 // Font Awesome
 library.add(fab);
 
+const getStripeKey = async (...args) => {
+	try {
+		const response = await axios.get(...args);
+		const data = await response.data;
+
+		return loadStripe(data.publishable_key);
+	} catch (error) {
+		// Debugging purpose only
+		// console.log('Error', error.config);
+		// console.log('Error', error.response.data);
+		// console.log('Error', error.response.headers);
+		// console.log('Error', error.response.status);
+		// console.log('Error', error.request);
+
+		throw error.message;
+	}
+};
+
+// const stripePromise = loadStripe(
+// 	stripeKey && stripeKey !== undefined && stripeKey
+// );
+
 const App = ({ Component, pageProps }) => {
-	const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY);
+	const stripeConfigApiEndpoint = '/api/stripe/config/';
+
+	const stripePromise = getStripeKey(stripeConfigApiEndpoint, useFetcher);
 
 	return (
 		<SWRConfig
