353b16eab3ea12d0ec5f7f90a2ad1b302a40bb45
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-03-09 22:08:05 +0800

    Update logout page

--- frontend/src/pages/logout/index.jsx
+++ frontend/src/pages/logout/index.jsx
@@ -1,56 +1,43 @@
 // React
 import React, { useEffect, useState } from 'react';
 
-// NextJS
-import { useRouter } from 'next/router';
-
 // External
-
 import { NextSeo } from 'next-seo';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import 'twin.macro';
 
 // Hooks
 import usePostMethod from 'src/hooks/usePostMethod';
 
+// Contexts
+import { useAuth } from 'src/contexts/auth';
+
 // Components
 import Layout from 'src/components/Layout';
-
-const LogoutDiv = styled.div``;
-
-const sleep = async (ms) => await new Promise((r) => setTimeout(r, ms));
 
 const Logout = () => {
 	const [logoutDetail, setLogoutDetail] = useState(null);
 
 	const pageTitle = 'Logout';
 	const logoutApiEndpoint = '/api/auth/logout/';
-	const loginApiEndpoint = '/';
 
-	const router = useRouter();
+	const { logout } = useAuth();
 
 	useEffect(() => {
 		(async () => {
-			await sleep(500);
-
 			const response = await usePostMethod(logoutApiEndpoint);
-			const data = await response.data;
 
-			if (response.statusText === 'OK' && response.status === 200) {
-				if (data) {
-					setLogoutDetail(data.detail);
+			if (Math.floor(response.status / 200) === 1) {
+				setLogoutDetail(response.data.detail);
 
-					window.setTimeout(() => {
-						router.push(loginApiEndpoint);
-					}, 1500);
-				}
+				setTimeout(async () => {
+					logout(response.data);
+				}, 1500);
 			} else {
-				const error = new Error(response.statusText);
-
-				error.response = response;
-				error.data = data;
-
-				throw error;
+				if (response.data) {
+					// FIXME: Django exception error in response.data.detail
+					console.error(response.data.detail);
+				}
 			}
 		})();
 	});
@@ -59,17 +46,15 @@ const Logout = () => {
 		<Layout>
 			<NextSeo title={pageTitle} />
 
-			<LogoutDiv className="bg-white p-3">
-				<p className="text-2xl font-bold leading-7 text-gray-900 sm:text-1xl sm:leading-9 sm:truncate">
+			<div tw="bg-white p-3">
+				<p tw="text-2xl font-bold leading-7 text-gray-900 sm:text-xl sm:leading-9 sm:truncate">
 					{logoutDetail}
 				</p>
-			</LogoutDiv>
+			</div>
 		</Layout>
 	);
 };
 
-Logout.propTypes = {
-	logoutDetail: PropTypes.string
-};
+Logout.propTypes = {};
 
 export default Logout;
