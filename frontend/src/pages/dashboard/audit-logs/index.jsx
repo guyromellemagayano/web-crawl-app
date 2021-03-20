fcc960f9f52b6c07ff1797e36770c28d8b0d6ac4
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-03-21 00:36:39 +0800

    Add coming soon page component, update audit logs page

--- frontend/src/pages/dashboard/audit-logs/index.jsx
+++ frontend/src/pages/dashboard/audit-logs/index.jsx
@@ -1,110 +1,56 @@
-import { useState, Fragment } from 'react';
-import Head from 'next/head';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import useUser from 'hooks/useUser';
-import Layout from 'components/Layout';
-import MobileSidebar from 'components/sidebar/MobileSidebar';
-import MainSidebar from 'components/sidebar/MainSidebar';
+// React
+import { useState, useEffect } from "react";
 
-const ReportsDiv = styled.section``;
+// External
+import { NextSeo } from "next-seo";
+import loadable from "@loadable/component";
+import PropTypes from "prop-types";
+import tw from "twin.macro";
+
+// Layout
+import Layout from "src/components/Layout";
+
+// Components
+const MainSidebar = loadable(() => import("src/components/sidebar/MainSidebar"));
+const MobileSidebar = loadable(() => import("src/components/sidebar/MobileSidebar"));
+const ComingSoon = loadable(() => import("src/components/layout/ComingSoon"));
 
 const Reports = () => {
 	const [openMobileSidebar, setOpenMobileSidebar] = useState(false);
-	const pageTitle = 'Audit Logs';
+	const [pageLoaded, setPageLoaded] = useState(false);
 
-	const { user: user, userError: userError } = useUser({
-		redirectTo: '/',
-		redirectIfFound: false
-	});
+	const pageTitle = "Audit Logs";
 
-	{
-		userError && <Layout>{userError.message}</Layout>;
-	}
+	const { user } = useAuth();
+
+	useEffect(() => {
+		if (user && user !== undefined) {
+			setTimeout(() => {
+				setPageLoaded(true);
+			}, 500);
+		}
+	}, [user]);
 
 	return (
 		<Layout>
-			{user ? (
-				<Fragment>
-					<Head>
-						<title>{pageTitle}</title>
-					</Head>
+			<NextSeo title={pageTitle} />
 
-					<ReportsDiv className={`h-screen flex overflow-hidden bg-gray-200`}>
-						<MobileSidebar show={openMobileSidebar} />
-						<MainSidebar />
+			<section tw="h-screen flex overflow-hidden bg-white">
+				{/* FIXME: fix mobile sidebar */}
+				{/* <MobileSidebar show={openMobileSidebar} setShow={setOpenMobileSidebar} /> */}
+				<MainSidebar />
 
-						<div className={`flex flex-col w-0 flex-1 overflow-hidden`}>
-							<div className={`md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3`}>
-								<button
-									className={`-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150`}
-									aria-label={`Open sidebar`}
-									onClick={() =>
-										setTimeout(
-											() => setOpenMobileSidebar(!openMobileSidebar),
-											150
-										)
-									}
-								>
-									<svg
-										className={`h-6 w-5`}
-										stroke={`currentColor`}
-										fill={`none`}
-										viewBox={`0 0 24 24`}
-									>
-										<path
-											strokeLinecap={`round`}
-											strokeLinejoin={`round`}
-											strokeWidth={`2`}
-											d={`M4 6h16M4 12h16M4 18h16`}
-										/>
-									</svg>
-								</button>
-							</div>
-							<main
-								className={`flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6`}
-								tabIndex={`0`}
-							>
-								<div
-									className={`max-w-full mx-auto px-4 md:py-4 sm:px-6 md:px-8`}
-								>
-									<div
-										className={`mt-2 md:flex md:items-center md:justify-between`}
-									>
-										<div className={`flex-1 min-w-0`}>
-											<h2
-												className={`text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate lg:overflow-visible`}
-											>
-												{pageTitle}
-											</h2>
-										</div>
-									</div>
-								</div>
-								<div className={`max-w-2xl px-4 py-4 sm:px-6 md:px-8`}>
-									<div
-										className={`max-w-full inset-0 flex items-center justify-center text-center rounded-md border-gray-900 border-2 border-dashed opacity-25`}
-									>
-										<div className={`p-6 md:py-8`}>
-											<h1
-												className={`lg:text-xl font-bold leading-7 text-center text-gray-900 sm:text-3xl sm:leading-9 sm:truncate lg:overflow-visible`}
-											>
-												Coming Soon...
-											</h1>
-										</div>
-									</div>
-								</div>
-							</main>
-						</div>
-					</ReportsDiv>
-				</Fragment>
-			) : null}
+				<ComingSoon
+					pageTitle={pageTitle}
+					pageLoaded={pageLoaded}
+					openMobileSidebar={openMobileSidebar}
+					setOpenMobileSidebar={setOpenMobileSidebar}
+				/>
+			</section>
 		</Layout>
 	);
 };
 
-export default Reports;
+Reports.propTypes = {};
 
-Reports.propTypes = {
-	openMobileSidebar: PropTypes.bool,
-	pageTitle: PropTypes.string
-};
+export default Reports;
