70165fe39112f07514d1443f2d243fabb94aa7bc
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-03-21 00:34:07 +0800

    Add success and alert components

--- frontend/src/components/alerts/ErrorMessageAlert.jsx
+++ frontend/src/components/alerts/ErrorMessageAlert.jsx
@@ -1,5 +1,4 @@
 // React
-import React from 'react';
 
 // External
 import tw from 'twin.macro';
@@ -8,15 +7,13 @@ import tw from 'twin.macro';
 import XCircleSvg from 'src/components/svg/XCircleSvg';
 
 const ErrorMessageAlert = ({ message }) => (
-	<div tw="rounded-md bg-red-100 p-4 mb-2">
-		<div tw="flex">
-			<div tw="flex-shrink-0">
+	<div tw='rounded-md bg-red-100 p-4 mb-2'>
+		<div tw='flex'>
+			<div tw='flex-shrink-0'>
 				<XCircleSvg className={tw`h-5 w-5 text-red-400`} />
 			</div>
-			<div tw="ml-3">
-				<h3 tw="text-sm leading-5 font-medium text-red-800 break-words">
-					{message}
-				</h3>
+			<div tw='ml-3'>
+				<h3 tw='text-sm leading-5 font-medium text-red-800 break-words'>{message}</h3>
 			</div>
 		</div>
 	</div>
--- frontend/src/components/alerts/SuccessMessageAlert.jsx
+++ frontend/src/components/alerts/SuccessMessageAlert.jsx
@@ -1,5 +1,4 @@
 // React
-import React from 'react';
 
 // External
 import tw from 'twin.macro';
@@ -8,15 +7,13 @@ import tw from 'twin.macro';
 import CheckCircleSvg from 'src/components/svg/CheckCircleSvg';
 
 const SuccessMessageAlert = ({ message }) => (
-	<div tw="rounded-md bg-green-100 p-4 mb-2">
-		<div tw="flex">
-			<div tw="flex-shrink-0">
+	<div tw='rounded-md bg-green-100 p-4 mb-2'>
+		<div tw='flex'>
+			<div tw='flex-shrink-0'>
 				<CheckCircleSvg className={tw`h-5 w-5 text-green-400`} />
 			</div>
-			<div tw="ml-3">
-				<h3 tw="text-sm leading-5 font-medium text-green-800 break-words">
-					{message}
-				</h3>
+			<div tw='ml-3'>
+				<h3 tw='text-sm leading-5 font-medium text-green-800 break-words'>{message}</h3>
 			</div>
 		</div>
 	</div>
