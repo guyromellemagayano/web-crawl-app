29974b122b291096dc4f8e2d11a4cfca655652b6
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-03-23 07:40:57 +0800

    [WIP] Fix user authentication React hook

--- frontend/src/hooks/useUser.js
+++ frontend/src/hooks/useUser.js
@@ -5,29 +5,64 @@ import { useEffect } from "react";
 import Router, { useRouter } from "next/router";
 
 // External
-import useSWR from "swr";
+import Cookies from "js-cookie";
 import PropTypes from "prop-types";
+import useSWR from "swr";
 
 // Hooks
 import useFetcher from "src/hooks/useFetcher";
 
-// Contexts
-import { redirectAuth, redirectUnauth } from "src/contexts/auth";
+const sleep = async (ms) => await new Promise((r) => setTimeout(r, ms));
 
-const useUser = ({ token = "", userInfo = [] }) => {
+const useUser = ({ redirectTo, redirectIfFound, token }) => {
 	const userApiEndpoint = "/api/auth/user/";
 
-	// const { asPath } = useRouter();
+	const { asPath } = useRouter();
 
-	const { data: user, mutate: mutateUser, error: userError } = useSWR(userApiEndpoint, useFetcher);
+	const { data: user, mutate: mutateUser, error: userError } = useSWR(userApiEndpoint, useFetcher, {
+		onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+			if (error.status === 404) return;
+			if (key === "/api/auth/user") return;
+			if (retryCount >= 10) return;
+
+			setTimeout(() => revalidate({ retryCount: retryCount + 1 }), 3000);
+		},
+	});
 
 	useEffect(() => {
-		console.log(user, userError, userInfo);
-	}, [user, userError, userInfo]);
+		(async () => {
+			await sleep(500);
+
+			if (userError && userError.status === 403 && !redirectIfFound) {
+				Router.push(redirectTo);
+				Cookies.set("errLogin", "You must log in to access the page!", {
+					expires: new Date(new Date().getTime() + 0.05 * 60 * 1000), // expires in 3s
+				});
+			}
+
+			if (user && user !== undefined && Object.keys(user).length > 0 && redirectIfFound) {
+				if (token && token !== undefined && token !== "") {
+					Router.push(redirectTo);
+				}
+			} else {
+				return;
+			}
+		})();
+	}, [user, userError, redirectIfFound, redirectTo, token]);
 
 	return { user, mutateUser, userError };
 };
 
-useUser.propTypes = {};
+useUser.defaultProps = {
+	token: "",
+	redirectTo: false,
+	redirectIfFound: false,
+};
+
+useUser.propTypes = {
+	token: PropTypes.string,
+	redirectIfFound: PropTypes.boolean,
+	redirectTo: PropTypes.boolean,
+};
 
 export default useUser;
