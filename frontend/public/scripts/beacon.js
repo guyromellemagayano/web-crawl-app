88b8a3a07a166eaa94240995d2d67d350a7354ea
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-03-23 07:41:55 +0800

    Update public scripts

--- frontend/public/scripts/beacon.js
+++ frontend/public/scripts/beacon.js
@@ -1,11 +1,11 @@
 // Beacon
 !(function (e, t, n) {
 	function a() {
-		const e = t.getElementsByTagName('script')[0];
-		const n = t.createElement('script');
-		(n.type = 'text/javascript'),
+		const e = t.getElementsByTagName("script")[0];
+		const n = t.createElement("script");
+		(n.type = "text/javascript"),
 			(n.async = !0),
-			(n.src = 'https://beacon-v2.helpscout.net'),
+			(n.src = "https://beacon-v2.helpscout.net"),
 			e.parentNode.insertBefore(n, e);
 	}
 
@@ -14,16 +14,14 @@
 			e.Beacon.readyQueue.push({
 				method: t,
 				options: n,
-				data: a
+				data: a,
 			});
 		}),
 		(n.readyQueue = []),
-		t.readyState === 'complete')
+		t.readyState === "complete")
 	)
 		return a();
-	e.attachEvent
-		? e.attachEvent('onload', a)
-		: e.addEventListener('load', a, !1);
+	e.attachEvent ? e.attachEvent("onload", a) : e.addEventListener("load", a, !1);
 })(window, document, window.Beacon || (() => {}));
 
-window.Beacon('init', '94d0425a-cb40-4582-909a-2175532bbfa9');
+window.Beacon("init", "94d0425a-cb40-4582-909a-2175532bbfa9");
