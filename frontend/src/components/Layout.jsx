767b84ea55925fa98b4784db2e74769b1fd752be
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-03-24 19:38:50 +0800

    Re-enable LogRocket

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,10 +1,29 @@
-// External
-import 'twin.macro';
+// React
+import { useEffect } from "react";
 
-const Layout = ({ children }) => {
+// External
+import "twin.macro";
+import LogRocket from "logrocket";
+import setupLogRocketReact from "logrocket-react";
+
+const Layout = ({ user, children }) => {
+	useEffect(() => {
+		if (user && user !== undefined && Object.keys(user).length > 0) {
+			if (typeof window !== "undefined") {
+				LogRocket.init("epic-design-labs/link-app");
+				setupLogRocketReact(LogRocket);
+			}
+
+			LogRocket.identify("epic-design-labs/link-app", {
+				name: user.first_name + " " + user.last_name,
+				email: user.email,
+			});
+		}
+	}, [user]);
+
 	return (
-		<div id='root-auth'>
-			<main tw='min-h-screen'>{children}</main>
+		<div id="root-auth">
+			<main tw="min-h-screen">{children}</main>
 		</div>
 	);
 };
