ef82e9ae7cc0eaf72d6448a6eb0dae86819da7a2
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-03-30 10:20:24 +0800

    Fix audit logs page errors

--- frontend/src/pages/audit-logs/index.jsx
+++ frontend/src/pages/audit-logs/index.jsx
@@ -1,48 +1,73 @@
 // React
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 // External
-import { NextSeo } from 'next-seo';
-import loadable from '@loadable/component';
-import PropTypes from 'prop-types';
-import tw from 'twin.macro';
+import { NextSeo } from "next-seo";
+import loadable from "@loadable/component";
+import PropTypes from "prop-types";
+import tw from "twin.macro";
+
+// Utils
+import { getCookie } from "src/utils/cookie";
+
+// Hooks
+import { useSite } from "src/hooks/useSite";
+import useUser from "src/hooks/useUser";
 
 // Layout
-import Layout from 'src/components/Layout';
+import Layout from "src/components/Layout";
 
 // Components
-const MainSidebar = loadable(() =>
-	import('src/components/sidebar/MainSidebar')
-);
-const MobileSidebar = loadable(() =>
-	import('src/components/sidebar/MobileSidebar')
-);
-const ComingSoon = loadable(() => import('src/components/Layout/ComingSoon'));
+const MainSidebar = loadable(() => import("src/components/sidebar/MainSidebar"));
+const MobileSidebar = loadable(() => import("src/components/sidebar/MobileSidebar"));
+const ComingSoon = loadable(() => import("src/components/layout/ComingSoon"));
 
-const Reports = () => {
+const Reports = ({ token }) => {
 	const [openMobileSidebar, setOpenMobileSidebar] = useState(false);
 	const [pageLoaded, setPageLoaded] = useState(false);
+	const [siteData, setSiteData] = useState([]);
+	const [userData, setUserData] = useState([]);
 
-	const pageTitle = 'Audit Logs';
+	const homeLabel = "Home";
+	const homePageLink = "/";
+	const pageTitle = "Audit Logs";
+	const siteApiEndpoint = "/api/site/?ordering=name";
 
-	const { user } = useAuth();
+	const { user: user, userError: userError } = useUser();
+	const { site: site, siteError: siteError } = useSite({
+		endpoint: siteApiEndpoint,
+		refreshInterval: 1000,
+	});
 
 	useEffect(() => {
-		if (user && user !== undefined) {
+		if (
+			token &&
+			token !== undefined &&
+			Object.keys(token).length > 0 &&
+			user &&
+			user !== undefined &&
+			Object.keys(user).length > 0 &&
+			site &&
+			site !== undefined &&
+			Object.keys(site).length > 0
+		) {
 			setTimeout(() => {
 				setPageLoaded(true);
 			}, 500);
+
+			setUserData(user);
+			setSiteData(site);
 		}
-	}, [user]);
+	}, [token, user, site]);
 
 	return (
-		<Layout>
+		<Layout user={user}>
 			<NextSeo title={pageTitle} />
 
-			<section tw='h-screen flex overflow-hidden bg-white'>
+			<section tw="h-screen flex overflow-hidden bg-white">
 				{/* FIXME: fix mobile sidebar */}
 				{/* <MobileSidebar show={openMobileSidebar} setShow={setOpenMobileSidebar} /> */}
-				<MainSidebar />
+				<MainSidebar user={userData} site={siteData} />
 
 				<ComingSoon
 					pageTitle={pageTitle}
@@ -58,3 +83,19 @@ const Reports = () => {
 Reports.propTypes = {};
 
 export default Reports;
+
+export async function getServerSideProps({ req }) {
+	let token = getCookie("token", req);
+
+	if (!token) {
+		return {
+			notFound: true,
+		};
+	}
+
+	return {
+		props: {
+			token: token,
+		},
+	};
+}
