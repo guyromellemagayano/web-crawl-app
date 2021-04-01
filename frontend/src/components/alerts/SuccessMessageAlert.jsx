cf42485d335a063c3911201136b9775fe10c8912
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-04-02 02:59:50 +0800

    Update error and success message alerts

--- frontend/src/components/alerts/SuccessMessageAlert.jsx
+++ frontend/src/components/alerts/SuccessMessageAlert.jsx
@@ -1,22 +1,57 @@
 // React
+import { useState, useEffect } from "react";
 
 // External
-import tw from 'twin.macro';
+import { Transition } from "@headlessui/react";
+import loadable from "@loadable/component";
+import tw from "twin.macro";
 
 // Components
-import CheckCircleSvg from 'src/components/svg/CheckCircleSvg';
+import CheckCircleSvg from "src/components/svg/CheckCircleSvg";
+import XSvg from "../svg/XSvg";
 
-const SuccessMessageAlert = ({ message }) => (
-	<div tw='rounded-md bg-green-100 p-4 mb-2'>
-		<div tw='flex'>
-			<div tw='flex-shrink-0'>
-				<CheckCircleSvg className={tw`h-5 w-5 text-green-400`} />
+const SuccessMessageAlert = ({ message }) => {
+	const [isOpen, setIsOpen] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsOpen(!isOpen);
+		}, 3000);
+	}, isOpen);
+
+	return (
+		<Transition
+			show={isOpen}
+			enter="transition-opacity duration-75"
+			enterFrom="opacity-0"
+			enterTo="opacity-100"
+			leave="transition-opacity duration-150"
+			leaveFrom="opacity-100"
+			leaveTo="opacity-0"
+			className="max-w-2xl z-10 origin-top fixed right-0 left-0 bottom-0 rounded-md bg-green-100 shadow-lg p-4 mt-1 mx-auto mb-10"
+		>
+			<div tw="flex items-center">
+				<div tw="flex-shrink-0">
+					<CheckCircleSvg className={tw`h-5 w-5 text-green-400`} />
+				</div>
+				<div tw="ml-3">
+					<h3 tw="text-sm leading-5 font-medium text-green-800 break-words">{message}</h3>
+				</div>
+				<div class="ml-auto pl-3">
+					<div class="flex items-center -mx-1.5">
+						<button
+							type="button"
+							class="inline-flex bg-green-50 rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600"
+							onClick={() => setIsOpen(!isOpen)}
+						>
+							<span class="sr-only">Dismiss</span>
+							<XSvg className={tw`h-5 w-5`} />
+						</button>
+					</div>
+				</div>
 			</div>
-			<div tw='ml-3'>
-				<h3 tw='text-sm leading-5 font-medium text-green-800 break-words'>{message}</h3>
-			</div>
-		</div>
-	</div>
-);
+		</Transition>
+	);
+};
 
 export default SuccessMessageAlert;
