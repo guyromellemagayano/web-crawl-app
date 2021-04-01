a5e3f337f98b4d22647de73b9c81b7ace8c15797
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-04-01 14:46:22 +0800

    Add conditional statements in useSite React hook

--- frontend/src/hooks/useSite.js
+++ frontend/src/hooks/useSite.js
@@ -4,6 +4,8 @@ import useSWR from "swr";
 // Hooks
 import useFetcher from "src/hooks/useFetcher";
 
+const siteApiEndpoint = "/api/site/";
+
 export const useSite = ({ endpoint, refreshInterval = 0 }) => {
 	const { data: site, mutate: mutateSite, error: siteError } = useSWR(endpoint, useFetcher, {
 		onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
@@ -20,17 +22,61 @@ export const useSite = ({ endpoint, refreshInterval = 0 }) => {
 };
 
 export const useSiteId = ({ querySid = 0 }) => {
-	const siteApiEndpoint = "/api/site/";
+	const { data: siteId, mutate: mutateSiteId, error: siteIdError } = useSWR(
+		() => (querySid && querySid !== 0 && querySid !== undefined ? siteApiEndpoint + querySid + "/" : null),
+		useFetcher,
+		{
+			onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+				if (error.status === 404) return;
+				if (key === siteApiEndpoint + querySid + "/") return;
+				if (retryCount >= 10) return;
 
-	const { data: siteId, error: siteIdError } = useSWR(siteApiEndpoint + querySid + "/", useFetcher, {
-		onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
-			if (error.status === 404) return;
-			if (key === siteApiEndpoint + querySid) return;
-			if (retryCount >= 10) return;
+				setTimeout(() => revalidate({ retryCount: retryCount + 1 }), 3000);
+			},
+		}
+	);
 
-			setTimeout(() => revalidate({ retryCount: retryCount + 1 }), 3000);
-		},
-	});
+	return { siteId, mutateSiteId, siteIdError };
+};
 
-	return { siteId, siteIdError };
+export const useScan = ({ querySid = 0 }) => {
+	const { data: scan, mutate: mutateScan, error: scanError } = useSWR(
+		() =>
+			querySid && querySid !== 0 && querySid !== undefined
+				? siteApiEndpoint + querySid + "/scan/?ordering=-finished_at/"
+				: null,
+		useFetcher,
+		{
+			onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+				if (error.status === 404) return;
+				if (key === siteApiEndpoint + querySid + "/scan/?ordering=-finished_at/") return;
+				if (retryCount >= 10) return;
+
+				setTimeout(() => revalidate({ retryCount: retryCount + 1 }), 3000);
+			},
+		}
+	);
+
+	return { scan, mutateScan, scanError };
+};
+
+export const useStats = ({ querySid = 0, scanObjId = 0 }) => {
+	const { data: stats, mutate: mutateStats, error: statsError } = useSWR(
+		() =>
+			querySid && querySid !== 0 && querySid !== undefined && scanObjId && scanObjId !== 0 && scanObjId !== undefined
+				? siteApiEndpoint + querySid + "/scan/" + scanObjId + "/"
+				: null,
+		useFetcher,
+		{
+			onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+				if (error.status === 404) return;
+				if (key === siteApiEndpoint + querySid + "/scan/" + scanObjId + "/") return;
+				if (retryCount >= 10) return;
+
+				setTimeout(() => revalidate({ retryCount: retryCount + 1 }), 3000);
+			},
+		}
+	);
+
+	return { stats, mutateStats, statsError };
 };
