a40084f51c8970be9f98c901fab70ccc5c143361
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-04-06 03:53:24 +0800

    Add mobile sidebar in global settings

--- frontend/src/pages/settings/global/index.jsx
+++ frontend/src/pages/settings/global/index.jsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 
 // External
 import { NextSeo } from "next-seo";
+import { withResizeDetector } from "react-resize-detector";
 import loadable from "@loadable/component";
 import PropTypes from "prop-types";
 import tw from "twin.macro";
@@ -17,24 +18,24 @@ import GlobalLabel from "public/labels/components/global/Global.json";
 import { getCookie } from "src/utils/cookie";
 
 // Hooks
-import useUser from "src/hooks/useUser";
 import { useSite } from "src/hooks/useSite";
+import useUser from "src/hooks/useUser";
 
 // Layout
 import Layout from "src/components/Layout";
 
 // Components
-const LargePageSizeSettings = loadable(() => import("src/components/settings/LargePageSize"));
+const AppLogo = loadable(() => import("src/components/logo/AppLogo"));
 const ChevronRightSvg = loadable(() => import("src/components/svg/solid/ChevronRightSvg"));
 const HomeSvg = loadable(() => import("src/components/svg/solid/HomeSvg"));
+const LargePageSizeSettings = loadable(() => import("src/components/settings/LargePageSize"));
 const MainSidebar = loadable(() => import("src/components/sidebar/MainSidebar"));
-const MobileSidebar = loadable(() => import("src/components/sidebar/MobileSidebar"));
 const MobileSidebarButton = loadable(() => import("src/components/sidebar/MobileSidebarButton"));
 const ProfileSkeleton = loadable(() => import("src/components/skeletons/ProfileSkeleton"));
 const SiteFooter = loadable(() => import("src/components/footer/SiteFooter"));
 const TimestampSettings = loadable(() => import("src/components/settings/Timestamp"));
 
-const GlobalSettings = ({ token }) => {
+const GlobalSettings = ({ width, token }) => {
 	const [openMobileSidebar, setOpenMobileSidebar] = useState(false);
 	const [siteData, setSiteData] = useState([]);
 	const [userData, setUserData] = useState([]);
@@ -69,6 +70,11 @@ const GlobalSettings = ({ token }) => {
 			setSiteData(site);
 			setUserData(user);
 		}
+
+		if (userError || siteError) {
+			// TODO: add generic alert here
+			console.log("ERROR: " + userError ? userError : siteError);
+		}
 	}, [user, site, token]);
 
 	return (
@@ -76,14 +82,28 @@ const GlobalSettings = ({ token }) => {
 			<NextSeo title={pageTitle} />
 
 			<section tw="h-screen flex overflow-hidden bg-white">
-				{/* FIXME: fix mobile sidebar */}
-				{/* <MobileSidebar show={openMobileSidebar} setShow={setOpenMobileSidebar} /> */}
-				<MainSidebar user={userData} site={siteData} />
+				<MainSidebar
+					width={width}
+					user={userData}
+					site={siteData}
+					openMobileSidebar={openMobileSidebar}
+					setOpenMobileSidebar={setOpenMobileSidebar}
+				/>
 
 				<div tw="flex flex-col w-0 flex-1 overflow-hidden">
-					<div tw="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
+					<div tw="relative z-10 flex-shrink-0 flex h-16 lg:h-0 bg-white border-b lg:border-0 border-gray-200 lg:mb-4">
 						<MobileSidebarButton openMobileSidebar={openMobileSidebar} setOpenMobileSidebar={setOpenMobileSidebar} />
+						<Link href={homePageLink} passHref>
+							<a tw="p-1 block w-full cursor-pointer lg:hidden">
+								<AppLogo
+									className={tw`mt-4 mx-auto h-8 w-auto`}
+									src="/images/logos/site-logo-dark.svg"
+									alt="app-logo"
+								/>
+							</a>
+						</Link>
 					</div>
+
 					<main tw="flex-1 relative z-0 overflow-y-auto focus:outline-none" tabIndex="0">
 						<div tw="w-full p-6 mx-auto grid gap-16 lg:grid-cols-3 lg:col-gap-5 lg:row-gap-12">
 							<div tw="lg:col-span-2 xl:col-span-2 xl:pr-8 xl:border-r xl:border-gray-200">
@@ -135,7 +155,7 @@ const GlobalSettings = ({ token }) => {
 	);
 };
 
-export default GlobalSettings;
+export default withResizeDetector(GlobalSettings);
 
 GlobalSettings.propTypes = {};
 
