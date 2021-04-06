8c4293613c7595585090737937c428a630267f69
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-04-07 03:44:00 +0800

    Install and setup @tailwindcss/jit yarn package

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,9 +1,4 @@
 // postcss.config.js
 module.exports = {
-	plugins: [
-		'postcss-import',
-		'tailwindcss',
-		'autoprefixer',
-		['postcss-preset-env', { stage: 4 }]
-	]
+	plugins: { "@tailwindcss/jit": {}, "postcss-import": {}, autoprefixer: {}, "postcss-preset-env": { stage: 4 } },
 };
