add5460373c5defa9ab1105d7218a016b2a6f51e
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-04-12 23:33:37 +0800

    Change color orange to yellow among components

--- frontend/src/components/badges/SiteWarningBadge.jsx
+++ frontend/src/components/badges/SiteWarningBadge.jsx
@@ -1,15 +1,15 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-const SiteWarningBadgeDiv = styled.span``
+const SiteWarningBadgeDiv = styled.span``;
 
-const SiteWarningBadge = props => {
+const SiteWarningBadge = (props) => {
   return (
     <SiteWarningBadgeDiv
-      className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-orange-100 text-orange-800`}
+      className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800`}
     >
       {props.text}
     </SiteWarningBadgeDiv>
-  )
-}
+  );
+};
 
-export default SiteWarningBadge
+export default SiteWarningBadge;
