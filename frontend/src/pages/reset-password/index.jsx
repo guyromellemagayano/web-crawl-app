e699ceb5a54cbca8bbfef701a0a5090d7d72e1b1
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-04-15 18:34:30 +0800

    Minor code updates on reset password page

--- frontend/src/pages/reset-password/index.jsx
+++ frontend/src/pages/reset-password/index.jsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { Formik } from "formik";
 import { NextSeo } from "next-seo";
 import * as Yup from "yup";
+import loadable from "@loadable/component";
 import PropTypes from "prop-types";
 import ReactHtmlParser from "react-html-parser";
 import tw from "twin.macro";
@@ -18,11 +19,13 @@ import ResetPasswordLabel from "public/labels/pages/reset-password.json";
 // Hooks
 import usePostMethod from "src/hooks/usePostMethod";
 
-// Components
-import ErrorMessageAlert from "src/components/alerts/ErrorMessageAlert";
+// Layout
 import Layout from "src/components/Layout";
-import LogoLabel from "src/components/labels/LogoLabel";
-import SuccessMessageAlert from "src/components/alerts/SuccessMessageAlert";
+
+// Components
+const ErrorMessageAlert = loadable(() => import("src/components/alerts/ErrorMessageAlert"));
+const LogoLabel = loadable(() => import("src/components/labels/LogoLabel"));
+const SuccessMessageAlert = loadable(() => import("src/components/alerts/SuccessMessageAlert"));
 
 const ResetPassword = () => {
 	const [errorMsg, setErrorMsg] = useState("");
@@ -45,14 +48,14 @@ const ResetPassword = () => {
 					<div tw="bg-white py-8 px-4 shadow-xl rounded-lg sm:px-10">
 						<Formik
 							initialValues={{
-								email: "",
+								email: ""
 							}}
 							validationSchema={Yup.object({
-								email: Yup.string().email(ResetPasswordLabel[1].label).required(ResetPasswordLabel[0].label),
+								email: Yup.string().email(ResetPasswordLabel[1].label).required(ResetPasswordLabel[0].label)
 							})}
 							onSubmit={async (values, { setSubmitting, resetForm }) => {
 								const body = {
-									email: values.email,
+									email: values.email
 								};
 
 								try {
@@ -96,7 +99,7 @@ const ResetPassword = () => {
 													tw`shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm rounded-md`,
 													isSubmitting &&
 														tw`opacity-50 bg-gray-300 cursor-not-allowed pointer-events-none pointer-events-none`,
-													errors.email || errorMsg ? tw`border-red-300` : tw`border-gray-300`,
+													errors.email || errorMsg ? tw`border-red-300` : tw`border-gray-300`
 												]}
 												aria-describedby="email"
 												onChange={handleChange}
@@ -120,7 +123,7 @@ const ResetPassword = () => {
 													tw`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600`,
 													isSubmitting
 														? tw`opacity-50 bg-indigo-300 cursor-not-allowed pointer-events-none`
-														: tw`hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`,
+														: tw`hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`
 												]}
 											>
 												{isSubmitting ? ResetPasswordLabel[6].label : ResetPasswordLabel[5].label}
