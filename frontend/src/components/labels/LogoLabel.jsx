874c2e21276f34b5eed9e3fcd4bc9d2df3736cff
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-04-15 18:11:50 +0800

    Update logo label component

--- frontend/src/components/labels/LogoLabel.jsx
+++ frontend/src/components/labels/LogoLabel.jsx
@@ -1,42 +1,38 @@
 // React
 
 // NextJS
-import Link from 'next/link';
+import Link from "next/link";
 
 // External
-import 'twin.macro';
-import loadable from '@loadable/component';
-import PropTypes from 'prop-types';
+import tw from "twin.macro";
+import loadable from "@loadable/component";
+import PropTypes from "prop-types";
 
 // Components
-const AppLogo = loadable(() => import('src/components/logo/AppLogo'));
+const AppLogo = loadable(() => import("src/components/logo/AppLogo"));
 
 const LogoLabel = ({ isLogin, isSignUp, isResetPassword, isAddPassword }) => {
-	const siteLogoDarkSvg = '/images/logos/site-logo-dark.svg';
+	const siteLogoDarkSvg = "/images/logos/site-logo-dark.svg";
 
 	return (
-		<div tw='sm:mx-auto sm:w-full sm:max-w-md'>
-			{!isLogin ? (
-				<Link href='/'>
+		<div tw="sm:mx-auto sm:w-full sm:max-w-md">
+			{!isLogin && !isSignUp ? (
+				<Link href="/" passHref>
 					<a>
-						<AppLogo
-							className='h-10 w-auto mx-auto'
-							src={siteLogoDarkSvg}
-							alt='app-logo'
-						/>
+						<AppLogo className={tw`h-12 w-auto mx-auto`} src={siteLogoDarkSvg} alt="app-logo" />
 					</a>
 				</Link>
 			) : null}
-			<h2 tw='mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900'>
+			<h2 tw="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
 				{isLogin
-					? 'Log In'
+					? "Log In"
 					: isSignUp
-					? 'Sign up'
+					? "Sign up"
 					: isResetPassword
-					? 'Reset Password'
+					? "Reset Password"
 					: isAddPassword
-					? 'Add Password'
-					: 'Reset Password Form'}
+					? "Add Password"
+					: "Reset Password Form"}
 			</h2>
 		</div>
 	);
