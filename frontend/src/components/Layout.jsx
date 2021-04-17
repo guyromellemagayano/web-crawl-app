e4cf5a782252e3cefc459785b4857c3e98235f79
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-04-17 21:35:12 +0800

    Run LogRocket only on production

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -8,16 +8,18 @@ import setupLogRocketReact from "logrocket-react";
 
 const Layout = ({ user, children }) => {
 	useEffect(() => {
-		if (user && user !== undefined && Object.keys(user).length > 0) {
-			if (typeof window !== "undefined") {
-				LogRocket.init("epic-design-labs/link-app");
-				setupLogRocketReact(LogRocket);
-			}
+		if (process.env.NODE_ENV === "production") {
+			if (user && user !== undefined && user !== [] && Object.keys(user).length > 0) {
+				if (typeof window !== "undefined") {
+					LogRocket.init("epic-design-labs/link-app");
+					setupLogRocketReact(LogRocket);
+				}
 
-			LogRocket.identify("epic-design-labs/link-app", {
-				name: user.first_name + " " + user.last_name,
-				email: user.email
-			});
+				LogRocket.identify("epic-design-labs/link-app", {
+					name: user.first_name + " " + user.last_name,
+					email: user.email
+				});
+			}
 		}
 	}, [user]);
 
