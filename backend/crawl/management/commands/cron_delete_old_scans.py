cb7e4503653b3e5e4c37f36d3fcd0c643cd1150a
matematik7 <domen.ipavec@gmail.com>
2021-04-29 11:48:35 +0000

    Optimize cron delete job

--- backend/crawl/management/commands/cron_delete_old_scans.py
+++ backend/crawl/management/commands/cron_delete_old_scans.py
@@ -10,18 +10,25 @@ class Command(BaseCommand):
     help = "Deletes old scans, keep first and last 3, archive stats"
 
     def handle(self, *args, **options):
+        scan_ids_to_be_deleted = []
         for site in Site.objects.all():
             scans = site.scan_set.filter(finished_at__isnull=False).order_by("started_at")
             scan_count = scans.count()
             if scan_count > 4:
                 for scan in scans[1 : scan_count - 3]:
-                    self._archive_and_delete_scan(scan)
+                    self._archive_scan(scan)
+                    scan_ids_to_be_deleted.append(scan.id)
+
+        self._delete_scans(scan_ids_to_be_deleted)
 
         self._vacuum()
 
         print("Delete job done", flush=True)
 
-    def _archive_and_delete_scan(self, scan):
+    def _archive_scan(self, scan):
+        if ScanArchive.objects.filter(site=scan.site, scan_id=scan.id).exists():
+            return
+
         print(f"Archiving {scan.id} for {scan.site.url}", flush=True)
 
         large_page_size_threshold = scan.site.large_page_size_threshold
@@ -42,13 +49,45 @@ class Command(BaseCommand):
         except IntegrityError:
             pass
 
-        for i, link in enumerate(scan.link_set.all()):
-            if i % 1000 == 0:
-                print(f"Deleting link {i} for {scan.id} for {scan.site.url}", flush=True)
-            link.delete()
-
-        print(f"Deleting {scan.id} for {scan.site.url}", flush=True)
-        scan.delete()
+    def _delete_scans(self, scan_ids):
+        scan_ids = tuple(scan_ids)
+        with connections["superuser"].cursor() as cursor:
+            print("Deleting link links", flush=True)
+            cursor.execute(
+                "DELETE FROM crawl_link_links WHERE from_link_id IN (SELECT id FROM crawl_link WHERE scan_id IN %s)",
+                [scan_ids],
+            )
+            print("Deleting link images", flush=True)
+            cursor.execute(
+                "DELETE FROM crawl_link_images WHERE from_link_id IN (SELECT id FROM crawl_link WHERE scan_id IN %s)",
+                [scan_ids],
+            )
+            print("Deleting link scripts", flush=True)
+            cursor.execute(
+                "DELETE FROM crawl_link_scripts WHERE from_link_id IN (SELECT id FROM crawl_link WHERE scan_id IN %s)",
+                [scan_ids],
+            )
+            print("Deleting link stylesheets", flush=True)
+            cursor.execute(
+                "DELETE FROM crawl_link_stylesheets WHERE from_link_id IN (SELECT id FROM crawl_link WHERE scan_id IN %s)",
+                [scan_ids],
+            )
+            print("Deleting pagedata", flush=True)
+            cursor.execute(
+                "DELETE FROM crawl_pagedata WHERE link_id IN (SELECT id FROM crawl_link WHERE scan_id IN %s)",
+                [scan_ids],
+            )
+            print("Deleting links", flush=True)
+            cursor.execute("DELETE FROM crawl_link WHERE scan_id IN %s", [scan_ids])
+            print("Deleting fifo relations", flush=True)
+            cursor.execute(
+                "DELETE FROM crawl_fiforelation WHERE entry_id IN (SELECT id FROM crawl_fifoentry WHERE scan_id IN %s)",
+                [scan_ids],
+            )
+            print("Deleting fifo entries", flush=True)
+            cursor.execute("DELETE FROM crawl_fifoentry WHERE scan_id IN %s", [scan_ids])
+            print("Deleting scans", flush=True)
+            cursor.execute("DELETE FROM crawl_scan WHERE id IN %s", [scan_ids])
 
     def _vacuum(self):
         tables = ["crawl_link", "crawl_link_images", "crawl_link_scripts", "crawl_link_stylesheets", "crawl_link_links"]
