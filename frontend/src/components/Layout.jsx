984de460a968c8ccd609cd94e1a414d5e9ac8713
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-05-03 16:25:24 +0800

    Update Sentry and LogRocket configuration

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,17 +4,11 @@ import { useEffect } from "react";
 // External
 import "twin.macro";
 import LogRocket from "logrocket";
-import setupLogRocketReact from "logrocket-react";
 
 const Layout = ({ user, children }) => {
 	useEffect(() => {
 		if (process.env.NODE_ENV === "production") {
 			if (user && user !== undefined && user !== [] && Object.keys(user).length > 0) {
-				if (typeof window !== "undefined") {
-					LogRocket.init("epic-design-labs/link-app");
-					setupLogRocketReact(LogRocket);
-				}
-
 				LogRocket.identify("epic-design-labs/link-app", {
 					name: user.first_name + " " + user.last_name,
 					email: user.email
--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -7,7 +7,9 @@ import { Integrations } from "@sentry/tracing";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import * as Sentry from "@sentry/react";
 import App from "next/app";
+import LogRocket, { sessionURL } from "logrocket";
 import PropTypes from "prop-types";
+import setupLogRocketReact from "logrocket-react";
 
 // Enums
 import appSeo from "src/enum/nextSeo";
@@ -22,11 +24,33 @@ library.add(fas);
 
 // Sentry
 if (process.env.NODE_ENV === "production") {
-	Sentry.init({
-		dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-		integrations: [new Integrations.BrowserTracing()],
-		tracesSampleRate: 0.5
-	});
+	if (typeof window !== "undefined") {
+		Sentry.init({
+			dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+			integrations: [new Integrations.BrowserTracing()],
+			tracesSampleRate: 0.5,
+			beforeSend(event) {
+				const logRocketSession = LogRocket.sessionURL;
+
+				if (logRocketSession !== null) {
+					event.extra["LogRocket"] = logRocketSession;
+
+					return event;
+				} else {
+					return event;
+				}
+			}
+		});
+
+		LogRocket.init("epic-design-labs/link-app");
+		setupLogRocketReact(LogRocket);
+
+		LogRocket.getSessionURL((sessionURL) => {
+			Sentry.configureScope((scope) => {
+				scope.setExtra("sessionURL", sessionURL);
+			});
+		});
+	}
 }
 
 const MyApp = ({ Component, pageProps }) => {
