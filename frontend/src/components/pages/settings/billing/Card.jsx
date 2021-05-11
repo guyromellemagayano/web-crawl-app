76e94ace4c59a338810dce7016913b2e307227f6
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-05-12 06:40:15 +0800

    [WIP] Fix billing settings

--- frontend/src/components/pages/settings/billing/Card.jsx
+++ frontend/src/components/pages/settings/billing/Card.jsx
@@ -17,11 +17,44 @@ import usePatchMethod from "src/hooks/usePatchMethod";
 // Components
 const ErrorNotification = loadable(() => import("src/components/notifications/ErrorNotification"));
 const SuccessNotification = loadable(() => import("src/components/notifications/SuccessNotification"));
-// TODO: Add SettingsCardSkeleton component here
 
-const SettingsCard = ({ user }) => {
+const SettingsCard = ({ user, card }) => {
+	const [cardCvc, setCardCvc] = useState("")
+	const [cardExpirationDate, setCardExpirationDate] = useState("")
+	const [cardNumber, setCardNumber] = useState("")
+	const [componentReady, setComponentReady] = useState(false);
+	const [disableForm, setDisableForm] = useState(true);
+	const [errorMsg, setErrorMsg] = useState("");
+	const [errorMsgLoaded, setErrorMsgLoaded] = useState(false);
+	const [successMsg, setSuccessMsg] = useState("");
+	const [successMsgLoaded, setSuccessMsgLoaded] = useState(false);
+
+	const userApiEndpoint = "/api/auth/user/";
+
+	const handleCardNumberInputChange = (e) => {
+		setCardNumber(e.target.value);
+	}
+
+	const handleCardExpirationDateInputChange = (e) => {
+		setCardExpirationDate(e.target.value);
+	}
+
+	useEffect(() => {
+		if (user && user !== undefined && Object.keys(user).length > 0 && card && card !== undefined) {
+			console.log(card)
+			setCardNumber()
+			setCardExpirationDate()
+
+			setTimeout(() => {
+				setComponentReady(true)
+			}, 500);
+		}
+	}, [user, card])
+
 	return (
+		<div>
 
+		</div>
 	)
 }
 
