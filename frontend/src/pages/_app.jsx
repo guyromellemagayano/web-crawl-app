964f9d96a33403b91b3568c0280461e5d0c23ed0
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-05-24 21:53:56 +0800

    Update Sentry config setup

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -5,7 +5,7 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { Integrations } from "@sentry/tracing";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import * as Sentry from "@sentry/react";
+import * as Sentry from "@sentry/browser";
 import App from "next/app";
 import LogRocket from "logrocket";
 import PropTypes from "prop-types";
