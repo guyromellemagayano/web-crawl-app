87f405eee0976d2a6c8121503fbdc9081cd51722
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-05-24 16:02:44 +0800

    Fix dashboard site sorting

--- frontend/src/components/helpers/sorting/SiteSorting.jsx
+++ frontend/src/components/helpers/sorting/SiteSorting.jsx
@@ -1,35 +1,88 @@
+// React
+import * as React from "react";
+
+// NextJS
+import { useRouter } from "next/router";
+
 // External
-import loadable from "@loadable/component";
-import PropTypes from "prop-types";
 import "twin.macro";
+import PropTypes from "prop-types";
 
 // Components
-const Sorting = loadable(() => import("src/components/helpers/sorting/Sorting"));
+import Sorting from "src/components/helpers/sorting/Sorting";
 
-const SiteSorting = (props) => {
+// Helpers
+import { removeURLParameter, slugToCamelcase, getSortKeyFromSlug } from "src/helpers/functions";
+
+const initialOrder = {
+	siteName: "asc",
+	crawlStatus: "default",
+	lastCrawled: "default",
+	totalIssues: "default"
+};
+
+const SiteSorting = ({ result, slug, mutateSite, dataTableHeadsContent, setPagePath }) => {
+	const [sortOrder, setSortOrder] = React.useState(initialOrder);
+
+	const { asPath } = useRouter();
+	const router = useRouter();
+
+	const handleSort = (slug, dir) => {
+		setSortOrder({ ...initialOrder });
+
+		let newPath = removeURLParameter(asPath, "ordering");
+
+		const sortItem = slugToCamelcase(slug);
+		const sortKey = getSortKeyFromSlug(dataTableHeadsContent, slug);
+
+		setSortOrder((prevState) => ({ ...prevState, [sortItem]: dir }));
+
+		if (dir == "asc") {
+			if (newPath.includes("?")) newPath += `&ordering=${sortKey}`;
+			else newPath += `?ordering=${sortKey}`;
+		} else if (dir == "desc") {
+			if (newPath.includes("?")) newPath += `&ordering=-${sortKey}`;
+			else newPath += `?ordering=-${sortKey}`;
+		} else {
+			newPath = removeURLParameter(newPath, "ordering");
+		}
+
+		if (newPath.includes("?")) setPagePath(`${removeURLParameter(newPath, "page")}&`);
+		else setPagePath(`${removeURLParameter(newPath, "page")}?`);
+
+		router.push(newPath);
+		mutateSite;
+	};
+
 	return (
 		<div tw="flex flex-row mr-3">
 			<div tw="inline-flex">
-				{props.slug === "site-name" ? (
+				{slug === "site-name" ? (
 					<Sorting
-						enabled={true}
-						direction={props.sortOrder.siteName}
-						onSortHandler={props.onSortHandler}
-						slug={props.slug}
+						setSortOrder={setSortOrder}
+						tableContent={dataTableHeadsContent}
+						ordering={result.ordering}
+						direction={sortOrder.siteName}
+						onSortHandler={handleSort}
+						slug={slug}
 					/>
-				) : props.slug === "crawl-status" ? (
+				) : slug === "crawl-status" ? (
 					<Sorting
-						enabled={true}
-						direction={props.sortOrder.crawlStatus}
-						onSortHandler={props.onSortHandler}
-						slug={props.slug}
+						setSortOrder={setSortOrder}
+						tableContent={dataTableHeadsContent}
+						ordering={result.ordering}
+						direction={sortOrder.crawlStatus}
+						onSortHandler={handleSort}
+						slug={slug}
 					/>
-				) : props.slug === "last-crawled" ? (
+				) : slug === "last-crawled" ? (
 					<Sorting
-						enabled={true}
-						direction={props.sortOrder.lastCrawled}
-						onSortHandler={props.onSortHandler}
-						slug={props.slug}
+						setSortOrder={setSortOrder}
+						tableContent={dataTableHeadsContent}
+						ordering={result.ordering}
+						direction={sortOrder.lastCrawled}
+						onSortHandler={handleSort}
+						slug={slug}
 					/>
 				) : null}
 			</div>
