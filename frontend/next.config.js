99e47084e87a17ceeccc10a193245798a82a94b9
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-05-24 23:44:00 +0800

    Update Sentry configuration setup

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,21 @@
-module.exports = {
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const SENTRY_ORG = process.env.SENTRY_ORG || process.env.NEXT_PUBLIC_SENTRY_ORG;
+const SENTRY_AUTH_TOKEN = process.env.SENTRY_AUTH_TOKEN || process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN;
+const SENTRY_PROJECT = process.env.SENTRY_PROJECT || process.env.NEXT_PUBLIC_SENTRY_PROJECT;
+
+const moduleExports = {
 	trailingSlash: true,
 	devIndicators: {
 		autoPrerender: false
 	}
 };
+
+const SentryWebpackPluginOptions = {
+	include: ".",
+	org: SENTRY_ORG || "epic-design-labs",
+	authToken: SENTRY_AUTH_TOKEN || "21024702c44e4bf3a4ac704e27d82a5e7cb2be29340046dc8d9c6d0b06a92ff1",
+	project: SENTRY_PROJECT || "sitecrawler-frontend"
+};
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -3,9 +3,7 @@ import "tailwindcss/tailwind.css";
 import { DefaultSeo } from "next-seo";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import { Integrations } from "@sentry/tracing";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import * as Sentry from "@sentry/browser";
 import App from "next/app";
 import LogRocket from "logrocket";
 import PropTypes from "prop-types";
@@ -25,31 +23,8 @@ library.add(fas);
 // Sentry
 if (process.env.NODE_ENV === "production") {
 	if (typeof window !== "undefined") {
-		Sentry.init({
-			dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-			integrations: [new Integrations.BrowserTracing()],
-			tracesSampleRate: 0.5,
-			beforeSend(event) {
-				const logRocketSession = LogRocket.sessionURL;
-
-				if (logRocketSession !== null) {
-					event.extra["LogRocket"] = logRocketSession;
-
-					return event;
-				} else {
-					return event;
-				}
-			}
-		});
-
 		LogRocket.init("epic-design-labs/link-app");
 		setupLogRocketReact(LogRocket);
-
-		LogRocket.getSessionURL((sessionURL) => {
-			Sentry.configureScope((scope) => {
-				scope.setExtra("sessionURL", sessionURL);
-			});
-		});
 	}
 }
 
@@ -72,4 +47,4 @@ MyApp.getInitialProps = async (appContext) => {
 	return { ...appProps };
 };
 
-export default Sentry.withProfiler(MyApp);
+export default MyApp;
