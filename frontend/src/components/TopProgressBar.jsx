7941c304f6ca69dc1f592922de0b356521c25dd7
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-06-04 05:54:57 +0800

    Update router dependency in top progress bar component

--- frontend/src/components/TopProgressBar.jsx
+++ frontend/src/components/TopProgressBar.jsx
@@ -1,5 +1,8 @@
+// React
+import * as React from "react";
+
 // NextJS
-import Router, { useRouter } from "next/router";
+import { useRouter } from "next/router";
 
 // External
 import "nprogress/nprogress.css";
@@ -9,55 +12,62 @@ const TopProgressBar = () => {
 	let state;
 	let activeRequests = 0;
 
+	const router = useRouter();
 	NProgress.configure({ showSpinner: false });
 
-	const load = () => {
-		if (state === "loading") {
-			return;
-		}
-
-		state = "loading";
-
-		NProgress.start();
-	};
-
-	const stop = () => {
-		if (activeRequests > 0) {
-			return;
-		}
-
-		state = "stop";
-
-		NProgress.done();
-	};
-
-	Router.events.on("routeChangeStart", load);
-	Router.events.on("routeChangeComplete", stop);
-	Router.events.on("routeChangeError", stop);
-
-	if (typeof window !== "undefined") {
-		const originalFetch = window.fetch;
-
-		window.fetch = async function (...args) {
-			if (activeRequests === 0) {
-				load();
+	React.useEffect(() => {
+		const load = () => {
+			if (state === "loading") {
+				return;
 			}
 
-			activeRequests++;
+			state = "loading";
 
-			try {
-				const response = await originalFetch(...args);
-				return response;
-			} catch (error) {
-				return Promise.reject(error);
-			} finally {
-				activeRequests -= 1;
-				if (activeRequests === 0) {
-					stop();
-				}
-			}
+			NProgress.start();
 		};
-	}
+
+		const stop = () => {
+			if (activeRequests > 0) {
+				return;
+			}
+
+			state = "stop";
+
+			NProgress.done();
+		};
+
+		router.events.on("routeChangeStart", load);
+		router.events.on("routeChangeComplete", stop);
+		router.events.on("routeChangeError", stop);
+
+		if (typeof window !== "undefined") {
+			const originalFetch = window.fetch;
+
+			window.fetch = async function (...args) {
+				if (activeRequests === 0) {
+					load();
+				}
+
+				activeRequests++;
+
+				try {
+					const response = await originalFetch(...args);
+					return response;
+				} catch (error) {
+					return Promise.reject(error);
+				} finally {
+					activeRequests -= 1;
+					if (activeRequests === 0) {
+						stop();
+					}
+				}
+			};
+		}
+
+		return () => {
+			router.events.off("routeChangeStart", load);
+		};
+	}, []);
 
 	return null;
 };
