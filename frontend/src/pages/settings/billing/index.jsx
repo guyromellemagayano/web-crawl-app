c8a14a5f3fe1173adc4ae3e4d0fac0d3303f6049
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-06-10 14:55:47 +0800

    Update billing settings page and coming soon component

--- frontend/src/pages/settings/billing/index.jsx
+++ frontend/src/pages/settings/billing/index.jsx
@@ -1,19 +1,19 @@
 // React
-import { useState, useEffect } from "react";
+import * as React from "react";
 
 // NextJS
 // import Link from "next/link";
 
 // External
 // import { ChevronRightIcon, HomeIcon } from "@heroicons/react/solid";
+import "twin.macro";
 import { NextSeo } from "next-seo";
 import { withResizeDetector } from "react-resize-detector";
 import loadable from "@loadable/component";
 import PropTypes from "prop-types";
-import tw from "twin.macro";
 
 // JSON
-// import BillingLabel from "public/labels/pages/settings/billing.json";
+import BillingLabel from "public/labels/pages/settings/billing.json";
 
 // Hooks
 import useUser from "src/hooks/useUser";
@@ -33,54 +33,30 @@ const ComingSoon = loadable(() => import("src/components/layouts/ComingSoon"));
 const Loader = loadable(() => import("src/components/layouts/Loader"));
 
 const Billing = ({ width }) => {
-	const [openMobileSidebar, setOpenMobileSidebar] = useState(false);
-	const [pageLoaded, setPageLoaded] = useState(false);
-	// const [paymentMethodData, setPaymentMethodData] = useState([]);
-	const [userData, setUserData] = useState([]);
+	// const [paymentMethodData, setPaymentMethodData] = React.useState([]);
+	const [openMobileSidebar, setOpenMobileSidebar] = React.useState(false);
 
-	const pageTitle = "Billing Settings";
+	const pageTitle = BillingLabel[0].label;
 	// const homeLabel = "Home";
 	// const homePageLink = "/";
 	// const paymentMethodApiEndpoint = '/api/stripe/payment-method/'
 
-	const { user: user } = useUser({
+	const { user } = useUser({
 		redirectIfFound: false,
 		redirectTo: "/login"
 	});
 
-	const { paymentMethods: paymentMethods } = usePaymentMethods({
-		refreshInterval: 0
-	})
+	// const { paymentMethods } = usePaymentMethods();
 
-	useEffect(() => {
-		if (
-			user &&
-			user !== undefined &&
-			Object.keys(user).length > 0 &&
-			paymentMethods &&
-			paymentMethods !== undefined
-		) {
-			setUserData(user);
-			// setPaymentMethodData(paymentMethods)
-
-			setTimeout(() => {
-				setPageLoaded(true);
-			}, 500);
-		}
-	}, [user, paymentMethods]);
-
-	console.log(width)
-
-	return pageLoaded ? (
+	return user ? (
 		<Layout user={user}>
 			<NextSeo title={pageTitle} />
 
 			<section tw="h-screen flex overflow-hidden bg-white">
 				<ComingSoon
 					width={width}
-					user={userData}
+					user={user}
 					pageTitle={pageTitle}
-					pageLoaded={pageLoaded}
 					openMobileSidebar={openMobileSidebar}
 					setOpenMobileSidebar={setOpenMobileSidebar}
 				/>
