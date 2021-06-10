74fa00d632df016e16a0c28022ebb42cb7bcefc3
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-06-10 14:53:13 +0800

    Update states in profile settings page

--- frontend/src/pages/settings/profile/index.jsx
+++ frontend/src/pages/settings/profile/index.jsx
@@ -51,73 +51,67 @@ const Profile = ({ width }) => {
 			<section className="h-screen flex overflow-hidden bg-white">
 				<MainSidebar
 					width={width}
-					user={user ? user : null}
+					user={user}
 					openMobileSidebar={openMobileSidebar}
 					setOpenMobileSidebar={setOpenMobileSidebar}
 				/>
 
-				{user ? (
-					<div tw="flex flex-col w-0 flex-1 overflow-hidden">
-						<div tw="relative z-10 flex-shrink-0 flex  lg:h-0 bg-white border-b lg:border-0 border-gray-200 lg:mb-4">
-							<MobileSidebarButton openMobileSidebar={openMobileSidebar} setOpenMobileSidebar={setOpenMobileSidebar} />
-							<Link href={homePageLink} passHref>
-								<a tw="p-1 block w-full cursor-pointer lg:hidden">
-									<AppLogo
-										className={tw`mt-4 mx-auto h-8 w-auto`}
-										src="/images/logos/site-logo-dark.svg"
-										alt="app-logo"
-									/>
-								</a>
-							</Link>
-						</div>
+				<div tw="flex flex-col w-0 flex-1 overflow-hidden">
+					<div tw="relative z-10 flex-shrink-0 flex  lg:h-0 bg-white border-b lg:border-0 border-gray-200 lg:mb-4">
+						<MobileSidebarButton openMobileSidebar={openMobileSidebar} setOpenMobileSidebar={setOpenMobileSidebar} />
+						<Link href={homePageLink} passHref>
+							<a tw="p-1 block w-full cursor-pointer lg:hidden">
+								<AppLogo
+									className={tw`mt-4 mx-auto h-8 w-auto`}
+									src="/images/logos/site-logo-dark.svg"
+									alt="app-logo"
+								/>
+							</a>
+						</Link>
+					</div>
 
-						<main tw="flex-1 relative z-0 overflow-y-auto focus:outline-none" tabIndex="0">
-							<div tw="w-full p-6 mx-auto grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12 min-h-screen">
-								<div tw="lg:col-span-2 xl:col-span-2 xl:pr-8 xl:border-r xl:border-gray-200">
-									<div className="max-w-full py-4 px-8">
-										<nav tw="flex pt-4 pb-8" aria-label="Breadcrumb">
-											<ol tw="flex items-center space-x-4">
-												<li>
-													<div>
-														<Link href={homePageLink} passHref>
-															<a tw="text-gray-400 hover:text-gray-500">
-																<HomeIcon tw="flex-shrink-0 h-5 w-5" />
-																<span tw="sr-only">{homeLabel}</span>
-															</a>
-														</Link>
-													</div>
-												</li>
-												<li>
-													<div tw="flex items-center">
-														<ChevronRightIcon tw="flex-shrink-0 h-5 w-5 text-gray-400" />
-														<p aria-current="page" tw="cursor-default ml-4 text-sm font-medium text-gray-700">
-															{pageTitle}
-														</p>
-													</div>
-												</li>
-											</ol>
-										</nav>
-										<div className="pt-4 m-auto">
-											<h4 className="text-2xl leading-6 font-medium text-gray-900">{ProfileLabel[0].label}</h4>
-										</div>
-									</div>
-
-									<div tw="space-y-12 divide-y divide-gray-200">
-										<SettingsPersonal user={user ? user : null} />
-										<SettingsPassword user={user ? user : null} />
+					<main tw="flex-1 relative z-0 overflow-y-auto focus:outline-none" tabIndex="0">
+						<div tw="w-full p-6 mx-auto grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12 min-h-screen">
+							<div tw="lg:col-span-2 xl:col-span-2 xl:pr-8 xl:border-r xl:border-gray-200">
+								<div className="max-w-full py-4 px-8">
+									<nav tw="flex pt-4 pb-8" aria-label="Breadcrumb">
+										<ol tw="flex items-center space-x-4">
+											<li>
+												<div>
+													<Link href={homePageLink} passHref>
+														<a tw="text-gray-400 hover:text-gray-500">
+															<HomeIcon tw="flex-shrink-0 h-5 w-5" />
+															<span tw="sr-only">{homeLabel}</span>
+														</a>
+													</Link>
+												</div>
+											</li>
+											<li>
+												<div tw="flex items-center">
+													<ChevronRightIcon tw="flex-shrink-0 h-5 w-5 text-gray-400" />
+													<p aria-current="page" tw="cursor-default ml-4 text-sm font-medium text-gray-700">
+														{pageTitle}
+													</p>
+												</div>
+											</li>
+										</ol>
+									</nav>
+									<div className="pt-4 m-auto">
+										<h4 className="text-2xl leading-6 font-medium text-gray-900">{ProfileLabel[0].label}</h4>
 									</div>
 								</div>
+
+								<div tw="space-y-12 divide-y divide-gray-200">
+									<SettingsPersonal user={user} />
+									<SettingsPassword user={user} />
+								</div>
 							</div>
-							<div tw="static bottom-0 w-full mx-auto px-12 py-4 bg-white border-t border-gray-200">
-								<SiteFooter />
-							</div>
-						</main>
-					</div>
-				) : (
-					<div tw="mx-auto">
-						<Loader />
-					</div>
-				)}
+						</div>
+						<div tw="static bottom-0 w-full mx-auto px-12 py-4 bg-white border-t border-gray-200">
+							<SiteFooter />
+						</div>
+					</main>
+				</div>
 			</section>
 		</Layout>
 	) : (
