dd3c0cf81eac0abee6fb5a93f4dc23f8c647aae3
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-06-13 06:43:57 +0800

    Update site settings page

--- frontend/src/pages/site/[siteId]/settings.jsx
+++ frontend/src/pages/site/[siteId]/settings.jsx
@@ -5,9 +5,9 @@ import * as React from "react";
 import Link from "next/link";
 
 // External
-import { ChevronRightIcon, HomeIcon } from "@heroicons/react/solid";
 import { NextSeo } from "next-seo";
 import { withResizeDetector } from "react-resize-detector";
+import loadable from "@loadable/component";
 import PropTypes from "prop-types";
 import tw, { styled } from "twin.macro";
 
@@ -23,26 +23,25 @@ import Layout from "src/components/Layout";
 
 // Components
 import AppLogo from "src/components/logos/AppLogo";
-import DeleteSiteSettings from "src/components/pages/settings/site/DeleteSite";
-import LargePageSizeSettings from "src/components/pages/settings/site/LargePageSize";
-import Loader from "src/components/layouts/Loader";
 import MainSidebar from "src/components/sidebar/MainSidebar";
 import MobileSidebarButton from "src/components/buttons/MobileSidebarButton";
 import SiteFooter from "src/components/layouts/Footer";
-import SiteInformationSettings from "src/components/pages/settings/site/SiteInformation";
+
+// Loadable
+const Breadcrumbs = loadable(() => import("src/components/breadcrumbs/Breadcrumbs"));
+const DeleteSiteSettings = loadable(() => import("src/components/pages/settings/site/DeleteSite"));
+const LargePageSizeSettings = loadable(() => import("src/components/pages/settings/site/LargePageSize"));
+const Loader = loadable(() => import("src/components/layouts/Loader"));
+const SiteInformationSettings = loadable(() => import("src/components/pages/settings/site/SiteInformation"));
 
 const SiteSettingsSection = styled.section``;
 
 const SiteSettings = ({ width, result }) => {
 	const [openMobileSidebar, setOpenMobileSidebar] = React.useState(false);
 
-	let pageTitle = "";
-	let homeLabel = "Home";
-	let homePageLink = `/site/${result.siteId}/overview`;
+	const sitesApiEndpoint = `/api/site/?ordering=name`;
 
-	let sitesApiEndpoint = `/api/site/?ordering=name`;
-
-	const { user: user } = useUser({
+	const { user } = useUser({
 		redirectIfFound: false,
 		redirectTo: "/login"
 	});
@@ -55,10 +54,8 @@ const SiteSettings = ({ width, result }) => {
 		querySid: result.siteId
 	});
 
-	siteId
-		? (pageTitle =
-				siteId.name && siteId.name !== null ? SettingsLabel[1].label + " - " + siteId.name : SettingsLabel[1].label)
-		: null;
+	const homePageLink = `/site/${result.siteId}/overview`;
+	const pageTitle = SettingsLabel[1].label + " - " + siteId?.name;
 
 	return user ? (
 		<Layout user={user}>
@@ -73,81 +70,61 @@ const SiteSettings = ({ width, result }) => {
 				/>
 
 				{siteId ? (
-					<>
-						<div tw="flex flex-col w-0 flex-1 overflow-hidden">
-							<div tw="relative flex-shrink-0 flex bg-white">
-								<div tw="border-b flex-shrink-0 flex">
-									<MobileSidebarButton
-										openMobileSidebar={openMobileSidebar}
-										setOpenMobileSidebar={setOpenMobileSidebar}
-									/>
-								</div>
-
-								<Link href={homePageLink} passHref>
-									<a tw="p-1 block w-full cursor-pointer lg:hidden">
-										<AppLogo
-											className={tw`mt-4 mx-auto h-8 w-auto`}
-											src="/images/logos/site-logo-dark.svg"
-											alt="app-logo"
-										/>
-									</a>
-								</Link>
+					<div tw="flex flex-col w-0 flex-1 overflow-hidden">
+						<div tw="relative flex-shrink-0 flex bg-white">
+							<div tw="border-b flex-shrink-0 flex">
+								<MobileSidebarButton
+									openMobileSidebar={openMobileSidebar}
+									setOpenMobileSidebar={setOpenMobileSidebar}
+								/>
 							</div>
 
-							<main tw="flex-1 relative z-0 overflow-y-auto focus:outline-none" tabIndex="0">
-								<div tw="w-full p-6 mx-auto grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
-									<div tw="lg:col-span-2 xl:col-span-2 xl:pr-8 xl:border-r xl:border-gray-200">
-										<div className="max-w-full py-4 px-8">
-											<nav tw="flex pt-4 pb-8" aria-label="Breadcrumb">
-												<ol tw="flex items-center space-x-4">
-													<li>
-														<div>
-															<Link href={homePageLink} passHref>
-																<a tw="text-gray-400 hover:text-gray-500">
-																	<HomeIcon tw="flex-shrink-0 h-5 w-5" />
-																	<span tw="sr-only">{homeLabel}</span>
-																</a>
-															</Link>
-														</div>
-													</li>
-													<li>
-														<div tw="flex items-center">
-															<ChevronRightIcon tw="flex-shrink-0 h-5 w-5 text-gray-400" />
-															<p aria-current="page" tw="cursor-default ml-4 text-sm font-medium text-gray-700">
-																{pageTitle}
-															</p>
-														</div>
-													</li>
-												</ol>
-											</nav>
-											<div className="pt-4 m-auto">
-												<h4 className="text-2xl leading-6 font-medium text-gray-900">{pageTitle}</h4>
-											</div>
-										</div>
+							<Link href={homePageLink} passHref>
+								<a tw="p-1 block w-full cursor-pointer lg:hidden">
+									<AppLogo
+										className={tw`mt-4 mx-auto h-8 w-auto`}
+										src="/images/logos/site-logo-dark.svg"
+										alt="app-logo"
+									/>
+								</a>
+							</Link>
+						</div>
 
-										<div tw="space-y-12 divide-y divide-gray-200">
-											{user && siteId ? (
-												<>
-													<SiteInformationSettings user={user} siteId={siteId} settingsLabel={SettingsLabel} />
-													<LargePageSizeSettings user={user} siteId={siteId} mutateSiteId={mutateSiteId} />
-													<DeleteSiteSettings
-														user={user}
-														siteId={siteId}
-														settingsLabel={SettingsLabel}
-														mutateSite={mutateSite}
-													/>
-												</>
-											) : null}
+						<main tw="flex-1 relative z-0 overflow-y-auto focus:outline-none" tabIndex="0">
+							<div tw="w-full p-6 mx-auto grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
+								<div tw="lg:col-span-2 xl:col-span-2 xl:pr-8 xl:border-r xl:border-gray-200">
+									<div className="max-w-full py-4 px-8">
+										<Breadcrumbs siteId={result.siteId} pageTitle={SettingsLabel[1].label} />
+
+										<div className="pt-4 m-auto">
+											<h2 tw="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+												{SettingsLabel[1].label}
+											</h2>
 										</div>
 									</div>
-								</div>
 
-								<div tw="static bottom-0 w-full mx-auto px-12 py-4 bg-white border-t border-gray-200">
-									<SiteFooter />
+									<div tw="space-y-12 divide-y divide-gray-200">
+										{user && siteId ? (
+											<>
+												<SiteInformationSettings user={user} siteId={siteId} settingsLabel={SettingsLabel} />
+												<LargePageSizeSettings user={user} siteId={siteId} mutateSiteId={mutateSiteId} />
+												<DeleteSiteSettings
+													user={user}
+													siteId={siteId}
+													settingsLabel={SettingsLabel}
+													mutateSite={mutateSite}
+												/>
+											</>
+										) : null}
+									</div>
 								</div>
-							</main>
-						</div>
-					</>
+							</div>
+
+							<div tw="static bottom-0 w-full mx-auto px-12 py-4 bg-white border-t border-gray-200">
+								<SiteFooter />
+							</div>
+						</main>
+					</div>
 				) : (
 					<div tw="mx-auto">
 						<Loader />
