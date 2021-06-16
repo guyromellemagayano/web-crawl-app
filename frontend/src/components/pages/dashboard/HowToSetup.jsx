e02aca5b9ee00a5efd752b59adb653725e30b489
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-06-17 01:05:27 +0800

    Update how to setup component and its skeleton

--- frontend/src/components/pages/dashboard/HowToSetup.jsx
+++ frontend/src/components/pages/dashboard/HowToSetup.jsx
@@ -1,72 +1,76 @@
 // React
-import { useState } from 'react';
+import * as React from "react";
 
 // External
-import tw from 'twin.macro';
-import ReactPlayer from 'react-player/lazy';
+import tw, { styled } from "twin.macro";
+import loadable from "@loadable/component";
+import ReactPlayer from "react-player/lazy";
 
 // JSON
-import HowToSetupLabel from 'public/labels/components/sites/HowToSetup.json';
+import HowToSetupLabel from "public/labels/components/sites/HowToSetup.json";
+
+// Loadable
+const Loader = loadable(() => import("src/components/layouts/Loader"));
 
 const tabItems = [
 	{
 		id: 1,
-		title: 'WordPress',
+		title: "WordPress",
 		video: {
-			src: 'https://youtu.be/FKTOwvjJmlY'
+			src: "https://youtu.be/FKTOwvjJmlY"
 		},
 		defaultvalue: true
 	},
 	{
 		id: 2,
-		title: 'BigCommerce',
+		title: "BigCommerce",
 		video: {
-			src: 'https://youtu.be/Pgoj-AwHOiM'
+			src: "https://youtu.be/Pgoj-AwHOiM"
 		},
 		defaultvalue: false
 	}
 ];
 
-const HowToSetup = () => {
-	const [tabActive, setTabActive] = useState(1);
+const HowToSetupDiv = styled.div``;
 
-	return (
-		<div tw='relative pt-12 pb-16 px-4 sm:px-6 lg:pt-16 lg:pb-20 lg:px-8'>
-			<div tw='absolute inset-0'>
-				<div tw='bg-white h-1/3 sm:h-2/3'></div>
-			</div>
-			<div tw='relative max-w-7xl mx-auto'>
-				<div tw='text-center mb-10'>
-					<h3 tw='text-2xl leading-9 tracking-tight font-bold text-gray-900 sm:text-3xl sm:leading-10'>
+const HowToSetup = () => {
+	const [componentReady, setComponentReady] = React.useState(false);
+	const [tabActive, setTabActive] = React.useState(1);
+
+	React.useEffect(() => {
+		setComponentReady(false);
+
+		setTimeout(() => {
+			setComponentReady(true);
+		}, 500);
+	}, []);
+
+	return componentReady ? (
+		<HowToSetupDiv tw="relative py-6 px-4 sm:px-6 lg:px-8">
+			<div tw="max-w-7xl mx-auto">
+				<div tw="text-center mb-10">
+					<h3 tw="text-2xl leading-9 tracking-tight font-bold text-gray-900 sm:text-3xl sm:leading-10">
 						{HowToSetupLabel[0].label}
 					</h3>
-					<p tw='mt-3 max-w-2xl mx-auto text-base leading-6 text-gray-500 sm:mt-4'>
-						{HowToSetupLabel[1].label}
-					</p>
+					<p tw="mt-3 max-w-2xl mx-auto text-base leading-6 text-gray-500 sm:mt-4">{HowToSetupLabel[1].label}</p>
 				</div>
-				<div tw='relative mx-auto w-full rounded-lg lg:max-w-md mb-8'>
+				<div tw="relative mx-auto w-full rounded-lg lg:max-w-md mb-8">
 					{tabItems.map(({ id, video }) => {
 						return tabActive === id ? (
-							<ReactPlayer
-								key={id}
-								url={video.src}
-								width={'auto'}
-								height={320}
-								controls={true}
-							/>
+							<ReactPlayer key={id} url={video.src} width={"auto"} height={320} controls={true} />
 						) : null;
 					})}
 				</div>
 				<div>
-					<div tw='sm:hidden'>
-						<label htmlFor='tabs' className='sr-only'>
+					<div tw="sm:hidden">
+						<label htmlFor="tabs" className="sr-only">
 							Platform select tabs
 						</label>
 						<select
-							id='tabs'
-							name='tabs'
-							aria-label='Selected tab'
-							tw='block w-full focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md'
+							id="tabs"
+							name="tabs"
+							aria-label="Selected tab"
+							tw="block w-full focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
 						>
 							{tabItems.map(({ title, defaultValue }, key) => (
 								<option key={key} selected={defaultValue}>
@@ -75,8 +79,8 @@ const HowToSetup = () => {
 							))}
 						</select>
 					</div>
-					<div tw='hidden sm:block'>
-						<nav tw='flex justify-center'>
+					<div tw="hidden sm:block">
+						<nav tw="flex justify-center">
 							{tabItems.map(({ title, id }, key) => (
 								<TabItem
 									key={key}
@@ -90,19 +94,23 @@ const HowToSetup = () => {
 					</div>
 				</div>
 			</div>
+		</HowToSetupDiv>
+	) : (
+		<div tw="mx-auto">
+			<Loader />
 		</div>
 	);
 };
 
 const TabItem = ({
-	id = '',
-	title = '',
-	onItemClicked = () => console.error('You passed no action to the component'),
+	id = "",
+	title = "",
+	onItemClicked = () => console.error("You passed no action to the component"),
 	isTabActive = false
 }) => {
 	return (
 		<a
-			href='#'
+			href="#"
 			css={[
 				tw`font-medium text-sm leading-5 rounded-md`,
 				isTabActive
@@ -110,7 +118,7 @@ const TabItem = ({
 					: tw`text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:bg-gray-100`,
 				id !== 1 ? tw`ml-2 px-3 py-2` : tw`px-3 py-2`
 			]}
-			aria-current='page'
+			aria-current="page"
 			onClick={onItemClicked}
 		>
 			{title}
