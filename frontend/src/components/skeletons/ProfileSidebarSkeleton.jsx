7ab3c94b3130f3cfa5b8997296e6cc9f05374c29
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-06-23 10:24:55 +0800

    Update profile sidebar skeleton, delete settings skeletons

--- frontend/src/components/skeletons/ProfileSidebarSkeleton.jsx
+++ frontend/src/components/skeletons/ProfileSidebarSkeleton.jsx
@@ -12,9 +12,6 @@ const ProfileSidebarSkeleton = () => {
 		<div className="group" tw="p-4 flex-shrink-0 w-full block bg-gray-900">
 			<div tw="flex items-center">
 				<div>
-					<Skeleton circle={true} duration={2} width={40} height={40} />
-				</div>
-				<div tw="ml-3">
 					<Skeleton duration={2} width={130} />
 					<Skeleton duration={2} width={130} />
 				</div>
