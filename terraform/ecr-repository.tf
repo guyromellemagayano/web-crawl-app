c851842f289281dfbb8048ed8c832eb0035ec3d9
matematik7 <domen.ipavec@gmail.com>
2021-07-22 08:40:25 +0000

    Add expire policy to keep only last 30 images to ecr

--- terraform/ecr-repository.tf
+++ terraform/ecr-repository.tf
@@ -1,34 +1,43 @@
-resource "aws_ecr_repository" "crawl-app-backend" {
-  name                 = "crawl-app-backend"
+variable "ecr_repositories" {
+  type = list(string)
+  default = [
+    "crawl-app-backend",
+    "crawl-app-crawler",
+    "crawl-app-frontend",
+    "crawl-app-uptimer",
+    "crawl-app-verifier",
+    "crawl-app-reverifier",
+    "crawl-app-scheduler",
+  ]
+}
+
+resource "aws_ecr_repository" "repository" {
+  for_each = toset(var.ecr_repositories)
+
+  name                 = each.value
   image_tag_mutability = "MUTABLE"
 }
 
-resource "aws_ecr_repository" "crawl-app-crawler" {
-  name                 = "crawl-app-crawler"
-  image_tag_mutability = "MUTABLE"
-}
+resource "aws_ecr_lifecycle_policy" "keep_last_30" {
+  for_each = aws_ecr_repository.repository
+  repository = each.value.id
 
-resource "aws_ecr_repository" "crawl-app-frontend" {
-  name                 = "crawl-app-frontend"
-  image_tag_mutability = "MUTABLE"
-}
-
-# resource "aws_ecr_repository" "crawl-app-scheduler" {
-#   name                 = "crawl-app-scheduler"
-#   image_tag_mutability = "MUTABLE"
-# }
-
-resource "aws_ecr_repository" "crawl-app-uptimer" {
-  name                 = "crawl-app-uptimer"
-  image_tag_mutability = "MUTABLE"
-}
-
-resource "aws_ecr_repository" "crawl-app-verifier" {
-  name                 = "crawl-app-verifier"
-  image_tag_mutability = "MUTABLE"
-}
-
-resource "aws_ecr_repository" "crawl-app-reverifier" {
-  name                 = "crawl-app-reverifier"
-  image_tag_mutability = "MUTABLE"
+  policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 30 images",
+        "selection": {
+          "tagStatus": "any",
+          "countType": "imageCountMoreThan",
+          "countNumber": 30
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+  EOF
 }
