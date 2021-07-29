be6685a320646ec466b31fe4e345d79bde12242a
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-07-30 04:34:31 +0800

    Fix layout issues on account exist page

--- frontend/src/pages/account-exist/index.jsx
+++ frontend/src/pages/account-exist/index.jsx
@@ -23,17 +23,17 @@ const AccountExist = () => {
 		<Layout>
 			<NextSeo title={pageTitle} />
 
-			<div tw="bg-gray-50 overflow-auto h-screen">
+			<div tw="overflow-auto h-screen">
 				<Scrollbars universal>
 					<div tw="flex flex-col justify-center h-full">
 						<div tw="relative py-12 sm:px-6 lg:px-8">
 							{AccountExistContent.map((val, key) => {
 								return (
-									<div key={key} tw="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+									<div key={key} tw="max-w-lg min-h-screen flex flex-col justify-center mx-auto py-12 sm:px-6 lg:px-8">
 										<div tw="bg-white rounded-lg">
 											<div tw="px-4 py-5 sm:p-6">
 												<h3 tw="text-lg leading-6 font-medium text-red-600">{val.title}</h3>
-												<div tw="mt-2 max-w-xl text-sm leading-5 text-gray-500">
+												<div tw="mt-2 text-sm leading-5 text-gray-500">
 													<p>{val.description}</p>
 												</div>
 												<div tw="mt-3 text-sm leading-5">
