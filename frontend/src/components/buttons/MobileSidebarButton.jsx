04b1688bf0ff60c34b68988b51a37b054f13fb59
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-10 19:33:55 +0800

    Add PropTypes to button components, update props

--- frontend/src/components/buttons/MobileSidebarButton.jsx
+++ frontend/src/components/buttons/MobileSidebarButton.jsx
@@ -1,19 +1,31 @@
+// React
+import * as React from "react";
+
 // External
 import "twin.macro";
 import { MenuIcon } from "@heroicons/react/solid";
+import PropTypes from "prop-types";
 
-const MobileSidebarButton = ({ openMobileSidebar, setOpenMobileSidebar }) => {
+const MobileSidebarButton = ({ handleOpenMobileSidebar }) => {
 	const openSidebarLabel = "Open sidebar";
 
 	return (
 		<button
 			tw="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-900 lg:hidden"
-			onClick={() => setOpenMobileSidebar(!openMobileSidebar)}
+			onClick={handleOpenMobileSidebar}
 		>
 			<span tw="sr-only">{openSidebarLabel}</span>
 			<MenuIcon tw="h-6 w-6" />
 		</button>
 	);
+};
+
+MobileSidebarButton.propTypes = {
+	handleOpenMobileSidebar: PropTypes.func
+};
+
+MobileSidebarButton.defaultProps = {
+	handleOpenMobileSidebar: null
 };
 
 export default MobileSidebarButton;
