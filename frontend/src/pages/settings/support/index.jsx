17086f76a762e26a1764e51455ff39660fff592f
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-11 00:25:02 +0800

    Update support page

--- frontend/src/pages/settings/support/index.jsx
+++ frontend/src/pages/settings/support/index.jsx
@@ -5,95 +5,81 @@ import * as React from "react";
 import Link from "next/link";
 
 // External
+import "twin.macro";
 import { NextSeo } from "next-seo";
 import { Scrollbars } from "react-custom-scrollbars-2";
 import { withResizeDetector } from "react-resize-detector";
-import loadable from "@loadable/component";
 import PropTypes from "prop-types";
-import tw from "twin.macro";
 
-// JSON
-import SupportLabel from "public/labels/pages/settings/support.json";
+// Enums
+import { ComponentReadyInterval, GlobalLabels, SiteLogoDark } from "@enums/GlobalValues";
+import { LoginLink, SitesLink } from "@enums/PageLinks";
+import { SupportFormLabels } from "@enums/SupportFormLabels";
 
 // Hooks
-import useUser from "src/hooks/useUser";
-
-// Layout
-import Layout from "src/components/Layout";
+import useUser from "@hooks/useUser";
 
 // Components
-import AppLogo from "src/components/logos/AppLogo";
-import MainSidebar from "src/components/sidebar/MainSidebar";
-import MobileSidebarButton from "src/components/buttons/MobileSidebarButton";
-import SiteFooter from "src/components/layouts/Footer";
-
-// Loadable
-const Breadcrumbs = loadable(() => import("src/components/breadcrumbs/Breadcrumbs"));
-const Loader = loadable(() => import("src/components/layouts/Loader"));
-const SupportForm = loadable(() => import("src/components/forms/SupportForm"));
+import AppLogo from "@components/logos/AppLogo";
+import Breadcrumbs from "@components/breadcrumbs";
+import Footer from "@components/layouts/Footer";
+import Layout from "@components/layouts";
+import Loader from "@components/loader";
+import MobileSidebarButton from "@components/buttons/MobileSidebarButton";
+import Sidebar from "@components/layouts/Sidebar";
+import SupportForm from "@components/forms/SupportForm";
 
 const Support = ({ width }) => {
 	const [componentReady, setComponentReady] = React.useState(false);
 	const [openMobileSidebar, setOpenMobileSidebar] = React.useState(false);
 
-	const appLogoAltText = "app-logo";
-	const homePageLink = "/sites/";
-	const pageTitle = SupportLabel[1].label;
+	const pageTitle = SupportFormLabels[1].label;
 
 	const { user } = useUser({
 		redirectIfFound: false,
-		redirectTo: "/login"
+		redirectTo: LoginLink
 	});
 
 	React.useEffect(() => {
-		user !== undefined
-			? (() => {
-					setTimeout(() => {
-						setComponentReady(true);
-					}, 500);
-			  })()
-			: null;
+		user ? setComponentReady(true) : setComponentReady(false);
 
-		return setComponentReady(false);
+		return { user };
 	}, [user]);
 
 	return (
 		<Layout user={componentReady ? user : null}>
-			<NextSeo title={pageTitle} />
+			<NextSeo title={componentReady ? pageTitle : null} />
 
 			<section tw="h-screen flex overflow-hidden bg-white">
-				<MainSidebar
-					width={width}
-					user={componentReady ? user : null}
-					openMobileSidebar={openMobileSidebar}
-					handleOpenMobileSidebar={() => setOpenMobileSidebar(!openMobileSidebar)}
-				/>
+				<Sidebar width={width} user={componentReady ? user : null} />
 
 				{componentReady ? (
 					<div tw="flex flex-col w-0 flex-1 overflow-hidden">
 						<div tw="relative flex-shrink-0 flex bg-white">
 							<div tw="border-b flex-shrink-0 flex">
 								<MobileSidebarButton
-									openMobileSidebar={openMobileSidebar}
-									setOpenMobileSidebar={setOpenMobileSidebar}
+									handleOpenMobileSidebar={() => setOpenMobileSidebar(!openMobileSidebar)}
 								/>
 							</div>
 
-							<Link href={homePageLink} passHref>
+							<Link href={SitesLink} passHref>
 								<a tw="p-1 block w-full cursor-pointer lg:hidden">
 									<AppLogo
 										tw="w-48 h-auto"
-										src="/images/logos/site-logo-dark.svg"
-										alt={appLogoAltText}
-										width={230}
-										height={40}
+										src={SiteLogoDark}
+										alt={GlobalLabels[0].label}
+										width={GlobalLabels[0].width}
+										height={GlobalLabels[0].height}
 									/>
 								</a>
 							</Link>
 						</div>
 
 						<Scrollbars universal>
-							<main tw="flex-1 relative z-0 max-w-screen-2xl mx-auto overflow-y-auto focus:outline-none" tabIndex="0">
+							<main
+								tw="flex-1 relative z-0 max-w-screen-2xl mx-auto overflow-y-auto focus:outline-none"
+								tabIndex="0"
+							>
 								<div tw="max-w-full p-4 sm:px-6 md:px-8">
 									<div tw="w-full py-6 mx-auto grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
 										<div tw="lg:col-span-2 xl:col-span-2 xl:pr-8 xl:border-r xl:border-gray-200">
@@ -101,18 +87,20 @@ const Support = ({ width }) => {
 												<Breadcrumbs isOther pageTitle={pageTitle} />
 
 												<div tw="pt-4 m-auto">
-													<h2 tw="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">{pageTitle}</h2>
+													<h2 tw="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+														{pageTitle}
+													</h2>
 												</div>
 											</div>
 
 											<div tw="space-y-12 divide-y divide-gray-200">
-												<SupportForm />
+												<SupportForm user={componentReady ? user : null} />
 											</div>
 										</div>
 									</div>
 
 									<div tw="static bottom-0 w-full mx-auto p-4 border-t border-gray-200 bg-white">
-										<SiteFooter />
+										<Footer />
 									</div>
 								</div>
 							</main>
@@ -128,6 +116,12 @@ const Support = ({ width }) => {
 	);
 };
 
-Support.propTypes = {};
+Support.propTypes = {
+	width: PropTypes.number
+};
+
+Support.defaultProps = {
+	width: null
+};
 
 export default withResizeDetector(Support);
