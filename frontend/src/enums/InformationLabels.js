d67712a3d9d3a7f6a3009d40ba8957b7439ab7bb
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-10 19:24:00 +0800

    Convert information labels as enums

---
+++ frontend/src/enums/InformationLabels.js
@@ -0,0 +1,68 @@
+export const InformationLabels = [
+	{
+		label: "Add a new site"
+	},
+	{
+		label: "Fill in site information"
+	},
+	{
+		label: "Verify site"
+	},
+	{
+		label: "Fill in site information",
+		description: "Add the name and url of your site to start the process."
+	},
+	{
+		label: "Site Name",
+		placeholder: "e.g. My Company Name"
+	},
+	{
+		label: "Site URL",
+		placeholder: "e.g. yourdomain.com"
+	},
+	{
+		label: "Proceed to Step 2"
+	},
+	{
+		label: "Required field"
+	},
+	{
+		label: "Enter a valid URL"
+	},
+	{
+		label: "Update Site Detail"
+	},
+	{
+		label: "Submitting..."
+	},
+	{
+		label: "Unfortunately, this site URL already exists. Please try again."
+	},
+	{
+		label: "An unexpected error occurred. Please try again."
+	},
+	{
+		label: "Step 1"
+	},
+	{
+		label: "Step 2"
+	},
+	{
+		label: "Close"
+	},
+	{
+		label: "Form Submission Failed"
+	},
+	{
+		label: "Dismiss"
+	},
+	{
+		label: "Site Addition Progress"
+	},
+	{
+		label: "Too short!"
+	},
+	{
+		label: "Too long!"
+	}
+];
