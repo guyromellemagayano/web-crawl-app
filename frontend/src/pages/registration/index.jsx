759207a18e1839683945643aed81d46e57a6bad1
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-11 00:19:30 +0800

    Fix registration page issues

--- frontend/src/pages/registration/index.jsx
+++ frontend/src/pages/registration/index.jsx
@@ -5,88 +5,65 @@ import * as React from "react";
 import Link from "next/link";
 
 // External
+import "twin.macro";
 import { NextSeo } from "next-seo";
 import { Scrollbars } from "react-custom-scrollbars-2";
-import PropTypes from "prop-types";
 import ReactHtmlParser from "react-html-parser";
-import tw from "twin.macro";
 
-// JSON
-import RegistrationLabel from "public/labels/pages/registration.json";
-
-// Layout
-import Layout from "src/components/Layout";
+// Enums
+import { LoginLink } from "@enums/PageLinks";
+import { RegistrationLabels } from "@enums/RegistrationLabels";
 
 // Components
-import SiteFooter from "src/components/layouts/Footer";
-import LogoLabel from "src/components/labels/LogoLabel";
-import RegistrationForm from "src/components/forms/RegistrationForm";
+import Layout from "@components/layouts";
+import LogoLabel from "@components/labels/LogoLabel";
+import RegistrationForm from "@components/forms/RegistrationForm";
 
 const Registration = () => {
-	const pageTitle = "Registration";
-
 	return (
 		<Layout>
-			<NextSeo title={pageTitle} />
+			<NextSeo title={RegistrationLabels[23].label} />
 
 			<div tw="bg-gray-50 overflow-auto h-screen">
 				<Scrollbars universal>
-					<div tw="flex flex-col justify-start mx-auto h-full xl:max-w-screen-xl">
-						<div tw="relative mt-8 sm:mt-16 md:mt-20 lg:mt-24">
+					<div tw="block mx-auto xl:max-w-screen-xl mb-8 sm:mb-16 md:mb-20 lg:mb-24">
+						<div tw="relative my-8 sm:my-16 md:my-20 lg:my-24">
 							<LogoLabel isSignUp />
 
 							<div tw="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-								<RegistrationForm
-									label={[
-										RegistrationLabel[0].label,
-										RegistrationLabel[1].label,
-										RegistrationLabel[10].label,
-										RegistrationLabel[11].label,
-										RegistrationLabel[12].label,
-										RegistrationLabel[13].label,
-										RegistrationLabel[14].label,
-										RegistrationLabel[15].label,
-										RegistrationLabel[2].label,
-										RegistrationLabel[3].label,
-										RegistrationLabel[5].label,
-										RegistrationLabel[6].label,
-										RegistrationLabel[7].label,
-										RegistrationLabel[8].label,
-										RegistrationLabel[9].label
-									]}
-								/>
+								<div tw="bg-white mt-8 py-8 px-4 shadow-xl rounded-lg sm:px-10">
+									<RegistrationForm />
+								</div>
 
 								<div tw="relative flex justify-center flex-wrap flex-row text-sm leading-5">
 									<span tw="text-center px-2 py-5 text-gray-500">
-										{ReactHtmlParser(RegistrationLabel[16].label)}
-										<Link href="/service-terms">
-											<a tw="font-medium text-indigo-600 cursor-pointer hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-												{RegistrationLabel[17].label}
-											</a>
-										</Link>
+										{ReactHtmlParser(RegistrationLabels[16].label)}
+										<a
+											href="/service-terms"
+											tw="font-medium text-indigo-600 cursor-pointer hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+										>
+											{RegistrationLabels[17].label}
+										</a>
 										&nbsp;and&nbsp;
-										<Link href="/privacy-policy">
-											<a tw="font-medium text-indigo-600 cursor-pointer hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-												{RegistrationLabel[18].label}
-											</a>
-										</Link>
+										<a
+											href="/privacy-policy"
+											tw="font-medium text-indigo-600 cursor-pointer hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+										>
+											{RegistrationLabels[18].label}
+										</a>
 									</span>
 								</div>
 
 								<div tw="relative flex justify-center flex-wrap flex-row text-sm leading-5">
 									<span tw="p-2 text-gray-600">
-										{ReactHtmlParser(RegistrationLabel[19].label)}
-										<Link href="/login">
+										{ReactHtmlParser(RegistrationLabels[19].label)}
+										<Link href={LoginLink}>
 											<a tw="font-medium text-indigo-600 cursor-pointer hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-												{RegistrationLabel[20].label}
+												{RegistrationLabels[20].label}
 											</a>
 										</Link>
 									</span>
 								</div>
-							</div>
-
-							<div tw="px-4 xl:px-10 xl:mt-16">
-								<SiteFooter />
 							</div>
 						</div>
 					</div>
@@ -95,7 +72,5 @@ const Registration = () => {
 		</Layout>
 	);
 };
-
-Registration.propTypes = {};
 
 export default Registration;
