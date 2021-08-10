6cd102e36f33b6f54af0dd64b7e58323c09059bd
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-11 00:16:07 +0800

    Add global values enum file

---
+++ frontend/src/enums/GlobalValues.js
@@ -0,0 +1,17 @@
+export const GlobalLabels = [
+	{
+		label: "app-logo",
+		width: 230,
+		height: 40
+	}
+];
+
+export const AlertLoadInterval = 3500;
+export const ComponentReadyInterval = 500;
+export const EndpointRefreshInterval = 7500;
+export const FormSubmissionInterval = 1000;
+export const LgScreenBreakpoint = 1024;
+export const MutateInterval = 2000;
+export const RevalidationInterval = 3000;
+export const SiteLogoDark = "/images/logos/site-logo-dark.svg";
+export const SiteLogoWhite = "/images/logos/site-logo-white.svg";
