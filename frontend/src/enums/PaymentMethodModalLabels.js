8d6a1f8a0a5639412a47626c6c12d650cea4c873
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-10 23:48:20 +0800

    Convert payment method modal labels as enums

---
+++ frontend/src/enums/PaymentMethodModalLabels.js
@@ -0,0 +1,7 @@
+export const PaymentMethodModalLabels = [
+	{
+		label: "Upgrade Plan",
+		description:
+			"User generated content in real-time will have multiple touchpoints for offshoring."
+	}
+];
