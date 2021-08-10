f63ca3296736f2009fe77a5bd85a69f2bd71777e
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-10 19:14:40 +0800

    Convert global settings labels as enums

---
+++ frontend/src/enums/GlobalSettingsLabels.js
@@ -0,0 +1,70 @@
+export const GlobalSettingsLabels = [
+	{
+		label: "Timestamp Settings",
+		description:
+			"User generated content in real-time will have multiple touchpoints for offshoring."
+	},
+	{
+		label: "Enable Local Time",
+		description: "Set all sites to use local times instead of UTC"
+	},
+	{
+		label: "Large Page Size Settings",
+		description:
+			"User generated content in real-time will have multiple touchpoints for offshoring."
+	},
+	{
+		label: "Large Page Size Threshold"
+	},
+	{
+		label: "Update"
+	},
+	{
+		label: "Cancel"
+	},
+	{
+		label: "Save"
+	},
+	{
+		label: "Form Submission Failed"
+	},
+	{
+		label: "Form Submission Success"
+	},
+	{
+		label: "Local time disabled globally."
+	},
+	{
+		label: "Local time enabled globally."
+	},
+	{
+		label: "An unexpected error occurred. Please try again."
+	},
+	{
+		label: "Required field"
+	},
+	{
+		label: "Large page size threshold updated successfully."
+	},
+	{
+		label: "Large page size threshold update failed. Please try again."
+	},
+	{
+		label: "Saving..."
+	},
+	{
+		label: "Update"
+	},
+	{
+		label: "Save Changes"
+	},
+	{
+		label: "Site information updated successfully"
+	},
+	{
+		label: "Site information update failed. Please try again"
+	},
+	{
+		label: "Global Settings"
+	}
+];
