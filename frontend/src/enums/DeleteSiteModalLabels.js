a95e00cfa7765f761486cc1828a72f4ac7f8424e
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-10 19:54:04 +0800

    Convert delete site modal labels as enums

---
+++ frontend/src/enums/DeleteSiteModalLabels.js
@@ -0,0 +1,31 @@
+export const DeleteSiteModalLabels = [
+	{
+		label: "Are you sure you want to delete this site?",
+		description:
+			"You will lose all this site's data permanently and cannot cancel this process any time."
+	},
+	{
+		label: "Request"
+	},
+	{
+		label: "Cancel"
+	},
+	{
+		label: "Request Failed",
+		description: "An unexpected error occurred. Please try again."
+	},
+	{
+		label: "Processing..."
+	},
+	{
+		label: "Close"
+	},
+	{
+		label: "Proceed"
+	},
+	{
+		label: "Request Success",
+		description:
+			"This site is now currently being deleted in our servers and might take a few minutes to complete. Redirecting you to <strong>Sites</strong> page..."
+	}
+];
