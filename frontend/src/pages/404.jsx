f7933dd7d57ff6394a19273592db06f004fd4ba7
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-11 00:27:48 +0800

    [WIP] Update 404 page

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,30 +1,33 @@
+// React
+import * as React from "react";
+
 // NextJS
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 // External
-import { NextSeo } from "next-seo";
-import PropTypes from "prop-types";
 import "twin.macro";
+import { NextSeo } from "next-seo";
 
-// JSON
-import Custom404Label from "public/labels/pages/custom-404.json";
+// Enums
+import { Custom404Labels } from "@enums/Custom404Labels";
 
 // Components
-import Layout from "src/components/Layout";
+import Layout from "@components/layouts";
 
 const Custom404 = () => {
-	const pageTitle = Custom404Label[0].label;
+	const pageTitle = Custom404Labels[0].label;
 
+	// TODO: Update UI of 404 page
 	return (
 		<Layout>
 			<NextSeo title={pageTitle} />
 
+			{/* TODO: Update UI of this page */}
 			<div tw="absolute inset-0 flex flex-col items-center justify-center w-full min-h-screen bg-gray-50">
 				<div tw="flex flex-col items-start justify-center max-w-4xl space-y-4">
-					<h1>{Custom404Label[0].label}</h1>
+					<h1>{Custom404Labels[0].label}</h1>
 					<Link href="/sites" passHref>
-						<a>{Custom404Label[1].label}</a>
+						<a>{Custom404Labels[1].label}</a>
 					</Link>
 				</div>
 			</div>
