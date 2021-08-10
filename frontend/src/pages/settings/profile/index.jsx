ef7fb8ea12d9b35a66f442d2e34270236cc44205
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-11 00:24:51 +0800

    Update profile settings page

--- frontend/src/pages/settings/profile/index.jsx
+++ frontend/src/pages/settings/profile/index.jsx
@@ -9,132 +9,129 @@ import "twin.macro";
 import { NextSeo } from "next-seo";
 import { Scrollbars } from "react-custom-scrollbars-2";
 import { withResizeDetector } from "react-resize-detector";
-import loadable from "@loadable/component";
 import PropTypes from "prop-types";
 
-// JSON
-import ProfileLabel from "public/labels/components/profile/Profile.json";
+// Enums
+import { ComponentReadyInterval, GlobalLabels, SiteLogoDark } from "@enums/GlobalValues";
+import { LoginLink, SitesLink } from "@enums/PageLinks";
+import { ProfileSettingsLabels } from "@enums/ProfileSettingsLabels";
 
 // Hooks
-import useUser from "src/hooks/useUser";
-
-// Layout
-import Layout from "src/components/Layout";
+import useUser from "@hooks/useUser";
 
 // Components
-import AppLogo from "src/components/logos/AppLogo";
-import MainSidebar from "src/components/sidebar/MainSidebar";
-import MobileSidebarButton from "src/components/buttons/MobileSidebarButton";
-import SiteFooter from "src/components/layouts/Footer";
-
-// Loadable
-const Breadcrumbs = loadable(() => import("src/components/breadcrumbs/Breadcrumbs"));
-const Loader = loadable(() => import("src/components/layouts/Loader"));
-const SettingsPassword = loadable(() => import("src/components/pages/settings/profile/Password"));
-const SettingsPersonal = loadable(() => import("src/components/pages/settings/profile/Personal"));
-const SettingsDeleteUserAccount = loadable(() => import("src/components/pages/settings/profile/DeleteUserAccount"));
+import AppLogo from "@components/logos/AppLogo";
+import Breadcrumbs from "@components/breadcrumbs";
+import Footer from "@components/layouts/Footer";
+import Layout from "@components/layouts";
+import MobileSidebarButton from "@components/buttons/MobileSidebarButton";
+import DeleteUserAccountSettings from "@components/settings/DeleteUserAccountSettings";
+import PasswordSettings from "@components/settings/PasswordSettings";
+import PersonalSettings from "@components/settings/PersonalSettings";
+import Sidebar from "@components/layouts/Sidebar";
 
 const Profile = ({ width }) => {
 	const [componentReady, setComponentReady] = React.useState(false);
 	const [openMobileSidebar, setOpenMobileSidebar] = React.useState(false);
 
-	const appLogoAltText = "app-logo";
-	const homePageLink = "/sites/";
-	const pageTitle = ProfileLabel[0].label;
-
 	const { user, mutateUser } = useUser({
 		redirectIfFound: false,
-		redirectTo: "/login"
+		redirectTo: LoginLink
 	});
 
 	React.useEffect(() => {
-		user ? setComponentReady(true) : setComponentReady(false);
+		user
+			? setTimeout(() => {
+					setComponentReady(true);
+			  }, ComponentReadyInterval)
+			: setComponentReady(false);
 
 		return { user };
 	}, [user]);
 
 	return (
 		<Layout user={componentReady ? user : null}>
-			<NextSeo title={componentReady ? pageTitle : null} />
+			<NextSeo title={componentReady ? ProfileSettingsLabels[0].label : null} />
 
 			<section tw="h-screen flex overflow-hidden bg-white">
-				<MainSidebar
-					width={width}
-					user={componentReady ? user : null}
-					openMobileSidebar={openMobileSidebar}
-					handleOpenMobileSidebar={() => setOpenMobileSidebar(!openMobileSidebar)}
-				/>
+				<Sidebar width={width} user={componentReady ? user : null} />
 
-				{componentReady ? (
-					<div tw="flex flex-col w-0 flex-1 overflow-hidden">
-						<div tw="relative flex-shrink-0 flex bg-white">
-							<div tw="border-b flex-shrink-0 flex">
-								<MobileSidebarButton
-									openMobileSidebar={openMobileSidebar}
-									setOpenMobileSidebar={setOpenMobileSidebar}
-								/>
-							</div>
-
-							<Link href={homePageLink} passHref>
-								<a tw="p-1 block w-full cursor-pointer lg:hidden">
-									<AppLogo
-										tw="w-48 h-auto"
-										src="/images/logos/site-logo-dark.svg"
-										alt={appLogoAltText}
-										width={230}
-										height={40}
-									/>
-								</a>
-							</Link>
+				<div tw="flex flex-col w-0 flex-1 overflow-hidden">
+					<div tw="relative flex-shrink-0 flex bg-white">
+						<div tw="border-b flex-shrink-0 flex">
+							<MobileSidebarButton
+								handleOpenMobileSidebar={() => setOpenMobileSidebar(!openMobileSidebar)}
+							/>
 						</div>
 
-						<Scrollbars universal>
-							<main tw="flex-1 relative z-0 max-w-screen-2xl mx-auto overflow-y-auto focus:outline-none" tabIndex="0">
-								<div tw="max-w-full p-4 sm:px-6 md:px-8">
-									<div tw="w-full py-6 mx-auto grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
-										<div tw="lg:col-span-2 xl:col-span-2 xl:pr-8 xl:border-r xl:border-gray-200">
-											<div tw="max-w-full p-4">
-												<Breadcrumbs isOther pageTitle={pageTitle} />
+						<Link href={SitesLink} passHref>
+							<a tw="p-1 block w-full cursor-pointer lg:hidden">
+								<AppLogo
+									tw="w-48 h-auto"
+									src={SiteLogoDark}
+									alt={GlobalLabels[0].label}
+									width={GlobalLabels[0].width}
+									height={GlobalLabels[0].height}
+								/>
+							</a>
+						</Link>
+					</div>
 
-												<div tw="pt-4 m-auto">
-													<h2 tw="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">{pageTitle}</h2>
-												</div>
-											</div>
+					<Scrollbars universal>
+						<main
+							tw="flex-1 relative z-0 max-w-screen-2xl mx-auto overflow-y-auto focus:outline-none"
+							tabIndex="0"
+						>
+							<div tw="max-w-full p-4 sm:px-6 md:px-8">
+								<div tw="w-full py-6 mx-auto grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
+									<div tw="lg:col-span-2 xl:col-span-2 xl:pr-8 xl:border-r xl:border-gray-200">
+										<div tw="max-w-full p-4">
+											<Breadcrumbs isOther pageTitle={ProfileSettingsLabels[0].label} />
 
-											<div tw="space-y-12 divide-y divide-gray-200">
-												<SettingsPersonal
-													user={componentReady ? user : null}
-													mutateUser={componentReady ? mutateUser : null}
-												/>
-												<SettingsPassword
-													user={componentReady ? user : null}
-													mutateUser={componentReady ? mutateUser : null}
-												/>
-												<SettingsDeleteUserAccount
-													user={componentReady ? user : null}
-													mutateUser={componentReady ? mutateUser : null}
-												/>
+											<div tw="pt-4 m-auto">
+												<h2 tw="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+													{ProfileSettingsLabels[0].label}
+												</h2>
 											</div>
 										</div>
-									</div>
 
-									<div tw="static bottom-0 w-full mx-auto p-4 border-t border-gray-200 bg-white">
-										<SiteFooter />
+										<div tw="space-y-12 divide-y divide-gray-200">
+											<PersonalSettings
+												componentReady={componentReady}
+												user={componentReady ? user : null}
+												mutateUser={componentReady ? mutateUser : null}
+											/>
+											<PasswordSettings
+												componentReady={componentReady}
+												mutateUser={componentReady ? mutateUser : null}
+											/>
+											<DeleteUserAccountSettings
+												componentReady={componentReady}
+												user={componentReady ? user : null}
+												mutateUser={componentReady ? mutateUser : null}
+											/>
+										</div>
 									</div>
 								</div>
-							</main>
-						</Scrollbars>
-					</div>
-				) : (
-					<div tw="mx-auto">
-						<Loader />
-					</div>
-				)}
+
+								<div tw="static bottom-0 w-full mx-auto p-4 border-t border-gray-200 bg-white">
+									<Footer />
+								</div>
+							</div>
+						</main>
+					</Scrollbars>
+				</div>
 			</section>
 		</Layout>
 	);
 };
 
-Profile.propTypes = {};
+Profile.propTypes = {
+	width: PropTypes.number
+};
+
+Profile.defaultProps = {
+	width: null
+};
 
 export default withResizeDetector(Profile);
