7eb3979cac955ad4ffdb8d365f5fe71d68974818
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-10 19:51:35 +0800

    Add PropTypes to logo components, update props

--- frontend/src/components/logos/AppLogo.jsx
+++ frontend/src/components/logos/AppLogo.jsx
@@ -6,9 +6,9 @@ import "twin.macro";
 import PropTypes from "prop-types";
 
 // Components
-import AppImage from "src/components/images/AppImage";
+import AppImage from "@components/images/AppImage";
 
-const AppLogo = ({ src, alt, width, height, "data-tw": dataTw }) => {
+const AppLogo = ({ alt, height, src, width, "data-tw": dataTw }) => {
 	return (
 		<div tw="flex w-auto">
 			<AppImage src={src} alt={alt} width={width} height={height} className={dataTw} />
@@ -16,6 +16,20 @@ const AppLogo = ({ src, alt, width, height, "data-tw": dataTw }) => {
 	);
 };
 
-AppLogo.propTypes = {};
+AppLogo.propTypes = {
+	alt: PropTypes.string,
+	height: PropTypes.number,
+	src: PropTypes.string,
+	width: PropTypes.number,
+	dataTw: PropTypes.string
+};
+
+AppLogo.defaultProps = {
+	alt: null,
+	height: null,
+	src: null,
+	width: null,
+	dataTw: null
+};
 
 export default AppLogo;
