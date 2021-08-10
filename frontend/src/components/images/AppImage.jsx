a816677f7ea2dcd633f3f755313f2c26ae0a97fc
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-10 19:44:21 +0800

    Add PropTypes to image components, update props

--- frontend/src/components/images/AppImage.jsx
+++ frontend/src/components/images/AppImage.jsx
@@ -7,14 +7,28 @@ import Image from "next/image";
 // External
 import PropTypes from "prop-types";
 
-const AppImage = (props) => {
+const AppImage = ({ alt, className, height, src, width }) => {
 	return (
-		<span className={props.className}>
-			<Image src={props.src} alt={props.alt} width={props.width} height={props.height} />
+		<span className={className}>
+			<Image src={src} alt={alt} width={width} height={height} />
 		</span>
 	);
 };
 
-AppImage.propTypes = {};
+AppImage.propTypes = {
+	alt: PropTypes.string,
+	className: PropTypes.string,
+	height: PropTypes.number,
+	src: PropTypes.string,
+	width: PropTypes.number
+};
+
+AppImage.defaultProps = {
+	alt: null,
+	className: null,
+	height: null,
+	src: null,
+	width: null
+};
 
 export default AppImage;
