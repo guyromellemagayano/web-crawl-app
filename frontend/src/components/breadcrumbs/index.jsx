9aae00f7d1c727fb1730f23d282b4907492d3381
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-10 19:33:27 +0800

    Add PropTypes to breadcrumbs component, update props

---
+++ frontend/src/components/breadcrumbs/index.jsx
@@ -0,0 +1,123 @@
+// React
+import * as React from "react";
+
+// NextJS
+import Link from "next/link";
+
+// External
+import "twin.macro";
+import { ChevronRightIcon, HomeIcon } from "@heroicons/react/solid";
+import PropTypes from "prop-types";
+
+// Enums
+import { BreadcrumbsLabels } from "@enums/BreadcrumbsLabels";
+import { SitesLink } from "@enums/PageLinks";
+
+const Breadcrumbs = ({
+	siteId,
+	isOther,
+	dataId,
+	pageDetailTitle,
+	isPages,
+	isImages,
+	isSeo,
+	pageTitle,
+	isSites
+}) => {
+	const homeLabel = BreadcrumbsLabels[0].label;
+	const siteIdOverviewPageLink = `/site/${siteId}/overview/`;
+
+	return (
+		<nav tw="flex pt-4 pb-8" aria-label="Breadcrumb">
+			<ol tw="flex items-center space-x-4">
+				<li>
+					<div>
+						<Link href={isOther && siteId == null ? SitesLink : siteIdOverviewPageLink} passHref>
+							<a tw="text-gray-400 hover:text-gray-500">
+								<HomeIcon tw="flex-shrink-0 h-5 w-5" />
+								<span tw="sr-only">{homeLabel}</span>
+							</a>
+						</Link>
+					</div>
+				</li>
+				<li>
+					<div tw="flex items-center">
+						<ChevronRightIcon tw="flex-shrink-0 h-5 w-5 text-gray-400" />
+
+						{dataId !== null && pageDetailTitle !== null ? (
+							<Link
+								href={`/site/${siteId}/${
+									isLinks ? "links" : isPages ? "pages" : isImages ? "images" : isSeo ? "seo" : null
+								}`}
+								passHref
+							>
+								<a
+									aria-current="page"
+									className="truncate-breadcrumbs"
+									tw="cursor-pointer ml-4 text-sm text-gray-700"
+								>
+									{pageTitle}
+								</a>
+							</Link>
+						) : isSites && pageDetailTitle !== null ? (
+							<Link href="/sites/" passHref>
+								<a
+									aria-current="page"
+									className="truncate-breadcrumbs"
+									tw="cursor-pointer ml-4 text-sm text-gray-700"
+								>
+									{pageTitle}
+								</a>
+							</Link>
+						) : (
+							<p aria-current="page" tw="cursor-default ml-4 text-sm font-medium text-gray-700">
+								{pageTitle}
+							</p>
+						)}
+					</div>
+				</li>
+
+				{(dataId !== null && pageDetailTitle !== null) || (isSites && pageDetailTitle !== null) ? (
+					<li>
+						<div tw="flex items-center">
+							<ChevronRightIcon tw="flex-shrink-0 h-5 w-5 text-gray-400" />
+							<p
+								aria-current="page"
+								className="truncate-breadcrumbs"
+								tw="cursor-default ml-4 text-sm font-medium text-gray-700"
+							>
+								{pageDetailTitle}
+							</p>
+						</div>
+					</li>
+				) : null}
+			</ol>
+		</nav>
+	);
+};
+
+Breadcrumbs.propTypes = {
+	dataId: PropTypes.number,
+	isImages: PropTypes.bool,
+	isOther: PropTypes.bool,
+	isPages: PropTypes.bool,
+	isSeo: PropTypes.bool,
+	isSites: PropTypes.bool,
+	pageDetailTitle: PropTypes.string,
+	pageTitle: PropTypes.string,
+	siteId: PropTypes.number
+};
+
+Breadcrumbs.defaultProps = {
+	dataId: null,
+	isImages: false,
+	isOther: false,
+	isPages: false,
+	isSeo: false,
+	isSites: false,
+	pageDetailTitle: null,
+	pageTitle: null,
+	siteId: null
+};
+
+export default Breadcrumbs;
