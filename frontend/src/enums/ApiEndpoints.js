094574bdc7aee97bdd7ccd157b3768e3e40b3210
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-11 00:15:46 +0800

    Add api endpoints enum file

---
+++ frontend/src/enums/ApiEndpoints.js
@@ -0,0 +1,17 @@
+export const ConfirmResetPasswordApiEndpoint = `/api/auth/password/reset/confirm/`;
+export const ContactApiEndpoint = `/api/support/contact/`;
+export const CurrentPaymentMethodApiEndpoint = `/api/stripe/payment-method/`;
+export const CurrentSubscriptionApiEndpoint = `/api/stripe/subscription/current/`;
+export const GoogleLoginApiEndpoint = `/auth/google/login/`;
+export const LoginApiEndpoint = `/api/auth/login/`;
+export const LogoutApiEndpoint = `/api/auth/logout/`;
+export const PasswordChangeApiEndpoint = `/api/auth/password/change/`;
+export const RegistrationApiEndpoint = `/api/auth/registration/`;
+export const ResetPasswordApiEndpoint = `/api/auth/password/reset/`;
+export const SiteApiEndpoint = `/api/site/`;
+export const UserApiEndpoint = `/api/auth/user/`;
+export const StripePromiseApiEndpoint = `/api/stripe/config/`;
+export const PaymentMethodApiEndpoint = `/api/stripe/payment-method/`;
+export const DefaultPaymentMethodApiEndpoint = `/api/stripe/payment-method/default/`;
+export const SubscriptionsApiEndpoint = `/api/stripe/subscription/`;
+export const DefaultSubscriptionApiEndpoint = `/api/stripe/subscription/current/`;
