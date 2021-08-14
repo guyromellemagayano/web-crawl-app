42f098f4b8cbc96ea1cd81681193dc8cee2e836d
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-15 02:43:14 +0800

    Add signup enum files

---
+++ frontend/src/enums/SignupLabels.js
@@ -0,0 +1,47 @@
+export const SignupLabels = [
+	{
+		label: "Required field"
+	},
+	{
+		label: "Too short!"
+	},
+	{
+		label: "Too long!"
+	},
+	{
+		label: "Both passwords need to be the same"
+	},
+	{
+		label: "Password details sent successfully. Redirecting you to Sites Dashboard"
+	},
+	{
+		label: "Password details sent failed. Please try again"
+	},
+	{
+		label: "Enter Password"
+	},
+	{
+		label: "Confirm Password"
+	},
+	{
+		label: "Submit Form"
+	},
+	{
+		label: "Submitting..."
+	},
+	{
+		label: "Too short!"
+	},
+	{
+		label: "Too long!"
+	},
+	{
+		label: "Already have an account?&nbsp;"
+	},
+	{
+		label: "Log In"
+	},
+	{
+		label: "Sign Up Form"
+	}
+];
