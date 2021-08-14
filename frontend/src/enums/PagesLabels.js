008a10da16ab08838cfdcfab67852a43f55ac919
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-14 22:07:10 +0800

    Add pages enum file

---
+++ frontend/src/enums/PagesLabels.js
@@ -0,0 +1,68 @@
+export const PagesLabels = [
+	{
+		label: "Back to Overview"
+	},
+	{
+		label: "Pages"
+	},
+	{
+		label: "pages found"
+	},
+	{
+		label: "No pages found"
+	},
+	{
+		label: "Start Site Recrawl"
+	},
+	{
+		label: "An unexpected error occurred. Please try again."
+	},
+	{
+		label: "page found"
+	},
+	{
+		label: "Created at"
+	},
+	{
+		label: "Page Size"
+	},
+	{
+		label: "Total Size of Images"
+	},
+	{
+		label: "Total Size of Scripts"
+	},
+	{
+		label: "Total Number of Working Images"
+	},
+	{
+		label: "Total Number of Non-Working Images"
+	},
+	{
+		label: "TLS Status (Page)"
+	},
+	{
+		label: "Broken Links"
+	},
+	{
+		label: "TLS Status (Resources)"
+	},
+	{
+		label: "Loading..."
+	},
+	{
+		label: "Page Detail"
+	},
+	{
+		label: "Total Size of Stylesheets"
+	},
+	{
+		label: "Total Number of Non-Secured Images"
+	},
+	{
+		label: "Total Number of Non-Secured Scripts"
+	},
+	{
+		label: "Total Number of Non-Secured Stylesheets"
+	}
+];
