fca64395a2334f5661f8cba9a536428ea8ac11fc
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-14 22:13:06 +0800

    Add PropTypes to layout components, update props

--- frontend/src/components/layouts/ComingSoon.jsx
+++ frontend/src/components/layouts/ComingSoon.jsx
@@ -6,44 +6,47 @@ import Link from "next/link";
 
 // External
 import "twin.macro";
-import loadable from "@loadable/component";
+import PropTypes from "prop-types";
 
-// JSON
-import ComingSoonLabel from "public/labels/components/layout/ComingSoon.json";
+// Enums
+import { ComingSoonLabels } from "@enums/ComingSoonLabels";
+import { GlobalLabels, SiteLogoDark } from "@enums/GlobalValues";
+import { SitesLink } from "@enums/PageLinks";
 
 // Components
-import AppLogo from "src/components/logos/AppLogo";
+import AppLogo from "@components/logos/AppLogo";
+import Breadcrumbs from "@components/breadcrumbs";
+import Footer from "@components/layouts/Footer";
 import MobileSidebarButton from "src/components/buttons/MobileSidebarButton";
-import SiteFooter from "src/components/layouts/Footer";
-
-// Loadable
-const Breadcrumbs = loadable(() => import("src/components/breadcrumbs/Breadcrumbs"));
 
 const ComingSoon = ({ pageTitle, openMobileSidebar, setOpenMobileSidebar }) => {
-	const appLogoAltText = "app-logo";
-	const homePageLink = "/sites/";
-
 	return (
 		<div tw="flex flex-col w-0 flex-1 overflow-hidden">
 			<div tw="relative flex-shrink-0 flex bg-white">
 				<div tw="border-b flex-shrink-0 flex">
-					<MobileSidebarButton openMobileSidebar={openMobileSidebar} setOpenMobileSidebar={setOpenMobileSidebar} />
+					<MobileSidebarButton
+						openSidebar={openMobileSidebar}
+						setOpenSidebar={setOpenMobileSidebar}
+					/>
 				</div>
 
-				<Link href={homePageLink} passHref>
+				<Link href={SitesLink} passHref>
 					<a tw="p-1 block w-full cursor-pointer lg:hidden">
 						<AppLogo
 							tw="w-48 h-auto"
-							src="/images/logos/site-logo-dark.svg"
-							alt={appLogoAltText}
-							width={230}
-							height={40}
+							src={SiteLogoDark}
+							alt={GlobalLabels[0].label}
+							width={GlobalLabels[0].width}
+							height={GlobalLabels[0].height}
 						/>
 					</a>
 				</Link>
 			</div>
 
-			<main tw="flex-1 relative z-0 max-w-screen-2xl mx-auto overflow-y-auto focus:outline-none" tabIndex="0">
+			<main
+				tw="flex-1 relative z-0 max-w-screen-2xl mx-auto overflow-y-auto focus:outline-none"
+				tabIndex="0"
+			>
 				<div tw="max-w-full p-4 sm:px-6 md:px-8">
 					<div tw="w-full py-6 mx-auto grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
 						<div tw="max-w-full p-4">
@@ -51,7 +54,7 @@ const ComingSoon = ({ pageTitle, openMobileSidebar, setOpenMobileSidebar }) => {
 
 							<div tw="pt-4 m-auto">
 								<h2 tw="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-									{ComingSoonLabel[0].label}
+									{ComingSoonLabels[0].label}
 								</h2>
 							</div>
 						</div>
@@ -60,7 +63,7 @@ const ComingSoon = ({ pageTitle, openMobileSidebar, setOpenMobileSidebar }) => {
 					</div>
 
 					<div tw="static bottom-0 w-full mx-auto p-4 border-t border-gray-200 bg-white">
-						<SiteFooter />
+						<Footer />
 					</div>
 				</div>
 			</main>
@@ -68,6 +71,16 @@ const ComingSoon = ({ pageTitle, openMobileSidebar, setOpenMobileSidebar }) => {
 	);
 };
 
-ComingSoon.propTypes = {};
+ComingSoon.propTypes = {
+	openMobileSidebar: PropTypes.bool,
+	pageTitle: PropTypes.string,
+	setOpenMobileSidebar: PropTypes.func
+};
+
+ComingSoon.defaultProps = {
+	openMobileSidebar: false,
+	pageTitle: null,
+	setOpenMobileSidebar: null
+};
 
 export default ComingSoon;
--- frontend/src/components/layouts/Sidebar.jsx
+++ frontend/src/components/layouts/Sidebar.jsx
@@ -16,7 +16,6 @@ import { LgScreenBreakpoint, EndpointRefreshInterval } from "@enums/GlobalValues
 import { SidebarMenuLabels } from "@enums/SidebarMenuLabels";
 
 // Hooks
-import { useComponentVisible } from "@hooks/useComponentVisible";
 import { useSite } from "@hooks/useSite";
 
 // Components
@@ -25,10 +24,8 @@ import ProfileMenu from "@components/menus/ProfileMenu";
 import SettingsMenu from "@components/menus/SettingsMenu";
 import SiteMenu from "@components/menus/SiteMenu";
 
-const Sidebar = ({ width, user }) => {
+const Sidebar = React.forwardRef(({ user, openSidebar, setOpenSidebar }, ref) => {
 	const [selectedMenu, setSelectedMenu] = React.useState(null);
-
-	const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(false);
 
 	const router = useRouter();
 
@@ -42,7 +39,7 @@ const Sidebar = ({ width, user }) => {
 	React.useEffect(() => {
 		switch (true) {
 			case router.pathname.includes("/site/"):
-				setSelectedMenu(<SiteMenu site={site} />);
+				setSelectedMenu(<SiteMenu site={site} siteId={parseInt(router?.query?.siteId)} />);
 				break;
 			case router.pathname.includes("/settings/"):
 				setSelectedMenu(<SettingsMenu user={user} site={site} />);
@@ -55,75 +52,79 @@ const Sidebar = ({ width, user }) => {
 		return selectedMenu;
 	}, [router, site, user]);
 
-	return width < LgScreenBreakpoint ? (
-		<Transition show={isComponentVisible}>
-			<div tw="fixed inset-0 flex z-40 lg:hidden" role="dialog" aria-modal="true">
-				<Transition.Child
-					enter="transition-opacity ease-linear duration-300"
-					enterFrom="opacity-0"
-					enterTo="opacity-100"
-					leave="transition-opacity ease-linear duration-300"
-					leaveFrom="opacity-100"
-					leaveTo="opacity-0"
-				>
-					<div tw="fixed inset-0 bg-gray-600 bg-opacity-75" aria-hidden="true"></div>
-				</Transition.Child>
+	return (
+		<>
+			<Transition show={openSidebar} tw="flex lg:hidden flex-shrink-0">
+				<div tw="fixed inset-0 flex z-40 lg:hidden" role="dialog" aria-modal="true">
+					<Transition.Child
+						enter="transition-opacity ease-linear duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="transition-opacity ease-linear duration-300"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<div tw="fixed inset-0 bg-gray-600 bg-opacity-75" aria-hidden="true"></div>
+					</Transition.Child>
 
-				<Transition.Child
-					enter="transition ease-in-out duration-300 transform"
-					enterFrom="-translate-x-full"
-					enterTo="translate-x-0"
-					leave="transition ease-in-out duration-300 transform"
-					leaveFrom="translate-x-0"
-					leaveTo="-translate-x-full"
-				>
-					<div className="bg-gray-1000" tw="relative flex-1 flex flex-col w-64">
-						<Transition.Child
-							enter="ease-in-out duration-300"
-							enterFrom="opacity-0"
-							enterTo="opacity-100"
-							leave="ease-in-out duration-300"
-							leaveFrom="opacity-100"
-							leaveTo="opacity-0"
-						>
-							<div tw="absolute top-0 right-0 -mr-12 pt-2">
-								<button
-									tw="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-									onClick={() => setIsComponentVisible(!isComponentVisible)}
-								>
-									<span tw="sr-only">{SidebarMenuLabels[0].label}</span>
-									<XIcon tw="h-6 w-6 text-white" />
-								</button>
-							</div>
+					<Transition.Child
+						enter="transition ease-in-out duration-300 transform"
+						enterFrom="-translate-x-full"
+						enterTo="translate-x-0"
+						leave="transition ease-in-out duration-300 transform"
+						leaveFrom="translate-x-0"
+						leaveTo="-translate-x-full"
+					>
+						<div ref={ref} className="bg-gray-1000" tw="relative flex-1 flex flex-col w-64">
+							<Transition.Child
+								enter="ease-in-out duration-300"
+								enterFrom="opacity-0"
+								enterTo="opacity-100"
+								leave="ease-in-out duration-300"
+								leaveFrom="opacity-100"
+								leaveTo="opacity-0"
+							>
+								<div tw="absolute top-0 right-0 -mr-12 pt-2">
+									<button
+										tw="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+										onClick={() => setOpenSidebar(!openSidebar)}
+									>
+										<span tw="sr-only">{SidebarMenuLabels[0].label}</span>
+										<XIcon tw="h-6 w-6 text-white" />
+									</button>
+								</div>
 
-							<div ref={ref} tw="flex flex-col w-64 h-screen">
-								<div tw="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto">{selectedMenu}</div>
+								<div tw="flex flex-col w-64 h-screen">
+									<div tw="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto">{selectedMenu}</div>
 
-								<ProfileMenu user={user} />
-							</div>
-						</Transition.Child>
-					</div>
-				</Transition.Child>
-			</div>
-		</Transition>
-	) : (
-		<aside className="bg-gray-1000" tw="hidden lg:flex lg:flex-shrink-0">
-			<div tw="flex flex-col w-64">
-				<div tw="h-0 flex-1 overflow-y-auto">{selectedMenu}</div>
+									<ProfileMenu user={user} />
+								</div>
+							</Transition.Child>
+						</div>
+					</Transition.Child>
+				</div>
+			</Transition>
 
-				<ProfileMenu user={user} />
-			</div>
-		</aside>
+			<aside className="bg-gray-1000" tw="hidden lg:flex lg:flex-shrink-0">
+				<div tw="flex flex-col w-64">
+					<div tw="h-0 flex-1 overflow-y-auto">{selectedMenu}</div>
+
+					<ProfileMenu user={user} />
+				</div>
+			</aside>
+		</>
 	);
-};
+});
 
 Sidebar.propTypes = {
-	user: PropTypes.object,
-	width: PropTypes.number
+	openSidebar: PropTypes.bool,
+	setOpenSidebar: PropTypes.func,
+	user: PropTypes.object
 };
 
 Sidebar.defaultProps = {
-	width: null,
+	openSidebar: null,
+	setOpenSidebar: null,
 	user: null
 };
 
