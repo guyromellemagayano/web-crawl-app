a5e56c5546c98394bd873759c0b9f0a8a05b5a32
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-14 22:22:39 +0800

    Add PropTypes to status components, update props

--- frontend/src/components/status/SiteInProgressStatus.jsx
+++ frontend/src/components/status/SiteInProgressStatus.jsx
@@ -1,14 +1,23 @@
 // External
 import "twin.macro";
 import { DotsCircleHorizontalIcon } from "@heroicons/react/outline";
+import PropTypes from "prop-types";
 
-const SiteInProgressStatus = (props) => {
+const SiteInProgressStatus = ({ text }) => {
 	return (
 		<span tw="inline-flex items-center text-sm leading-5 font-semibold rounded-full text-gray-400">
 			<DotsCircleHorizontalIcon tw="w-6 h-6 mr-2" />
-			{props.text}
+			{text}
 		</span>
 	);
+};
+
+SiteInProgressStatus.propTypes = {
+	text: PropTypes.string
+};
+
+SiteInProgressStatus.defaultProps = {
+	text: null
 };
 
 export default SiteInProgressStatus;
--- frontend/src/components/status/SiteWarningStatus.jsx
+++ frontend/src/components/status/SiteWarningStatus.jsx
@@ -1,14 +1,23 @@
 // External
 import "twin.macro";
 import { ExclamationIcon } from "@heroicons/react/outline";
+import PropTypes from "prop-types";
 
-const SiteWarningStatus = (props) => {
+const SiteWarningStatus = ({ text }) => {
 	return (
 		<span tw="inline-flex items-center text-sm leading-5 font-semibold rounded-full text-yellow-800">
 			<ExclamationIcon tw="w-6 h-6 mr-2" />
-			{props.text}
+			{text}
 		</span>
 	);
+};
+
+SiteWarningStatus.propTypes = {
+	text: PropTypes.string
+};
+
+SiteWarningStatus.defaultProps = {
+	text: null
 };
 
 export default SiteWarningStatus;
