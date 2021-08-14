abb3a54968ce2352b00dd61020d5ab3a05ad7c61
Guy Romelle Magayano <guy@epicdesignlabs.com>
2021-08-14 22:21:22 +0800

    Add delete site enum file

---
+++ frontend/src/enums/DeleteSiteLabels.js
@@ -0,0 +1,11 @@
+export const DeleteSiteLabels = [
+	{
+		label: "Delete Site"
+	},
+	{
+		label: "User generated content in real-time will have multiple touchpoints for offshoring."
+	},
+	{
+		label: "Delete"
+	}
+];
