77a84ae41da2abecb36448a70d89b70dc62b09e5
matematik7 <domen.ipavec@gmail.com>
2021-08-18 09:56:25 +0000

    Better fix

--- backend/payments/models/stripe_customer.py
+++ backend/payments/models/stripe_customer.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.db import models
 
 
 class StripeCustomer(models.Model):
-    team = models.OneToOneField("teams.Team", on_delete=models.CASCADE)
+    team = models.OneToOneField("teams.Team", on_delete=models.CASCADE, related_name="stripe_customer")
 
     customer_id = models.CharField(max_length=63)
