7c14f3f95d5b22bff192b11b0a3ee4fc35d8c504
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-09-25 23:02:47 +0800

    Bump packages, setup next bundle analyzer

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,12 @@
 const { withSentryConfig } = require("@sentry/nextjs");
+const withPlugins = require("next-compose-plugins");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+	enabled: process.env.ANALYZE === "true"
+});
 
 const SENTRY_URL = process.env.SENTRY_URL || process.env.NEXT_PUBLIC_SENTRY_URL;
 const SENTRY_ORG = process.env.SENTRY_ORG || process.env.NEXT_PUBLIC_SENTRY_ORG;
-const SENTRY_AUTH_TOKEN =
-	process.env.SENTRY_AUTH_TOKEN || process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN;
+const SENTRY_AUTH_TOKEN = process.env.SENTRY_AUTH_TOKEN || process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN;
 const SENTRY_PROJECT = process.env.SENTRY_PROJECT || process.env.NEXT_PUBLIC_SENTRY_PROJECT;
 
 const moduleExports = {
@@ -27,9 +30,8 @@ const SentryWebpackPluginOptions = {
 	include: ".",
 	url: SENTRY_URL || "https://sentry.io/organizations/epic-design-labs/",
 	org: SENTRY_ORG || "epic-design-labs",
-	authToken:
-		SENTRY_AUTH_TOKEN || "21024702c44e4bf3a4ac704e27d82a5e7cb2be29340046dc8d9c6d0b06a92ff1",
+	authToken: SENTRY_AUTH_TOKEN || "21024702c44e4bf3a4ac704e27d82a5e7cb2be29340046dc8d9c6d0b06a92ff1",
 	project: SENTRY_PROJECT || "sitecrawler-frontend"
 };
 
-module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
+module.exports = withPlugins([withBundleAnalyzer({}), withSentryConfig(moduleExports, SentryWebpackPluginOptions)]);
