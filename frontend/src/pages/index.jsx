70467c5ef596f22354996edabba93d6a7b008ce0
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-10-23 00:04:57 +0800

    Update redirection feature of home page with SSR and Sentry error reporting

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,22 +1,76 @@
+import { server } from "@configs/envSetup";
+import { UserApiEndpoint } from "@enums/ApiEndpoints";
+import { EndpointRefreshInterval } from "@enums/GlobalValues";
+import { LoginLink, SitesLink } from "@enums/PageLinks";
+import useFetcher from "@hooks/useFetcher";
+import * as Sentry from "@sentry/nextjs";
+import { sleep } from "@utils/sleep";
+import axios from "axios";
+import Cookies from "js-cookie";
 import { useRouter } from "next/router";
+import PropTypes from "prop-types";
 import * as React from "react";
-import useUser from "src/hooks/useUser";
+import useSWR, { SWRConfig } from "swr";
 
-const Home = () => {
+const AuthLayout = ({ fallback }) => {
+	const { data: user } = useSWR(UserApiEndpoint, useFetcher, { fallbackData: fallback });
+
 	const router = useRouter();
 
-	const { user } = useUser({
-		redirectIfFound: false,
-		redirectTo: "/login"
-	});
-
 	React.useEffect(() => {
-		user !== undefined ? router.push("/sites") : router.push("/login");
-	}, []);
+		user.hasOwnProperty("user") && user.user == null ? router.push(LoginLink) : router.push(SitesLink);
+	}, [user]);
 
 	return null;
 };
 
-Home.propTypes = {};
+const Home = ({ fallback }) => {
+	return (
+		<SWRConfig value={{ fallback }}>
+			<AuthLayout fallback={fallback} />
+		</SWRConfig>
+	);
+};
+
+Home.propTypes = {
+	user: PropTypes.any
+};
+
+export async function getServerSideProps() {
+	axios.defaults.headers.post["X-CSRFToken"] = Cookies.get("csrftoken");
+
+	const user = await axios
+		.get(`${server + UserApiEndpoint}`, {
+			headers: {
+				"Accept": "application/json",
+				"Content-Type": "application/json"
+			}
+		})
+		.then(sleep(EndpointRefreshInterval))
+		.then((response) => response)
+		.catch((error) => {
+			error.response
+				? (() => {
+						Sentry.captureException(error.response.data);
+						Sentry.captureException(error.response.status);
+						Sentry.captureException(error.response.headers);
+				  })()
+				: error.request
+				? (() => {
+						Sentry.captureException(error.request);
+				  })()
+				: Sentry.captureException(error.message);
+
+			Sentry.captureException(error.config);
+		});
+
+	return {
+		props: {
+			fallback: {
+				user: user || null
+			}
+		}
+	};
+}
 
 export default Home;
