cbc6a502fd98af56335f36a671ac744cb16d1344
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-10-26 17:45:12 +0800

    Refactor useFetcher React hook

--- frontend/src/hooks/useFetcher.js
+++ frontend/src/hooks/useFetcher.js
@@ -1,38 +1,12 @@
-// External
-import axios from "axios";
-import Cookies from "js-cookie";
-
-// Enums
 import { ComponentReadyInterval } from "@enums/GlobalValues";
+import AxiosApiInstance from "@helpers/axios";
+import { sleep } from "@utils/sleep";
 
 const useFetcher = async (...args) => {
-	const sleep = (ms) => {
-		return new Promise((resolve) => {
-			setTimeout(() => {
-				resolve;
-			}, ms);
-		});
-	};
-
-	return await axios
-		.get(...args, {
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json",
-				"X-CSRFToken": Cookies.get("csrftoken")
-			}
-		})
+	return await AxiosApiInstance.get(...args)
 		.then(sleep(ComponentReadyInterval))
-		.then((response) => {
-			if (Math.floor(response.status / 200) == 1) {
-				return response.data;
-			} else {
-				return false;
-			}
-		})
-		.catch((error) => {
-			return error.message;
-		});
+		.then((response) => response)
+		.catch((error) => error.message);
 };
 
 export default useFetcher;
