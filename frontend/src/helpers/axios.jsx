2317e194a3432aa09dfa3c69757a22e9c05101b5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-10-26 17:44:36 +0800

    Add custom `axios` instance helper

---
+++ frontend/src/helpers/axios.jsx
@@ -0,0 +1,39 @@
+import { EndpointRefreshInterval } from "@enums/GlobalValues";
+import * as Sentry from "@sentry/nextjs";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const ApplicationJson = "application/json";
+
+// Global axios defaults
+axios.defaults.headers.common["Accept"] = ApplicationJson;
+axios.defaults.headers.common["Content-Type"] = ApplicationJson;
+axios.defaults.headers.common["X-CSRFToken"] = Cookies.get("csrftoken");
+
+const AxiosApiInstance = axios.create({
+	timeout: EndpointRefreshInterval
+});
+
+AxiosApiInstance.interceptors.request.use(
+	async (config) => config,
+	async (error) => {
+		Sentry.captureException(error);
+
+		return Promise.reject(error);
+	}
+);
+
+AxiosApiInstance.interceptors.response.use(
+	async (response) => response,
+	async (error) => {
+		error.response.status === 403
+			? Cookies.remove("csrftoken")
+			: (() => {
+					Sentry.captureException(error);
+
+					return Promise.reject(error);
+			  })();
+	}
+);
+
+export default AxiosApiInstance;
