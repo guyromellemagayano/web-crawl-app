2097113e2b128a27b33245d5d819bdce28bd954b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-10-26 21:37:07 +0800

    Refactor `useUser` React hooks

--- frontend/src/hooks/useUser.js
+++ frontend/src/hooks/useUser.js
@@ -1,41 +1,14 @@
-// NextJS
-import { useRouter } from "next/router";
-
-// External
-import useSWR from "swr";
-
-import { RevalidationInterval } from "@enums/GlobalValues";
-
-// Hooks
 import { UserApiEndpoint } from "@enums/ApiEndpoints";
 import useFetcher from "@hooks/useFetcher";
+import useSWR from "swr";
 
-const useUser = ({ redirectIfFound = false, redirectTo = "", refreshInterval = 0 }) => {
-	const router = useRouter();
-
+export const useUser = () => {
 	const {
 		data: user,
 		mutate: mutateUser,
-		error: userError
-	} = useSWR(UserApiEndpoint, useFetcher, {
-		onErrorRetry: (error, key, revalidate, { retryCount }) => {
-			if (error && error !== undefined && error.status === 404) return;
-			if (key === UserApiEndpoint) return;
-			if (retryCount >= 10) return;
+		error: errorUser,
+		isValidating: validatingUser
+	} = useSWR(UserApiEndpoint, useFetcher);
 
-			setTimeout(() => revalidate({ retryCount: retryCount + 1 }), RevalidationInterval);
-		},
-		onSuccess: (data) => {
-			if (data !== undefined && Object.keys(data).length > 0 && !redirectIfFound) {
-				return;
-			} else {
-				router.push({ pathname: redirectTo });
-			}
-		},
-		refreshInterval: refreshInterval
-	});
-
-	return { user, mutateUser, userError };
+	return { user, mutateUser, errorUser, validatingUser };
 };
-
-export default useUser;
