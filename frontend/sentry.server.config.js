ef84acfe233e9b74869b7ca06aa12753e743479a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-10-27 22:07:23 +0800

    Update `sentry` client and server configs

--- frontend/sentry.server.config.js
+++ frontend/sentry.server.config.js
@@ -2,9 +2,10 @@ import * as Sentry from "@sentry/nextjs";
 
 const isProduction = process.env.NODE_ENV === "production";
 const SentryDsn = isProduction ? process.env.SENTRY_DSN : null;
+const SentryEnv = process.env.SENTRY_ENVIRONMENT;
 
 Sentry.init({
-	environment: isProduction ? "production" : "development",
+	environment: SentryEnv,
 	tracesSampleRate: 0.01,
 	dsn: SentryDsn
 });
