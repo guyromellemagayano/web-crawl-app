acdeda5c0b68558cad2702cd7c97cca62abc3a08
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-10-27 22:10:37 +0800

    Update sites table labels with translation locales

--- frontend/src/configs/SitesTableLabels.js
+++ frontend/src/configs/SitesTableLabels.js
@@ -1,26 +1,29 @@
 import useTranslation from "next-translate/useTranslation";
 
-const { t } = useTranslation("sites");
+export const SitesTableLabels = () => {
+	const { t } = useTranslation("sites");
+	const siteName = t("siteName");
+	const lastCrawled = t("lastCrawled");
+	const totalIssues = t("totalIssues");
+	const totalLinks = t("totalLinks");
+	const totalPages = t("totalPages");
+	const totalImages = t("totalImages");
 
-export const SitesTableLabels = [
-	{
-		label: t("siteName"),
-		slug: "site-name",
-		key: "name"
-	},
-	{
-		label: t("lastCrawled")
-	},
-	{
-		label: t("totalIssues")
-	},
-	{
-		label: t("totalLinks")
-	},
-	{
-		label: t("totalPages")
-	},
-	{
-		label: t("totalImages")
-	}
-];
+	const labelsArray = new Array();
+
+	const siteNameTableLabel = { label: siteName, slug: "site-name", key: "name" };
+	const lastCrawledTableLabel = { label: lastCrawled };
+	const totalIssuesTableLabel = { label: totalIssues };
+	const totalLinksTableLabel = { label: totalLinks };
+	const totalPagesTableLabel = { label: totalPages };
+	const totalImagesTableLabel = { label: totalImages };
+
+	labelsArray.push(siteNameTableLabel);
+	labelsArray.push(lastCrawledTableLabel);
+	labelsArray.push(totalIssuesTableLabel);
+	labelsArray.push(totalLinksTableLabel);
+	labelsArray.push(totalPagesTableLabel);
+	labelsArray.push(totalImagesTableLabel);
+
+	return labelsArray;
+};
