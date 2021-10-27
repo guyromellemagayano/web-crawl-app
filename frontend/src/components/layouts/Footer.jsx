72e43414014939debad4011bd91635adb3fe820b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-10-27 08:27:39 +0800

    Update footer labels with translation locales

--- frontend/src/components/layouts/Footer.jsx
+++ frontend/src/components/layouts/Footer.jsx
@@ -1,18 +1,19 @@
-// React
+import { FooterLabels } from "@configs/FooterLabels";
+import useTranslation from "next-translate/useTranslation";
 import * as React from "react";
-
-// External
 import "twin.macro";
 
-// Enums
-import { FooterLabels } from "@enums/FooterLabels";
-
 const Footer = () => {
+	const labelsArray = FooterLabels();
+
+	const { t } = useTranslation("common");
+	const allRightsReserved = t("allRightsReserved");
+
 	return (
 		<footer tw="max-w-full md:flex-col lg:flex-row p-3 md:flex md:items-center md:justify-between">
 			<div tw="flex justify-center md:order-1">
 				<nav tw="-mx-4 -my-2 flex flex-wrap justify-center">
-					{FooterLabels.map((value, index) => (
+					{labelsArray.map((value, index) => (
 						<div key={index} tw="px-3 py-2">
 							<a
 								href={value.link}
@@ -26,7 +27,7 @@ const Footer = () => {
 			</div>
 			<div tw="md:order-2">
 				<p tw="text-center text-sm leading-6 text-gray-500">
-					&copy; {new Date().getFullYear()} SiteCrawler, Inc. All Rights Reserved.
+					&copy; {new Date().getFullYear()} SiteCrawler, Inc. {allRightsReserved}.
 				</p>
 			</div>
 		</footer>
--- frontend/src/configs/FooterLabels.js
+++ frontend/src/configs/FooterLabels.js
@@ -1,18 +1,23 @@
-export const FooterLabels = [
-	{
-		label: "About",
-		link: `/about/`
-	},
-	{
-		label: "Privacy Policy",
-		link: `/privacy-policy/`
-	},
-	{
-		label: "Terms",
-		link: `/terms/`
-	},
-	{
-		label: "Support",
-		link: `/support/`
-	}
-];
+import useTranslation from "next-translate/useTranslation";
+
+export const FooterLabels = () => {
+	const { t } = useTranslation("common");
+	const about = t("about");
+	const privacyPolicy = t("privacyPolicy");
+	const terms = t("terms");
+	const support = t("support");
+
+	const labelsArray = new Array();
+
+	const aboutLink = { label: about, link: "/about/" };
+	const privacyPolicyLink = { label: privacyPolicy, link: "/privacy-policy/" };
+	const termsLink = { label: terms, link: "/terms/" };
+	const supportLink = { label: support, link: "/support/" };
+
+	labelsArray.push(aboutLink);
+	labelsArray.push(privacyPolicyLink);
+	labelsArray.push(termsLink);
+	labelsArray.push(supportLink);
+
+	return labelsArray;
+};
