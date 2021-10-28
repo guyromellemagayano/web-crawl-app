5cf53afd5de527b836b109b3202cf6ffb24f36cd
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-10-28 17:13:06 +0800

    Update `useFetcher` React hook

--- frontend/src/hooks/useFetcher.js
+++ frontend/src/hooks/useFetcher.js
@@ -1,12 +1,16 @@
-import { EndpointRefreshInterval } from "@configs/GlobalValues";
+import { FormSubmissionInterval } from "@configs/GlobalValues";
 import AxiosApiInstance from "@utils/axios";
 import { sleep } from "@utils/sleep";
 
 const useFetcher = async (...args) => {
 	return await AxiosApiInstance.get(...args)
-		.then(sleep(EndpointRefreshInterval))
-		.then((response) => response)
-		.catch((error) => error.message);
+		.then(sleep(FormSubmissionInterval))
+		.then((response) => {
+			return response;
+		})
+		.catch((error) => {
+			return error;
+		});
 };
 
 export default useFetcher;
