0fdbf182081d72f5202207d23d9de5e87aef9e5e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-01 16:03:42 +0800

    Update `handleRemoveURLParameter` helper function

--- frontend/src/helpers/handleRemoveUrlParameter.js
+++ frontend/src/helpers/handleRemoveUrlParameter.js
@@ -1,13 +1,18 @@
-export const removeURLParameter = (url, parameter) => {
-	//prefer to use l.search if you have a location/link object
+/**
+ * Helper function that handles removing URL parameters.
+ *
+ * @param {*} url
+ * @param {*} parameter
+ * @returns {string} url
+ */
+export const handleRemoveURLParameter = (url, parameter) => {
 	const urlparts = url.split("?");
+
 	if (urlparts.length >= 2) {
 		const prefix = encodeURIComponent(parameter) + "=";
 		const pars = urlparts[1].split(/[&;]/g);
 
-		//reverse iteration as may be destructive
 		for (var i = pars.length; i-- > 0; ) {
-			//idiom for string.startsWith
 			if (pars[i].lastIndexOf(prefix, 0) !== -1) {
 				pars.splice(i, 1);
 			}
