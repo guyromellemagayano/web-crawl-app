bda1d78d2d769f3c071e3ea37ab8803fe5c67c62
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-01 16:27:23 +0800

    Refactor `Layout` component

--- frontend/src/components/layouts/index.jsx
+++ frontend/src/components/layouts/index.jsx
@@ -1,27 +1,17 @@
-import { FallbackMessageAlert } from "@components/alerts/FallbackMessageAlert";
-import Loader from "@components/loader";
-import { useUser } from "@hooks/useUser";
-import * as Sentry from "@sentry/nextjs";
 import PropTypes from "prop-types";
 import * as React from "react";
 import "twin.macro";
 
-const fallbackMessageAlert = () => <FallbackMessageAlert />;
-
-export const AuthLayout = ({ children }) => {
-	const { validatingUser } = useUser();
-
-	return !validatingUser ? (
-		<Sentry.ErrorBoundary fallback={fallbackMessageAlert}>
-			<div id="root-auth">
-				<main tw="h-screen">{children}</main>
-			</div>
-		</Sentry.ErrorBoundary>
-	) : (
-		<Loader />
+const Layout = ({ children }) => {
+	return (
+		<div id="root-auth">
+			<main tw="h-screen">{children}</main>
+		</div>
 	);
 };
 
-AuthLayout.propTypes = {
+Layout.propTypes = {
 	children: PropTypes.any
 };
+
+export default Layout;
