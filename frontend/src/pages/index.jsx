6037a84ca356e409534a4c8a5df5ce6eb419d9f6
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-01 15:47:05 +0800

    [WIP] Fix SSR data fetching of all pages

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,8 +1,21 @@
-import { AuthLayout } from "@components/layouts";
+import Layout from "@components/layouts";
 import * as React from "react";
 
-const Home = () => {
-	return <AuthLayout />;
+const Home = ({ res }) => {
+	console.log(res);
+
+	return <Layout />;
 };
+
+export async function getServerSideProps() {
+	const req = await fetch("http://0.0.0.0:8000/api/auth/user/?format=json");
+	const res = await req.json();
+
+	return {
+		props: {
+			res: res
+		}
+	};
+}
 
 export default Home;
