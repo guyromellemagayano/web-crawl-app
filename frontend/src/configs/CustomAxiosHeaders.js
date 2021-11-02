534dd79cf50634251a35d09ffea89eb91ee7cdee
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-02 14:02:19 +0800

    Add nullish coalescing operator for `csrftoken` cookie

--- frontend/src/configs/CustomAxiosHeaders.js
+++ frontend/src/configs/CustomAxiosHeaders.js
@@ -3,5 +3,5 @@ import Cookies from "js-cookie";
 export const customAxiosHeaders = {
 	"Accept": "application/json",
 	"Content-Type": "application/json",
-	"X-CSRFToken": Cookies.get("csrftoken")
+	"X-CSRFToken": Cookies.get("csrftoken") ?? null
 };
