8b052542fb2787b1adf88df80595909dc4acdfd9
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-02 19:31:12 +0800

    Add `NextSeo` component to `FallbackError` message component, update locale deps

--- frontend/src/components/messages/FallbackError.jsx
+++ frontend/src/components/messages/FallbackError.jsx
@@ -1,17 +1,22 @@
+import { NextSeo } from "next-seo";
 import useTranslation from "next-translate/useTranslation";
 import * as React from "react";
 import "twin.macro";
 
 const FallbackErrorMessage = () => {
-	const { t } = useTranslation("common");
+	const { t } = useTranslation("error");
 	const fallbackErrorMessage = t("fallbackErrorMessage");
 
 	return (
-		<section tw="flex flex-col justify-center min-h-screen">
-			<div tw="px-4 py-5 sm:p-6 flex items-center justify-center">
-				<h3 tw="text-lg leading-6 font-medium text-gray-500">{fallbackErrorMessage}</h3>
-			</div>
-		</section>
+		<React.Fragment>
+			<NextSeo title={fallbackErrorMessage} />
+
+			<section tw="flex flex-col justify-center min-h-screen">
+				<div tw="px-4 py-5 sm:p-6 flex items-center justify-center">
+					<h3 tw="text-lg leading-6 font-medium text-gray-500">{fallbackErrorMessage}</h3>
+				</div>
+			</section>
+		</React.Fragment>
 	);
 };
 
