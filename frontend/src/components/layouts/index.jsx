49e93cb6c8a64157880047b7e7e25dd59a79a3eb
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-06 21:32:58 +0800

    Add Sentry `ErrorBoundary` component in main `Layout` component

--- frontend/src/components/layouts/index.jsx
+++ frontend/src/components/layouts/index.jsx
@@ -1,12 +1,16 @@
+import { FallbackMessageAlert } from "@components/alerts";
+import * as Sentry from "@sentry/nextjs";
 import PropTypes from "prop-types";
 import * as React from "react";
 import "twin.macro";
 
 const Layout = ({ children }) => {
 	return (
-		<div id="root-auth">
-			<main tw="h-screen">{children}</main>
-		</div>
+		<Sentry.ErrorBoundary fallback={FallbackMessageAlert}>
+			<div id="root-auth">
+				<main tw="h-screen">{children}</main>
+			</div>
+		</Sentry.ErrorBoundary>
 	);
 };
 
