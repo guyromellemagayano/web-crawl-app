06bc91f0ecbe357361fb497773b0652416f0c764
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-06 21:24:41 +0800

    Add `DashboardRoute` to global values

--- frontend/src/configs/GlobalValues.js
+++ frontend/src/configs/GlobalValues.js
@@ -24,8 +24,12 @@ export const LgScreenBreakpoint = 1024;
 export const MutateInterval = 2000;
 export const RevalidationInterval = 3000;
 export const RedirectInterval = 1250;
+export const NoInterval = 0;
 
 // File Locations
 export const SiteLogoDark = "/images/logos/site-logo-dark.svg";
 export const SiteLogoWhite = "/images/logos/site-logo-white.svg";
 export const SubscriptionSuccessBadge = "/images/backgrounds/subscription-success-badge.png";
+
+// Routes
+export const DashboardRoute = "/dashboard";
