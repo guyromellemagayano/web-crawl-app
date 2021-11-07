b4fce318c2d9b259f50a450c65568fe7f7386b74
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-07 17:51:26 +0800

    Remove Sentry `Error.Boundary` component in Layout component

--- frontend/src/components/layouts/index.jsx
+++ frontend/src/components/layouts/index.jsx
@@ -1,16 +1,12 @@
-import { FallbackMessageAlert } from "@components/alerts";
-import * as Sentry from "@sentry/nextjs";
 import PropTypes from "prop-types";
 import * as React from "react";
 import "twin.macro";
 
 const Layout = ({ children }) => {
 	return (
-		<Sentry.ErrorBoundary fallback={FallbackMessageAlert}>
-			<div id="root-auth">
-				<main tw="h-screen">{children}</main>
-			</div>
-		</Sentry.ErrorBoundary>
+		<div id="root-auth">
+			<main tw="h-screen">{children}</main>
+		</div>
 	);
 };
 
