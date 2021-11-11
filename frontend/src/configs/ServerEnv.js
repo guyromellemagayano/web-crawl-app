e841facda6e3c7d3f15ae28fc5261192395e207d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-11 17:55:17 +0800

    Fix site url for production to handle any hostname and protocol

--- frontend/src/configs/ServerEnv.js
+++ frontend/src/configs/ServerEnv.js
@@ -1,11 +1,8 @@
 // Environment
 export const isProd = process.env.NODE_ENV === "production";
 export const isDev = process.env.NODE_ENV === "development";
-export const isStaging = process.env.NODE_ENV === "staging";
 
 // Server
 export const server = isDev
 	? process.env.NEXT_PUBLIC_DEVELOPMENT_SITE_URL
-	: isProd
-	? process.env.NEXT_PUBLIC_PRODUCTION_SITE_URL
-	: process.env.NEXT_PUBLIC_STAGING_SITE_URL;
+	: process.env.NEXT_PUBLIC_PRODUCTION_SITE_URL;
