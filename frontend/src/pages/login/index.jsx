88f95c3e403fe55f4852e9c3075ac481fd1c49cf
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-16 11:13:57 +0800

    Update logic of URL redirection during SSR for both login and registration pages

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -9,35 +9,30 @@ import useTranslation from "next-translate/useTranslation";
 import { useRouter } from "next/router";
 import * as React from "react";
 
-// Pre-render `user` data with NextJS SSR. Redirect to a 404 page if the user is not found, redirect to a login page if current user is not allowed to access that page (403 Forbidden) or redirect to the sites dashboard page if the user is still currently logged in (200 OK).
+// Pre-render `user` data with NextJS SSR. Redirect to a login page if current user is not allowed to access that page (403 Forbidden) or redirect to the sites dashboard page if the user is still currently logged in (200 OK).
 export async function getServerSideProps({ req }) {
 	const userResponse = await useGetMethod(`${server + UserApiEndpoint}`, req.headers);
 	const userData = userResponse.data ?? null;
 	const userStatus = userResponse.status ?? null;
 
-	if (typeof userData === "undefined" || userData === null) {
-		if (userStatus === 404) {
-			return {
-				notFound: true
-			};
-		} else {
-			return {
-				props: {}
-			};
-		}
+	if (
+		typeof userData !== "undefined" &&
+		userData !== null &&
+		!userData.detail &&
+		typeof userData === "object" &&
+		Object.keys(userData).length > 0 &&
+		Math.round(userStatus / 200 === 1)
+	) {
+		return {
+			redirect: {
+				destination: SitesLink,
+				permanent: false
+			}
+		};
 	} else {
-		if (userData.detail) {
-			return {
-				props: {}
-			};
-		} else {
-			return {
-				redirect: {
-					destination: SitesLink,
-					permanent: false
-				}
-			};
-		}
+		return {
+			props: {}
+		};
 	}
 }
 
