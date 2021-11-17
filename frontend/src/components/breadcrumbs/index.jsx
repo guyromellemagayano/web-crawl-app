3237bdaa9c11291fc87852e0b5156e81df4391a6
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-18 00:05:12 +0800

    Refactor `Breadcrumbs` component, update locales

--- frontend/src/components/breadcrumbs/index.jsx
+++ frontend/src/components/breadcrumbs/index.jsx
@@ -1,42 +1,47 @@
-// React
-import * as React from "react";
-
-// NextJS
-import Link from "next/link";
-
-// External
-import "twin.macro";
-import { ChevronRightIcon, HomeIcon } from "@heroicons/react/solid";
-import PropTypes from "prop-types";
-
-// Enums
-import { BreadcrumbsLabels } from "@enums/BreadcrumbsLabels";
 import { SitesLink } from "@enums/PageLinks";
+import { ChevronRightIcon, HomeIcon } from "@heroicons/react/solid";
+import useTranslation from "next-translate/useTranslation";
+import Link from "next/link";
+import PropTypes from "prop-types";
+import * as React from "react";
+import "twin.macro";
 
-const Breadcrumbs = ({
-	dataId,
-	isImages,
-	isLinks,
-	isOther,
-	isPages,
-	isSeo,
-	isSites,
-	pageDetailTitle,
-	pageTitle,
-	siteId
-}) => {
-	const homeLabel = BreadcrumbsLabels[0].label;
-	const siteIdOverviewPageLink = `/site/${siteId}/overview/`;
+const Breadcrumbs = (
+	dataId = null,
+	isImages = false,
+	isLinks = false,
+	isOther = false,
+	isPages = false,
+	isSeo = false,
+	isSites = false,
+	pageDetailTitle = null,
+	pageTitle = null,
+	siteId = null
+) => {
+	const sanitizedSiteId =
+		typeof siteId !== "undefined" && siteId !== null
+			? (() => {
+					typeof siteId !== "string" ? parseInt(siteId) : siteId;
+			  })()
+			: null;
+	const siteIdOverviewPageLink = `/site/${sanitizedSiteId}/overview/`;
+
+	// Translations
+	const { t } = useTranslation("common");
+	const home = t("home");
 
 	return (
 		<nav tw="flex pt-4 pb-8" aria-label="Breadcrumb">
 			<ol tw="flex items-center space-x-4">
 				<li>
 					<div>
-						<Link href={isOther && siteId == null ? SitesLink : siteIdOverviewPageLink} passHref>
+						<Link
+							href={isOther && typeof siteId !== "undefined" && siteId == null ? SitesLink : siteIdOverviewPageLink}
+							passHref
+						>
 							<a tw="text-gray-400 hover:text-gray-500">
 								<HomeIcon tw="flex-shrink-0 h-5 w-5" />
-								<span tw="sr-only">{homeLabel}</span>
+								<span tw="sr-only">{home}</span>
 							</a>
 						</Link>
 					</div>
@@ -45,28 +50,28 @@ const Breadcrumbs = ({
 					<div tw="flex items-center">
 						<ChevronRightIcon tw="flex-shrink-0 h-5 w-5 text-gray-400" />
 
-						{dataId !== null && pageDetailTitle !== null ? (
+						{typeof dataId !== "undefined" &&
+						dataId !== null &&
+						dataId !== 0 &&
+						typeof pageDetailTitle !== "undefined" &&
+						pageDetailTitle !== null &&
+						pageDetailTitle !== "" ? (
 							<Link
-								href={`/site/${siteId}/${
+								href={`/site/${sanitizedSiteId}/${
 									isLinks ? "links" : isPages ? "pages" : isImages ? "images" : isSeo ? "seo" : null
 								}`}
 								passHref
 							>
-								<a
-									aria-current="page"
-									className="truncate-breadcrumbs"
-									tw="cursor-pointer ml-4 text-sm text-gray-700"
-								>
+								<a aria-current="page" className="truncate-breadcrumbs" tw="cursor-pointer ml-4 text-sm text-gray-700">
 									{pageTitle}
 								</a>
 							</Link>
-						) : isSites && pageDetailTitle !== null ? (
+						) : isSites &&
+						  typeof pageDetailTitle !== "undefined" &&
+						  pageDetailTitle !== null &&
+						  pageDetailTitle !== "" ? (
 							<Link href="/sites/" passHref>
-								<a
-									aria-current="page"
-									className="truncate-breadcrumbs"
-									tw="cursor-pointer ml-4 text-sm text-gray-700"
-								>
+								<a aria-current="page" className="truncate-breadcrumbs" tw="cursor-pointer ml-4 text-sm text-gray-700">
 									{pageTitle}
 								</a>
 							</Link>
@@ -78,7 +83,13 @@ const Breadcrumbs = ({
 					</div>
 				</li>
 
-				{(dataId !== null && pageDetailTitle !== null) || (isSites && pageDetailTitle !== null) ? (
+				{(typeof dataId !== "undefined" &&
+					dataId !== null &&
+					dataId !== 0 &&
+					typeof pageDetailTitle !== "undefined" &&
+					pageDetailTitle !== null &&
+					pageDetailTitle !== "") ||
+				(isSites && typeof pageDetailTitle !== "undefined" && pageDetailTitle !== null && pageDetailTitle !== "") ? (
 					<li>
 						<div tw="flex items-center">
 							<ChevronRightIcon tw="flex-shrink-0 h-5 w-5 text-gray-400" />
@@ -108,19 +119,6 @@ Breadcrumbs.propTypes = {
 	pageDetailTitle: PropTypes.string,
 	pageTitle: PropTypes.string,
 	siteId: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
-};
-
-Breadcrumbs.defaultProps = {
-	dataId: null,
-	isImages: false,
-	isLinks: false,
-	isOther: false,
-	isPages: false,
-	isSeo: false,
-	isSites: false,
-	pageDetailTitle: null,
-	pageTitle: null,
-	siteId: null
 };
 
 export default Breadcrumbs;
--- frontend/src/enums/BreadcrumbsLabels.js
+++
@@ -1,5 +0,0 @@
-export const BreadcrumbsLabels = [
-	{
-		label: "Home"
-	}
-];
