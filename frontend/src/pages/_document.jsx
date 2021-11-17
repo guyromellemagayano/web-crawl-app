76455dfa5778f2d9e2ea9af5349bfd5ca899c358
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-17 19:18:36 +0800

    Fix missing imports on `_document` page

--- frontend/src/pages/_document.jsx
+++ frontend/src/pages/_document.jsx
@@ -1,4 +1,4 @@
-import Document, { Head, Html } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import Script from "next/script";
 import * as React from "react";
 import { ServerStyleSheet } from "styled-components";
