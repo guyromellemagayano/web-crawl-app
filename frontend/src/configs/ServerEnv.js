49ee1682bf75bc9e881850247e4a1d7525a52b5d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-17 18:54:10 +0800

    Fix falsy values issue on server environment config

--- frontend/src/configs/ServerEnv.js
+++ frontend/src/configs/ServerEnv.js
@@ -3,8 +3,7 @@ export const isProd = process.env.NODE_ENV === "production";
 export const isDev = process.env.NODE_ENV === "development";
 
 // Server
-export const server = isDev
-	? process.env.NEXT_PUBLIC_DEVELOPMENT_SITE_URL
-	: typeof window !== "undefined"
-	? window.location.protocol + "//" + window.location.hostname
-	: "";
+export const server =
+	typeof window !== "undefined"
+		? window.location.protocol + "//" + window.location.hostname
+		: process.env.NEXT_PUBLIC_DEVELOPMENT_SITE_URL;
