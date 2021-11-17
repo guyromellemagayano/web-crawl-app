44f45c78c0c61a1cc4f8e1746374eb188e0dc7fb
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-17 23:32:01 +0800

    Add documentation on `usePage` React hook

--- frontend/src/hooks/usePage.js
+++ frontend/src/hooks/usePage.js
@@ -1,11 +1,17 @@
 import useSWR from "swr";
 
+/**
+ * SWR React hook that will handle a site's `page` information
+ *
+ * @param {string} endpoint
+ * @returns {object} data, error, isValidating
+ */
 export const usePage = (endpoint = null) => {
 	const {
 		data: page,
-		error: pageError,
+		error: errorPage,
 		isValidating: validatingPage
 	} = useSWR(typeof endpoint !== "undefined" && endpoint !== null && endpoint !== "" ? endpoint : null);
 
-	return { page, validatingPage, pageError };
+	return { page, errorPage, validatingPage };
 };
