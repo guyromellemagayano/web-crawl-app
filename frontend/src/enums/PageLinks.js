fa3799e875771377d35bc0b16a806a96f71cebf6
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-19 16:41:58 +0800

    Remove API endpoints and page links enums

--- frontend/src/enums/PageLinks.js
+++
@@ -1,4 +0,0 @@
-export const AddNewSiteLink = "/sites/add-new-site/";
-export const LoginLink = "/login/";
-export const SitesLink = "/sites/";
-export const SubscriptionPlansLink = "/settings/subscription-plans/";
