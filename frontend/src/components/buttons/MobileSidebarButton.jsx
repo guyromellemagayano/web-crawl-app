4f7e818f193490ce0a53db5360bf7bb02b405e19
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-20 21:05:09 +0800

    Refactor `MobileSidebarButton` component

--- frontend/src/components/buttons/MobileSidebarButton.jsx
+++ frontend/src/components/buttons/MobileSidebarButton.jsx
@@ -1,33 +1,27 @@
-// React
-import * as React from "react";
-
-// External
-import "twin.macro";
 import { MenuIcon } from "@heroicons/react/solid";
-import PropTypes from "prop-types";
+import { useComponentVisible } from "@hooks/useComponentVisible";
+import useTranslation from "next-translate/useTranslation";
+import * as React from "react";
+import "twin.macro";
 
-const MobileSidebarButton = ({ openSidebar, setOpenSidebar }) => {
-	const openSidebarLabel = "Open sidebar";
+/**
+ * Memoized function to render the `MobileSidebarButton` component.
+ */
+export const MobileSidebarButton = React.memo(() => {
+	// React hook for managing the visibility of the component.
+	const { isComponentVisible, setIsComponentVisible } = useComponentVisible(false);
+
+	// Translations
+	const { t } = useTranslation("common");
+	const openSidebar = t("openSidebar");
 
 	return (
 		<button
 			tw="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-900 lg:hidden"
-			onClick={() => setOpenSidebar(!openSidebar)}
+			onClick={() => setIsComponentVisible(!isComponentVisible)}
 		>
-			<span tw="sr-only">{openSidebarLabel}</span>
+			<span tw="sr-only">{openSidebar}</span>
 			<MenuIcon tw="h-6 w-6" />
 		</button>
 	);
-};
-
-MobileSidebarButton.propTypes = {
-	openSidebar: PropTypes.bool,
-	setOpenSidebar: PropTypes.func
-};
-
-MobileSidebarButton.defaultProps = {
-	openSidebar: false,
-	setOpenSidebar: null
-};
-
-export default MobileSidebarButton;
+});
