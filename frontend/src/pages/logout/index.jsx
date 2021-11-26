9c70165231bd4fc6f8f7f810861942cd0a48e901
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-26 16:34:10 +0800

    Refactor `Logout` page, apply SSR for the `logout` request

--- frontend/src/pages/logout/index.jsx
+++ frontend/src/pages/logout/index.jsx
@@ -1,59 +1,49 @@
-import Layout from "@components/layouts";
-import Loader from "@components/loader";
-import { LogoutApiEndpoint, UserApiEndpoint } from "@configs/ApiEndpoints";
-import { LoginLink } from "@configs/PageLinks";
+import { Loader } from "@components/loaders";
+import { LogoutApiEndpoint } from "@configs/ApiEndpoints";
+import { HomeLink } from "@configs/PageLinks";
+import { server } from "@configs/ServerEnv";
 import { usePostMethod } from "@hooks/useHttpMethod";
-import { useUser } from "@hooks/useUser";
 import { NextSeo } from "next-seo";
-import { useRouter } from "next/router";
 import * as React from "react";
 import "twin.macro";
 
-const Logout = () => {
-	const [logoutDetail, setLogoutDetail] = React.useState(null);
+// Pre-render `logout` data with NextJS SSR. Redirect to a login page for successful `logout` response (200 OK).
+export async function getServerSideProps({ req }) {
+	const logoutResponse = await usePostMethod(`${server + LogoutApiEndpoint}`, req.headers);
+	const logoutData = logoutResponse?.data ?? null;
+	const logoutStatus = logoutResponse?.status ?? null;
 
-	const pageTitle = "Logout";
+	if (
+		typeof logoutData !== "undefined" &&
+		logoutData !== null &&
+		logoutData?.detail &&
+		Object.keys(logoutData)?.length > 0 &&
+		Math.round(logoutStatus / 200 === 1)
+	) {
+		return {
+			redirect: {
+				destination: HomeLink,
+				permanent: false
+			}
+		};
+	} else {
+		return {
+			notFound: true
+		};
+	}
+}
 
-	const router = useRouter();
-	const { user, mutateUser, validatingUser } = useUser();
+const Logout = React.memo(() => {
+	// Translations
+	const { t } = useTranslation("common");
+	const logout = t("logout");
 
-	React.useEffect(() => {
-		!validatingUser ? (!user ? router.push(LoginLink) : null) : null;
-	}, [user]);
-
-	return !validatingUser && typeof user !== undefined ? (
-		(async () => {
-			const response = await usePostMethod(LogoutApiEndpoint);
-			const data = response?.data || null;
-			const status = response?.status || null;
-
-			Math.floor(status / 200) === 1 &&
-				(() => {
-					data?.detail
-						? (() => {
-								setLogoutDetail(data?.detail);
-								mutateUser(UserApiEndpoint);
-
-								data?.detail ? (window.location.href = LoginLink) : null;
-						  })()
-						: null;
-				})();
-
-			return (
-				<Layout>
-					<NextSeo title={pageTitle} />
-
-					<section tw="flex flex-col justify-center min-h-screen bg-white">
-						<div tw="px-4 py-5 sm:p-6 flex items-center justify-center">
-							<h3 tw="text-lg leading-6 font-medium text-gray-500">{logoutDetail}</h3>
-						</div>
-					</section>
-				</Layout>
-			);
-		})()
-	) : (
-		<Loader />
+	return (
+		<React.Fragment>
+			<NextSeo title={logout} />
+			<Loader />
+		</React.Fragment>
 	);
-};
+});
 
 export default Logout;
