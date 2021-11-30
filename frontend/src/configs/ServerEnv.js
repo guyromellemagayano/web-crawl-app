818326c55bd0d272f4e367d9226820a4b31c2aaa
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-30 10:42:59 +0800

    Update `ServerEnv` config

--- frontend/src/configs/ServerEnv.js
+++ frontend/src/configs/ServerEnv.js
@@ -4,6 +4,6 @@ export const isDev = process.env.NODE_ENV === "development";
 
 // Server
 export const server =
-	typeof window !== "undefined"
+	typeof window !== "undefined" && isProd
 		? window.location.protocol + "//" + window.location.hostname
-		: process.env.NEXT_PUBLIC_DEVELOPMENT_SITE_URL;
+		: process.env.NEXT_PUBLIC_DEVELOPMENT_SITE_SERVER_URL;
