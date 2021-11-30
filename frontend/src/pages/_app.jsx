def0dce9dda323ed609a3f2414918b9ee282cfce
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-30 17:21:57 +0800

    Fix SSR in `GlobalStyles` component, fix client routing issue

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,14 +1,12 @@
 import TopProgressBar from "@components/top-progress-bar";
 import AppSeo from "@configs/AppSeo";
-import { DashboardSitesLink, LoginLink } from "@configs/PageLinks";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import GlobalStyles from "@styles/GlobalStyles";
 import LogRocket from "logrocket";
 import setupLogRocketReact from "logrocket-react";
 import { DefaultSeo } from "next-seo";
-import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
 import PropTypes from "prop-types";
 import { useEffect } from "react";
 import "tailwindcss/tailwind.css";
@@ -17,18 +15,16 @@ import "tailwindcss/tailwind.css";
 library.add(fab);
 library.add(fas);
 
-const MyApp = ({ Component, pageProps }) => {
-	// Router
-	const router = useRouter();
+/**
+ * Dymamic imports
+ */
+const GlobalStyles = dynamic(() => import("@styles/GlobalStyles"), { ssr: true });
 
+const MyApp = ({ Component, pageProps }) => {
 	// Use the layout defined at the page level, if available
 	const getLayout = Component.getLayout || ((page) => page);
 
 	useEffect(() => {
-		// Prefetch sites page for faster loading
-		router.prefetch(DashboardSitesLink);
-		router.prefetch(LoginLink);
-
 		// LogRocket setup
 		if (process.env.NODE_ENV === "production") {
 			LogRocket.init("epic-design-labs/link-app");
