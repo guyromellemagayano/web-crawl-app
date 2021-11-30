d2cf2f7f18dc76e8fd16686fb19e3c72897ed55e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-11-30 10:43:55 +0800

    Fix logout page not logging out properly

--- frontend/src/pages/logout/index.jsx
+++ frontend/src/pages/logout/index.jsx
@@ -1,42 +1,74 @@
+import Layout from "@components/layouts";
 import { Loader } from "@components/loaders";
-import { LogoutApiEndpoint } from "@configs/ApiEndpoints";
-import { HomeLink } from "@configs/PageLinks";
+import { LogoutApiEndpoint, UserApiEndpoint } from "@configs/ApiEndpoints";
+import { DashboardSitesLink, LoginLink } from "@configs/PageLinks";
 import { server } from "@configs/ServerEnv";
-import { usePostMethod } from "@hooks/useHttpMethod";
+import { useGetMethod, usePostMethod } from "@hooks/useHttpMethod";
 import { NextSeo } from "next-seo";
-import { memo } from "react";
+import useTranslation from "next-translate/useTranslation";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useSWRConfig } from "swr";
 import "twin.macro";
 
-// Pre-render `logout` data with NextJS SSR. Redirect to a login page for successful `logout` response (200 OK).
+// Pre-render `user` data with NextJS SSR. Redirect to a login page if current user is not allowed to access that page (403 Forbidden) or redirect to the sites dashboard page if the user is still currently logged in (200 OK).
 export async function getServerSideProps({ req }) {
-	const logoutResponse = await usePostMethod(`${server + LogoutApiEndpoint}`, req.headers);
-	const logoutData = logoutResponse?.data ?? null;
-	const logoutStatus = logoutResponse?.status ?? null;
+	const userResponse = await useGetMethod(`${server + UserApiEndpoint}`, req.headers);
+	const userData = userResponse?.data ?? null;
+	const userStatus = userResponse?.status ?? null;
 
 	if (
-		typeof logoutData !== "undefined" &&
-		logoutData !== null &&
-		logoutData?.detail &&
-		Object.keys(logoutData)?.length > 0 &&
-		Math.round(logoutStatus / 200 === 1)
+		typeof userData !== "undefined" &&
+		userData !== null &&
+		!userData?.detail &&
+		Object.keys(userData)?.length > 0 &&
+		Math.round(userStatus / 200 === 1)
 	) {
 		return {
 			redirect: {
-				destination: HomeLink,
+				destination: DashboardSitesLink,
 				permanent: false
 			}
 		};
 	} else {
 		return {
-			notFound: true
+			props: {}
 		};
 	}
 }
 
-const Logout = memo(() => {
+const Logout = () => {
+	// Router
+	const router = useRouter();
+
 	// Translations
 	const { t } = useTranslation("common");
 	const logout = t("logout");
+
+	// SWR hook for global mutations
+	const { mutate } = useSWRConfig();
+
+	useEffect(() => {
+		(async () => {
+			const logoutResponse = await usePostMethod(LogoutApiEndpoint);
+			const logoutData = logoutResponse?.data ?? null;
+			const logoutStatus = logoutResponse?.status ?? null;
+
+			if (
+				typeof logoutData !== "undefined" &&
+				logoutData !== null &&
+				logoutData?.detail &&
+				Object.keys(logoutData)?.length > 0 &&
+				Math.round(logoutStatus / 200 === 1)
+			) {
+				// Mutate `user` endpoint after successful 200 OK or 201 Created response is issued
+				mutate(UserApiEndpoint, false);
+
+				// Redirect to login page after data mutation is completed
+				router.push(LoginLink);
+			}
+		})();
+	}, []);
 
 	return (
 		<>
@@ -44,6 +76,10 @@ const Logout = memo(() => {
 			<Loader />
 		</>
 	);
-});
+};
+
+Logout.getLayout = function getLayout(page) {
+	return <Layout>{page}</Layout>;
+};
 
 export default Logout;
