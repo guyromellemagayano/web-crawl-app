12c6b71d40c9967d24fe2ed3561bde42fce0622d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-12-06 00:13:57 +0800

    Memoize confirm email page

--- frontend/pages/confirm-email/[...id].jsx
+++ frontend/pages/confirm-email/[...id].jsx
@@ -1,15 +1,15 @@
 import { Layout } from "@components/layouts";
-import ConfirmEmailPageLayout from "@components/layouts/pages/ConfirmEmail";
+import { MemoizedConfirmEmailPageLayout } from "@components/layouts/pages/ConfirmEmail";
 import { UserApiEndpoint } from "@constants/ApiEndpoints";
 import { DashboardSitesLink } from "@constants/PageLinks";
-import { server } from "@constants/ServerEnv";
+import { SSR_URL } from "@constants/ServerEnv";
 import AppAxiosInstance from "@utils/axios";
 import { NextSeo } from "next-seo";
 import useTranslation from "next-translate/useTranslation";
 
 // Pre-render `user` data with NextJS SSR. Redirect to a login page if current user is not allowed to access that page (403 Forbidden) or redirect to the sites dashboard page if the user is still currently logged in (200 OK).
-export async function getStaticProps() {
-	const userResponse = await AppAxiosInstance.get(`${server + UserApiEndpoint}`);
+export async function getServerSideProps() {
+	const userResponse = await AppAxiosInstance.get(`${SSR_URL + UserApiEndpoint}`);
 	const userData = userResponse?.data ?? null;
 	const userStatus = userResponse?.status ?? null;
 
@@ -41,7 +41,7 @@ export default function ConfirmEmail() {
 	return (
 		<>
 			<NextSeo title={emailConfirmation} />
-			<ConfirmEmailPageLayout />
+			<MemoizedConfirmEmailPageLayout />
 		</>
 	);
 }
