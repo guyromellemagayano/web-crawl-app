2f15f3623d00bcfbc5e751d46e3ec1a024397bff
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-12-05 10:35:18 +0800

    Update `ServerEnv` component

--- frontend/constants/ServerEnv.js
+++ frontend/constants/ServerEnv.js
@@ -2,8 +2,14 @@
 export const isProd = process.env.NODE_ENV === "production";
 export const isDev = process.env.NODE_ENV === "development";
 
-// Server
-export const server =
+// Site URL
+export const SITE_URL =
 	typeof window !== "undefined" && isProd
 		? window.location.protocol + "//" + window.location.hostname
 		: process.env.NEXT_PUBLIC_DEVELOPMENT_SITE_URL;
+
+// SSR URL
+export const SSR_URL =
+	typeof window !== "undefined" && isProd
+		? window.location.protocol + "//" + window.location.hostname
+		: process.env.NEXT_PUBLIC_DEVELOPMENT_SITE_SERVER_URL;
