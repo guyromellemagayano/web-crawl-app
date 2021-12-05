3b63d1ed90c94ea5e09958b96002752617c279f9
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-12-05 10:31:22 +0800

    Memoize `LogoLabel` component

--- frontend/components/labels/LogoLabel.jsx
+++ frontend/components/labels/LogoLabel.jsx
@@ -1,12 +1,11 @@
-import AppLogo from "@components/logos/AppLogo";
+import { MemoizedAppLogo } from "@components/logos/AppLogo";
 import { NoAuthAppLogo, SiteLogoDark } from "@constants/GlobalValues";
 import useTranslation from "next-translate/useTranslation";
-import PropTypes from "prop-types";
 import { memo } from "react";
 import "twin.macro";
 
 /**
- * Memoized function to render the `LogoLabel` component.
+ * Custom function to render the `LogoLabel` component
  *
  * @param {boolean} isLogin
  * @param {boolean} isSignUp
@@ -15,57 +14,49 @@ import "twin.macro";
  * @param {boolean} isAccountExist
  * @param {boolean} isConfirmEmail
  */
-const LogoLabel = memo(
-	({
-		isLogin = false,
-		isSignUp = false,
-		isResetPassword = false,
-		isAddPassword = false,
-		isAccountExist = false,
-		isConfirmEmail = false
-	}) => {
-		const { t } = useTranslation("common");
-		const appLogo = t("appLogo");
-		const isLoginText = t("isLogin");
-		const isSignUpText = t("isSignUp");
-		const isResetPasswordText = t("isResetPassword");
-		const isAddPasswordText = t("isAddPassword");
-		const isResetPasswordFormText = t("isResetPasswordForm");
-		const isAccountExistText = t("isAccountExist");
-		const isConfirmEmailText = t("isConfirmEmail");
+export function LogoLabel({
+	isLogin = false,
+	isSignUp = false,
+	isResetPassword = false,
+	isAddPassword = false,
+	isAccountExist = false,
+	isConfirmEmail = false
+}) {
+	const { t } = useTranslation("common");
+	const appLogo = t("appLogo");
+	const isLoginText = t("isLogin");
+	const isSignUpText = t("isSignUp");
+	const isResetPasswordText = t("isResetPassword");
+	const isAddPasswordText = t("isAddPassword");
+	const isResetPasswordFormText = t("isResetPasswordForm");
+	const isAccountExistText = t("isAccountExist");
+	const isConfirmEmailText = t("isConfirmEmail");
 
-		return (
-			<div tw="sm:mx-auto sm:w-full sm:max-w-md flex justify-center flex-col">
-				{!isLogin ? (
-					<AppLogo src={SiteLogoDark} alt={appLogo} width={NoAuthAppLogo.width} height={NoAuthAppLogo.height} />
-				) : null}
-				<h2 tw="my-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
-					{isLogin
-						? isLoginText
-						: isSignUp
-						? isSignUpText
-						: isResetPassword
-						? isResetPasswordText
-						: isAddPassword
-						? isAddPasswordText
-						: isAccountExist
-						? isAccountExistText
-						: isConfirmEmail
-						? isConfirmEmailText
-						: isResetPasswordFormText}
-				</h2>
-			</div>
-		);
-	}
-);
+	return (
+		<div tw="sm:mx-auto sm:w-full sm:max-w-md flex justify-center flex-col">
+			{!isLogin ? (
+				<MemoizedAppLogo src={SiteLogoDark} alt={appLogo} width={NoAuthAppLogo.width} height={NoAuthAppLogo.height} />
+			) : null}
+			<h2 tw="my-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+				{isLogin
+					? isLoginText
+					: isSignUp
+					? isSignUpText
+					: isResetPassword
+					? isResetPasswordText
+					: isAddPassword
+					? isAddPasswordText
+					: isAccountExist
+					? isAccountExistText
+					: isConfirmEmail
+					? isConfirmEmailText
+					: isResetPasswordFormText}
+			</h2>
+		</div>
+	);
+}
 
-LogoLabel.propTypes = {
-	isAccountExist: PropTypes.bool,
-	isAddPassword: PropTypes.bool,
-	isConfirmEmail: PropTypes.bool,
-	isLogin: PropTypes.bool,
-	isResetPassword: PropTypes.bool,
-	isSignUp: PropTypes.bool
-};
-
-export default LogoLabel;
+/**
+ * Memoized custom `LogoLabel` component
+ */
+export const MemoizedLogoLabel = memo(LogoLabel);
