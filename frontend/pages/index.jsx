0138ee49b3d1b190f3b567d90f6c7824b282676c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-12-05 10:47:16 +0800

    Update `Home` page

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -2,15 +2,14 @@ import { Layout } from "@components/layouts";
 import { Loader } from "@components/loaders";
 import { UserApiEndpoint } from "@constants/ApiEndpoints";
 import { DashboardSitesLink, LoginLink } from "@constants/PageLinks";
-import { server } from "@constants/ServerEnv";
+import { SSR_URL } from "@constants/ServerEnv";
 import AppAxiosInstance from "@utils/axios";
 import { NextSeo } from "next-seo";
 import useTranslation from "next-translate/useTranslation";
-import PropTypes from "prop-types";
 
 // Pre-render `user` data with NextJS SSR. Redirect to a 404 page if the user is not found, redirect to a login page if current user is not allowed to access that page (403 Forbidden) or redirect to the sites dashboard page if the user is still currently logged in (200 OK).
 export async function getStaticProps() {
-	const userResponse = await AppAxiosInstance.get(`${server + UserApiEndpoint}`);
+	const userResponse = await AppAxiosInstance.get(`${SSR_URL + UserApiEndpoint}`);
 	const userData = userResponse?.data ?? null;
 	const userStatus = userResponse?.status ?? null;
 
@@ -50,11 +49,8 @@ export default function Home() {
 	);
 }
 
-Home.propTypes = {
-	userData: PropTypes.object,
-	userStatus: PropTypes.number
-};
-
 Home.getLayout = function getLayout(page) {
 	return <Layout>{page}</Layout>;
 };
+
+Home.requiresAuth = false;
