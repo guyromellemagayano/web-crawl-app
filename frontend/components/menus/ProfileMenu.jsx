0c9915551ec90e39c91c54dd09562111c0c05284
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-12-09 02:41:48 +0800

    [WIP] Fix menu issues, refactor code

--- frontend/components/menus/ProfileMenu.jsx
+++ frontend/components/menus/ProfileMenu.jsx
@@ -1,59 +1,32 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { MemoizedAlert } from "@components/alerts";
 import { MemoizedProfileMenuDropdown } from "@components/dropdowns/ProfileMenuDropdown";
 import { MemoizedProfileSidebarSkeleton } from "@components/skeletons/ProfileSidebarSkeleton";
 import { ChevronUpIcon } from "@heroicons/react/solid";
 import { useComponentVisible } from "@hooks/useComponentVisible";
+import { useGetErrorHandler } from "@hooks/useErrorHandler";
+import { useLoading } from "@hooks/useLoading";
 import { useUser } from "@hooks/useUser";
-import * as Sentry from "@sentry/nextjs";
-import { memo, useCallback, useEffect, useState } from "react";
+import { memo } from "react";
 import tw from "twin.macro";
 
 /**
  * Custom function to render the `ProfileMenu` component
  */
 export function ProfileMenu() {
-	const [errorMessage, setErrorMessage] = useState([]);
-
 	// SWR hooks
 	const { user, errorUser, validatingUser } = useUser();
 
 	// Custom hooks
 	const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(false);
-
-	// Handle errors
-	const handleError = useCallback(async () => {
-		let errorStatusCodeMessage = "";
-
-		switch (errorUser) {
-			default:
-				errorStatusCodeMessage = errorUser;
-				break;
-		}
-
-		setErrorMessage((prevState) => [
-			...prevState,
-			prevState.indexOf(errorStatusCodeMessage) !== -1
-				? prevState.find((prevState) => prevState === errorStatusCodeMessage)
-				: errorStatusCodeMessage
-		]);
-
-		// Capture unknown errors and send to Sentry
-		Sentry.captureException(new Error(errorUser));
-	}, [user]);
-
-	useEffect(() => {
-		if (!validatingUser && !user && errorUser) {
-			return handleError();
-		}
-	}, [handleError, user, errorUser, validatingUser]);
+	const { isComponentReady } = useLoading();
+	const { errorMessage: userErrorMessage } = useGetErrorHandler("user", user, errorUser, validatingUser);
 
 	return (
 		<>
-			{errorMessage !== [] && errorMessage.length > 0 ? (
+			{userErrorMessage !== [] && userErrorMessage?.length > 0 ? (
 				<div tw="fixed right-6 bottom-6 grid grid-flow-row gap-4">
-					{errorMessage.map((value, key) => (
+					{userErrorMessage.map((value, key) => (
 						<MemoizedAlert key={key} message={value} isError />
 					))}
 				</div>
@@ -65,16 +38,16 @@ export function ProfileMenu() {
 						type="button"
 						css={[
 							tw`p-4 flex items-center justify-between flex-shrink-0 w-full focus:outline-none transition ease-in-out duration-150 bg-gray-900 `,
-							!validatingUser ? tw`cursor-pointer hover:bg-gray-1100` : tw`cursor-default`
+							isComponentReady ? tw`cursor-pointer hover:bg-gray-1100` : tw`cursor-default`
 						]}
 						id="options-menu"
 						aria-haspopup="true"
-						aria-expanded={isComponentVisible && !validatingUser ? "true" : "false"}
-						onClick={!validatingUser ? () => setIsComponentVisible(!isComponentVisible) : null}
+						aria-expanded={isComponentVisible && isComponentReady ? "true" : "false"}
+						onClick={isComponentReady ? () => setIsComponentVisible(!isComponentVisible) : null}
 					>
 						<div tw="flex items-center">
 							<div tw="flex flex-col flex-wrap text-left">
-								{!validatingUser ? (
+								{isComponentReady ? (
 									<>
 										<p className="truncate-profile-text" tw="text-sm leading-tight mb-1 font-medium text-white">
 											{user?.data?.first_name}
@@ -91,7 +64,8 @@ export function ProfileMenu() {
 								)}
 							</div>
 						</div>
-						{!validatingUser ? (
+
+						{isComponentReady ? (
 							<div>
 								<ChevronUpIcon tw="w-4 h-4 text-white" />
 							</div>
--- frontend/components/menus/SettingsMenu.jsx
+++ frontend/components/menus/SettingsMenu.jsx
@@ -1,15 +1,18 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { MemoizedAlert } from "@components/alerts";
 import { MemoizedAppLogo } from "@components/logos/AppLogo";
 import { MemoizedSiteSelect } from "@components/select/SiteSelect";
-import { AuthAppLogo, ComponentReadyInterval, SiteLogoWhite } from "@constants/GlobalValues";
+import { AuthAppLogo, SiteLogoWhite } from "@constants/GlobalValues";
 import { DashboardSitesLink, SettingsSlug } from "@constants/PageLinks";
 import { SidebarMenus } from "@constants/SidebarMenus";
 import { GlobeIcon } from "@heroicons/react/outline";
 import { ArrowLeftIcon, CreditCardIcon, SupportIcon, UserCircleIcon, ViewBoardsIcon } from "@heroicons/react/solid";
+import { useGetErrorHandler } from "@hooks/useErrorHandler";
+import { useLoading } from "@hooks/useLoading";
+import { useUser } from "@hooks/useUser";
 import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { memo, useEffect, useState } from "react";
+import { memo } from "react";
 import { Scrollbars } from "react-custom-scrollbars-2";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
@@ -19,23 +22,6 @@ import tw from "twin.macro";
  * Custom function to render the `SettingsMenu` component
  */
 export function SettingsMenu() {
-	const [isComponentReady, setIsComponentReady] = useState(false);
-
-	// Router
-	const { isReady, pathname } = useRouter();
-
-	useEffect(() => {
-		if (isReady && pathname) {
-			setTimeout(() => {
-				setIsComponentReady(true);
-			}, ComponentReadyInterval);
-		}
-
-		return () => {
-			setIsComponentReady(false);
-		};
-	}, [isReady, pathname]);
-
 	// Translations
 	const { t } = useTranslation("common");
 	const appLogo = t("appLogo");
@@ -43,126 +29,151 @@ export function SettingsMenu() {
 	// Sidebar menus
 	const { SettingsSidebarMenus } = SidebarMenus();
 
+	// SWR hooks
+	const { user, errorUser, validatingUser } = useUser();
+
+	// Custom hooks
+	const { isComponentReady, pathname } = useLoading();
+	const { errorMessage: userErrorMessage } = useGetErrorHandler("user", user, errorUser, validatingUser);
+
 	return (
-		<Scrollbars renderThumbVertical={(props) => <div {...props} className="scroll-dark-bg" />} universal>
-			<div tw="flex flex-col min-h-screen py-4 lg:py-8">
-				<div tw="flex items-center flex-shrink-0 flex-row px-3 mb-0">
-					<Link href={DashboardSitesLink} passHref>
-						<a tw="p-1 block w-full cursor-pointer">
-							<MemoizedAppLogo
-								className="flex"
-								src={SiteLogoWhite}
-								alt={appLogo}
-								width={AuthAppLogo.width}
-								height={AuthAppLogo.height}
-							/>
-						</a>
-					</Link>
+		<>
+			{userErrorMessage !== [] && userErrorMessage?.length > 0 ? (
+				<div tw="fixed right-6 bottom-6 grid grid-flow-row gap-4">
+					{userErrorMessage.map((value, key) => (
+						<MemoizedAlert key={key} message={value} isError />
+					))}
 				</div>
+			) : null}
 
-				<div tw="flex-1 flex flex-col overflow-y-auto">
-					<nav tw="flex-1 px-4">
-						{SettingsSidebarMenus.filter((e) => {
-							return !pathname.includes(SettingsSlug) ? e.slug !== "navigation" : true;
-						}).map((value, index) => {
-							return (
-								<div key={index} tw="mb-4">
-									<h3 tw="mt-8 text-xs leading-4 font-semibold text-gray-200 uppercase tracking-wider inline-block">
-										{isComponentReady ? value.category : <Skeleton duration={2} width={128} height={16} />}
-									</h3>
+			<Scrollbars renderThumbVertical={(props) => <div {...props} className="scroll-dark-bg" />} universal>
+				<div tw="flex flex-col min-h-screen py-4 lg:py-8">
+					<div tw="flex items-center flex-shrink-0 flex-row px-3 mb-0">
+						<Link href={DashboardSitesLink} passHref>
+							<a tw="p-1 block w-full cursor-pointer">
+								<MemoizedAppLogo
+									className="flex"
+									src={SiteLogoWhite}
+									alt={appLogo}
+									width={AuthAppLogo.width}
+									height={AuthAppLogo.height}
+								/>
+							</a>
+						</Link>
+					</div>
 
-									<div tw="my-3" role="group">
-										{value.links ? (
-											value.links.map((value2, index2) => {
-												return value2.slug !== "go-back-to-sites" && value2.slug !== "logout" ? (
-													<Link key={index2} href={value2.url} passHref>
-														<a
-															className="group"
-															css={[
-																tw`cursor-pointer`,
-																value2.url.includes(pathname) && isComponentReady ? tw`bg-gray-1100` : null,
-																value2.url.includes(pathname)
-																	? tw`mt-1 flex items-center px-3 py-2 text-sm leading-5 font-medium text-gray-100 rounded-md`
-																	: tw`mt-1 flex items-center px-3 py-2 text-sm leading-5 font-medium text-gray-400 rounded-md hover:text-gray-100 focus:outline-none transition ease-in-out duration-150 hover:bg-gray-1100 focus:bg-gray-1100`
-															]}
-														>
-															{value2.slug === "profile-settings" ? (
-																isComponentReady ? (
-																	<UserCircleIcon tw="mr-3 h-6 w-5" />
-																) : (
-																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
-																)
-															) : value2.slug === "subscription-plans" ? (
-																isComponentReady ? (
-																	<ViewBoardsIcon tw="mr-3 h-6 w-5" />
-																) : (
-																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
-																)
-															) : value2.slug === "billing-settings" ? (
-																isComponentReady ? (
-																	<CreditCardIcon tw="mr-3 h-6 w-5" />
-																) : (
-																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
-																)
-															) : value2.slug === "global-settings" ? (
-																isComponentReady ? (
-																	<GlobeIcon tw="mr-3 h-6 w-5" />
-																) : (
-																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
-																)
-															) : value2.slug === "subscription-plans" ? (
-																isComponentReady ? (
-																	<ViewBoardsIcon tw="mr-3 h-6 w-5" />
-																) : (
-																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
-																)
-															) : value2.slug === "help-support" ? (
-																isComponentReady ? (
-																	<SupportIcon tw="mr-3 h-6 w-5" />
-																) : (
-																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
-																)
-															) : null}
+					<div tw="flex-1 flex flex-col overflow-y-auto">
+						<nav tw="flex-1 px-4">
+							{SettingsSidebarMenus.filter((e) => {
+								return !pathname.includes(SettingsSlug) ? e.slug !== "navigation" : true;
+							}).map((value, index) => {
+								return (
+									<div key={index} tw="mb-4">
+										<h3 tw="mt-8 text-xs leading-4 font-semibold text-gray-200 uppercase tracking-wider inline-block">
+											{isComponentReady ? value.category : <Skeleton duration={2} width={128} height={16} />}
+										</h3>
 
-															{value2.title ? (
-																<span>
-																	{isComponentReady ? value2.title : <Skeleton duration={2} width={128} height={20} />}
-																</span>
-															) : null}
-														</a>
-													</Link>
-												) : value2.slug !== "logout" ? (
-													<Link key={index} href={value2.url} passHref>
-														<a
-															className="group"
-															tw="cursor-pointer mt-1 flex items-center py-2 text-sm leading-5 font-medium text-gray-400 rounded-md hover:text-gray-100 focus:outline-none focus:text-white"
-														>
-															{isComponentReady ? (
-																<ArrowLeftIcon tw="mr-3 h-6 w-5" />
-															) : (
-																<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
-															)}
+										<div tw="my-3" role="group">
+											{value.links ? (
+												value.links.map((value2, index2) => {
+													return value2.slug !== "go-back-to-sites" && value2.slug !== "logout" ? (
+														<Link key={index2} href={value2.url} passHref>
+															<a
+																className="group"
+																css={[
+																	tw`cursor-pointer`,
+																	value2.url.includes(pathname) && isComponentReady ? tw`bg-gray-1100` : null,
+																	value2.url.includes(pathname)
+																		? tw`mt-1 flex items-center px-3 py-2 text-sm leading-5 font-medium text-gray-100 rounded-md`
+																		: tw`mt-1 flex items-center px-3 py-2 text-sm leading-5 font-medium text-gray-400 rounded-md hover:text-gray-100 focus:outline-none transition ease-in-out duration-150 hover:bg-gray-1100 focus:bg-gray-1100`
+																]}
+															>
+																{value2.slug === "profile-settings" ? (
+																	isComponentReady ? (
+																		<UserCircleIcon tw="mr-3 h-6 w-5" />
+																	) : (
+																		<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
+																	)
+																) : value2.slug === "subscription-plans" ? (
+																	isComponentReady ? (
+																		<ViewBoardsIcon tw="mr-3 h-6 w-5" />
+																	) : (
+																		<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
+																	)
+																) : value2.slug === "billing-settings" ? (
+																	isComponentReady ? (
+																		<CreditCardIcon tw="mr-3 h-6 w-5" />
+																	) : (
+																		<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
+																	)
+																) : value2.slug === "global-settings" ? (
+																	isComponentReady ? (
+																		<GlobeIcon tw="mr-3 h-6 w-5" />
+																	) : (
+																		<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
+																	)
+																) : value2.slug === "subscription-plans" ? (
+																	isComponentReady ? (
+																		<ViewBoardsIcon tw="mr-3 h-6 w-5" />
+																	) : (
+																		<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
+																	)
+																) : value2.slug === "help-support" ? (
+																	isComponentReady ? (
+																		<SupportIcon tw="mr-3 h-6 w-5" />
+																	) : (
+																		<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
+																	)
+																) : null}
 
-															{value2.title ? (
-																<span>
-																	{isComponentReady ? value2.title : <Skeleton duration={2} width={128} height={20} />}
-																</span>
-															) : null}
-														</a>
-													</Link>
-												) : null;
-											})
-										) : (
-											<MemoizedSiteSelect />
-										)}
+																{value2.title ? (
+																	<span>
+																		{isComponentReady ? (
+																			value2.title
+																		) : (
+																			<Skeleton duration={2} width={128} height={20} />
+																		)}
+																	</span>
+																) : null}
+															</a>
+														</Link>
+													) : value2.slug !== "logout" ? (
+														<Link key={index} href={value2.url} passHref>
+															<a
+																className="group"
+																tw="cursor-pointer mt-1 flex items-center py-2 text-sm leading-5 font-medium text-gray-400 rounded-md hover:text-gray-100 focus:outline-none focus:text-white"
+															>
+																{isComponentReady ? (
+																	<ArrowLeftIcon tw="mr-3 h-6 w-5" />
+																) : (
+																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
+																)}
+
+																{value2.title ? (
+																	<span>
+																		{isComponentReady ? (
+																			value2.title
+																		) : (
+																			<Skeleton duration={2} width={128} height={20} />
+																		)}
+																	</span>
+																) : null}
+															</a>
+														</Link>
+													) : null;
+												})
+											) : (
+												<MemoizedSiteSelect />
+											)}
+										</div>
 									</div>
-								</div>
-							);
-						})}
-					</nav>
+								);
+							})}
+						</nav>
+					</div>
 				</div>
-			</div>
-		</Scrollbars>
+			</Scrollbars>
+		</>
 	);
 }
 
