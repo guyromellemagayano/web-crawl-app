b09ac8d644f2d1c33cee78ec2bf251933527a32c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-12-09 21:41:27 +0800

    Add default `Page` component, add footer skeletons

--- frontend/components/layouts/components/Footer.jsx
+++ frontend/components/layouts/components/Footer.jsx
@@ -1,6 +1,9 @@
 import { FooterLabels } from "@constants/FooterLabels";
+import { useLoading } from "@hooks/useLoading";
 import useTranslation from "next-translate/useTranslation";
 import { memo } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import "twin.macro";
 
 /**
@@ -14,27 +17,38 @@ export function Footer() {
 	// Footer Labels
 	const labelsArray = FooterLabels();
 
+	// Custom hook
+	const { isComponentReady } = useLoading();
+
 	return (
-		<footer tw="w-full pt-8 pb-4 border-t border-gray-200 ">
+		<footer tw="flex-none w-full pt-8 pb-4 border-t border-gray-200">
 			<div tw="flex md:flex-col lg:flex-row md:flex md:items-center md:justify-between px-4">
 				<div tw="flex justify-center md:order-1">
-					<nav tw="-mx-4 -my-2 flex flex-wrap justify-center">
+					<nav tw="-mx-8 -my-2 flex flex-wrap justify-center">
 						{labelsArray.map((value, index) => (
 							<div key={index} tw="px-3 py-2">
-								<a
-									href={value.link}
-									tw="text-sm leading-6 text-gray-500 hover:text-indigo-500 transition duration-150 ease-in-out"
-								>
-									{value.label}
-								</a>
+								{isComponentReady ? (
+									<a
+										href={value.link}
+										tw="text-sm leading-6 text-gray-500 hover:text-indigo-500 transition duration-150 ease-in-out"
+									>
+										{value.label}
+									</a>
+								) : (
+									<Skeleton duration={2} width={65} height={17} />
+								)}
 							</div>
 						))}
 					</nav>
 				</div>
 				<div tw="md:order-2">
-					<p tw="text-center text-sm leading-6 text-gray-500">
-						&copy; {new Date().getFullYear()} SiteCrawler, Inc. {allRightsReserved}.
-					</p>
+					{isComponentReady ? (
+						<p tw="text-center text-sm leading-6 text-gray-500">
+							&copy; {new Date().getFullYear()} SiteCrawler, Inc. {allRightsReserved}.
+						</p>
+					) : (
+						<Skeleton duration={2} width={289} height={17} />
+					)}
 				</div>
 			</div>
 		</footer>
