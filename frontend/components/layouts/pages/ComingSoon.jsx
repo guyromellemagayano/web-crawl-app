1e11fa7be6f2a1c2a0d85585d85af528e099ff81
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-12-09 21:50:12 +0800

    Update `ComingSoon` page layout component

--- frontend/components/layouts/pages/ComingSoon.jsx
+++ frontend/components/layouts/pages/ComingSoon.jsx
@@ -1,61 +1,31 @@
-import { MemoizedBreadcrumbs } from "@components/breadcrumbs";
-import { Loader } from "@components/loaders";
-import { ComponentReadyInterval } from "@constants/GlobalValues";
+import { useLoading } from "@hooks/useLoading";
 import useTranslation from "next-translate/useTranslation";
-import { useRouter } from "next/router";
-import PropTypes from "prop-types";
-import { memo, useEffect, useState } from "react";
+import { memo } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import "twin.macro";
-import { MemoizedFooter } from "../components/Footer";
 
 /**
  * Custom function to render the `ComingSoonPageLayout` component
- *
- * @param {string} pageTitle
  */
-export function ComingSoonPageLayout({ pageTitle = null }) {
-	const [isComponentReady, setIsComponentReady] = useState(false);
-
-	const { isReady } = useRouter();
-
-	useEffect(() => {
-		if (isReady) {
-			setTimeout(() => {
-				setIsComponentReady(true);
-			}, ComponentReadyInterval);
-		}
-
-		return () => {
-			setIsComponentReady(false);
-		};
-	}, [isReady]);
-
+export function ComingSoonPageLayout() {
 	// Translations
 	const { t } = useTranslation("common");
 	const comingSoon = t("comingSoon");
 
-	return isComponentReady ? (
-		<section tw="min-h-screen flex flex-col justify-center px-4 py-12">
-			<MemoizedBreadcrumbs isOther pageTitle={pageTitle} />
+	// Custom hooks
+	const { isComponentReady } = useLoading();
 
-			<div tw="flex pt-12 pb-4">
-				<h2 tw="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">{pageTitle}</h2>
-			</div>
-
-			<div tw="flex p-4 m-auto">
+	return (
+		<div tw="flex-grow flex justify-center items-center p-4 m-auto">
+			{isComponentReady ? (
 				<h4 tw="text-lg leading-6 font-medium text-gray-500">{comingSoon}</h4>
-			</div>
-
-			<MemoizedFooter />
-		</section>
-	) : (
-		<Loader />
+			) : (
+				<Skeleton duration={2} width={196} height={16} />
+			)}
+		</div>
 	);
 }
-
-ComingSoonPageLayout.propTypes = {
-	pageTitle: PropTypes.string
-};
 
 /**
  * Memoized custom `ComingSoonPageLayout` component
