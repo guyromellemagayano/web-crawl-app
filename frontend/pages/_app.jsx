4583c4762d42a9ac6cf083f7a08756a62c25ed4d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-12-09 21:48:05 +0800

    Remove custom context and apply URL redirection and authorization to main `_app` page

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,21 +1,61 @@
 import ProgressBar from "@components/progress-bar";
 import AppSeo from "@constants/AppSeo";
+import { DashboardSitesLink, DashboardSlug, LoginLink } from "@constants/PageLinks";
 import { isProd } from "@constants/ServerEnv";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import { AuthProvider } from "@hooks/useAuth";
+import { useUser } from "@hooks/useUser";
 import { GlobalCustomStyles } from "@styles/GlobalCustomStyles";
 import LogRocket from "logrocket";
 import setupLogRocketReact from "logrocket-react";
 import { DefaultSeo } from "next-seo";
-import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useCallback, useEffect } from "react";
+import { SWRConfig } from "swr";
 
 // Font Awesome
 library.add(fab);
 library.add(fas);
 
 export default function App({ Component, pageProps, err }) {
+	// Router
+	const { pathname } = useRouter();
+	const router = useRouter();
+
+	// SWR hooks
+	const { user, errorUser, validatingUser } = useUser();
+
+	// User authentication
+	const authenticate = useCallback(async () => {
+		// If user is not authenticated, redirect to login page
+		if (!validatingUser) {
+			if (
+				!errorUser &&
+				typeof user !== "undefined" &&
+				user !== null &&
+				Object.keys(user)?.length > 0 &&
+				Math.round(user?.status / 200) === 1
+			) {
+				if (!pathname.includes(DashboardSlug)) {
+					router.push(DashboardSitesLink);
+				} else {
+					return;
+				}
+			} else {
+				if (!pathname.includes(DashboardSlug)) {
+					return;
+				} else {
+					router.push(LoginLink);
+				}
+			}
+		}
+	}, [validatingUser, errorUser, user, pathname, router]);
+
+	useEffect(() => {
+		authenticate();
+	}, [authenticate]);
+
 	// Use the layout defined at the page level, if available
 	const getLayout = Component.getLayout || ((page) => page);
 
@@ -28,11 +68,15 @@ export default function App({ Component, pageProps, err }) {
 	}, []);
 
 	return getLayout(
-		<AuthProvider>
+		<SWRConfig
+			value={{
+				refreshWhenHidden: true
+			}}
+		>
 			<GlobalCustomStyles />
 			<DefaultSeo {...AppSeo} />
 			<ProgressBar />
 			<Component {...pageProps} err={err} />
-		</AuthProvider>
+		</SWRConfig>
 	);
 }
