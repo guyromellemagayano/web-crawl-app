9a14925c8f5bffb72ab15f39982e2fd8e08e954c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2021-12-17 21:36:24 +0800

    Fix hover, focus, and active states on `SettingsMenu` component

--- frontend/components/menus/SettingsMenu.jsx
+++ frontend/components/menus/SettingsMenu.jsx
@@ -7,7 +7,6 @@ import { SidebarMenus } from "@constants/SidebarMenus";
 import { GlobeIcon } from "@heroicons/react/outline";
 import { ArrowLeftIcon, CreditCardIcon, SupportIcon, UserCircleIcon, ViewBoardsIcon } from "@heroicons/react/solid";
 import { useLoading } from "@hooks/useLoading";
-import { useUser } from "@hooks/useUser";
 import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -26,13 +25,10 @@ export function SettingsMenu() {
 	const appLogo = t("appLogo");
 
 	// Router
-	const { asPath, pathname } = useRouter();
+	const { asPath } = useRouter();
 
 	// Sidebar menus
 	const { SettingsSidebarMenus } = SidebarMenus();
-
-	// SWR hooks
-	const { user, errorUser, validatingUser } = useUser();
 
 	// Custom hooks
 	const { isComponentReady } = useLoading();
@@ -42,14 +38,7 @@ export function SettingsMenu() {
 			<div tw="flex flex-col min-h-screen py-4 lg:py-8">
 				<div tw="flex items-center flex-shrink-0 flex-row px-3 mb-0">
 					<Link href={DashboardSitesLink} passHref>
-						<a
-							css={[
-								tw`p-1 block w-full`,
-								isComponentReady && !validatingUser && !errorUser && typeof user !== "undefined" && user !== null
-									? tw`cursor-pointer`
-									: null
-							]}
-						>
+						<a css={[tw`p-1 block w-full`, isComponentReady ? tw`cursor-pointer` : null]}>
 							<MemoizedAppLogo
 								className="flex"
 								src={SiteLogoWhite}
@@ -64,20 +53,12 @@ export function SettingsMenu() {
 				<div tw="flex-1 flex flex-col overflow-y-auto">
 					<nav tw="flex-1 px-4">
 						{SettingsSidebarMenus.filter((e) => {
-							return !pathname?.includes(SettingsSlug) ? e.slug !== "navigation" : true;
+							return !asPath?.includes(SettingsSlug) ? e.slug !== "navigation" : true;
 						}).map((value, index) => {
 							return (
 								<div key={index} tw="mb-4">
 									<h3 tw="mt-8 text-xs leading-4 font-semibold text-gray-200 uppercase tracking-wider inline-block">
-										{isComponentReady &&
-										!validatingUser &&
-										!errorUser &&
-										typeof user !== "undefined" &&
-										user !== null ? (
-											value.category
-										) : (
-											<Skeleton duration={2} width={128} height={16} />
-										)}
+										{isComponentReady ? value.category : <Skeleton duration={2} width={128} height={16} />}
 									</h3>
 
 									<div tw="my-3" role="group">
@@ -88,81 +69,51 @@ export function SettingsMenu() {
 														<a
 															className="group"
 															css={[
-																value2.url.includes(pathname) &&
-																isComponentReady &&
-																!validatingUser &&
-																!errorUser &&
-																typeof user !== "undefined" &&
-																user !== null
-																	? tw`bg-gray-1100`
+																tw`mt-1 flex items-center px-3 py-2 text-sm leading-5 font-medium rounded-md `,
+																asPath.includes(value2.url) && isComponentReady
+																	? tw`bg-gray-1100 !cursor-default`
 																	: null,
-																(value2.url.includes(pathname) || asPath?.includes(SettingsSlug)) &&
-																value2.url.includes(SettingsSlug) &&
-																isComponentReady &&
-																!validatingUser &&
-																!errorUser &&
-																typeof user !== "undefined" &&
-																user !== null
-																	? tw`cursor-pointer mt-1 flex items-center px-3 py-2 text-sm leading-5 font-medium text-gray-100 rounded-md hover:text-gray-100 focus:outline-none transition ease-in-out duration-150 hover:bg-gray-1100 focus:bg-gray-1100`
-																	: tw`mt-1 flex items-center px-3 py-2 text-sm leading-5 font-medium text-gray-400 rounded-md cursor-default`
+																asPath.includes(value2.url) ||
+																(asPath.includes(SettingsSlug) && SettingsSlug.includes(value2.url))
+																	? tw`text-gray-100`
+																	: tw`text-gray-400`,
+																isComponentReady
+																	? tw`cursor-pointer hover:text-gray-100 focus:outline-none transition ease-in-out duration-150 hover:bg-gray-1100 focus:bg-gray-1100`
+																	: tw`cursor-default`
 															]}
 														>
 															{value2.slug === "profile-settings" ? (
-																isComponentReady &&
-																!validatingUser &&
-																!errorUser &&
-																typeof user !== "undefined" &&
-																user !== null ? (
+																isComponentReady ? (
 																	<UserCircleIcon tw="mr-3 h-6 w-5" />
 																) : (
 																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
 																)
 															) : value2.slug === "subscription-plans" ? (
-																isComponentReady &&
-																!validatingUser &&
-																!errorUser &&
-																typeof user !== "undefined" &&
-																user !== null ? (
+																isComponentReady ? (
 																	<ViewBoardsIcon tw="mr-3 h-6 w-5" />
 																) : (
 																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
 																)
 															) : value2.slug === "billing-settings" ? (
-																isComponentReady &&
-																!validatingUser &&
-																!errorUser &&
-																typeof user !== "undefined" &&
-																user !== null ? (
+																isComponentReady ? (
 																	<CreditCardIcon tw="mr-3 h-6 w-5" />
 																) : (
 																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
 																)
 															) : value2.slug === "global-settings" ? (
-																isComponentReady &&
-																!validatingUser &&
-																!errorUser &&
-																typeof user !== "undefined" &&
-																user !== null ? (
+																isComponentReady ? (
 																	<GlobeIcon tw="mr-3 h-6 w-5" />
 																) : (
 																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
 																)
 															) : value2.slug === "subscription-plans" ? (
-																isComponentReady &&
-																!validatingUser &&
-																!errorUser &&
-																typeof user !== "undefined" &&
-																user !== null ? (
+																isComponentReady ? (
 																	<ViewBoardsIcon tw="mr-3 h-6 w-5" />
 																) : (
 																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
 																)
 															) : value2.slug === "help-support" ? (
-																isComponentReady &&
-																!validatingUser &&
-																!errorUser &&
-																typeof user !== "undefined" &&
-																user !== null ? (
+																isComponentReady ? (
 																	<SupportIcon tw="mr-3 h-6 w-5" />
 																) : (
 																	<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
@@ -171,15 +122,7 @@ export function SettingsMenu() {
 
 															{value2.title ? (
 																<span>
-																	{isComponentReady &&
-																	!validatingUser &&
-																	!errorUser &&
-																	typeof user !== "undefined" &&
-																	user !== null ? (
-																		value2.title
-																	) : (
-																		<Skeleton duration={2} width={128} height={20} />
-																	)}
+																	{isComponentReady ? value2.title : <Skeleton duration={2} width={128} height={20} />}
 																</span>
 															) : null}
 														</a>
@@ -190,20 +133,10 @@ export function SettingsMenu() {
 															className="group"
 															css={[
 																tw`mt-1 flex items-center py-2 text-sm leading-5 font-medium text-gray-400 rounded-md hover:text-gray-100 focus:outline-none focus:text-white`,
-																isComponentReady &&
-																!validatingUser &&
-																!errorUser &&
-																typeof user !== "undefined" &&
-																user !== null
-																	? tw`cursor-pointer`
-																	: null
+																isComponentReady ? tw`cursor-pointer` : null
 															]}
 														>
-															{isComponentReady &&
-															!validatingUser &&
-															!errorUser &&
-															typeof user !== "undefined" &&
-															user !== null ? (
+															{isComponentReady ? (
 																<ArrowLeftIcon tw="mr-3 h-6 w-5" />
 															) : (
 																<Skeleton duration={2} width={20} height={20} circle={true} tw="mr-3" />
@@ -211,15 +144,7 @@ export function SettingsMenu() {
 
 															{value2.title ? (
 																<span>
-																	{isComponentReady &&
-																	!validatingUser &&
-																	!errorUser &&
-																	typeof user !== "undefined" &&
-																	user !== null ? (
-																		value2.title
-																	) : (
-																		<Skeleton duration={2} width={128} height={20} />
-																	)}
+																	{isComponentReady ? value2.title : <Skeleton duration={2} width={128} height={20} />}
 																</span>
 															) : null}
 														</a>
