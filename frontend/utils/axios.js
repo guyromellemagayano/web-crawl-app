66b61384294bf232b5c62f26a61a379ed7c0aa0c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-01-25 00:31:35 +0800

    Refactor `axios` util function

--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -1,45 +1,28 @@
 import { customAxiosHeaders } from "@constants/CustomAxiosHeaders";
 import { SITE_URL } from "@constants/ServerEnv";
-import * as Sentry from "@sentry/nextjs";
 import axios from "axios";
 
+// Custom `axios` instance
 const AppAxiosInstance = axios.create({
 	baseURL: SITE_URL,
 	headers: {
 		...customAxiosHeaders
 	},
 	validateStatus: function (status) {
-		return status >= 200 && status < 500;
+		return status >= 200 && status < 600;
 	},
 	withCredentials: true
 });
 
 // Use `axios` interceptors for all HTTP methods (GET, POST, PUT, DELETE, etc.)
-axios.interceptors.request.use(
+AppAxiosInstance.interceptors.request.use(
 	(req) => req,
 	(err) => Promise.reject(err)
 );
 
-axios.interceptors.response.use(
+AppAxiosInstance.interceptors.response.use(
 	(res) => res,
-	(err) => {
-		if (err.response) {
-			// Capture `response` errors and send to Sentry
-			Sentry.captureException(err.response);
-
-			return err.response;
-		} else if (err.request) {
-			// Capture `request` errors and send to Sentry
-			Sentry.captureException(err.request);
-
-			return err.request;
-		} else {
-			// Capture other errors and send to Sentry
-			Sentry.captureException(err);
-
-			return Promise.reject(err);
-		}
-	}
+	(err) => Promise.reject(err)
 );
 
 export default AppAxiosInstance;
