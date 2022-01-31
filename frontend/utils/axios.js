96b7b4c67f6be44183ac1c1376de133c20333760
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-01-31 16:37:00 +0800

    Apply timeout in request and response `axios` interceptors

--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -1,28 +1,49 @@
-import { customAxiosHeaders } from "@constants/CustomAxiosHeaders";
+import { MutateInterval } from "@constants/GlobalValues";
 import { SITE_URL } from "@constants/ServerEnv";
+import * as Sentry from "@sentry/nextjs";
 import axios from "axios";
+import Cookies from "js-cookie";
+import { sleep } from "./sleep";
 
 // Custom `axios` instance
 const AppAxiosInstance = axios.create({
 	baseURL: SITE_URL,
 	headers: {
-		...customAxiosHeaders
+		"Accept": "application/json",
+		"Content-Type": "application/json",
+		"X-CSRFToken": Cookies.get("csrftoken") ?? null
 	},
 	validateStatus: function (status) {
 		return status >= 200 && status < 600;
-	},
-	withCredentials: true
+	}
 });
 
 // Use `axios` interceptors for all HTTP methods (GET, POST, PUT, DELETE, etc.)
 AppAxiosInstance.interceptors.request.use(
-	(req) => req,
+	(req) => sleep(MutateInterval, req),
 	(err) => Promise.reject(err)
 );
 
 AppAxiosInstance.interceptors.response.use(
-	(res) => res,
-	(err) => Promise.reject(err)
+	(res) => sleep(MutateInterval, res),
+	(err) => {
+		if (err.response) {
+			// Capture `response` errors and send to Sentry
+			Sentry.captureException(err.response);
+
+			return err.response;
+		} else if (err.request) {
+			// Capture `request` errors and send to Sentry
+			Sentry.captureException(err.request);
+
+			return err.request;
+		} else {
+			// Capture other errors and send to Sentry
+			Sentry.captureException(err);
+
+			return Promise.reject(err);
+		}
+	}
 );
 
 export default AppAxiosInstance;
