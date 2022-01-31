1fabfc22481a1a9cb64572316e8950a18a803620
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-02-01 06:21:48 +0800

    Refactor loading message component

--- frontend/components/messages/LoadingMessage.jsx
+++ frontend/components/messages/LoadingMessage.jsx
@@ -14,11 +14,9 @@ const LoadingMessage = ({ message = null }) => {
 	const loaderMessage = t("loaderMessage");
 
 	return (
-		<section tw="flex flex-col justify-center min-h-screen">
-			<div tw="px-4 py-5 sm:p-6 flex items-center justify-center">
-				<h3 tw="text-lg leading-6 font-medium text-gray-500">{message !== null ? message : loaderMessage}</h3>
-			</div>
-		</section>
+		<h3 tw="text-lg leading-6 font-medium text-gray-500">
+			{message !== null && message?.length > 0 && typeof message === "string" ? message : loaderMessage}
+		</h3>
 	);
 };
 
