c69791c1adb8afd4adcd30402c5fca066d968a71
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-02-08 06:10:53 +0800

    Refactor sites dashboard page layout

--- frontend/pages/dashboard/sites/index.jsx
+++ frontend/pages/dashboard/sites/index.jsx
@@ -1,38 +1,23 @@
 import { Layout } from "@components/layouts";
 import { MemoizedPageLayout } from "@components/layouts/components/Page";
 import { MemoizedComingSoonPageLayout } from "@components/layouts/pages/ComingSoon";
-import { MemoizedLoginPageLayout } from "@components/layouts/pages/Login";
-import { MemoizedLoader } from "@components/loaders";
-import { useUser } from "@hooks/useUser";
 import { NextSeo } from "next-seo";
 import useTranslation from "next-translate/useTranslation";
-import { useEffect } from "react";
 
 export default function Sites() {
-	// SWR hooks
-	const { user } = useUser();
-
 	// Translations
 	const { t } = useTranslation("sites");
 	const sitesDashboard = t("sitesDashboard");
 
-	useEffect(() => {
-		if (Math.round(user?.status / 100) === 4) {
-			return <MemoizedLoginPageLayout />;
-		} else {
-			if (Math.round(user?.status / 100) === 2) {
-				<>
-					<NextSeo title={sitesDashboard} />
-					<MemoizedPageLayout pageTitle={sitesDashboard}>
-						<MemoizedComingSoonPageLayout />
-						{/* <MemoizedSitesDashboardPageLayout /> */}
-					</MemoizedPageLayout>
-				</>;
-			}
-		}
-	}, [user]);
-
-	return <MemoizedLoader />;
+	return (
+		<>
+			<NextSeo title={sitesDashboard} />
+			<MemoizedPageLayout pageTitle={sitesDashboard}>
+				<MemoizedComingSoonPageLayout />
+				{/* <MemoizedSitesDashboardPageLayout /> */}
+			</MemoizedPageLayout>
+		</>
+	);
 }
 
 Sites.getLayout = function getLayout(page) {
