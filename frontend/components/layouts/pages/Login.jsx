6d7e74d60928d552cb7e5d902382b15f288cff06
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-02-08 00:42:21 +0800

    Fix UI of login page layout

--- frontend/components/layouts/pages/Login.jsx
+++ frontend/components/layouts/pages/Login.jsx
@@ -28,44 +28,42 @@ const LoginPageLayout = () => {
 			<NextSeo title={loginText} />
 			<Scrollbars universal>
 				<section tw="h-screen bg-gray-50 flex flex-col justify-center relative overflow-auto py-12 sm:px-6 lg:px-8">
-					<div tw="mt-8 sm:mt-16 md:mt-20 lg:mt-24">
-						<div tw="mx-auto max-w-screen-xl">
-							<div tw="lg:grid lg:grid-cols-12 lg:gap-8">
-								<div tw="px-4 sm:px-6 sm:text-center md:max-w-2xl md:mx-auto lg:col-span-7 lg:text-left lg:flex lg:items-center">
-									<div>
-										<SiteLogoDark className="flex" width={NoAuthAppLogo.width} height={NoAuthAppLogo.height} />
-										<h4 tw="mt-4 text-4xl tracking-tight text-center lg:text-left leading-10 font-bold text-gray-900 sm:mt-5 sm:leading-none">
-											{headline}&nbsp;
-											<span tw="text-red-600">{highlight}</span>
-											<br tw="hidden md:inline" />
-										</h4>
-									</div>
+					<div tw="mx-auto max-w-screen-xl">
+						<div tw="lg:grid lg:grid-cols-12 lg:gap-8">
+							<div tw="px-4 sm:px-6 sm:text-center md:max-w-2xl md:mx-auto lg:col-span-7 lg:text-left lg:flex lg:items-center">
+								<div>
+									<SiteLogoDark className="flex" width={NoAuthAppLogo.width} height={NoAuthAppLogo.height} />
+									<h4 tw="mt-4 text-4xl tracking-tight text-center lg:text-left leading-10 font-bold text-gray-900 sm:mt-5 sm:leading-none">
+										{headline}&nbsp;
+										<span tw="text-red-600">{highlight}</span>
+										<br tw="hidden md:inline" />
+									</h4>
 								</div>
-								<div tw="mt-12 sm:mt-16 lg:mt-0 lg:col-span-5">
-									<MemoizedLogoLabel isLogin />
+							</div>
+							<div tw="mt-12 sm:mt-16 lg:mt-0 lg:col-span-5">
+								<MemoizedLogoLabel isLogin />
 
-									<div tw="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-										<div tw="bg-white mt-8 py-8 px-4 shadow-xl rounded-lg sm:px-10">
-											<MemoizedLoginForm />
-										</div>
+								<div tw="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+									<div tw="bg-white mt-8 py-8 px-4 shadow-xl rounded-lg sm:px-10">
+										<MemoizedLoginForm />
+									</div>
 
-										<div tw="relative flex justify-center flex-wrap flex-row text-sm leading-5">
-											<span tw="px-2 py-5 text-gray-500">
-												{noAccount}&nbsp;
-												<Link href={RegistrationLink} passHref>
-													<a tw="font-medium text-indigo-600 cursor-pointer hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-														{createAccount}
-													</a>
-												</Link>
-											</span>
-										</div>
+									<div tw="relative flex justify-center flex-wrap flex-row text-sm leading-5">
+										<span tw="px-2 py-5 text-gray-500">
+											{noAccount}&nbsp;
+											<Link href={RegistrationLink} passHref>
+												<a tw="font-medium text-indigo-600 cursor-pointer hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+													{createAccount}
+												</a>
+											</Link>
+										</span>
 									</div>
 								</div>
 							</div>
+						</div>
 
-							<div tw="px-4 xl:px-10 xl:mt-32">
-								<MemoizedFooter />
-							</div>
+						<div tw="px-4 xl:px-10 xl:mt-32">
+							<MemoizedFooter />
 						</div>
 					</div>
 				</section>
