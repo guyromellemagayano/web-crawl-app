b9dcf8a96bab01fe00ee8c68d39734fb61dd6f29
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-02-08 16:16:50 +0800

    Fix list components UI and sites loading features

--- frontend/components/lists/SiteList.jsx
+++ frontend/components/lists/SiteList.jsx
@@ -1,12 +1,11 @@
-import { MemoizedAlert } from "@components/alerts";
 import { DashboardSitesLink, SiteOverviewSlug } from "@constants/PageLinks";
-import { useAlertMessage } from "@hooks/useAlertMessage";
+import { useNotificationMessage } from "@hooks/useNotificationMessage";
 import { useScan } from "@hooks/useScan";
 import { useStats } from "@hooks/useStats";
 import { handleConversionStringToBoolean, handleConversionStringToNumber } from "@utils/convertCase";
 import Link from "next/link";
 import PropTypes from "prop-types";
-import { memo, useCallback, useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import tw from "twin.macro";
@@ -23,173 +22,109 @@ const SiteList = ({ data = null }) => {
 	const [scanObjId, setScanObjId] = useState(null);
 
 	// Custom hooks
-	const { state, setConfig } = useAlertMessage();
+	const { state, setConfig } = useNotificationMessage();
 
 	// SWR hooks
-	const { scan, errorScan, validatingScan } = useScan(data?.id ?? null);
-
-	// TODO: Error handling for failed response
-	useEffect(() => {
-		// Show alert message after failed response is issued
-		errorScan
-			? setConfig({
-					isScan: true,
-					method: errorScan?.config?.method ?? null,
-					status: errorScan?.status ?? null
-			  })
-			: null;
-	}, [errorScan]);
+	const { scan, errorScan } = useScan(data?.id ?? null);
 
 	// Handle `scan` response
-	const handleScanResponse = useCallback(
-		async (isMounted) => {
-			if (!isMounted) return;
-
-			if (!validatingScan) {
-				const scanResponse = scan;
-				const scanResponseData = scanResponse?.data ?? null;
-
-				if (scanResponseData !== null && Object.keys(scanResponseData)?.length > 0) {
-					const currentScanCount = scanResponseData?.count
-						? handleConversionStringToNumber(scanResponseData.count)
-						: null;
-					const currentScanFinishedAt = scanResponseData?.results?.[0]?.finished_at
-						? scanResponseData.results[0].finished_at
-						: null;
-					const currentScanForcehttps = scanResponseData?.results?.[0]?.force_https
-						? handleConversionStringToBoolean(scanResponseData.results[0].force_https)
-						: null;
-					const currentScanObjId =
-						currentScanFinishedAt !== null && currentScanForcehttps !== null && currentScanCount > 1
-							? scanResponseData?.results?.[1]?.id
-								? handleConversionStringToNumber(scanResponseData.results[1].id)
-								: null
-							: scanResponseData?.results?.[0]?.id
-							? handleConversionStringToNumber(scanResponseData.results[0].id)
-							: null;
-
-					setScanFinishedAt(currentScanFinishedAt);
-					setScanForceHttps(currentScanForcehttps);
-					setScanCount(currentScanCount);
-					setScanObjId(currentScanObjId);
-
-					return { scanFinishedAt, scanForceHttps, scanCount, scanObjId };
-				} else return;
-			} else return;
-		},
-		[scan]
-	);
-
 	useEffect(() => {
-		let isMounted = true;
+		if (scan) {
+			const currentScanCount = scan.data.count ? handleConversionStringToNumber(scan?.data.count) : null;
+			const currentScanFinishedAt = scan.data.results?.[0]?.finished_at ? scan?.data.results[0].finished_at : null;
+			const currentScanForcehttps = scan.data.results?.[0]?.force_https
+				? handleConversionStringToBoolean(scan?.data.results[0].force_https)
+				: null;
+			const currentScanObjId =
+				currentScanFinishedAt !== null && currentScanForcehttps !== null && currentScanCount > 1
+					? scan.data.results?.[1]?.id
+						? handleConversionStringToNumber(scan?.data.results[1].id)
+						: null
+					: scan.data.results?.[0]?.id
+					? handleConversionStringToNumber(scan?.data.results[0].id)
+					: null;
 
-		handleScanResponse(isMounted);
+			setScanFinishedAt(currentScanFinishedAt);
+			setScanForceHttps(currentScanForcehttps);
+			setScanCount(currentScanCount);
+			setScanObjId(currentScanObjId);
 
-		return () => {
-			isMounted = false;
-		};
-	}, [handleScanResponse]);
+			return { scanFinishedAt, scanForceHttps, scanCount, scanObjId };
+		} else return;
+	}, [scan]);
 
 	// SWR hooks
-	const { stats, errorStats, validatingStats } = useStats(data?.id, scanObjId);
+	const { stats, errorStats } = useStats(data?.id, scanObjId);
 
-	// TODO: Error handling for failed response
-	useEffect(() => {
-		// Show alert message after failed response is issued
-		errorStats
-			? setConfig({
-					isStats: true,
-					method: errorStats?.config?.method ?? null,
-					status: errorStats?.status ?? null
-			  })
-			: null;
-	}, [errorStats]);
+	return !stats && !scan ? (
+		<li>
+			<div tw="cursor-default w-full select-none block relative py-2 pl-3 pr-9">
+				<div tw="flex items-center space-x-3">
+					<Skeleton circle={true} duration={2} width={10} height={10} tw="relative top-0.5" />
 
-	return data !== null && !errorStats ? (
-		<>
-			{state?.responses !== [] && state?.responses?.length > 0 ? (
-				<div tw="fixed right-6 bottom-6 grid grid-flow-row gap-4">
-					{state?.responses?.map((value, key) => {
-						// Alert Messsages
-						const responseText = value?.responseText ?? null;
-						const isSuccess = value?.isSuccess ?? null;
-
-						return <MemoizedAlert key={key} responseText={responseText} isSuccess={isSuccess} />;
-					}) ?? null}
+					<span tw="block">
+						<Skeleton duration={2} width={130} />
+					</span>
 				</div>
-			) : null}
-
-			{!validatingStats && !validatingScan ? (
-				<li
-					id={`listbox-item-${data.id}`}
-					role="option"
-					aria-selected={
-						scanCount > 0 && (stats?.data?.num_links > 0 || stats?.data?.num_pages > 0 || stats?.data?.num_images > 0)
-							? true
-							: false
+			</div>
+		</li>
+	) : (
+		<li
+			id={`listbox-item-${data.id}`}
+			role="option"
+			aria-selected={
+				scanCount > 0 && (stats?.data?.num_links > 0 || stats?.data?.num_pages > 0 || stats?.data?.num_images > 0)
+					? true
+					: false
+			}
+		>
+			<Link
+				href={{
+					pathname: `${DashboardSitesLink}[siteId]${SiteOverviewSlug}`,
+					query: {
+						siteId: data.id
 					}
+				}}
+				passHref
+				replace
+			>
+				<a
+					css={[
+						tw`w-full select-none block relative py-2 pl-3 pr-9 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900`,
+						(scanCount > 0 &&
+							(stats?.data?.num_links > 0 || stats?.data?.num_pages > 0 || stats?.data?.num_images > 0)) ||
+						(scan && stats)
+							? tw`cursor-pointer`
+							: tw`cursor-not-allowed`
+					]}
 				>
-					<Link
-						href={{
-							pathname: `${DashboardSitesLink}[siteId]${SiteOverviewSlug}`,
-							query: {
-								siteId: data.id
+					<div tw="flex items-center space-x-3">
+						<span
+							aria-label={
+								data.verified
+									? scanFinishedAt !== null && scanForceHttps !== null
+										? "Verified"
+										: "Recrawling in Process"
+									: "Not Verified"
 							}
-						}}
-						passHref
-						replace
-					>
-						<a
 							css={[
-								tw`w-full select-none block relative py-2 pl-3 pr-9 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900`,
-								(scanCount > 0 &&
-									(stats?.data?.num_links > 0 || stats?.data?.num_pages > 0 || stats?.data?.num_images > 0)) ||
-								(!validatingScan && !validatingStats)
-									? tw`cursor-pointer`
-									: tw`cursor-not-allowed`
+								tw`flex-shrink-0 inline-block h-2 w-2 rounded-full`,
+								data.verified
+									? scanFinishedAt !== null && scanForceHttps !== null
+										? tw`bg-green-400`
+										: tw`bg-yellow-400`
+									: tw`bg-red-400`
 							]}
-						>
-							<div tw="flex items-center space-x-3">
-								<span
-									aria-label={
-										data.verified
-											? scanFinishedAt !== null && scanForceHttps !== null
-												? "Verified"
-												: "Recrawling in Process"
-											: "Not Verified"
-									}
-									css={[
-										tw`flex-shrink-0 inline-block h-2 w-2 rounded-full`,
-										data.verified
-											? scanFinishedAt !== null && scanForceHttps !== null
-												? tw`bg-green-400`
-												: tw`bg-yellow-400`
-											: tw`bg-red-400`
-									]}
-								/>
+						/>
 
-								<span css={[tw`font-medium block truncate`, data.verified ? tw`text-gray-500` : tw`text-gray-400`]}>
-									{data.name}
-								</span>
-							</div>
-						</a>
-					</Link>
-				</li>
-			) : (
-				<li>
-					<div tw="cursor-default w-full select-none block relative py-2 pl-3 pr-9">
-						<div tw="flex items-center space-x-3">
-							<Skeleton circle={true} duration={2} width={10} height={10} tw="relative top-0.5" />
-
-							<span tw="block">
-								<Skeleton duration={2} width={130} />
-							</span>
-						</div>
+						<span css={[tw`font-medium block truncate`, data.verified ? tw`text-gray-500` : tw`text-gray-400`]}>
+							{data.name}
+						</span>
 					</div>
-				</li>
-			)}
-		</>
-	) : null;
+				</a>
+			</Link>
+		</li>
+	);
 };
 
 SiteList.propTypes = {
