03e93ccc9c3bdfcf157274d0f757b106cbc6570a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-02-08 08:25:30 +0800

    Refactor delete user account modal to use updated notifications

--- frontend/components/modals/DeleteUserAccountModal.jsx
+++ frontend/components/modals/DeleteUserAccountModal.jsx
@@ -1,13 +1,15 @@
 import { UserApiEndpoint } from "@constants/ApiEndpoints";
 import { ModalDisplayInterval } from "@constants/GlobalValues";
-import { HomeLink, LoginLink } from "@constants/PageLinks";
+import { LoginLink } from "@constants/PageLinks";
 import { Transition } from "@headlessui/react";
 import { handleDeleteMethod } from "@helpers/handleHttpMethods";
 import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/outline";
+import { useNotificationMessage } from "@hooks/useNotificationMessage";
 import { useUser } from "@hooks/useUser";
 import useTranslation from "next-translate/useTranslation";
 import { useRouter } from "next/router";
-import { forwardRef, memo, useCallback, useEffect, useState } from "react";
+import { forwardRef, memo, useCallback, useEffect, useMemo, useState } from "react";
+import { useSWRConfig } from "swr";
 import tw from "twin.macro";
 
 /**
@@ -18,24 +20,12 @@ import tw from "twin.macro";
  */
 const DeleteUserAccountModal = ({ showModal = false, setShowModal }, ref) => {
 	const [userIdApiEndpoint, setUserIdApiEndpoint] = useState(null);
-	const [errorHeadline, setErrorHeadline] = useState(null);
-	const [errorSubheadline, setErrorSubheadline] = useState(null);
-	const [successHeadline, setSuccessHeadline] = useState(null);
-	const [successSubheadline, setSuccessSubheadline] = useState(null);
 	const [hideButtons, setHideButtons] = useState(false);
 	const [isDeleted, setIsDeleted] = useState(false);
 	const [disableDeleteUser, setDisableDeleteUser] = useState(false);
 
 	// Translations
 	const { t } = useTranslation();
-	const deleteUserAccountModalRequestHeadline = t("settings:deleteUserAccountModalRequest.headline");
-	const deleteUserAccountModalRequestSubheadline = t("settings:deleteUserAccountModalRequest.subHeadline");
-	const deleteUserAccountModalRequestSuccessHeadline = t("settings:deleteUserAccountModalRequestSuccess.headline");
-	const deleteUserAccountModalRequestSuccessSubheadline = t(
-		"settings:deleteUserAccountModalRequestSuccess.subHeadline"
-	);
-	const deleteUserAccountModalRequestFailedHeadline = t("settings:deleteUserAccountModalRequestFailed.headline");
-	const deleteUserAccountModalRequestFailedSubheadline = t("settings:deleteUserAccountModalRequestFailed.subHeadline");
 	const processing = t("common:processing");
 	const proceed = t("common:proceed");
 	const cancel = t("common:cancel");
@@ -43,13 +33,19 @@ const DeleteUserAccountModal = ({ showModal = false, setShowModal }, ref) => {
 	// SWR hooks
 	const { user, errorUser, validatingUser } = useUser();
 
+	// Custom context
+	const { state, setConfig } = useNotificationMessage();
+
 	// Router
 	const router = useRouter();
+
+	// SWR hook for global mutations
+	const { mutate } = useSWRConfig();
 
 	// Handle the `userId` API endpoint
 	const handleUserIdApiEndpoint = useCallback(async () => {
 		if (!validatingUser) {
-			if (!errorUser && typeof user !== "undefined" && user !== null && !user?.data?.detail) {
+			if (!errorUser && user && !user?.data?.detail && user?.data?.id) {
 				setUserIdApiEndpoint(`${UserApiEndpoint + user?.data?.id}`);
 			}
 		}
@@ -69,44 +65,61 @@ const DeleteUserAccountModal = ({ showModal = false, setShowModal }, ref) => {
 		const deleteUserAccountResponse = await handleDeleteMethod(userIdApiEndpoint);
 		const deleteUserAccountResponseData = deleteUserAccountResponse?.data ?? null;
 		const deleteUserAccountResponseStatus = deleteUserAccountResponse?.status ?? null;
+		const deleteUserAccountResponseMethod = deleteUserAccountResponse?.config?.method ?? null;
 
 		if (deleteUserAccountResponseData !== null && Math.round(deleteUserAccountResponseStatus / 200) === 1) {
-			setDisableDeleteUser(false);
-			setSuccessHeadline(deleteUserAccountModalRequestSuccessHeadline);
-			setSuccessSubheadline(deleteUserAccountModalRequestSuccessSubheadline);
+			// Mutate `user` endpoint after successful 200 OK or 201 Created response is issued
+			await mutate(UserApiEndpoint, false);
 
+			// Show alert message after successful 200 OK or 201 Created response is issued
+			setConfig({
+				isUser: true,
+				method: deleteUserAccountResponseMethod,
+				status: deleteUserAccountResponseStatus
+			});
+
+			// Close the modal after successful 200 OK or 201 Created response is issued
 			setTimeout(() => {
 				setShowModal(!showModal);
 			}, ModalDisplayInterval);
 
+			// Reenable submission as soon as 200 OK or 201 Created response is issued
+			setDisableDeleteUser(false);
 			setHideButtons(true);
 			setIsDeleted(true);
 		} else {
-			setDisableDeleteUser(false);
-			setErrorHeadline(deleteUserAccountModalRequestFailedHeadline);
-			setErrorSubheadline(deleteUserAccountModalRequestFailedSubheadline);
+			// Show alert message after successful 200 OK or 201 Created response is issued
+			setConfig({
+				isUser: true,
+				method: deleteUserAccountResponseMethod,
+				status: deleteUserAccountResponseStatus
+			});
 
+			// Reenable submission as soon as 200 OK or 201 Created response is issued
+			setDisableDeleteUser(false);
 			setHideButtons(false);
 			setIsDeleted(false);
 		}
 	};
 
-	useEffect(() => {
-		return () => {
+	useMemo(() => {
+		let isMounted = true;
+
+		(() => {
+			if (!isMounted) return;
+
 			if (isDeleted && hideButtons) {
-				setSuccessHeadline(null);
-				setSuccessSubheadline(null);
 				setHideButtons(false);
 				setIsDeleted(false);
 
-				router.push(HomeLink);
+				router.push(LoginLink);
 			}
+		})();
+
+		return () => {
+			isMounted = false;
 		};
 	}, [isDeleted, hideButtons]);
-
-	useEffect(() => {
-		router.prefetch(LoginLink);
-	}, []);
 
 	return (
 		<Transition show={showModal}>
@@ -134,91 +147,83 @@ const DeleteUserAccountModal = ({ showModal = false, setShowModal }, ref) => {
 					leaveFrom="delete-user-account-modal-second-child-leave-from"
 					leaveTo="delete-user-account-modal-second-child-leave-to"
 				>
-					<div
-						aria-labelledby="modal-headline"
-						aria-modal="true"
-						role="dialog"
-						tw="bg-white rounded-lg px-4 pt-5 pb-4 overflow-hidden transform transition-all sm:max-w-lg sm:w-full sm:p-6"
-					>
-						<div tw="sm:flex sm:items-start">
+					{state?.responses?.map((value, key) => {
+						// Alert Messsages
+						const responseTitle = value.responseTitle ?? null;
+						const responseText = value.responseText ?? null;
+						const isSuccess = value.isSuccess ?? null;
+
+						return (
 							<div
-								css={[
-									tw`mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full sm:mx-0 sm:h-10 sm:w-10`,
-									successHeadline !== null && successSubheadline !== null ? tw`bg-green-100` : tw`bg-red-100`
-								]}
+								key={key}
+								aria-labelledby="modal-headline"
+								aria-modal="true"
+								role="dialog"
+								tw="bg-white rounded-lg px-4 pt-5 pb-4 overflow-hidden transform transition-all sm:max-w-lg sm:w-full sm:p-6"
 							>
-								{successHeadline !== null && successSubheadline !== null ? (
-									<CheckCircleIcon tw="h-6 w-6 text-green-600" />
-								) : (
-									<XCircleIcon tw="h-6 w-6 text-red-600" />
-								)}
-							</div>
-							<div tw="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-								<h3 tw="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-									{errorHeadline !== null
-										? errorHeadline
-										: successHeadline !== null
-										? successHeadline
-										: deleteUserAccountModalRequestHeadline}
-								</h3>
-
-								<div tw="mt-2">
-									<p tw="text-sm leading-5 text-gray-500">
-										{errorSubheadline !== null
-											? errorSubheadline
-											: successSubheadline !== null
-											? successSubheadline
-											: deleteUserAccountModalRequestSubheadline}
-									</p>
-								</div>
-							</div>
-						</div>
-
-						{!hideButtons ? (
-							<div tw="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-								<span tw="flex w-full sm:w-auto">
-									{errorHeadline !== null &&
-									errorHeadline.length > 0 &&
-									errorSubheadline !== null &&
-									errorSubheadline?.length > 0 ? null : (
-										<button
-											type="button"
-											disabled={disableDeleteUser}
-											css={[
-												tw`sm:ml-3 cursor-pointer inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-red-600 text-sm leading-5 font-medium text-white shadow-sm sm:text-sm sm:leading-5 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition ease-in-out duration-150`,
-												disableDeleteUser
-													? tw`opacity-50 cursor-not-allowed`
-													: tw`hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 active:bg-red-700`
-											]}
-											aria-label="Delete User"
-											onClick={handleUserDeletion}
-										>
-											{disableDeleteUser ? processing : proceed}
-										</button>
-									)}
-
-									<button
-										type="button"
-										disabled={disableDeleteUser}
+								<div tw="sm:flex sm:items-start">
+									<div
 										css={[
-											tw`inline-flex justify-center w-full rounded-md border border-gray-300 sm:ml-3 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 shadow-sm sm:text-sm sm:leading-5`,
-											disableDeleteUser
-												? tw`opacity-50 cursor-not-allowed`
-												: tw`cursor-pointer hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition ease-in-out duration-150`
+											tw`mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full sm:mx-0 sm:h-10 sm:w-10`,
+											isSuccess ? tw`bg-green-100` : tw`bg-red-100`
 										]}
-										onClick={() => setShowModal(false)}
 									>
-										{errorHeadline !== null &&
-										errorHeadline.length > 0 &&
-										errorSubheadline !== null &&
-										errorSubheadline?.length > 0
-											? close
-											: cancel}
-									</button>
-								</span>
+										{isSuccess ? (
+											<CheckCircleIcon tw="h-6 w-6 text-green-600" />
+										) : (
+											<XCircleIcon tw="h-6 w-6 text-red-600" />
+										)}
+									</div>
+									<div tw="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+										<h3 tw="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+											{responseTitle}
+										</h3>
+
+										<div tw="mt-2">
+											<p tw="text-sm leading-5 text-gray-500">{responseText}</p>
+										</div>
+									</div>
+								</div>
+
+								{!hideButtons ? (
+									<div tw="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+										<span tw="flex w-full sm:w-auto">
+											{!isSuccess ? null : (
+												<button
+													type="button"
+													disabled={disableDeleteUser}
+													css={[
+														tw`sm:ml-3 cursor-pointer inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-red-600 text-sm leading-5 font-medium text-white shadow-sm sm:text-sm sm:leading-5 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition ease-in-out duration-150`,
+														disableDeleteUser
+															? tw`opacity-50 cursor-not-allowed`
+															: tw`hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 active:bg-red-700`
+													]}
+													aria-label="Delete User"
+													onClick={handleUserDeletion}
+												>
+													{disableDeleteUser ? processing : proceed}
+												</button>
+											)}
+
+											<button
+												type="button"
+												disabled={disableDeleteUser}
+												css={[
+													tw`inline-flex justify-center w-full rounded-md border border-gray-300 sm:ml-3 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 shadow-sm sm:text-sm sm:leading-5`,
+													disableDeleteUser
+														? tw`opacity-50 cursor-not-allowed`
+														: tw`cursor-pointer hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition ease-in-out duration-150`
+												]}
+												onClick={() => setShowModal(false)}
+											>
+												{!isSuccess ? close : cancel}
+											</button>
+										</span>
+									</div>
+								) : null}
 							</div>
-						) : null}
-					</div>
+						);
+					}) ?? null}
 				</Transition.Child>
 			</div>
 		</Transition>
