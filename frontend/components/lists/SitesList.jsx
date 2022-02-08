d49e1be79e9174466773c64c73cac61674e063a9
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-02-08 15:59:29 +0800

    Refactor sites list component

--- frontend/components/lists/SitesList.jsx
+++ frontend/components/lists/SitesList.jsx
@@ -1,5 +1,4 @@
-import { MemoizedAlert } from "@components/alerts";
-import { useAlertMessage } from "@hooks/useAlertMessage";
+import { useNotificationMessage } from "@hooks/useNotificationMessage";
 import { useSites } from "@hooks/useSites";
 import useTranslation from "next-translate/useTranslation";
 import PropTypes from "prop-types";
@@ -22,12 +21,11 @@ const SitesList = ({ isOpen = false }) => {
 	const loaderMessage = t("common:loaderMessage");
 
 	// Custom hooks
-	const { state, setConfig } = useAlertMessage();
+	const { setConfig } = useNotificationMessage();
 
 	// `sites` SWR hook
 	const { sites, errorSites, validatingSites } = useSites();
 
-	// TODO: Error handling for `sites` SWR hook
 	useMemo(() => {
 		// Show alert message after failed `sites` SWR hook fetch
 		errorSites
@@ -39,48 +37,24 @@ const SitesList = ({ isOpen = false }) => {
 			: null;
 	}, [errorSites]);
 
-	return (
-		// Load HTML is `sites` hook are not revalidating
-		<>
-			{state?.responses !== [] && state?.responses?.length > 0 ? (
-				<div tw="fixed right-6 bottom-6 grid grid-flow-row gap-4">
-					{state?.responses?.map((value, key) => {
-						// Alert Messsages
-						const responseText = value?.responseText ?? null;
-						const isSuccess = value?.isSuccess ?? null;
-
-						return <MemoizedAlert key={key} responseText={responseText} isSuccess={isSuccess} />;
-					}) ?? null}
-				</div>
-			) : null}
-
-			{!errorSites && !validatingSites ? (
-				sites?.data?.count > 0 && sites?.data?.results?.length > 0 ? (
-					<ul
-						tabIndex="-1"
-						role="listbox"
-						aria-labelledby="listbox-label"
-						tw="pt-2 h-48 text-base leading-6 overflow-auto focus:outline-none sm:text-sm sm:leading-5"
-					>
-						<Scrollbars universal>
-							{sites.data.results.map((value) => (
-								<MemoizedSiteList key={value.id} data={value} />
-							))}
-						</Scrollbars>
-					</ul>
-				) : (
-					// Show message if no sites are available
-					<span tw="w-full h-48 flex items-center justify-center">
-						<p tw="text-sm pt-6 pb-2 leading-6 font-medium text-gray-500">{noAvailableSites}</p>
-					</span>
-				)
-			) : (
-				// Show loading message if `sites` SWR hook is revalidating
-				<span tw="w-full h-48 flex items-center justify-center">
-					<p tw="text-sm pt-6 pb-2 leading-6 font-medium text-gray-500">{loaderMessage}</p>
-				</span>
-			)}
-		</>
+	return sites?.data?.count > 0 && sites?.data?.results?.length > 0 ? (
+		<ul
+			tabIndex="-1"
+			role="listbox"
+			aria-labelledby="listbox-label"
+			tw="h-48 text-base leading-6 overflow-auto focus:outline-none sm:text-sm sm:leading-5"
+		>
+			<Scrollbars universal>
+				{sites.data.results.map((value) => (
+					<MemoizedSiteList key={value.id} data={value} />
+				))}
+			</Scrollbars>
+		</ul>
+	) : (
+		// Show message if no sites are available
+		<span tw="w-full h-48 flex items-center justify-center">
+			<p tw="text-sm pt-6 pb-2 leading-6 font-medium text-gray-500">{noAvailableSites}</p>
+		</span>
 	);
 };
 
