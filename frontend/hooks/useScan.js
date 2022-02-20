e581426b2db279dc20056149bbac706284a5546f
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-02-20 15:01:33 +0800

    Fix crawl button and its crawl mechanism

--- frontend/hooks/useScan.js
+++ frontend/hooks/useScan.js
@@ -66,12 +66,14 @@ export const useScan = (querySid = null, options = null) => {
 			const startScanSlug = "/start_scan/";
 			let endpoint = `${SitesApiEndpoint + querySid + startScanSlug}`;
 
-			const currentSiteResponse = await handlePostMethod(endpoint);
-			const currentSiteResponseData = currentSiteResponse?.data ?? null;
-			const currentSiteResponseStatus = currentSiteResponse?.status ?? null;
-			const currentSiteResponseMethod = currentSiteResponse?.config?.method ?? null;
+			const currentScanResponse = await handlePostMethod(endpoint);
+			const currentScanResponseData = currentScanResponse?.data ?? null;
+			const currentScanResponseStatus = currentScanResponse?.status ?? null;
+			const currentScanResponseMethod = currentScanResponse?.config?.method ?? null;
 
-			if (currentSiteResponseData !== null && Math.round(currentSiteResponseStatus / 200) === 1) {
+			console.log(currentScanResponseData, currentScanResponseStatus, currentScanResponseMethod);
+
+			if (currentScanResponseData !== null && Math.round(currentScanResponseStatus / 200) === 1) {
 				// Set `isProcessing` state
 				setIsProcessing(false);
 
@@ -84,11 +86,9 @@ export const useScan = (querySid = null, options = null) => {
 				// Show alert message after successful `user` SWR hook fetch
 				setScanConfig({
 					isScan: true,
-					method: currentSiteResponseData?.config?.method ?? null,
-					status: currentSiteResponseData?.status ?? null
+					method: currentScanResponseData?.config?.method ?? null,
+					status: currentScanResponseData?.status ?? null
 				});
-
-				return true;
 			} else {
 				// Set `isProcessing` state
 				setIsProcessing(false);
@@ -102,13 +102,13 @@ export const useScan = (querySid = null, options = null) => {
 				// Show alert message after failed `user` SWR hook fetch
 				setScanConfig({
 					isScan: true,
-					method: currentSiteResponseData?.config?.method ?? null,
-					status: currentSiteResponseData?.status ?? null
+					method: currentScanResponseData?.config?.method ?? null,
+					status: currentScanResponseData?.status ?? null
 				});
-
-				return false;
 			}
 		}
+
+		return { isCrawlStarted, isCrawlFinished, isProcessing };
 	};
 
 	useMemo(async () => {
@@ -142,15 +142,23 @@ export const useScan = (querySid = null, options = null) => {
 			setCurrentScan(currentScanResult);
 			setPreviousScan(previousScanResult);
 
+			if (currentScan !== null) {
+				setIsCrawlStarted(true);
+				setIsCrawlFinished(false);
+			} else {
+				setIsCrawlStarted(false);
+				setIsCrawlFinished(true);
+			}
+
 			if ((currentScan !== null && previousScan !== null) || (currentScan == null && previousScan !== null)) {
-				setScanObjId(previousScan.id);
-			} else if (currentScan !== null && previousScan == null) {
-				setScanObjId(currentScan.id);
+				setScanObjId(previousScan?.id);
+			} else {
+				setScanObjId(currentScan?.id);
 			}
 		}
 
-		return { scanResults, scanCount, currentScan, previousScan, scanObjId };
-	}, [scan, scanResults, scanCount, currentScan, previousScan, scanObjId]);
+		return { scanResults, scanCount, currentScan, previousScan, scanObjId, isCrawlStarted, isCrawlFinished };
+	}, [scan, scanResults, scanCount, currentScan, previousScan, scanObjId, isCrawlStarted, isCrawlFinished]);
 
 	return {
 		currentScan,
