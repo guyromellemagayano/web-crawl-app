6c6a96a4f74ec7b63892146e9ad0a4aea5d0da31
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-02-21 17:01:11 +0800

    Update how links are loaded in links table component

--- frontend/components/tables/LinksTable.jsx
+++ frontend/components/tables/LinksTable.jsx
@@ -32,41 +32,43 @@ const LinksTable = ({ count = 0, results = [], validatingLinks = false }) => {
 		<section
 			css={[
 				tw`flex flex-col w-full min-h-full h-full`,
-				count > 0 && results?.length > 0 ? tw`justify-start` : tw`justify-center`
+				!validatingLinks && count > 0 && results?.length > 0 ? tw`justify-start` : tw`justify-center`
 			]}
 		>
-			{count > 0 && results?.length > 0 ? (
-				<table tw="relative w-full">
-					<thead>
-						<tr>
-							{labelsArray?.map((label) => (
-								<th
-									key={label.label}
-									className="min-width-adjust"
-									tw="px-6 py-3 border-b border-gray-200 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-								>
-									<span tw="flex items-center justify-start">
-										<MemoizedDataSorting slug={label.slug} labels={labelsArray} />
-										<span tw="flex items-center">{label.label}</span>
-									</span>
-								</th>
-							)) ?? null}
-						</tr>
-					</thead>
+			{!validatingLinks ? (
+				count > 0 && results?.length > 0 ? (
+					<table tw="relative w-full">
+						<thead>
+							<tr>
+								{labelsArray?.map((label) => (
+									<th
+										key={label.label}
+										className="min-width-adjust"
+										tw="px-6 py-3 border-b border-gray-200 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+									>
+										<span tw="flex items-center justify-start">
+											<MemoizedDataSorting slug={label.slug} labels={labelsArray} />
+											<span tw="flex items-center">{label.label}</span>
+										</span>
+									</th>
+								)) ?? null}
+							</tr>
+						</thead>
 
-					<tbody tw="relative divide-y divide-gray-200">
-						{results?.map((result) => {
-							return <MemoizedLinksData key={result.id} link={result} validatingLinks={validatingLinks} />;
-						}) ?? null}
-					</tbody>
-				</table>
+						<tbody tw="relative divide-y divide-gray-200">
+							{results?.map((result) => {
+								return <MemoizedLinksData key={result.id} link={result} validatingLinks={validatingLinks} />;
+							}) ?? null}
+						</tbody>
+					</table>
+				) : (
+					<div tw="px-4 py-5 sm:p-6 flex items-center justify-center">
+						<MemoizedLoadingMessage message={noAvailableLinks} />
+					</div>
+				)
 			) : (
 				<div tw="px-4 py-5 sm:p-6 flex items-center justify-center">
-					{validatingLinks ? (
-						<Skeleton duration={2} width={120} height={24} />
-					) : !validatingLinks && count === 0 && results?.length === 0 ? (
-						<MemoizedLoadingMessage message={noAvailableLinks} />
-					) : null}
+					<Skeleton duration={2} width={120} height={24} />
 				</div>
 			)}
 		</section>
