58c585ef7a425d6bb1c41f9f224976fbb93a16c4
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-02-22 23:11:54 +0800

    Refactor icon components

--- frontend/components/icons/SiteDangerIcon.jsx
+++ frontend/components/icons/SiteDangerIcon.jsx
@@ -1,6 +1,9 @@
 import { ShieldExclamationIcon } from "@heroicons/react/solid";
+import { memo } from "react";
 import "twin.macro";
 
-export const SiteDangerIcon = () => {
+const SiteDangerIcon = () => {
 	return <ShieldExclamationIcon tw="inline-block h-5 w-5 text-red-400" />;
 };
+
+export const MemoizedSiteDanagerIcon = memo(SiteDangerIcon);
