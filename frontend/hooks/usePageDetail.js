6d63223aa24638d67f8c8e92e3a3618ff886bdd8
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-02-23 22:27:18 +0800

    Update some custom hooks

--- frontend/hooks/usePageDetail.js
+++ frontend/hooks/usePageDetail.js
@@ -1,5 +1,7 @@
 import { SitesApiEndpoint } from "@constants/ApiEndpoints";
 import { ScanSlug, SitePageSlug } from "@constants/PageLinks";
+import { SiteCrawlerAppContext } from "@pages/_app";
+import { useContext, useMemo, useState } from "react";
 import { useMainSWRConfig } from "./useMainSWRConfig";
 
 /**
@@ -12,6 +14,12 @@ import { useMainSWRConfig } from "./useMainSWRConfig";
  * @returns {object} pageDetail, errorPageDetail, validatingPageDetail
  */
 export const usePageDetail = (querySid = null, scanObjId = null, linkId = null, options = null) => {
+	const [pageDetailId, setPageDetailId] = useState(0);
+	const [pageDetailPages, setPageDetailPages] = useState([]);
+
+	// Custom context
+	const { setConfig: setLinkDetailConfig } = useContext(SiteCrawlerAppContext);
+
 	const currentEndpoint =
 		querySid !== null &&
 		typeof querySid === "number" &&
@@ -24,11 +32,39 @@ export const usePageDetail = (querySid = null, scanObjId = null, linkId = null, 
 			? SitesApiEndpoint + querySid + ScanSlug + scanObjId + SitePageSlug + linkId + "/"
 			: null;
 
+	// SWR hook
 	const {
 		data: pageDetail,
 		error: errorPageDetail,
 		isValidating: validatingPageDetail
 	} = useMainSWRConfig(currentEndpoint, options);
+
+	useMemo(async () => {
+		if (errorPageDetail) {
+			// Show alert message after failed `pageDetail` SWR hook fetch
+			errorPageDetail
+				? setLinkDetailConfig({
+						isStats: true,
+						method: errorPageDetail?.config?.method ?? null,
+						status: errorPageDetail?.status ?? null
+				  })
+				: null;
+		}
+	}, [errorPageDetail]);
+
+	useMemo(async () => {
+		if (pageDetail?.data) {
+			if (pageDetail.data?.id) {
+				setPageDetailId(pageDetail.data.id);
+			}
+
+			if (pageDetail.data?.pages) {
+				setPageDetailPages(pageDetail.data.pages);
+			}
+		}
+
+		return { pageDetailId, pageDetailPages };
+	}, [pageDetail, pageDetailId, pageDetailPages]);
 
 	return { pageDetail, errorPageDetail, validatingPageDetail };
 };
