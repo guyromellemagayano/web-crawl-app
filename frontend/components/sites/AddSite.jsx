c3051ecb755ab913ff5611f917eb87a216006e28
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-03-02 07:08:12 +0800

    Fix buttons styling on add site component

--- frontend/components/sites/AddSite.jsx
+++ frontend/components/sites/AddSite.jsx
@@ -177,7 +177,7 @@ const AddSite = ({ handleOpenSidebar }) => {
 					hasSiteLimitReached ? (
 						<button
 							type="button"
-							className="inline-flex w-full cursor-pointer items-center justify-center rounded-md border border-transparent bg-yellow-600 px-4 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 active:bg-yellow-700"
+							className="inline-flex w-full cursor-pointer items-center rounded-md border border-transparent bg-yellow-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 active:bg-yellow-700"
 							onClick={() => setIsSiteLimitReachedModalVisible(!isSiteLimitReachedModalVisible)}
 						>
 							<span className="flex items-center space-x-2">
