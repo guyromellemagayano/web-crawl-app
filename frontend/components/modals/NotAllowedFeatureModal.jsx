98d1987fcd51f27f0b1f601c7e7625175bb7142c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-03-02 00:08:22 +0800

    Fix not allowed feature modal UI issue

--- frontend/components/modals/NotAllowedFeatureModal.jsx
+++ frontend/components/modals/NotAllowedFeatureModal.jsx
@@ -18,7 +18,7 @@ const NotAllowedFeatureModal = ({ showModal = false, setShowModal }, ref) => {
 
 	return (
 		<Transition.Root show={showModal} as={Fragment}>
-			<Dialog as="div" className="not-allowed-feature-modal-dialog" initialFocus={ref} onClose={setShowModal}>
+			<Dialog as="div" className="fixed inset-0 z-50 overflow-y-auto" initialFocus={ref} onClose={setShowModal}>
 				<div className="flex min-h-screen items-end justify-center px-4 pt-4 pb-20 text-center sm:block sm:p-0">
 					<Transition.Child
 						as={Fragment}
@@ -48,31 +48,29 @@ const NotAllowedFeatureModal = ({ showModal = false, setShowModal }, ref) => {
 					>
 						<div className="inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6 sm:align-middle">
 							<div className="sm:flex sm:items-start">
-								<div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+								<div className="flex mx-auto h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
 									<ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
 								</div>
 								<div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
 									<Dialog.Title as="h3" className="text-lg font-medium text-gray-900">
 										{notAllowedFeatureTitle}
 									</Dialog.Title>
-
 									<div className="mt-2">
-										<Dialog.Description as="p" className="mt-4 mb-3 text-sm text-gray-500">
+										<Dialog.Description as="p" className="text-sm text-gray-500">
 											{notAllowedFeatureMessage}
 										</Dialog.Description>
 									</div>
 								</div>
 							</div>
 							<div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-								<span className="mt-3 flex w-full sm:mt-0 sm:w-auto">
-									<button
-										type="button"
-										className="mr-3 inline-flex w-full cursor-pointer justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium  text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-										onClick={() => setShowModal(false)}
-									>
-										{closeText}
-									</button>
-								</span>
+								<button
+									type="button"
+									className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm"
+									onClick={() => setShowModal(false)}
+									ref={ref}
+								>
+									{closeText}
+								</button>
 							</div>
 						</div>
 					</Transition.Child>
