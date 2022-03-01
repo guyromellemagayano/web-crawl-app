fe172e4c2c40dcc8710c206f6e1a1e28da1754e5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-03-02 07:02:42 +0800

    Code style formatting changes

--- frontend/components/sites/AddSite.jsx
+++ frontend/components/sites/AddSite.jsx
@@ -194,7 +194,7 @@ const AddSite = ({ handleOpenSidebar }) => {
 								onClick={handleRouterOnClick}
 								aria-hidden="true"
 								className={classnames(
-									"inline-flex items-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white shadow-sm ",
+									"inline-flex items-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white shadow-sm",
 									asPath.includes(AddNewSiteLink)
 										? "cursor-pointer bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 active:bg-red-700"
 										: isLoading
