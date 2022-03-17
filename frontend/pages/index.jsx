25e950e280f209899647de248b984f3817be0ef8
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-03-18 02:00:24 +0800

    Eliminate SSR in home page; Refactor entire home page

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,46 +1,10 @@
+import { MemoizedLayout } from "@components/layouts";
 import { MemoizedLoader } from "@components/loaders";
-import { UserApiEndpoint } from "@constants/ApiEndpoints";
-import { DashboardSitesLink, LoginLink } from "@constants/PageLinks";
-import { SSR_SITE_URL } from "@constants/ServerEnv";
-import { useUser } from "@hooks/useUser";
-import AppAxiosInstance from "@utils/axios";
 import { NextSeo } from "next-seo";
 import useTranslation from "next-translate/useTranslation";
 import { useContext } from "react";
 import { SWRConfig } from "swr";
 import { SiteCrawlerAppContext } from "./_app";
-
-// Pre-render `user` data with NextJS SSR. Redirect to a login page if current user is not allowed to access that page (403 Forbidden) or redirect to the sites dashboard page if the user is still currently logged in (200 OK).
-export async function getServerSideProps({ req }) {
-	const userResponse = await AppAxiosInstance.get(`${SSR_SITE_URL + UserApiEndpoint}`, {
-		headers: {
-			cookie: req.headers.cookie ?? null
-		}
-	});
-	const userData = userResponse?.data ?? null;
-	const userStatus = userResponse?.status ?? null;
-
-	if (
-		userData !== null &&
-		!userData?.detail &&
-		Object.keys(userData)?.length > 0 &&
-		Math.round(userStatus / 200) === 1
-	) {
-		return {
-			redirect: {
-				destination: DashboardSitesLink,
-				permanent: false
-			}
-		};
-	} else {
-		return {
-			redirect: {
-				destination: LoginLink,
-				permanent: false
-			}
-		};
-	}
-}
 
 const HomeAuth = () => {
 	// Translations
@@ -50,20 +14,17 @@ const HomeAuth = () => {
 	// Custom context
 	const { isComponentReady } = useContext(SiteCrawlerAppContext);
 
-	// SWR hooks
-	const { user } = useUser("/api/auth/user/");
-
 	return (
-		<>
+		<MemoizedLayout>
 			<NextSeo title={homeText} />
-			<MemoizedLoader />
-		</>
+			{isComponentReady ? null : <MemoizedLoader />}
+		</MemoizedLayout>
 	);
 };
 
-export default function Home({ fallback }) {
+export default function Home() {
 	return (
-		<SWRConfig value={{ fallback }}>
+		<SWRConfig>
 			<HomeAuth />
 		</SWRConfig>
 	);
