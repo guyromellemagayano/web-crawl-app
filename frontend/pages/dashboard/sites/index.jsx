a650cb717b0c59b4bcc5047c01cb11dc79089f56
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-03-21 23:10:45 +0800

    Readd SSR to all dashboard pages

--- frontend/pages/dashboard/sites/index.jsx
+++ frontend/pages/dashboard/sites/index.jsx
@@ -1,9 +1,42 @@
 import { MemoizedLayout } from "@components/layouts";
 import { MemoizedPageLayout } from "@components/layouts/components/Page";
 import { MemoizedSitesDashboardPageLayout } from "@components/layouts/pages/SitesDashboard";
+import { UserApiEndpoint } from "@constants/ApiEndpoints";
+import { LoginLink } from "@constants/PageLinks";
+import { SSR_SITE_URL } from "@constants/ServerEnv";
+import AppAxiosInstance from "@utils/axios";
 import { NextSeo } from "next-seo";
 import useTranslation from "next-translate/useTranslation";
 import { SWRConfig } from "swr";
+
+// Pre-render `user` data with NextJS SSR. Redirect to a login page if current user is not allowed to access that page (403 Forbidden) or redirect to the sites dashboard page if the user is still currently logged in (200 OK).
+export async function getServerSideProps({ req }) {
+	const userResponse = await AppAxiosInstance.get(`${SSR_SITE_URL + UserApiEndpoint}`, {
+		headers: {
+			cookie: req.headers.cookie ?? null
+		}
+	});
+	const userData = userResponse?.data ?? null;
+	const userStatus = userResponse?.status ?? null;
+
+	if (
+		userData !== null &&
+		!userData?.detail &&
+		Object.keys(userData)?.length > 0 &&
+		Math.round(userStatus / 200) === 1
+	) {
+		return {
+			props: {}
+		};
+	} else {
+		return {
+			redirect: {
+				destination: LoginLink,
+				permanent: false
+			}
+		};
+	}
+}
 
 const SitesAuth = () => {
 	// Translations
