8202a676749c5e648f1c4ef8c04b02badebebbd0
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-03-21 23:15:01 +0800

    Update how site name in links page is loaded (via SSR)

--- frontend/pages/dashboard/sites/[siteId]/links.jsx
+++ frontend/pages/dashboard/sites/[siteId]/links.jsx
@@ -4,11 +4,9 @@ import { MemoizedSiteLinksPageLayout } from "@components/layouts/pages/SiteLinks
 import { SitesApiEndpoint, UserApiEndpoint } from "@constants/ApiEndpoints";
 import { DashboardSitesLink, LoginLink } from "@constants/PageLinks";
 import { SSR_SITE_URL } from "@constants/ServerEnv";
-import { SiteCrawlerAppContext } from "@pages/_app";
 import AppAxiosInstance from "@utils/axios";
 import { NextSeo } from "next-seo";
 import useTranslation from "next-translate/useTranslation";
-import { useContext } from "react";
 import { SWRConfig } from "swr";
 
 // Pre-render `user` data with NextJS SSR. Redirect to a login page if current user is not allowed to access that page (403 Forbidden) or redirect to the sites dashboard page if the user is still currently logged in (200 OK).
@@ -66,17 +64,13 @@ export async function getServerSideProps({ req, query }) {
 	}
 }
 
-const SiteLinksAuth = () => {
+const SiteLinksAuth = ({ siteName }) => {
 	// Translations
 	const { t } = useTranslation("sites");
 	const sitesLinksText = t("sitesLinks");
 
-	// Custom context
-	const { isComponentReady, siteId } = useContext(SiteCrawlerAppContext);
-
 	// Custom variables
-	const siteName = siteId?.data?.name ?? null;
-	const sitesLinksPageTitle = siteName ? sitesLinksText + " - " + siteName : "";
+	const sitesLinksPageTitle = sitesLinksText + " - " + siteName;
 
 	return (
 		<MemoizedLayout>
@@ -88,10 +82,10 @@ const SiteLinksAuth = () => {
 	);
 };
 
-export default function SiteLinks() {
+export default function SiteLinks({ siteName }) {
 	return (
 		<SWRConfig>
-			<SiteLinksAuth />
+			<SiteLinksAuth siteName={siteName} />
 		</SWRConfig>
 	);
 }
