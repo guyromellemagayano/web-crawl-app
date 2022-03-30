f955d66891c94b26c87224120e7b29a9fed6cb3a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-03-30 18:27:30 +0800

    Fix link detail page

--- frontend/components/breadcrumbs/index.jsx
+++ frontend/components/breadcrumbs/index.jsx
@@ -1,11 +1,18 @@
-import { DashboardSitesLink } from "@constants/PageLinks";
+import {
+	AddNewSiteSlug,
+	DashboardSettingsSlug,
+	DashboardSitesLink,
+	SiteImagesSlug,
+	SiteLinksSlug,
+	SitePagesSlug
+} from "@constants/PageLinks";
 import { ChevronRightIcon, HomeIcon } from "@heroicons/react/solid";
 import { SiteCrawlerAppContext } from "@pages/_app";
 import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import PropTypes from "prop-types";
-import { memo, useContext } from "react";
+import { memo, useContext, useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
@@ -14,36 +21,46 @@ import "react-loading-skeleton/dist/skeleton.css";
  *
  * @param {boolean} isImages
  * @param {boolean} isLinks
- * @param {boolean} isOther
  * @param {boolean} isPages
  * @param {boolean} isSites
+ * @param {boolean} isSettings
  * @param {string} pageDetailTitle
  * @param {string} pageTitle
- * @param {number} siteId
  */
 const Breadcrumbs = ({
 	isImages = false,
 	isLinks = false,
-	isOther = false,
 	isPages = false,
 	isSites = false,
+	isSettings = false,
 	pageDetailTitle = null,
-	pageTitle = null,
-	siteId = null
+	pageTitle = null
 }) => {
+	const [siteQueries, setSiteQueries] = useState(null);
+
 	// Router
-	const { asPath } = useRouter();
+	const { asPath, isReady } = useRouter();
 
 	// Translations
-	const { t } = useTranslation("common");
-	const home = t("home");
+	const { t } = useTranslation();
+	const home = t("common:home");
+	const sitesOverviewText = t("sites:sitesOverview");
+	const sitesLinksText = t("sites:sitesLinks");
+	const sitesPagesText = t("sites:sitesPages");
+	const sitesImagesText = t("sites:sitesImages");
 
 	// Custom context
-	const { isComponentReady, querySiteId } = useContext(SiteCrawlerAppContext);
+	const { isComponentReady, querySiteId, queryLinkId, queryPageId, queryImageId } = useContext(SiteCrawlerAppContext);
 
 	// Custom variables
 	const sitesIdOverviewPageLink =
 		querySiteId && !asPath.includes(DashboardSitesLink) ? `${DashboardSitesLink + querySiteId + "/"}` : null;
+
+	useEffect(() => {
+		isReady ? setSiteQueries(window.location.search) : setSiteQueries(null);
+
+		return { siteQueries };
+	}, [isReady]);
 
 	return (
 		<nav className="w-full flex-none border-b border-gray-200 pt-4 pb-8" aria-label="Breadcrumb">
@@ -51,7 +68,7 @@ const Breadcrumbs = ({
 				<li>
 					<div>
 						{isComponentReady ? (
-							<Link href={isOther && siteId == null ? DashboardSitesLink : sitesIdOverviewPageLink} passHref replace>
+							<Link href={DashboardSitesLink} passHref replace>
 								<a className="text-gray-400 hover:text-gray-500">
 									<HomeIcon className="h-4 w-4 flex-shrink-0" />
 									<span className="sr-only">{home}</span>
@@ -62,71 +79,126 @@ const Breadcrumbs = ({
 						)}
 					</div>
 				</li>
-				<li>
-					<div className="flex items-center">
-						{isComponentReady ? (
-							<ChevronRightIcon className="h-4 w-4 flex-shrink-0 text-gray-400" />
-						) : (
-							<Skeleton duration={2} width={20} height={20} />
-						)}
 
-						{pageTitle && pageTitle?.length > 0 ? (
-							pageDetailTitle && pageDetailTitle?.length > 0 ? (
-								isSites ? (
-									isComponentReady ? (
-										<Link href={DashboardSitesLink} passHref>
+				{isComponentReady ? (
+					(pageTitle?.length > 0 &&
+						!isSites &&
+						(asPath.includes(
+							DashboardSitesLink +
+								querySiteId +
+								(asPath.includes("links")
+									? SiteLinksSlug
+									: asPath.includes("pages")
+									? SitePagesSlug
+									: asPath.includes("images")
+									? SiteImagesSlug
+									: "") +
+								(queryLinkId ? queryLinkId + "/" : "") +
+								(queryPageId ? queryPageId + "/" : "") +
+								(queryImageId ? queryImageId + "/" : "") +
+								siteQueries
+						) ||
+							asPath.includes(DashboardSettingsSlug))) ||
+					asPath.includes(AddNewSiteSlug) ? (
+						<>
+							<li>
+								<div className="flex items-center">
+									<ChevronRightIcon className="h-4 w-4 flex-shrink-0 text-gray-400" />
+
+									{!asPath.includes(DashboardSettingsSlug) &&
+									!asPath.includes(AddNewSiteSlug) &&
+									asPath !== DashboardSitesLink + querySiteId + "/" ? (
+										<Link href={DashboardSitesLink + querySiteId + "/"} passHref>
+											<a aria-current="page" className="ml-4 cursor-pointer text-sm text-gray-700">
+												{sitesOverviewText}
+											</a>
+										</Link>
+									) : (
+										<Link href={asPath} passHref>
 											<a aria-current="page" className="ml-4 cursor-pointer text-sm text-gray-700">
 												{pageTitle}
 											</a>
 										</Link>
-									) : (
-										<Skeleton duration={2} width={128} height={20} className="ml-4" />
-									)
-								) : isComponentReady ? (
-									<Link
-										href={`${
-											DashboardSitesLink +
-											querySiteId +
-											"/" +
-											(isLinks ? "links" : isPages ? "pages" : isImages ? "images" : "seo")
-										}`}
-										passHref
-									>
-										<a aria-current="page" className="ml-4 cursor-pointer text-sm text-gray-700">
-											{pageTitle}
-										</a>
-									</Link>
-								) : (
-									<Skeleton duration={2} width={128} height={20} className="ml-4" />
-								)
-							) : (
-								<p aria-current="page" className="ml-4 cursor-default text-sm font-medium text-gray-700">
-									{isComponentReady ? pageTitle : <Skeleton duration={2} width={128} height={20} />}
-								</p>
-							)
-						) : null}
-					</div>
-				</li>
+									)}
+								</div>
+							</li>
 
-				{pageDetailTitle && pageDetailTitle?.length > 0 ? (
-					isComponentReady ? (
+							{!asPath.includes(DashboardSettingsSlug) &&
+							!asPath.includes(AddNewSiteSlug) &&
+							asPath !== DashboardSitesLink + querySiteId + "/" ? (
+								queryLinkId || queryPageId || queryImageId ? (
+									<>
+										<li>
+											<div className="flex items-center">
+												<ChevronRightIcon className="h-4 w-4 flex-shrink-0 text-gray-400" />
+
+												<Link
+													href={
+														queryLinkId
+															? DashboardSitesLink + querySiteId + SiteLinksSlug
+															: queryPageId
+															? DashboardSitesLink + querySiteId + SitePagesSlug
+															: DashboardSitesLink + querySiteId + SiteImagesSlug
+													}
+													passHref
+												>
+													<a aria-current="page" className="ml-4 cursor-pointer text-sm text-gray-700">
+														{queryLinkId ? sitesLinksText : queryPageId ? sitesPagesText : sitesImagesText}
+													</a>
+												</Link>
+											</div>
+										</li>
+
+										<li>
+											<div className="flex items-center">
+												<ChevronRightIcon className="h-4 w-4 flex-shrink-0 text-gray-400" />
+
+												<Link href={asPath} passHref>
+													<a aria-current="page" className="ml-4 cursor-pointer text-sm text-gray-700">
+														{pageTitle}
+													</a>
+												</Link>
+											</div>
+										</li>
+									</>
+								) : (
+									<li>
+										<div className="flex items-center">
+											<ChevronRightIcon className="h-4 w-4 flex-shrink-0 text-gray-400" />
+
+											<Link href={asPath} passHref>
+												<a aria-current="page" className="ml-4 cursor-pointer text-sm text-gray-700">
+													{pageTitle}
+												</a>
+											</Link>
+										</div>
+									</li>
+								)
+							) : null}
+						</>
+					) : null
+				) : (
+					<>
 						<li>
 							<div className="flex items-center">
-								<ChevronRightIcon className="h-4 w-4 flex-shrink-0 text-gray-400" />
-								<p aria-current="page" className="ml-4 cursor-default text-sm font-medium text-gray-700">
-									{pageDetailTitle}
-								</p>
-							</div>
-						</li>
-					) : (
-						<li>
-							<div className="flex items-center">
-								<ChevronRightIcon className="h-4 w-4 flex-shrink-0 text-gray-400" />
+								<Skeleton duration={2} width={20} height={20} />
 								<Skeleton duration={2} width={128} height={20} className="ml-4" />
 							</div>
 						</li>
-					)
-				) : null}
+						<li>
+							<div className="flex items-center">
+								<Skeleton duration={2} width={20} height={20} />
+								<Skeleton duration={2} width={128} height={20} className="ml-4" />
+							</div>
+						</li>
+						<li>
+							<div className="flex items-center">
+								<Skeleton duration={2} width={20} height={20} />
+								<Skeleton duration={2} width={128} height={20} className="ml-4" />
+							</div>
+						</li>
+					</>
+				)}
 			</ol>
 		</nav>
 	);
@@ -135,12 +207,10 @@ const Breadcrumbs = ({
 Breadcrumbs.propTypes = {
 	isImages: PropTypes.bool,
 	isLinks: PropTypes.bool,
-	isOther: PropTypes.bool,
 	isPages: PropTypes.bool,
 	isSites: PropTypes.bool,
 	pageDetailTitle: PropTypes.string,
-	pageTitle: PropTypes.string,
-	siteId: PropTypes.number
+	pageTitle: PropTypes.string
 };
 
 /**
