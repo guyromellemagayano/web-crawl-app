70473b80c6d10a8396bcc3c5b1861bd2a8e5f78a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-04-01 15:22:26 +0800

    Temporarily disable uptime stats feature

--- frontend/components/layouts/pages/SiteOverview.jsx
+++ frontend/components/layouts/pages/SiteOverview.jsx
@@ -5,7 +5,7 @@ import { MemoizedIssuesStats } from "@components/stats/Issues";
 import { MemoizedLinksStats } from "@components/stats/Links";
 import { MemoizedOverviewStats } from "@components/stats/Overview";
 import { MemoizedPagesStats } from "@components/stats/Pages";
-import { MemoizedResponseTimeStats } from "@components/stats/ResponseTime";
+// import { MemoizedResponseTimeStats } from "@components/stats/ResponseTime";
 import { MemoizedSeoStats } from "@components/stats/Seo";
 import { memo } from "react";
 import { MemoizedComingSoonPageLayout } from "./ComingSoon";
@@ -19,12 +19,6 @@ const SiteOverviewPageLayout = () => {
 			<MemoizedPageOption isOverview />
 			<div className="flex flex-auto flex-grow flex-col items-center justify-center p-4 focus:outline-none sm:px-6 md:px-0">
 				<div className="h-full w-full flex-1 overflow-y-hidden py-2">
-					<div className="mb-8 grid grid-flow-row grid-cols-1 gap-8 md:grid-cols-4">
-						<div className="md:col-span-4">
-							<MemoizedResponseTimeStats />
-						</div>
-					</div>
-
 					<div className="mb-8 grid grid-flow-row grid-cols-1 gap-8 md:grid-cols-3">
 						<div>
 							<MemoizedOverviewStats />
@@ -63,6 +57,12 @@ const SiteOverviewPageLayout = () => {
 							<MemoizedComingSoonPageLayout />
 						</div>
 					</div>
+
+					{/* <div className="mb-8 grid grid-flow-row grid-cols-1 gap-8 md:grid-cols-4">
+						<div className="md:col-span-4">
+							<MemoizedResponseTimeStats />
+						</div>
+					</div> */}
 				</div>
 			</div>
 		</>
